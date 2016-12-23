3219adcd63735cea4bcb58cb1c178a26b03b6b3e
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-12-22 16:12:57 -0800

    adding body IDs for views

--- scripts/build-pug.js
+++ scripts/build-pug.js
@@ -6,22 +6,23 @@ let shelljs = require(`shelljs`);
 
 let rawStrings = shelljs.cat(`locales/en-US/general.properties`);
 
-let localizedStrings = {
-  strings: propertiesToObject(rawStrings.toString())
-};
-
 function buildPage(template, target) {
+  let viewData = {
+    strings: propertiesToObject(rawStrings.toString()),
+    templateID: template
+  };
+
   let fn = pug.compileFile(`source/pug/views/${template}.pug`, {
     pretty: true
   });
 
-  let html = fn(localizedStrings);
-  let path = `dest/${target}`;
+  let html = fn(viewData);
+  let path = `dest${target}`;
 
   shelljs.mkdir(`-p`, path);
   shelljs.ShellString(html).to(`${path}/index.html`);
 }
 
-buildPage(`home`, ``);
-buildPage(`subpage1`, `subpage1`);
-buildPage(`subpage2`, `subpage2`);
+buildPage(`home`, `/`);
+buildPage(`subpage1`, `/subpage1`);
+buildPage(`subpage2`, `/subpage2`);
