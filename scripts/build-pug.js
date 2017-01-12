0b20348d056671e7575d79f1b53e374252a949b6
Gavin Lazar Suntop <gavin@gsuntop.com>
2017-01-12 12:26:30 -0800

    adding virtual root via env

--- scripts/build-pug.js
+++ scripts/build-pug.js
@@ -2,10 +2,20 @@ let propertiesToObject = require(`java.properties.js`).default;
 let pug = require(`pug`);
 let shelljs = require(`shelljs`);
 
+let envFlag = process.argv[2];
+let environmentVariables;
+
+if (envFlag && envFlag === `--staging`) {
+  environmentVariables = JSON.parse((shelljs.cat(`env/staging.json`).toString()));
+} else {
+  environmentVariables = JSON.parse((shelljs.cat(`env/default.json`).toString()));
+}
+
 let rawStrings = shelljs.cat(`locales/en-US/general.properties`);
 
 function buildPage(template, target) {
   let viewData = {
+    env: environmentVariables,
     strings: propertiesToObject(rawStrings.toString()),
     templateID: template
   };
