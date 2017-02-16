3003b862c0b505e0280f2de726b2169455aee696
Gavin Lazar Suntop <gavin@gsuntop.com>
2017-02-16 14:23:36 -0800

    fixing upcoming link from nav

--- source/js/components/nav/nav.jsx
+++ source/js/components/nav/nav.jsx
@@ -46,7 +46,7 @@ export default class PrimaryNav extends React.Component {
             <li><a href="/programs">Programs</a></li>
             <li><a href="/projects">Projects</a></li>
             <li><a href="/campaigns">Campaigns</a></li>
-            <li><a href="/upcoming">Upcoming</a></li>
+            <li><a href="/programs/upcoming">Upcoming</a></li>
             <li><a href="/about">About</a></li>
           </ul>
           <ul className="pb-2">
