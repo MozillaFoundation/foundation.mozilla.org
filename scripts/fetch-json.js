a2392b9f673ae0107ede403acc6bd90444945b13
Gavin Lazar Suntop <gavin@gsuntop.com>
2017-02-17 10:57:30 -0800

    changing url for pulse pull

--- scripts/fetch-json.js
+++ scripts/fetch-json.js
@@ -7,7 +7,7 @@ let chalk = require(`chalk`);
 shell.mkdir(`-p`, `source/json/temp/`);
 
 // Pulse
-request(`https://network-pulse-api-staging.herokuapp.com/entries/?ordering=-created&page_size=996&format=json&featured=True&page=1`, (error, response, body) => {
+request(`https://network-pulse-api-production.herokuapp.com/entries/?ordering=-created&page_size=996&format=json&featured=True&page=1`, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     console.log(chalk.green(`Pulled Pulse JSON`));
     shell.ShellString(body).to(`source/json/temp/pulse.json`);
