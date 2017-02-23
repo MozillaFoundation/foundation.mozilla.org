bead51be7e79fdb25c8dfdd9f6b892359f24dc15
Gavin Lazar Suntop <gavin@gsuntop.com>
2017-02-23 15:53:18 -0800

    styling primary nav

--- source/js/components/nav/nav.jsx
+++ source/js/components/nav/nav.jsx
@@ -38,26 +38,33 @@ export default class PrimaryNav extends React.Component {
   render() {
     return (
       <header hidden={this.state.isHidden}>
-        <div className="container p-4">
-          <button onClick={this.hide}>X</button>
-          <p className="text-center"><a href="/"><img src="/_images/logo.svg" style={{width:`150px`}}/></a></p>
-          <ul className="py-3 menu-nav">
-            <li><a href="/people">People</a></li>
-            <li><a href="/programs">Programs</a></li>
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/campaigns">Campaigns</a></li>
-            <li><a href="/programs/upcoming">Upcoming</a></li>
-            <li><a href="/about">About</a></li>
-          </ul>
-          <ul className="pb-2">
-            <li><a href="#TODO">Twitter</a></li>
-            <li><a href="#TODO">Internet Health</a></li>
-            <li><a href="#TODO">Email Us</a></li>
-            <li><a href="#TODO">Donate</a></li>
-          </ul>
-          <ul className="pb-2">
-            <li><em><a href="/style-guide">Style Guide</a></em></li>
-          </ul>
+        <div className="container">
+          <div className="row p-4">
+            <button className="btn-close text-hide" onClick={this.hide}>X</button>
+          </div>
+          <div className="row d-flex justify-content-between">
+            <div className="col align-self-end">
+              <ul className="px-5 menu-nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/people">People</a></li>
+                <li><a href="/programs">Programs</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/campaigns">Campaigns</a></li>
+                <li><a href="/programs/upcoming">Upcoming</a></li>
+                <li><a href="/about">About</a></li>
+              </ul>
+            </div>
+            <div className="col pb-3 px-3 align-self-end flex-grow-0">
+              <ul className="pb-2 menu-nav menu-nav-side">
+                <li><a className="d-flex align-items-center twitter" href="https://twitter.com/mozilla">Twitter</a></li>
+                <li><a className="d-flex align-items-center chat" href="#TODO">Chat</a></li>
+                <li><a className="d-flex align-items-center email" href="#TODO">Email</a></li>
+                <li><a className="d-flex align-items-center donate" href="https://donate.mozilla.org">Donate</a></li>
+                <li><em><a style={{color:`yellow`}} href="/style-guide">Style Guide</a></em></li>
+              </ul>
+              <a href="https://mozilla.org" className="text-hide logo">Mozilla</a>
+            </div>
+          </div>
         </div>
       </header>
     );
