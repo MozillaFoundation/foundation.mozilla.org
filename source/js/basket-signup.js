2090c343eda4f97e2fa16e89b980181da0eeeba3
Alan Mooiman <amooiman@gmail.com>
2017-03-10 17:31:43 -0500

    Join us CRM integration and error states (#126)
    * Basket submission
    * Form error states for now
    * Make form hideable after submission

---
+++ source/js/basket-signup.js
@@ -0,0 +1,66 @@
+// TODO: make this dynamic based on env. Staging is allizom.org. https://github.com/mozilla/network/issues/118
+var url = `https://www.mozilla.org/en-US/newsletter/`;
+
+var basketSignup = function(transaction, onSuccessCallback, onFailCallback) {
+  var payload = {
+    format: `H`, // HTML emails
+    newsletter: `mozilla-leadership-network`,
+    triggerWelcome: `N`,
+    email: transaction.email,
+    privacy: transaction.privacy || false
+  };
+
+  var errorArray = [];
+  var newsletterErrors = [];
+
+  while (newsletterErrors.firstChild) { newsletterErrors.removeChild(newsletterErrors.firstChild); }
+
+  var params = `email=` + encodeURIComponent(payload.email) +
+                  `&newsletters=` + payload.newsletter +
+                   `&privacy=`+ payload.privacy +
+                  `&fmt=` + payload.format +
+                  `&source_url=` + encodeURIComponent(document.location.href);
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function(r) {
+    if (r.target.status >= 200 && r.target.status < 300) {
+      var response = r.target.response;
+
+      if(response === null ) {
+        onFailCallback(new Error());
+        return;
+      }
+
+      if (response.success === true) {
+        onSuccessCallback();
+      } else {
+        if(response.errors) {
+          for (var i = 0; i < response.errors.length; i++) {
+            errorArray.push(response.errors[i]);
+          }
+        }
+        onFailCallback(new Error(errorArray));
+      }
+    } else {
+      onFailCallback(new Error());
+    }
+  };
+
+  xhr.onerror = function(e) {
+    onFailCallback(e);
+  };
+
+  xhr.open(`POST`, url, true);
+  xhr.setRequestHeader(`Content-type`, `application/x-www-form-urlencoded`);
+  xhr.setRequestHeader(`X-Requested-With`,`XMLHttpRequest`);
+  xhr.timeout = 5000;
+  xhr.ontimeout = onFailCallback;
+  xhr.responseType = `json`;
+  xhr.send(params);
+
+  return false;
+
+};
+
+module.exports = basketSignup;
--- source/js/components/join/join.jsx
+++ source/js/components/join/join.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classNames';
+import basketSignup from '../../basket-signup.js';
 
 export default class JoinUs extends React.Component {
   show () {
@@ -12,12 +14,33 @@ export default class JoinUs extends React.Component {
       isHidden: true
     });
   }
+  submitForm(event) {
+    this.setState({userSubmitted: true});
+    event.preventDefault();
+    if(this.refs.email.value && this.refs.privacy.checked){
+      basketSignup({
+        email: this.refs.email.value,
+        privacy: this.refs.privacy.checked
+      }, this.formSubmissionSuccessful, this.formSubmissionFailure);
+    }
+  }
+
+  formSubmissionSuccessful() {
+    this.setState({signupSuccess: true});
+  }
+  formSubmissionFailure(e) {
+    console.error(e);
+    this.setState({signupFailed: true});
+  }
 
   constructor(props) {
     super(props);
 
     this.show = this.show.bind(this);
     this.hide = this.hide.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+    this.formSubmissionSuccessful = this.formSubmissionSuccessful.bind(this);
+    this.formSubmissionFailure = this.formSubmissionFailure.bind(this);
 
     this.state = {
       isHidden: typeof this.props.isHidden === `boolean` ? this.props.isHidden : true
@@ -25,6 +48,16 @@ export default class JoinUs extends React.Component {
   }
 
   render() {
+
+    let inputGroupClass = classNames({
+      'has-danger': !this.state.signupSuccess && this.state.userSubmitted && !this.refs.email.value || this.state.signupFailed
+    });
+
+    let privacyClass = classNames({
+      'form-check': true,
+      'has-danger': !this.state.signupSuccess && this.state.userSubmitted && !this.refs.privacy.checked
+    });
+
     return (
       <div hidden={this.state.isHidden} className="row p-4">
         <div className="col-12">
@@ -37,13 +70,27 @@ export default class JoinUs extends React.Component {
           <p className="body-black">Access to bright minds, passionate community, and invaluable resources.</p>
         </div>
         <div className="col-6">
-          <form>
-            <input placeholder="Email Address"/>
-            <button className="btn btn-normal">Join Us</button>
-            <label>
-              <input type="checkbox"/><span className="small-gray">&nbsp;I'm okay with Mozilla handling my info as explained in this <a href="#TODO">Privacy Notice</a></span>
-            </label>
+          {!this.state.signupSuccess ?
+          <form onSubmit={this.submitForm}>
+            <div className={inputGroupClass}>
+              <div className="input-group">
+                <input type="email" className="form-control" placeholder="Email Address" ref="email"/>
+                <span className="input-group-btn">
+                  <button className="btn btn-normal">Join Us</button>
+                </span>
+              </div>
+              {this.state.userSubmitted && !this.refs.email.value ? <small className="form-check form-control-feedback">Please enter your email</small> : null }
+              {this.state.signupFailed? <small className="form-check form-control-feedback">Something went wrong. Please check your email address and try again</small> : null }
+            </div>
+            <div className={privacyClass}>
+              <label className="form-check-label">
+                <input type="checkbox" className="form-check-input" id="PrivacyCheckbox" ref="privacy" />
+                <span className="small-gray form-text">&nbsp;I'm okay with Mozilla handling my info as explained in this <a href="#TODO">Privacy Notice</a></span>
+                {this.state.userSubmitted && !this.refs.privacy.checked ? <small className="has-danger">Please check this box if you want to proceed</small> : null }
+              </label>
+            </div>
           </form>
+          : `Thank you!`}
         </div>
       </div>
     );
