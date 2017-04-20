66867e5cead8dcdd7001e957914c87fafe0ad0d1
gavin lazar suntop <gavin@gsuntop.com>
2017-04-20 11:06:55 -0700

    Adding first visit notice (#324)
    * basic takeover code
    
    * cookie-ing modal impression
    
    * mobile tweaks
    
    * eliminating double scroll & extraneous methods

--- source/js/main.js
+++ source/js/main.js
@@ -2,10 +2,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
 
 import JoinUs from './components/join/join.jsx';
 import PrimaryNav from './components/nav/nav.jsx';
 import People from './components/people/people.jsx';
+import Takeover from './components/takeover/takeover.jsx';
 
 let main = {
   init () {
@@ -40,6 +42,24 @@ let main = {
           reactPrimaryNav.toggle();
         });
       }
+    }
+
+    // Show Takeover for new visitors
+    if (!Cookies.get(`seen-takeover`) && document.querySelector(`#view-home .takeover`)) {
+      let elWrapper = document.querySelector(`#view-home > .wrapper`);
+
+      // Don't allow the content block to scroll
+      elWrapper.style.overflow = `hidden`;
+      elWrapper.style.height = `100vh`;
+
+      let onTakeoverHide = () => {
+        // Allow scrolling again when the takeover is dismissed
+        elWrapper.style.overflow = null;
+        elWrapper.style.height = null;
+      };
+
+      ReactDOM.render(<Takeover onHide={onTakeoverHide}/>, document.querySelector(`#view-home .takeover`));
+      Cookies.set(`seen-takeover`, `true`, { expires: 365 });
     }
 
     // Embed additional instances of the Join Us box that don't need an API exposed (eg: Homepage)
