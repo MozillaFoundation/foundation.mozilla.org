45fd05343ad1f9a1af7879e9c564f76e9d473bc2
Mavis Ou <mmmavis@users.noreply.github.com>
2017-04-21 10:59:20 -0700

    pug only version & only show 2 items on xs devices and 3 on small devices

--- source/js/main.js
+++ source/js/main.js
@@ -7,7 +7,6 @@ import Cookies from 'js-cookie';
 import JoinUs from './components/join/join.jsx';
 import PrimaryNav from './components/nav/nav.jsx';
 import People from './components/people/people.jsx';
-import PeopleBox from './components/people-box/people-box.jsx';
 import Takeover from './components/takeover/takeover.jsx';
 
 let main = {
@@ -79,11 +78,6 @@ let main = {
     if (document.getElementById(`people`)) {
       ReactDOM.render(<People/>, document.getElementById(`people`));
     }
-
-    if (document.getElementById(`people-box`)) {
-      ReactDOM.render(<PeopleBox/>, document.getElementById(`people-box`));
-    }
-
   }
 };
 
