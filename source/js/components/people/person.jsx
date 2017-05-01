b510aa7faebeb7482ddbfbd915f47af9ad250174
Gavin Lazar Suntop <gavin@gsuntop.com>
2017-05-01 16:34:08 -0700

    restructuring and removing flip

--- source/js/components/people/person.jsx
+++ source/js/components/people/person.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
 
 export default class Person extends React.Component {
   constructor(props) {
@@ -30,8 +29,6 @@ export default class Person extends React.Component {
   }
 
   render() {
-
-
     let issues = this.props.metadata.internet_health_issues.map((issue, index) => {
       return (
         <span key={index} className="issue-link small d-inline-block mr-1">{issue}</span>
@@ -68,44 +65,30 @@ export default class Person extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="col d-flex flex-column">
-              <div className="justify-content-end row no-gutters slanty-background-gray">
-                <div className="col col-auto">
-                  <button className="more-details btn btn-sm" onClick={this.flip} >{this.state.flipped?`MORE DETAILS`:`SEE QUOTE`}</button>
-                </div>
-              </div>
+            <div className="col">
               <div className={`row flippable-card flip-${this.state.flipped}`} style={this.state.flippableStyle}>
-                <div ref="bioContent" style={this.state.bioStyle} className="col-12 bio-content pt-3">
-                  <h2 className="h5-black mb-2">{this.props.metadata.name}</h2>
-                  <div className="person-role-location small">{this.props.metadata.role} / {this.props.metadata.location}</div>
-                  <div className="person-issues">{issues}</div>
-                  <div className="person-affiliations small-gray mt-2">{this.props.metadata.affiliations.join(`; `)}</div>
-                  {this.props.metadata.partnership_logo &&
-                    <div className="my-1">
-                      <img className="partnership_logo" src={this.props.metadata.partnership_logo} alt="Logo of partnered organization"/>
-                    </div>
-                  }
-                  <div className="person-bio body-black">
-                    <ReactMarkdown source={this.props.metadata.bio} />
-                  </div>
-                  <div className="person-social-links mt-3">
-                    {socialLinks}
-                  </div>
-                </div>
                 <div ref="quoteContent" style={this.state.quoteStyle} className="col-12 quote-content d-flex pt-3">
-                  <div className="col d-flex flex-column flex-1">
+                  <div className="col">
                       <div className="row my-5">
                         <div className="person-quote quote-small">{this.props.metadata.quote}</div>
                       </div>
-                      <div className="row justify-content-end">
-                        <div className="quote-attribution text-right">
-                          <div className="h5-black mb-2"> – {this.props.metadata.name}</div>
+                      <div className="row">
+                        <div className="quote-attribution">
+                          <div className="h5-black mb-2">{this.props.metadata.name}</div>
+                          <div className="person-role-location small">{this.props.metadata.role} / {this.props.metadata.location}</div>
+                          <div className="person-issues mb-2">{issues}</div>
                           <div className="small-gray">{this.props.metadata.affiliations[0]}</div>
                           {this.props.metadata.partnership_logo &&
                             <div className="my-1">
                               <img className="partnership_logo" src={this.props.metadata.partnership_logo} alt="Logo of partnered organization"/>
                             </div>
                           }
+                        </div>
+                      </div>
+                      <div className="row d-flex person-social-links mt-3 justify-content-between">
+                        <div>{socialLinks}</div>
+                        <div>
+                          <a className="cta-link" href="#TODO">Read Interview</a>
                         </div>
                       </div>
                   </div>
