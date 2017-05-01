34126a77296bc8fc48b2f40a7427aae24bb901db
Gavin Lazar Suntop <gavin@gsuntop.com>
2017-05-01 12:37:30 -0700

    picking up env from process.env

--- scripts/build-env.js
+++ scripts/build-env.js
@@ -4,7 +4,14 @@ let shelljs = require(`shelljs`);
 // Pull in env defaults
 let defaultEnv = dotenv.parse(shelljs.cat(`defaults.env`));
 
-// Check for local env overrides
+// Check for process env variables and override defaults where found
+Object.keys(defaultEnv).forEach((key) => {
+  if (process.env[key]) {
+    defaultEnv[key] = process.env[key];
+  }
+});
+
+// Check for local .env overrides (these take highest priority)
 let localEnv = shelljs.test(`-f`, `.env`) ? dotenv.parse(shelljs.cat(`.env`)) : {};
 
 // Merge overrides into defaults
