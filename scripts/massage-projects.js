f9134719de96f63479ac61fc69d33a8da6350473
Gavin Lazar Suntop <gavin@gsuntop.com>
2017-05-09 11:04:11 -0500

    adding middleware for dedupe/backfill of pulse projects - closes #372

---
+++ scripts/massage-projects.js
@@ -0,0 +1,65 @@
+// This is essentially middleware between the Pulse API and the Network.
+// Eventually this should be replaced by new or modified routes on Pulse that return only the data we need.
+// More info -> https://github.com/mozilla/network/issues/372
+
+let unfiltered = {
+  pulseInnovation: require(`../source/json/temp/pulse-innovation.json`).results,
+  pulseInclusion: require(`../source/json/temp/pulse-inclusion.json`).results,
+  pulseDecentralization: require(`../source/json/temp/pulse-decentralization.json`).results,
+  pulsePrivacy: require(`../source/json/temp/pulse-privacy.json`).results,
+  pulseLiteracy: require(`../source/json/temp/pulse-literacy.json`).results
+};
+
+let maxProjectsPerCategory = 2;
+
+let massaged = {
+  pulseInnovation: unfiltered.pulseInnovation.slice(0, maxProjectsPerCategory),
+  pulseInclusion: undefined,
+  pulseDecentralization: undefined,
+  pulsePrivacy: undefined,
+  pulseLiteracy: undefined
+};
+
+// Get pool of all projects currently used
+let getPool = (currentCategory) => {
+  let pool = [];
+
+  Object.keys(massaged).forEach((key) => {
+    if (key !== currentCategory && massaged[key]) {
+      pool = pool.concat(massaged[key]);
+    }
+  });
+
+  return pool;
+};
+
+Object.keys(massaged).forEach((key) => {
+  let pool = getPool(key);
+  let projects = unfiltered[key];
+  let filtered = [];
+
+  // Remove any duplicates from current project list
+  projects.forEach((project) => {
+    let isDuplicate = false;
+
+    pool.forEach((item) => {
+      if (project.id === item.id) {
+        isDuplicate = true;
+      }
+    });
+
+    if (!isDuplicate) {
+      filtered.push(project);
+    }
+  });
+
+  // Trim
+  filtered = filtered.slice(0, maxProjectsPerCategory);
+
+  // Assign
+  massaged[key] = filtered;
+});
+
+// console.log(massaged);
+
+module.exports = massaged;
