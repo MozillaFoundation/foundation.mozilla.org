a2d42f7a5117c9a8b163712f974134fc3fdd1284
Gavin Lazar Suntop <gavin@gsuntop.com>
2017-07-11 14:56:46 -0700

    oops

--- source/js/components/member-notice/member-notice.jsx
+++ source/js/components/member-notice/member-notice.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Takeover extends React.Component {
+export default class MemberNotice extends React.Component {
   constructor(props) {
     super(props);
 
