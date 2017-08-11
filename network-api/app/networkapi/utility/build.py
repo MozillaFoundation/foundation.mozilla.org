645217ef04738dadcb4e0dec382f4f62f64349a5
Christopher DeCairos <christopherd@mozillafoundation.org>
2017-08-11 14:08:39 -0400

    Add in Heroku token settings, fix lint issues (#664)

--- network-api/app/networkapi/utility/build.py
+++ network-api/app/networkapi/utility/build.py
@@ -12,9 +12,16 @@ logger = logging.getLogger(__name__)
     settings.BUILD_THROTTLE_SECONDS
 )
 def build_static_site(sender, instance, **kwargs):
-    if not settings.HEROKU_APP_BUILD_URL or not settings.GITHUB_PROJECT_MASTER_TAR_URL:
+    if not settings.HEROKU_APP_BUILD_URL:
         return logger.warn('settings.HEROKU_APP_BUILD_URL '
-                           'and settings.GITHUB_PROJECT_MASTER_TAR_URL '
+                           'must be set to trigger builds')
+
+    if not settings.GITHUB_PROJECT_MASTER_TAR_URL:
+        return logger.warn('settings.GITHUB_PROJECT_MASTER_TAR_URL '
+                           'must be set to trigger builds')
+
+    if not settings.HEROKU_API_TOKEN:
+        return logger.warn('settings.HEROKU_API_TOKEN '
                            'must be set to trigger builds')
 
     data = {
@@ -25,7 +32,8 @@ def build_static_site(sender, instance, **kwargs):
 
     headers = {
         'Content-Type': 'application/json',
-        'Accept': 'application/vnd.heroku+json; version=3'
+        'Accept': 'application/vnd.heroku+json; version=3',
+        'Authorization': 'Bearer {}'.format(settings.HEROKU_API_TOKEN)
     }
 
     build_request = requests.post(
@@ -39,6 +47,8 @@ def build_static_site(sender, instance, **kwargs):
 
     try:
         responseJSON = build_request.json()
-        logger.info('Build started. output stream at: {}'.format(responseJSON.output_stream_url))
+        logger.info('Build started. output stream at: {}'.format(
+            responseJSON.output_stream_url
+        ))
     except Exception as e:
         logger.error('Error parsing response JSON from Heroku: {}'.format(e))
