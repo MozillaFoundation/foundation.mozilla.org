df9370af6bea76a861b35e65ff31af2d79a1a96b
Christopher DeCairos <christopherd@mozillafoundation.org>
2017-08-16 15:22:37 -0400

    only trigger a build if none are pending, to work around concurrency … (#670)
    * only trigger a build if none are pending, to work around concurrency issues with the debounce and throttle code
    
    * Update settings.py
    
    * fix lint issues
    
    * remove old format call

--- network-api/app/networkapi/utility/build.py
+++ network-api/app/networkapi/utility/build.py
@@ -7,13 +7,20 @@ from networkapi.utility.decorators import debounce_and_throttle
 logger = logging.getLogger(__name__)
 
 
+def getJSON(req):
+    try:
+        return req.json()
+    except Exception:
+        return None
+
+
 @debounce_and_throttle(
     settings.BUILD_DEBOUNCE_SECONDS,
     settings.BUILD_THROTTLE_SECONDS
 )
 def build_static_site(sender, instance, **kwargs):
-    if not settings.HEROKU_APP_BUILD_URL:
-        return logger.warn('settings.HEROKU_APP_BUILD_URL '
+    if not settings.HEROKU_APP_NAME:
+        return logger.warn('settings.HEROKU_APP_NAME '
                            'must be set to trigger builds')
 
     if not settings.GITHUB_PROJECT_MASTER_TAR_URL:
@@ -24,11 +31,9 @@ def build_static_site(sender, instance, **kwargs):
         return logger.warn('settings.HEROKU_API_TOKEN '
                            'must be set to trigger builds')
 
-    data = {
-        'source_blob': {
-            'url': settings.GITHUB_PROJECT_MASTER_TAR_URL
-        }
-    }
+    build_url = 'https://api.heroku.com/apps/{}/builds'.format(
+        settings.HEROKU_APP_NAME
+    )
 
     headers = {
         'Content-Type': 'application/json',
@@ -36,19 +41,41 @@ def build_static_site(sender, instance, **kwargs):
         'Authorization': 'Bearer {}'.format(settings.HEROKU_API_TOKEN)
     }
 
+    build_payload = {
+        'source_blob': {
+            'url': settings.GITHUB_PROJECT_MASTER_TAR_URL
+        }
+    }
+
+    # get a list of builds for the app
+    list_builds = requests.get(build_url, headers=headers)
+
+    if list_builds.status_code != 200:
+        return logger.error('Could not list builds: {}'.format(list_builds.text))  # noqa
+
+    responseJSON = getJSON(list_builds)
+
+    if not responseJSON:
+        return logger.error('did not receive valid JSON from the Heroku build and release API')  # noqa
+
+    # return early if there's already a build in process for this app
+    if responseJSON[0].status == 'pending':
+        return logger.info('A Build is already in progress')
+
     build_request = requests.post(
-        settings.HEROKU_APP_BUILD_URL,
-        json=data,
+        build_url,
+        json=build_payload,
         headers=headers
     )
 
     if build_request.status_code != 201:
         return logger.error('The Build was not started: {}'.format(build_request.text))  # noqa
 
-    try:
-        responseJSON = build_request.json()
-        logger.info('Build started. output stream at: {}'.format(
-            responseJSON['output_stream_url']
-        ))
-    except Exception as e:
-        logger.error('Error parsing response JSON from Heroku: {}'.format(e))
+    responseJSON = getJSON(build_request)
+
+    if not responseJSON:
+        return logger.error('Error parsing response JSON from Heroku')
+
+    logger.info('Build started. output stream at: {}'.format(
+        responseJSON['output_stream_url']
+    ))
