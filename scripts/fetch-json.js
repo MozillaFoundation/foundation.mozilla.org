a4d16088145790567f3c5355a2250f5ac1e6a10a
gavin lazar suntop <gavin@gsuntop.com>
2017-09-21 11:42:37 -0700

    Reactify Homepage (#768)
    * reactifying homepage
    
    * only fetch homepage data for homepage
    
    * fixups
    
    * guarding against null data

--- scripts/fetch-json.js
+++ scripts/fetch-json.js
@@ -27,5 +27,4 @@ fetchJSON(`pulse-decentralization`, `https://${environment[`PULSE_API_DOMAIN`]}/
 fetchJSON(`pulse-literacy`, `https://${environment[`PULSE_API_DOMAIN`]}/api/pulse/entries/?issue=Web%20Literacy&featured=True&ordering=-created`);
 fetchJSON(`people`, `https://${environment[`NETWORK_API_DOMAIN`]}/api/people/?format=json&featured=True&page=1`);
 fetchJSON(`news`, `https://${environment[`NETWORK_API_DOMAIN`]}/api/news/?format=json&featured=True&page=1`);
-fetchJSON(`highlights`, `https://${environment[`NETWORK_API_DOMAIN`]}/api/highlights`);
 fetchJSON(`upcoming`, `https://${environment[`NETWORK_API_DOMAIN`]}/api/milestones`);
