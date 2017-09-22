0456ebd7127101816d66de70a09fce7554359208
Pomax <pomax@nihongoresources.com>
2017-09-22 14:12:15 -0700

    updated the serializer to yield a full glyph path

--- network-api/app/networkapi/news/serializers.py
+++ network-api/app/networkapi/news/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
 
+from mezzanine.conf import settings
 from networkapi.news.models import News
 
-
 class NewsSerializer(serializers.ModelSerializer):
+
+    glyph = serializers.SerializerMethodField()
+
+    def get_glyph(self, instance):
+        print (settings.USE_S3)
+
+        # Remote hosted? Return the remote URL
+        if settings.USE_S3:
+            return "{host}{glyph}".format(
+                host=settings.MEDIA_URL,
+                glyph=instance.glyph
+            )
+
+        # Stored on the local filesystem: return as an absolute URL
+        request = self.context['request']
+
+        return "{protocol}://{root}{url}{glyph}".format(
+            protocol='https' if request.is_secure() else 'http',
+            root=request.META['HTTP_HOST'],
+            url=settings.MEDIA_URL,
+            glyph=str(instance.glyph).replace(' ','%20')
+        )
+
+
     """
     Serializes a News object
     """
