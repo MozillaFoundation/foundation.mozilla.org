9ef8d701702981dcf3a968440751ff42e429a1d7
Christopher De Cairos <cade@mozillafoundation.org>
2017-09-28 15:56:07 -0400

    clean up

--- network-api/app/networkapi/middleware.py
+++ network-api/app/networkapi/middleware.py
@@ -5,14 +5,8 @@ referrer_value = 'same-origin'
 if settings.REFERRER_HEADER_VALUE:
     referrer_value = settings.REFERRER_HEADER_VALUE
 
-class ReferrerMiddleware: 
 
-        # def __init__(self, get_response):
-        #     self.get_response = get_response
-
-        # def __call__(self, request):
-        # response = self.get_response(request)
+class ReferrerMiddleware:
     def process_response(self, request, response):
         response['Referrer-Policy'] = referrer_value
-        print("wat")
         return response
