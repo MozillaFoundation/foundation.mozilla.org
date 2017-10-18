2f9141801c77ca5d458d6ebd843074b6d3dea067
Christopher De Cairos <cade@mozillafoundation.org>
2017-10-18 15:15:38 -0400

    Add middleware (off by default) for redirecting requests to the target domain of the foundation site

---
+++ network-api/app/networkapi/utility/middleware.py
@@ -0,0 +1,31 @@
+from django.http.response import HttpResponseRedirectBase
+from mezzanine.conf import settings
+
+target_domain = settings.TARGET_DOMAIN
+
+
+class HttpResponseTemporaryRedirect(HttpResponseRedirectBase):
+    status_code = 307
+
+
+class TargetDomainRedirectMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # print('in middleware')
+        if settings.DOMAIN_REDIRECT_MIDDLWARE_ENABLED:
+            request_host = request.META['HTTP_HOST']
+
+            if request_host != target_domain:
+                protocol = 'https' if request.is_secure() else 'http'
+
+                redirect_url = '{}://{}{}'.format(
+                    protocol,
+                    target_domain,
+                    request.get_full_path()
+                )
+
+                return HttpResponseTemporaryRedirect(redirect_url)
+
+        return self.get_response(request)
