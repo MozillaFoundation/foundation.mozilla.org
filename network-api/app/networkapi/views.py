1dcc817bf48ddc7e003812272214e3166ca1f115
Lucie Daeye <lucied@mozillafoundation.org>
2017-10-19 12:42:19 +0200

    Add a new route environment to expose whitelisted env var as JSON

---
+++ network-api/app/networkapi/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from django.views import View
+from mezzanine.conf import settings
+
+
+class EnvVariablesView(View):
+    """
+    A view that permits a GET to expose whitelisted environment
+    variables in JSON.
+    """
+
+    def get(self):
+        return JsonResponse(settings.FRONTEND)
