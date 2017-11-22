5a193618b58c6e7b8a1b3321265c66d518c9aaf1
Jay <jayyin11043@hotmail.com>
2017-11-22 07:35:16 -0500

    rebased master, rebranched and redone changes.

--- network-api/networkapi/news/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save, post_delete
-
-from networkapi.utility.build import build_static_site
-from networkapi.news.models import News
-
-
-def setup_signals():
-    post_save.connect(build_static_site, sender=News)
-    post_delete.connect(build_static_site, sender=News)
