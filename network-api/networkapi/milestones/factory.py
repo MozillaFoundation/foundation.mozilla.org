d439ae86d8374567ddb0251234e1304c2fb199d4
Christopher DeCairos <cade@mozillafoundation.org>
2018-01-10 15:48:42 -0500

    make the milestone duration a datetime.datetime, and make it configurable

--- network-api/networkapi/milestones/factory.py
+++ network-api/networkapi/milestones/factory.py
@@ -10,11 +10,16 @@ class MilestoneFactory(DjangoModelFactory):
 
     class Meta:
         model = Milestone
+        exclude = ('milestone_length',)
 
+    # Model Attributes
     headline = Faker('sentence', nb_words=4)
     photo = Faker('image_url')
-    start_date = Faker('future_date', end_date='+30d')
-    end_date = LazyAttribute(lambda o: o.start_date + timedelta(randrange(30)))
+    start_date = Faker('future_datetime', end_date='+30d')
+    end_date = LazyAttribute(lambda o: o.start_date + timedelta(days=o.milestone_length))
     description = Faker('paragraphs', nb=2)
     link_url = Faker('url')
     link_label = Faker('sentence', nb_words=4)
+
+    # LazyAttribute helper values
+    milestone_length = LazyAttribute(lambda o: randrange(30))
