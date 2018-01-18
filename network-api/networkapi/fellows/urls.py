950ea318840063d96d04ac8f0250e2ca8da1a53b
ScottDowne <scott.downe@gmail.com>
2018-01-18 13:33:51 -0500

    Adding some stub urls for the fellowships site.

--- network-api/networkapi/fellows/urls.py
+++ network-api/networkapi/fellows/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from networkapi.fellows import views
 
 urlpatterns = [
-    url('', views.fellows_home),
+    url(r'^$', views.fellows_home),
+    url(r'^directory/$', views.fellows_directory),
+    url(r'^support/$', views.fellows_support),
+    url(r'^apply/$', views.fellows_apply),
 ]
--- network-api/networkapi/fellows/views.py
+++ network-api/networkapi/fellows/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def fellows_home(request):
     return render(request, 'fellows_home.html')
+
+def fellows_directory(request):
+    return render(request, 'fellows_directory.html')
+
+def fellows_support(request):
+    return render(request, 'fellows_support.html')
+
+def fellows_apply(request):
+    return render(request, 'fellows_apply.html')
