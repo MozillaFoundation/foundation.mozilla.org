b27f0891d3c5a951326d537bb42ad0e472771fb7
Christopher DeCairos <christopherd@mozillafoundation.org>
2018-01-19 13:46:01 -0500

    Django 1.11 (#976)
    * 1. Use the lastest master branch commit of Mezzanine (Django 1.11 support)
    2. Update Django to 1.11
    3. Fix custom adminsortable template tags that were passing Context into the render method
    4. Remove deprecated Middleware
    
    * remove leftover context.update calls
    
    * Use the latest filebrowser_safe commit that support 1.11

--- network-api/networkapi/settings.py
+++ network-api/networkapi/settings.py
@@ -157,8 +157,6 @@ MIDDLEWARE = [
 
     'mezzanine.core.request.CurrentRequestMiddleware',
     'mezzanine.core.middleware.RedirectFallbackMiddleware',
-    'mezzanine.core.middleware.TemplateForDeviceMiddleware',
-    'mezzanine.core.middleware.TemplateForHostMiddleware',
     'mezzanine.core.middleware.AdminLoginInterfaceSelectorMiddleware',
     'mezzanine.core.middleware.SitePermissionMiddleware',
     'mezzanine.pages.middleware.PageMiddleware',
