6b6bae035e712d68746454be5f3201dbd601aba5
ScottDowne <scott.downe@gmail.com>
2018-01-22 17:30:56 -0500

    Adding some stub urls for the fellowships site. (#971)
    * Adding some stub urls for the fellowships site.
    
    * Update some of the urls a bit more

--- network-api/networkapi/fellows/urls.py
+++ network-api/networkapi/fellows/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from networkapi.fellows import views
 
 urlpatterns = [
-    url('', views.fellows_home),
+    url(r'^$', views.fellows_home),
+    url(r'^directory/$', views.fellows_directory),
+    url(r'^support/$', views.fellows_support),
+    url(r'^science/$', views.fellows_science),
+    url(r'^open-web/$', views.fellows_openweb),
 ]
