75d3b1a1d58dce9a108e023abb37542b3fc72f29
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-22 17:40:30 -0800

    fixed linting error

--- network-api/networkapi/fellows/urls.py
+++ network-api/networkapi/fellows/urls.py
@@ -3,7 +3,9 @@ from networkapi.fellows import views
 
 urlpatterns = [
     url(r'^$', views.fellows_home, name='fellowships-home'),
-    url(r'^directory/$', views.fellows_directory, name='fellowships-directory'),
+    url(r'^directory/$',
+        views.fellows_directory,
+        name='fellowships-directory'),
     url(r'^support/$', views.fellows_support, name='fellowships-support'),
     url(r'^science/$', views.fellows_science, name='fellowships-science'),
     url(r'^open-web/$', views.fellows_openweb, name='fellowships-open-web'),
