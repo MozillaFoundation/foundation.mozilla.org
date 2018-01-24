255b8e85e71691de68003335a40accef590c12ac
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-24 09:21:08 -0800

    fellowships homepage wireframe (#965)

--- network-api/networkapi/fellows/urls.py
+++ network-api/networkapi/fellows/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from networkapi.fellows import views
 
 urlpatterns = [
-    url(r'^$', views.fellows_home),
-    url(r'^directory/$', views.fellows_directory),
-    url(r'^support/$', views.fellows_support),
-    url(r'^science/$', views.fellows_science),
-    url(r'^open-web/$', views.fellows_openweb),
+    url(r'^$', views.fellows_home, name='fellowships-home'),
+    url(r'^directory/$',
+        views.fellows_directory,
+        name='fellowships-directory'),
+    url(r'^support/$', views.fellows_support, name='fellowships-support'),
+    url(r'^science/$', views.fellows_science, name='fellowships-science'),
+    url(r'^open-web/$', views.fellows_openweb, name='fellowships-open-web'),
 ]
