58bbe7634fda3ea5a84e5ef293205a41c0f73981
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-24 15:03:11 -0800

    added fellowship directory link to <Person>

--- source/js/main.js
+++ source/js/main.js
@@ -244,7 +244,8 @@ let main = {
         location: `City, Country`,
         image: `https://images.pexels.com/photos/264206/pexels-photo-264206.jpeg?w=500`,
         quote: `Quote quote quote quote quote quote quote quote quote quote quote quote.`,
-        affiliations: [ `Stanford University Professor; YouthLAB founder`]
+        affiliations: [ `Stanford University Professor; YouthLAB founder`],
+        fellow_directory_link: { type: `science`, link: `/fellowships/directory` }
       };
 
       ReactDOM.render(<Person metadata={metadata} />, document.getElementById(`featured-science-fellow`));
