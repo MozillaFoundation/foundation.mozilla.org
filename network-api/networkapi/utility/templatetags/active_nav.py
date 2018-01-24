6f59bfa667e1983649bf2aa2fb9ac1002de0368f
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-24 09:21:08 -0800

    fellowships homepage wireframe (#965)

---
+++ network-api/networkapi/utility/templatetags/active_nav.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(name='active_nav')
+def active_nav(request, view_name):
+
+    from django.core.urlresolvers import resolve, Resolver404
+    path = resolve(request.path_info)
+    if not request:
+        return ""
+    try:
+        return "active" if path.url_name == view_name else ""
+    except Resolver404:
+        return ""
