3ee274843e58242c20e5c25dc5109b9505099a48
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-25 13:36:03 -0800

    fixed linting errors

--- network-api/networkapi/fellows/urls.py
+++ network-api/networkapi/fellows/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import url
-
-from networkapi.fellows import views
 from networkapi.fellows.views import (
     FellowHomeView,
     FellowDirectoryView,
@@ -11,8 +9,16 @@ from networkapi.fellows.views import (
 
 urlpatterns = [
     url(r'^$', FellowHomeView.as_view(), name='fellowships-home'),
-    url(r'^directory/$', FellowDirectoryView.as_view(), name='fellowships-directory'),
-    url(r'^support/$', FellowSupportView.as_view(), name='fellowships-support'),
-    url(r'^science/$', FellowScienceView.as_view(), name='fellowships-science'),
-    url(r'^open-web/$', FellowOpenWebView.as_view(), name='fellowships-open-web'),
+    url(r'^directory/$',
+        FellowDirectoryView.as_view(),
+        name='fellowships-directory'),
+    url(r'^support/$',
+        FellowSupportView.as_view(),
+        name='fellowships-support'),
+    url(r'^science/$',
+        FellowScienceView.as_view(),
+        name='fellowships-science'),
+    url(r'^open-web/$',
+        FellowOpenWebView.as_view(),
+        name='fellowships-open-web'),
 ]
--- network-api/networkapi/fellows/views.py
+++ network-api/networkapi/fellows/views.py
@@ -4,29 +4,29 @@ from django.shortcuts import render
 
 class FellowHomeView(View):
 
-  def get(self, request):
-      return render(request, 'fellows_home.html')
+    def get(self, request):
+        return render(request, 'fellows_home.html')
 
 
 class FellowDirectoryView(View):
 
-  def get(self, request):
-      return render(request, 'fellows_directory.html')
+    def get(self, request):
+        return render(request, 'fellows_directory.html')
 
 
 class FellowSupportView(View):
 
-  def get(self, request):
-      return render(request, 'fellows_support.html')
+    def get(self, request):
+        return render(request, 'fellows_support.html')
 
 
 class FellowScienceView(View):
 
-  def get(self, request):
-      return render(request, 'fellows_science.html')
+    def get(self, request):
+        return render(request, 'fellows_science.html')
 
 
 class FellowOpenWebView(View):
 
-  def get(self, request):
-      return render(request, 'fellows_openweb.html')
+    def get(self, request):
+        return render(request, 'fellows_openweb.html')
