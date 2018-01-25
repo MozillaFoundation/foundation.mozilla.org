abbab1f98c53aab3afbb42dd36bb27f8cdada0de
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-25 13:32:24 -0800

    switched to class-based views for fellows

--- network-api/networkapi/fellows/urls.py
+++ network-api/networkapi/fellows/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
+
 from networkapi.fellows import views
+from networkapi.fellows.views import (
+    FellowHomeView,
+    FellowDirectoryView,
+    FellowSupportView,
+    FellowScienceView,
+    FellowOpenWebView,
+)
 
 urlpatterns = [
-    url(r'^$', views.fellows_home, name='fellowships-home'),
-    url(r'^directory/$',
-        views.fellows_directory,
-        name='fellowships-directory'),
-    url(r'^support/$', views.fellows_support, name='fellowships-support'),
-    url(r'^science/$', views.fellows_science, name='fellowships-science'),
-    url(r'^open-web/$', views.fellows_openweb, name='fellowships-open-web'),
+    url(r'^$', FellowHomeView.as_view(), name='fellowships-home'),
+    url(r'^directory/$', FellowDirectoryView.as_view(), name='fellowships-directory'),
+    url(r'^support/$', FellowSupportView.as_view(), name='fellowships-support'),
+    url(r'^science/$', FellowScienceView.as_view(), name='fellowships-science'),
+    url(r'^open-web/$', FellowOpenWebView.as_view(), name='fellowships-open-web'),
 ]
--- network-api/networkapi/fellows/views.py
+++ network-api/networkapi/fellows/views.py
@@ -1,22 +1,32 @@
+from django.views import View
 from django.shortcuts import render
 
 
-# Create your views here.
-def fellows_home(request):
-    return render(request, 'fellows_home.html')
+class FellowHomeView(View):
+
+  def get(self, request):
+      return render(request, 'fellows_home.html')
 
 
-def fellows_directory(request):
-    return render(request, 'fellows_directory.html')
+class FellowDirectoryView(View):
+
+  def get(self, request):
+      return render(request, 'fellows_directory.html')
 
 
-def fellows_support(request):
-    return render(request, 'fellows_support.html')
+class FellowSupportView(View):
+
+  def get(self, request):
+      return render(request, 'fellows_support.html')
 
 
-def fellows_science(request):
-    return render(request, 'fellows_science.html')
+class FellowScienceView(View):
+
+  def get(self, request):
+      return render(request, 'fellows_science.html')
 
 
-def fellows_openweb(request):
-    return render(request, 'fellows_openweb.html')
+class FellowOpenWebView(View):
+
+  def get(self, request):
+      return render(request, 'fellows_openweb.html')
