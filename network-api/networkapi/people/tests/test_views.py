b3209f197ee7c7e790e44efa72ed173f5dd778b0
anweshknayak <anweshknayak@gmail.com>
2018-01-31 01:03:09 +0530

    flake fix

--- network-api/networkapi/people/tests/test_views.py
+++ network-api/networkapi/people/tests/test_views.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 
 from networkapi.people.factory import PersonFactory
-from networkapi.people.views import PeopleListView,PersonView
+from networkapi.people.views import PeopleListView, PersonView
 
 
 class TestPersonView(TestCase):
