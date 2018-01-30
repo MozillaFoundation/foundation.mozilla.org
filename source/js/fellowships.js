0e60574c72360048069bfba483a4ded285cb31bc
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-30 10:40:44 -0800

    Fellowships support page (#1006)
    Fellowships support page

--- source/js/fellowships.js
+++ source/js/fellowships.js
@@ -34,13 +34,31 @@ function injectReactComponents() {
       name: `Firstname Surname`,
       role: `[Area Fellowship] Fellow, [Year]`,
       location: `City, Country`,
-      image: `https://images.pexels.com/photos/264206/pexels-photo-264206.jpeg?w=500`,
+      image: `https://images.pexels.com/photos/802112/pexels-photo-802112.jpeg?w=500`,
       quote: `Quote quote quote quote quote quote quote quote quote quote quote quote.`,
       affiliations: [`Stanford University Professor; YouthLAB founder`],
       'fellow_directory_link': { type: `science`, link: `/fellowships/directory` }
     };
 
     ReactDOM.render(<Person metadata={metadata} />, document.getElementById(`featured-open-web-fellow`));
+  }
+
+  // Featured fellow on Support page
+  if (document.getElementById(`featured-fellow-support-page`)) {
+    let metadata = {
+      featured: true,
+      'internet_health_issues': [ `Decentralization`, `Open Innovation` ],
+      links: [],
+      name: `Firstname Surname`,
+      role: `[Area Fellowship] Fellow, [Year]`,
+      location: `City, Country`,
+      image: `https://static.pexels.com/photos/416138/pexels-photo-416138.jpeg?w=500`,
+      quote: `Quote quote quote quote quote quote quote quote quote quote quote quote.`,
+      affiliations: [ `Stanford University Professor; YouthLAB founder`],
+      'fellow_directory_link': { type: `science`, link: `/fellowships/directory` }
+    };
+
+    ReactDOM.render(<Person metadata={metadata} />, document.getElementById(`featured-fellow-support-page`));
   }
 }
 
