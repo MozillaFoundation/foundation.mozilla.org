1fd88c37cfb7a4dac25d43782324f46822080d4b
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-30 22:15:13 -0800

    Related to #1019 - repsonsive fellowships nav + #977 - better multi-page nav on mobile

--- source/js/components/multipage-nav/multipage-nav.jsx
+++ source/js/components/multipage-nav/multipage-nav.jsx
@@ -18,18 +18,31 @@ export default class MultipageNav extends React.Component {
   }
 
   render() {
+    let activeLinkLabel;
     let links = this.props.links.map((link, index) => {
       let className = `opp-link${ link.isActive ? ` opp-link-active` : `` }`;
+
+      if (link.isActive) {
+        activeLinkLabel = <a className={`active-link-label d-inline-block ${className}`}>{link.label}</a>;
+      }
 
       return (
         <div key={`link-${index}`}>
           <a href={link.href} className={className}>{link.label}</a>
-          {index === 0 &&
-            <button className="expander" onClick={this.toggle}></button>
-          }
         </div>
       );
     });
+
+    console.log(activeLinkLabel);
+
+    links.unshift(<div key="current-active-link">
+      <button className="expander" onClick={this.toggle}>
+        <div className="d-flex justify-content-between">
+          {activeLinkLabel}
+          <div className="d-inline-block control"></div>
+        </div>
+      </button>
+    </div>);
 
     return (
       <div className={`multipage-nav${this.state.isOpen ? ` multipage-nav-open` : ``}`}>
