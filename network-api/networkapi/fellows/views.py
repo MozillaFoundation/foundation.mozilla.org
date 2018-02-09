90ab0580288ff83a10741b9082bc86d24109b851
Mavis Ou <mmmavis@users.noreply.github.com>
2018-02-09 11:33:41 -0800

    Related to #955 - fellowships directory pages

--- network-api/networkapi/fellows/views.py
+++ network-api/networkapi/fellows/views.py
@@ -6,20 +6,50 @@ def fellows_home(request):
     return render(request, 'fellows_home.html')
 
 
-def fellows_directory(request):
-    return render(request, 'fellows_directory.html')
-
-
-def fellows_support(request):
-    return render(request, 'fellows_support.html')
-
-
 def fellows_science(request):
     return render(request, 'fellows_science.html')
 
 
 def fellows_openweb(request):
     return render(request, 'fellows_openweb.html')
+
+
+def fellows_directory(request):
+    return render(request, 'fellows_directory.html')
+
+
+def fellows_directory_senior(request):
+    context = {'type': 'senior'}
+
+    return render(request, 'fellows_directory_type.html', context)
+
+
+def fellows_directory_science(request):
+    context = {'type': 'science'}
+
+    return render(request, 'fellows_directory_type.html', context)
+
+
+def fellows_directory_open_web(request):
+    context = {'type': 'open web'}
+
+    return render(request, 'fellows_directory_type.html', context)
+
+
+def fellows_directory_tech_policy(request):
+    context = {'type': 'tech policy'}
+
+    return render(request, 'fellows_directory_type.html', context)
+
+
+def fellows_directory_media(request):
+    context = {'type': 'media'}
+
+    return render(request, 'fellows_directory_type.html', context)
+
+
+def fellows_support(request):
+    return render(request, 'fellows_support.html')
 
 
 def fellows_apply(request):
