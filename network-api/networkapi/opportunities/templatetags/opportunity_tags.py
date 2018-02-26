6b8eb3a1d30a19926dd15745c6721851afd22585
Pomax <pomax@nihongoresources.com>
2018-02-26 10:37:00 -0800

    flake8

--- network-api/networkapi/opportunities/templatetags/opportunity_tags.py
+++ network-api/networkapi/opportunities/templatetags/opportunity_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.inclusion_tag('opportunities/tags/mini_site_sidebar.html', takes_context=True)
 def mini_site_sidebar(context, page):
 
-	# do we have children? If so, we're the root page of a mini-site
+    # do we have children? If so, we're the root page of a mini-site
     children = page.get_children().live()
     has_children = len(children) > 0
 
@@ -15,7 +15,7 @@ def mini_site_sidebar(context, page):
     # the same kind of page and we're a child-page in a mini-site, or
     # it's not and we're possibly the root of a mini-site
     ancestors = page.get_ancestors()
-    root = next((root for root in ancestors if root.specific_class==page.specific_class), page)
+    root = next((root for root in ancestors if root.specific_class == page.specific_class), page)
     is_top_page = (root == page)
 
     # We can now tell whether or not we're part of a mini-site
@@ -23,14 +23,14 @@ def mini_site_sidebar(context, page):
 
     # Grab the necessary level of tree for building the sidebar menu
     if page is not root:
-	    children = root.get_children().live()
+        children = root.get_children().live()
 
-	# Return the list of values we need to have our template
-	# generate the appropriate sidebar HTML.
+    # Return the list of values we need to have our template
+    # generate the appropriate sidebar HTML.
     return {
-    	'singleton': singleton,
-    	'top_level': is_top_page,
-    	'root': root,
-    	'current': page,
+        'singleton': singleton,
+        'top_level': is_top_page,
+        'root': root,
+        'current': page,
         'menu_pages': [root] + list(children.in_menu()),
     }
