ebf5f0a9b25824a2fd8f945183a42ca5e84bb3f4
Chris DeCairos <cade@mozillafoundation.org>
2018-04-04 16:48:54 -0400

    Fix code style

--- network-api/networkapi/tests.py
+++ network-api/networkapi/tests.py
@@ -1,22 +1,20 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from unittest.mock import MagicMock
-from mezzanine.conf import settings
 
 from networkapi.middleware import ReferrerMiddleware
 
 
 class ReferrerMiddlewareTests(TestCase):
 
-	def setUp(self):
-		self.middleware = ReferrerMiddleware()
-		self.request = MagicMock()
-		self.response = MagicMock()
+    def setUp(self):
+        self.middleware = ReferrerMiddleware()
+        self.request = MagicMock()
+        self.response = MagicMock()
 
-	def test_requestProcessing(self):
-		"""
-		Ensure that the middleware assigns a Referrer-Policy header to the response object
-		"""
+    def test_requestProcessing(self):
+        """
+        Ensure that the middleware assigns a Referrer-Policy header to the response object
+        """
 
-		response = self.middleware.process_response(self.request, self.response)
-		response.__setitem__.assert_called_with('Referrer-Policy', 'same-origin')
-
+        response = self.middleware.process_response(self.request, self.response)
+        response.__setitem__.assert_called_with('Referrer-Policy', 'same-origin')
