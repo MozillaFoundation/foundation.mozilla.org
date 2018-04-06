f8d5881187de8616a5a28882000f490b5ed53740
Pomax <pomax@nihongoresources.com>
2018-04-06 14:51:37 -0700

    add a tag for homepage images that shims the urls

---
+++ network-api/networkapi/wagtailpages/templatetags/homepage_tags.py
@@ -0,0 +1,29 @@
+from django import template
+from django.conf import settings;
+
+register = template.Library()
+
+# A special tag for homepage images that use the correct URL, because S3 troubles
+@register.inclusion_tag('wagtailpages/tags/homepage_image.html', takes_context=True)
+def homepage_image(context, path):
+    return homepage_image_with_class(context, path, '')
+
+# A special tag for homepage images that use the correct URL, because S3 troubles
+@register.inclusion_tag('wagtailpages/tags/homepage_image.html', takes_context=True)
+def homepage_image_with_class(context, path, classname):
+    root = settings.MEDIA_URL;
+
+    if settings.USE_S3:
+        awsl = settings.AWS_LOCATION
+        awscd = settings.AWS_S3_CUSTOM_DOMAIN
+        if awscd in root and awsl not in root:
+            old = awscd
+            new = awscd + '/' + awsl
+            root = root.replace(old, new)
+
+    url = '{}{}'.format(root, path)
+
+    return {
+        'url': url,
+        'classname': classname,
+    }
