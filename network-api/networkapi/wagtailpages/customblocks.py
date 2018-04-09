26f081fd79fc51ab5ecd93ad9c59aec90ae97034
Pomax <pomax@nihongoresources.com>
2018-04-09 16:35:55 -0700

    .

--- network-api/networkapi/wagtailpages/customblocks.py
+++ network-api/networkapi/wagtailpages/customblocks.py
@@ -74,6 +74,7 @@ class AlignedImageBlock(ImageBlock):
             ('', 'Do not apply any explicit alignment classes.'),
             ('left-align', 'Left-align this image with the page content.'),
             ('right-align', 'Right-align this image with the page content.'),
+            ('center', 'Center this image with the page content.'),
             ('full-width', 'Make this image full-width.'),
         ],
         default='',
