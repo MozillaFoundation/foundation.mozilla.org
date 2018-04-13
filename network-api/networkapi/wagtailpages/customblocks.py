0fafa9b2bb2fce238a4a67ee1a6b96e2b45aa5bd
Alan Mooiman <amooiman@gmail.com>
2018-04-13 16:31:41 -0400

    Streamfield quote (#1340)
    * Quote block first pass
    
    * Future provisions for multiple quotes

--- network-api/networkapi/wagtailpages/customblocks.py
+++ network-api/networkapi/wagtailpages/customblocks.py
@@ -192,3 +192,20 @@ class VideoBlock(blocks.StructBlock):
 
     class Meta:
         template = 'wagtailpages/blocks/video_block.html'
+
+
+class QuoteBlock(blocks.StructBlock):
+
+    # The goal is to be able to cycle through multiple quotes,
+    # so let's accept multiple quotes to start even if we only show one for now.
+    # This way we don't have to migrate the model again later
+
+    quotes = blocks.ListBlock(blocks.StructBlock([
+        ('quote', blocks.CharBlock()),
+        ('attribution', blocks.CharBlock())
+    ]))
+
+    class Meta:
+        template = 'wagtailpages/blocks/quote_block.html'
+        icon = 'openquote'
+        help_text = 'Multiple quotes can be entered, but for now we are only using the first'
