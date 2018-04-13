636f342c89b459080c04edaed4a9bc1cc3234ff4
Alan Mooiman <amooiman@gmail.com>
2018-04-13 11:57:32 -0400

    Future provisions for multiple quotes

--- network-api/networkapi/wagtailpages/customblocks.py
+++ network-api/networkapi/wagtailpages/customblocks.py
@@ -195,9 +195,17 @@ class VideoBlock(blocks.StructBlock):
 
 
 class QuoteBlock(blocks.StructBlock):
-    quote = blocks.CharBlock()
-    attribution = blocks.CharBlock()
+
+    # The goal is to be able to cycle through multiple quotes,
+    # so let's accept multiple quotes to start even if we only show one for now.
+    # This way we don't have to migrate the model again later
+
+    quotes = blocks.ListBlock(blocks.StructBlock([
+        ('quote', blocks.CharBlock()),
+        ('attribution', blocks.CharBlock())
+    ]))
 
     class Meta:
         template = 'wagtailpages/blocks/quote_block.html'
-        icon = 'quote'
+        icon = 'openquote'
+        help_text = 'Multiple quotes can be entered, but for now we are only using the first'
