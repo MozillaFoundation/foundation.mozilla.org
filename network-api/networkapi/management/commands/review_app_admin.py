4a35bbaa3eb6a66b5fd8479d6d2270ada32b639a
Chris DeCairos <cade@mozillafoundation.org>
2018-05-08 15:47:33 -0400

    modify review_app_user command to not throw

--- network-api/networkapi/management/commands/review_app_admin.py
+++ network-api/networkapi/management/commands/review_app_admin.py
@@ -2,7 +2,7 @@
 Management command called during the Heroku Review App post-deployment phase.
 Creates an admin user and prints the password to the build logs.
 """
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from factory import Faker
 from django.contrib.auth.models import User
@@ -12,13 +12,17 @@ class Command(BaseCommand):
     help = 'Create a superuser for use on Heroku review apps'
 
     def handle(self, *args, **options):
-        password = Faker(
-            'password',
-            length=16,
-            special_chars=True,
-            digits=True,
-            upper_case=True,
-            lower_case=True
-        ).generate({})
-        User.objects.create_superuser('admin', 'admin@example.com', password)
-        self.stdout.write(f'superuser created. Password: {password}')
+        try:
+            User.objects.get(username='admin')
+            self.stdout.write('super user already exists')
+        except ObjectDoesNotExist:
+            password = Faker(
+                'password',
+                length=16,
+                special_chars=True,
+                digits=True,
+                upper_case=True,
+                lower_case=True
+            ).generate({})
+            User.objects.create_superuser('admin', 'admin@example.com', password)
+            self.stdout.write(f'superuser created. Password: {password}')
