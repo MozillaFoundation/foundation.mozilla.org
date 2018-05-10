a123431061096e6a9cabd75af9b1a237607fa234
Christopher DeCairos <christopherd@mozillafoundation.org>
2018-05-10 17:08:55 -0400

    Wagtail data (#1509)
    * WIP
    
    * Generate pages for wagtail, and generate a superuser login for review apps
    
    * modify review_app_user command to not throw
    
    * delte CTAs in flush_models, lower the number of output sentences for description_faker
    
    * code style fix
    
    * update README
    
    * Update deployment steps to reflect new scripts
    
    * fix bad reference to manage.py in postdeploy heroku script
    
    * Bump Django to 1.11.13
    
    * Fix grammar in the README
    
    * Further README fixes
    
    * remove unnecessary try-except from flush_models
    
    * generate Homepage highlights and news, add some missing pages
    
    * fix code style
    
    * Wrap single/multi page generation in try-except blocks to prevent trying to create duplicate pages. Delete Highlight records when flushing models
    
    * Call build instead of create when generating featured news and highlights so the factories don't automatically generate new homepages

---
+++ network-api/networkapi/management/commands/review_app_admin.py
@@ -0,0 +1,28 @@
+"""
+Management command called during the Heroku Review App post-deployment phase.
+Creates an admin user and prints the password to the build logs.
+"""
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand
+from factory import Faker
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create a superuser for use on Heroku review apps'
+
+    def handle(self, *args, **options):
+        try:
+            User.objects.get(username='admin')
+            self.stdout.write('super user already exists')
+        except ObjectDoesNotExist:
+            password = Faker(
+                'password',
+                length=16,
+                special_chars=True,
+                digits=True,
+                upper_case=True,
+                lower_case=True
+            ).generate({})
+            User.objects.create_superuser('admin', 'admin@example.com', password)
+            self.stdout.write(f'superuser created. Password: {password}')
---
+++ network-api/networkapi/wagtailpages/factory.py
@@ -0,0 +1,159 @@
+from factory.django import DjangoModelFactory
+from wagtail_factories import (
+    PageFactory,
+    ImageFactory
+)
+from factory import (
+    Faker,
+    SubFactory,
+    LazyAttribute,
+    Trait)
+import networkapi.wagtailpages.models
+from networkapi.highlights.factory import HighlightFactory
+from networkapi.news.factory import NewsFactory
+
+sentence_faker: Faker = Faker('sentence', nb_words=3, variable_nb_words=False)
+header_faker: Faker = Faker('sentence', nb_words=6, variable_nb_words=True)
+description_faker: Faker = Faker('paragraphs', nb=6)
+
+
+class CTAFactory(DjangoModelFactory):
+    class Meta:
+        abstract = True
+        exclude = (
+            'header_text',
+            'description_text',
+        )
+
+    name = Faker('text', max_nb_chars=80)
+    header = LazyAttribute(lambda o: o.header_text.rstrip('.'))
+    description = LazyAttribute(lambda o: ''.join(o.description_text))
+    newsletter = Faker('word')
+
+    # Lazy Values
+    description_text = description_faker
+    header_text = header_faker
+
+
+class PetitionFactory(CTAFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.Petition
+
+
+class SignupFactory(CTAFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.Signup
+
+
+class WagtailHomepageFactory(PageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.Homepage
+
+    hero_headline = Faker('text', max_nb_chars=140)
+    hero_story_description = Faker('paragraph', nb_sentences=5, variable_nb_sentences=True)
+    hero_button_text = Faker('text', max_nb_chars=50)
+    hero_button_url = Faker('url')
+    hero_image = SubFactory(ImageFactory)
+
+
+class CMSPageFactory(PageFactory):
+    class Meta:
+        abstract = True
+        exclude = (
+            'title_text',
+            'header_text',
+        )
+
+    header = LazyAttribute(lambda o: o.header_text.rstrip('.'))
+    title = LazyAttribute(lambda o: o.title_text.rstrip('.'))
+    narrowed_page_content = Faker('boolean', chance_of_getting_true=50)
+
+    # Lazy Values
+    title_text = sentence_faker
+    header_text = header_faker
+
+
+class PrimaryPageFactory(CMSPageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.PrimaryPage
+
+
+class CampaignPageFactory(CMSPageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.CampaignPage
+
+    class Params:
+        no_cta = Trait(cta=None)
+
+    cta = SubFactory(PetitionFactory)
+
+
+class MiniSiteNameSpaceFactory(PageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.MiniSiteNameSpace
+
+
+class PeoplePageFactory(PageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.PeoplePage
+
+    title = 'people'
+
+
+class NewsPageFactory(PageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.NewsPage
+
+    title = 'news'
+
+
+class StyleguideFactory(PageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.Styleguide
+
+    title = 'styleguide'
+
+
+class InitiativesPageFactory(PageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.InitiativesPage
+
+    title = 'initiatives'
+
+
+class ParticipatePageFactory(PageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.ParticipatePage
+
+    title = 'participate'
+
+
+class OpportunityPageFactory(CMSPageFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.OpportunityPage
+
+    class Params:
+        no_cta = Trait(cta=None)
+
+    cta = SubFactory(SignupFactory)
+
+
+class FeaturedFactory(DjangoModelFactory):
+    class Meta:
+        abstract = True
+
+    page = SubFactory(WagtailHomepageFactory)
+
+
+class HomepageFeaturedNewsFactory(FeaturedFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.HomepageFeaturedNews
+
+    news = SubFactory(NewsFactory)
+
+
+class HomepageFeaturedHighlightsFactory(FeaturedFactory):
+    class Meta:
+        model = networkapi.wagtailpages.models.HomepageFeaturedHighlights
+
+    highlight = SubFactory(HighlightFactory)
