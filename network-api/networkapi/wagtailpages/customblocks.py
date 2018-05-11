152bbf0aa5ee2f6b5ff498c64649100aaa0142d2
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-05-11 13:56:39 -0700

    iframe component revisions

--- network-api/networkapi/wagtailpages/customblocks.py
+++ network-api/networkapi/wagtailpages/customblocks.py
@@ -202,7 +202,13 @@ class iFrameBlock(blocks.StructBlock):
     url = blocks.CharBlock(
         help_text='Please note that only URLs from white-listed domains will work.'
     )
-    height = blocks.IntegerBlock(default=450)
+    caption = blocks.CharBlock(
+        required=False
+    )
+    captionURL = blocks.CharBlock(
+        required=False,
+        help_text='Optional URL that this caption should link out to.'
+    )
 
     class Meta:
         template = 'wagtailpages/blocks/iframe_block.html'
