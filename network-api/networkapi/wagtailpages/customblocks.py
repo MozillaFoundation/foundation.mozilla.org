a728b8233e68f2eab1d7eb1e125ac67c9224bdf4
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-05-15 10:20:51 -0700

    Revert "Updating Video component (#1520)"
    This reverts commit 8430fdc7da528beb62adabc87df812ccefecd809.

--- network-api/networkapi/wagtailpages/customblocks.py
+++ network-api/networkapi/wagtailpages/customblocks.py
@@ -218,13 +218,7 @@ class VideoBlock(blocks.StructBlock):
     url = blocks.CharBlock(
         help_text='Please make sure this is a proper embed URL, or your video will not show up on the page.'
     )
-    caption = blocks.CharBlock(
-        required=False,
-    )
-    captionURL = blocks.CharBlock(
-        required=False,
-        help_text='Optional URL for caption to link to.',
-    )
+    height = blocks.IntegerBlock(default=450)
 
     class Meta:
         template = 'wagtailpages/blocks/video_block.html'
