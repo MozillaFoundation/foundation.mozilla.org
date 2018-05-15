3acea562b826d55b97f08941aadd02545dc6a2e7
gavin lazar suntop <gavin@gsuntop.com>
2018-05-15 13:11:47 -0700

    Updating Video component (redo) (#1527)
    * updating video component - #1511
    
    * sparkling fresh migration

--- network-api/networkapi/wagtailpages/customblocks.py
+++ network-api/networkapi/wagtailpages/customblocks.py
@@ -218,7 +218,13 @@ class VideoBlock(blocks.StructBlock):
     url = blocks.CharBlock(
         help_text='Please make sure this is a proper embed URL, or your video will not show up on the page.'
     )
-    height = blocks.IntegerBlock(default=450)
+    caption = blocks.CharBlock(
+        required=False,
+    )
+    captionURL = blocks.CharBlock(
+        required=False,
+        help_text='Optional URL for caption to link to.'
+    )
 
     class Meta:
         template = 'wagtailpages/blocks/video_block.html'
