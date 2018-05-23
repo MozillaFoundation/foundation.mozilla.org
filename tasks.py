9ea75a26a9f83cee7d1c006329663174d8c389bd
Lucie <lucie.daeye@gmail.com>
2018-05-23 19:33:20 +0200

    fix issue around python command output on windows (#1556)

--- tasks.py
+++ tasks.py
@@ -9,17 +9,24 @@ os.environ.pop('__PYVENV_LAUNCHER__', None)
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
 
+# Python commands's outputs are not rendering properly. Setting pty for *Nix system and
+# "PYTHONUNBUFFERED" env var for Windows at True.
+if platform == 'win32':
+    PLATFORM_ARG = dict(env={'PYTHONUNBUFFERED': 'True'})
+else:
+    PLATFORM_ARG = dict(pty=True)
+
 
 @task(optional=['option', 'flag'])
 def manage(ctx, command, option=None, flag=None):
     """Shorthand to manage.py. inv manage [COMMAND] [-o OPTION] [-f FLAG]. ex: inv manage runserver -o 3000"""
     with ctx.cd(ROOT):
         if option:
-            ctx.run(f"pipenv run python network-api/manage.py {command} {option}", pty=True)
+            ctx.run(f"pipenv run python network-api/manage.py {command} {option}", **PLATFORM_ARG)
         elif flag:
-            ctx.run(f"pipenv run python network-api/manage.py {command} --{flag}", pty=True)
+            ctx.run(f"pipenv run python network-api/manage.py {command} --{flag}", **PLATFORM_ARG)
         else:
-            ctx.run(f"pipenv run python network-api/manage.py {command}", pty=True)
+            ctx.run(f"pipenv run python network-api/manage.py {command}", **PLATFORM_ARG)
 
 
 @task
@@ -44,7 +51,7 @@ def makemigrations(ctx):
 def test(ctx):
     """Run tests"""
     print("Running flake8")
-    ctx.run(f"pipenv run flake8 network-api", pty=True)
+    ctx.run(f"pipenv run flake8 network-api", **PLATFORM_ARG)
     print("Running tests")
     manage(ctx, "test")
 
@@ -56,9 +63,9 @@ def setup(ctx):
         print("Copying default environment variables.")
         copy("env.default", ".env")
         print("Installing npm dependencies and build.")
-        ctx.run("npm install && npm run build", pty=True)
+        ctx.run("npm install && npm run build")
         print("Installing Python dependencies.")
-        ctx.run("pipenv install --dev", pty=True)
+        ctx.run("pipenv install --dev", **PLATFORM_ARG)
         print("Applying database migrations.")
         ctx.run("inv migrate")
         print("Creating fake data")
