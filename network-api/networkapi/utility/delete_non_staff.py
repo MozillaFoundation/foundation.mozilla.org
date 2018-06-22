2035837af1bd0c56f728885de2630792a45f52a1
Lucie Daeye <lucied@mozillafoundation.org>
2018-06-22 16:21:11 +0200

    let's print who we are actualling removing

--- network-api/networkapi/utility/delete_non_staff.py
+++ network-api/networkapi/utility/delete_non_staff.py
@@ -4,5 +4,8 @@ from django.db.models import Q
 
 def delete_non_staff():
     print("Deleting non staff users")
-    User.objects.exclude(Q(email__contains='mozillafoundation,org') | Q(is_staff=True)).delete()
+    non_staff = User.objects.exclude(Q(email__contains='mozillafoundation,org') | Q(is_staff=True))
+    for e in non_staff:
+        print(f'Deleting {e.username}')
+        e.delete()
     print("Done!")
