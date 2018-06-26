2a1b3879aed6fee74e6223cc543dd8ee2086da34
Lucie Daeye <lucied@mozillafoundation.org>
2018-06-26 14:34:12 +0200

    One test per case

--- network-api/networkapi/tests.py
+++ network-api/networkapi/tests.py
@@ -3,7 +3,6 @@ from django.core.management import call_command
 from django.test import TestCase
 from unittest.mock import MagicMock
 
-from delete_non_staff import delete_non_staff
 from networkapi.middleware import ReferrerMiddleware
 
 
@@ -32,22 +31,62 @@ class MissingMigrationsTests(TestCase):
         call_command('makemigrations', interactive=False, dry_run=True, check_changes=True)
 
 
-class DeleteNonStaffTests(TestCase):
+class DeleteNonStaffTest(TestCase):
 
     def setUp(self):
-        group = Group.objects.create(name='TestGroup')
-        group.user_set.create(username='Sam')
-        User.objects.bulk_create([
-            User(username='Alex'),
-            User(username='Bob', email='bob@mozillafoundation.org'),
-            User(username='Alice', is_staff=True)
-        ])
+        User.objects.create(username='Alex'),
 
     def test_non_staff_is_deleted(self):
         """
-        Test that only non_staff users are deleted
+        Simple users are deleted
         """
 
-        delete_non_staff()
+        call_command('delete_non_staff')
 
-        self.assertEqual(User.objects.count(), 3)
+        self.assertEqual(User.objects.count(), 0)
+
+
+class IsStaffNotDeletedTest(TestCase):
+
+    def setUp(self):
+        User.objects.create(username='Alex', is_staff=True)
+
+    def test_is_staff_not_deleted(self):
+        """
+        Users with 'is_staff' flag at True are not deleted
+        """
+
+        call_command('delete_non_staff')
+
+        self.assertEqual(User.objects.count(), 1)
+
+
+class InGroupNotDeletedTest(TestCase):
+
+    def setUp(self):
+        group = Group.objects.create(name='TestGroup')
+        group.user_set.create(username='Alex')
+
+    def test_in_group_not_deleted(self):
+        """
+        Users in a group are not deleted
+        """
+
+        call_command('delete_non_staff')
+
+        self.assertEqual(User.objects.count(), 1)
+
+
+class MozillaFoundationUsersNotDeletedTest(TestCase):
+
+    def setUp(self):
+        User.objects.create(username='Alex', email='alex@mozillafoundation.org')
+
+    def test_mozilla_Foundation_users_not_deleted(self):
+        """
+        Mozilla Foundation Users are not deleted
+        """
+
+        call_command('delete_non_staff')
+
+        self.assertEqual(User.objects.count(), 1)
