818b0f52958795bf4c870f4480f8438345f3501b
Pomax <pomax@nihongoresources.com>
2018-07-25 11:53:11 -0700

    just relocate the DOM node instead of stickying

--- source/js/components/petition/donation-modal.jsx
+++ source/js/components/petition/donation-modal.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import classNames from 'classnames';
 
+/**
+ * Supported event handlers:
+ *
+ *   - onClose
+ *   - onShare
+ *   - onDonate
+ *
+ * Supported plain props:
+ *
+ *   - ctn, the campaign tracing number
+ *   - dmi, the donate modal id
+ *   - heading, the modal header
+ *   - bodyText, the main text
+ *   - donateText, the "donate" button label
+ *   - shareText, the "no donate, just share" button label
+ */
 class DonationModal extends React.Component {
+
+  componentDidMount() {
+    // Thanks to Safari's poor support of the "sticky"
+    // CSS position property, we need to relocate the DOM
+    // node so that we get the full-viewport effect.
+    let body = document.body,
+        c1 = body.children[0],
+        n = this.fragment;
+
+    if (!c1) {
+      body.appendChild(n);
+    } else {
+      body.insertBefore(n, c1);
+    }
+  }
+
+  componentWillUnmount() {
+    // Make sure to put the dom node back where it belongs
+    // before unmounting, so that React doesn't get too confused.
+    this.domLocation.appendChild(this.fragment);
+  }
 
   render() {
     return (
-      <div className="modal-underlay">
-        <div className="modal show" role="dialog">
-          <div className="modal-dialog modal-lg" role="document">
-            {this.getModalContent()}
+      <div ref={e => this.domLocation = e}>
+        <div className="modal-underlay" ref={e => this.fragment = e}>
+          <div className="modal show" role="dialog">
+            <div className="modal-dialog modal-lg" role="document">
+              {this.getModalContent()}
+            </div>
           </div>
         </div>
       </div>
@@ -27,7 +66,7 @@ class DonationModal extends React.Component {
     return (
       <div className="modal-content" tabIndex="0">
         <div className="modal-header text-right">
-          <button className="close" data-dismiss="modal" aria-label="Close" onClick={this.props.onClose}>
+          <button className="close" data-dismiss="modal" aria-label="Close" onClick={e => this.props.onClose(e)}>
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
