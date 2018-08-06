62aaa2ff250426cbd58623d8564a7be935ed256e
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-08-06 16:33:51 -0700

    adding commitment to style guide

--- network-api/networkapi/wagtailpages/templatetags/card_tags.py
+++ network-api/networkapi/wagtailpages/templatetags/card_tags.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.inclusion_tag('wagtailpages/tags/card.html')
-def card(md_width, image, title, description, link_url, link_label, commitment):
+def card(md_width, image, title, description, link_url, link_label, commitment=None):
     return {
         'md_width': md_width,
         'image': image,
