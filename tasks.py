602acc3a10d28b91165a5b96dbaf2218ecaa3725
Pomax <pomax@nihongoresources.com>
2018-08-16 09:17:19 -0700

    Squashed changes from original_messy_branch

--- tasks.py
+++ tasks.py
@@ -48,6 +48,19 @@ def makemigrations(ctx):
 
 
 @task
+def l10n_sync(ctx):
+    """Sync localizable fields in the database"""
+    manage(ctx, "sync_page_translation_fields")
+
+
+@task
+def l10n_update(ctx):
+    """Update localizable field data (copies from
+    original unlocalized to default localized field)"""
+    manage(ctx, "update_translation_fields")
+
+
+@task
 def test(ctx):
     """Run tests"""
     print("Running flake8")
@@ -72,6 +85,10 @@ def setup(ctx):
         ctx.run("inv manage load_fake_data")
         print("Updating block information")
         ctx.run("inv manage block_inventory")
+        print("Updating localizable fields");
+        ctx.run("inv l10n-sync")
+        ctx.run("inv l10n-update")
+
         # Windows doesn't support pty, skipping this step
         if platform == 'win32':
             print("All done!\n"
@@ -93,3 +110,6 @@ def catch_up(ctx):
     ctx.run("inv migrate")
     print("Updating block information")
     ctx.run("inv manage block_inventory")
+    print("Updating localizable fields");
+    ctx.run("inv l10n-sync")
+    ctx.run("inv l10n-update")
