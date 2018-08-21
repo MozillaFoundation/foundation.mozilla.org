6a68eea1f6f76a3a8088074a6cde2aa3b57bdc59
gavin lazar suntop <gavin@gsuntop.com>
2018-08-21 15:20:56 -0700

    Revert "New Participate Page (#1748)"
    This reverts commit 8b108ccaea8d1ae0865a31777574eaa6ed3c8211.

--- network-api/networkapi/wagtailpages/templatetags/card_tags.py
+++ network-api/networkapi/wagtailpages/templatetags/card_tags.py
@@ -4,51 +4,12 @@ register = template.Library()
 
 
 @register.inclusion_tag('wagtailpages/tags/card.html')
-def card(image, title, description, link_url, link_label, commitment=None):
+def card(md_width, image, title, description, link_url, link_label):
     return {
+        'md_width': md_width,
         'image': image,
         'title': title,
         'description': description,
         'link_url': link_url,
-        'link_label': link_label,
-        'commitment': commitment,
-    }
-
-
-@register.inclusion_tag('wagtailpages/tags/card-cta.html')
-def cardCTA(
-    image,
-    title,
-    description,
-    link_url,
-    link_label,
-    commitment=None,
-    facebook=None,
-    twitter=None,
-    email_subject=None,
-    email_body=None
-):
-    return {
-        'image': image,
-        'title': title,
-        'description': description,
-        'link_url': link_url,
-        'link_label': link_label,
-        'commitment': commitment,
-        'facebook': facebook,
-        'twitter': twitter,
-        'email_subject': email_subject,
-        'email_body': email_body,
-    }
-
-
-@register.inclusion_tag('wagtailpages/tags/card-large.html')
-def cardLarge(image, title, description, link_url, link_label, commitment=None):
-    return {
-        'image': image,
-        'title': title,
-        'description': description,
-        'link_url': link_url,
-        'link_label': link_label,
-        'commitment': commitment,
+        'link_label': link_label
     }
