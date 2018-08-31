9ef62ede61da401d922d2a20659a9b65ceeb8dbf
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-08-31 14:45:57 -0700

    js architecture

--- source/js/buyers-guide/bg-main.js
+++ source/js/buyers-guide/bg-main.js
@@ -1,1 +1,25 @@
-// TODO
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Creepometer from './components/creepometer/creepometer.jsx';
+
+let main = {
+  init() {
+    this.injectReactComponents();
+  },
+
+  // Embed various React components based on the existence of containers within the current page
+  injectReactComponents() {
+    if (document.querySelectorAll(`.creepometer`)) {
+      var elements = Array.from(document.querySelectorAll(`.creepometer`));
+
+      if (elements.length) {
+        elements.forEach(element => {
+          ReactDOM.render(<Creepometer/>, element);
+        });
+      }
+    }
+  }
+};
+
+main.init();
--- source/js/buyers-guide/components/creepometer/creepometer.jsx
+++ source/js/buyers-guide/components/creepometer/creepometer.jsx
@@ -1,1 +1,15 @@
-// TODO
+import React from 'react';
+
+export default class Creepometer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Creepometer!</div>
+    );
+  }
+}
