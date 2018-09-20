3790ea39886a48efc907c57b3e0585b9e8dc2ed4
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-09-20 16:52:45 -0700

    adding interactive criterion component

--- source/js/buyers-guide/bg-main.js
+++ source/js/buyers-guide/bg-main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Creepometer from './components/creepometer/creepometer.jsx';
+import Criterion from './components/criterion/criterion.jsx';
 
 let main = {
   init() {
@@ -13,6 +14,14 @@ let main = {
     if (document.querySelectorAll(`.creepometer`)) {
       Array.from(document.querySelectorAll(`.creepometer`)).forEach(element => {
         ReactDOM.render(<Creepometer/>, element);
+      });
+    }
+
+    if (document.querySelectorAll(`.criterion-target`)) {
+      Array.from(document.querySelectorAll(`.criterion-target`)).forEach(element => {
+        let meta = JSON.parse(element.dataset.meta);
+
+        ReactDOM.render(<Criterion meta={meta}></Criterion>, element);
       });
     }
   }
---
+++ source/js/buyers-guide/components/criterion/criterion.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default class Criterion extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      hasHelptext: this.props.meta.helptext.length > 0,
+      helptextVisible: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      helptextVisible: !this.state.helptextVisible
+    });
+  }
+
+  render() {
+    let fullClass = `criterion criterion-${this.props.meta.class} criterion-${this.props.meta.id}`;
+
+    return (
+      <div className={fullClass}>
+        <div className="primary-info">
+          <p className="d-flex align-items-center">
+            {this.props.meta.question}
+            {this.state.hasHelptext &&
+              <button onClick={this.toggle} className={this.state.helptextVisible ? `open` : `closed`}></button>
+            }
+          </p>
+          <p className="rating">{ this.props.meta.answer }</p>
+        </div>
+        {this.state.helptextVisible &&
+        <div className="helptext mt-3">
+          {this.props.meta.helptext}
+        </div>
+        }
+      </div>
+    );
+  }
+}
