92159453d0a8734b82320280e7e696c5dd8d10ab
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-10-02 15:03:56 -0700

    meter basics

--- source/js/buyers-guide/components/creepometer/creepometer.jsx
+++ source/js/buyers-guide/components/creepometer/creepometer.jsx
@@ -4,12 +4,58 @@ export default class Creepometer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      isHandleGrabbed: false,
+      handleOffset: 0
+    };
+
+    this.handleWidth = 70;
+
+    this.slideStart = this.slideStart.bind(this);
+    this.slideMove = this.slideMove.bind(this);
+    this.slideStop = this.slideStop.bind(this);
+
+    this.setSliderRef = element => {
+      this.sliderElement = element;
+    };
+
+  }
+
+  slideStart() {
+    this.setState({
+      isHandleGrabbed: true
+    });
+  }
+
+  slideMove(e) {
+    if (this.state.isHandleGrabbed && e.nativeEvent.target.className === `slider`) {
+      this.setState({
+        handleOffset: e.nativeEvent.offsetX
+      });
+    }
+  }
+
+  slideStop() {
+    this.setState({
+      isHandleGrabbed: false
+    });
   }
 
   render() {
+    let handleX = this.state.handleOffset - this.handleWidth / 2;
+
+    // Don't let handle overflow slider's left side
+    handleX = handleX < 0 ? 0 : handleX;
+
+    // Don't let handle overflow slider's right side
+    if (this.sliderElement && handleX > this.sliderElement.scrollWidth - this.handleWidth) {
+      handleX = this.sliderElement.scrollWidth - this.handleWidth;
+    }
+
     return (
-      <div>Creepometer 😬</div>
+      <div className="slider" ref={this.setSliderRef} onMouseLeave={this.slideStop} onMouseMove={this.slideMove} onMouseDown={this.slideStart} onMouseUp={this.slideStop}>
+        <div className="handle" style={{left: `${handleX}px`}}></div>
+      </div>
     );
   }
 }
