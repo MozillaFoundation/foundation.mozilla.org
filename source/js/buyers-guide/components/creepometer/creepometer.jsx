9a8ce9cda2ce9f7473a6746ad12e56bac8d1f834
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-10-04 15:12:10 -0700

    jank slide

--- source/js/buyers-guide/components/creepometer/creepometer.jsx
+++ source/js/buyers-guide/components/creepometer/creepometer.jsx
@@ -10,6 +10,7 @@ export default class Creepometer extends React.Component {
     };
 
     this.handleWidth = 70;
+    this.faceCount = 40; // Number of face frames
 
     this.slideStart = this.slideStart.bind(this);
     this.slideMove = this.slideMove.bind(this);
@@ -22,6 +23,7 @@ export default class Creepometer extends React.Component {
   }
 
   slideStart() {
+    console.log(`slideStart`);
     this.setState({
       isHandleGrabbed: true
     });
@@ -43,18 +45,27 @@ export default class Creepometer extends React.Component {
 
   render() {
     let handleX = this.state.handleOffset - this.handleWidth / 2;
+    let frameChoice = Math.floor(this.faceCount / 2);
 
     // Don't let handle overflow slider's left side
     handleX = handleX < 0 ? 0 : handleX;
 
-    // Don't let handle overflow slider's right side
-    if (this.sliderElement && handleX > this.sliderElement.scrollWidth - this.handleWidth) {
-      handleX = this.sliderElement.scrollWidth - this.handleWidth;
+    if (this.sliderElement) {
+      // Don't let handle overflow slider's right side
+      if (handleX > this.sliderElement.scrollWidth - this.handleWidth) {
+        handleX = this.sliderElement.scrollWidth - this.handleWidth;
+      }
+
+      frameChoice = Math.floor(handleX / this.sliderElement.scrollWidth * this.faceCount) + 1;
+      console.log(frameChoice);
     }
+
+    // let frameChoice = this.state.handleOffset /
+    let faceImgSrc = `/_images/buyers-guide/faces/${frameChoice}.png`;
 
     return (
       <div className="slider" ref={this.setSliderRef} onMouseLeave={this.slideStop} onMouseMove={this.slideMove} onMouseDown={this.slideStart} onMouseUp={this.slideStop}>
-        <div className="handle" style={{left: `${handleX}px`}}></div>
+        <div className="handle" style={{background: `url("${faceImgSrc}") 0 0 / contain, #f2b946`, left: `${handleX}px`}}></div>
       </div>
     );
   }
