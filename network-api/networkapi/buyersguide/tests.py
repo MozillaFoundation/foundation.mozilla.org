26a5c6291fb169d778216c4c2a5a075f848be84c
Pomax <pomax@nihongoresources.com>
2018-10-16 09:51:02 -0700

    with tests even

--- network-api/networkapi/buyersguide/tests.py
+++ network-api/networkapi/buyersguide/tests.py
@@ -317,3 +317,11 @@ class BuyersGuideViewTest(TestCase):
         request = self.factory.get('/privacynotincluded/products/this is not a product')
         request.user = self.user
         self.assertRaises(Http404, product_view, request, 'this is not a product')
+
+    def test_category_view_404(self):
+        """
+        Test that the category view raises an Http404 if the category name doesn't exist
+        """
+        request = self.factory.get('/privacynotincluded/categories/this is not a category')
+        request.user = self.user
+        self.assertRaises(Http404, product_view, request, 'this is not a category')
