13a1e7f206343454c26b1fa67e5685f7fc2e60ab
Pomax <pomax@nihongoresources.com>
2018-10-25 09:25:11 -0700

    open post-vote shares in new tabs, and try to track clicks if allowed

--- source/js/buyers-guide/components/social-share/social-share.jsx
+++ source/js/buyers-guide/components/social-share/social-share.jsx
@@ -1,33 +1,49 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+import DNT from '../../dnt.js';
 
 const SocialShareLink = (props) => {
   let classes = `social-button`;
   let srLabel = ``;
   let link = `PrivacyNotINcluded.org`;
   let shareText = `I think ${props.productName} is ${props.creepType.toUpperCase()}. What do you think? Check out the Creep-O-Meter over on @mozilla’s ${link} holiday buyer’s guide.`;
+  let shareEvent = {
+    category: `product`,
+    action: `share tap`,
+    label: `share vote `,
+    transport: `beacon`
+  };
 
-  switch (props.type) {
-    case 'facebook':
+  if (props.type === `facebook`) {
       classes += " social-button-fb";
       srLabel = `Facebook`;
+      shareEvent.label += `to facebook`;
       link = `https://www.facebook.com/sharer/sharer.php?u=https://${link}`;
-
-      break;
-    case 'twitter':
-      classes += " social-button-twitter";
-      srLabel = `Twitter`;
-      link = `https://twitter.com/intent/tweet?text=${encodeURIComponent(shareText)}`;
-
-      break;
-    case 'email':
-      classes += " social-button-email";
-      srLabel = `Email`;
-      link = `mailto:?&body=${encodeURIComponent(shareText)}`;
-
-      break;
   }
 
-  return <a className={classes} href={link}><span class="sr-only">{srLabel}</span></a>;
+  if (props.type === `twitter`) {
+      classes += " social-button-twitter";
+      srLabel = `Twitter`;
+      shareEvent.label += `to twitter`;
+      link = `https://twitter.com/intent/tweet?text=${encodeURIComponent(shareText)}`;
+  }
+
+  if (props.type === `email`) {
+      classes += " social-button-email";
+      srLabel = `Email`;
+      shareEvent.label += `via email`;
+      link = `mailto:?&body=${encodeURIComponent(shareText)}`;
+  }
+
+  let trackShareAction = () => {};
+
+  if (DNT.allowTracking) {
+    trackShareAction = evt => {
+      ReactGA.event(shareEvent);
+    };
+  }
+
+  return <a target="_blank" className={classes} href={link} onClick={trackShareAction}><span class="sr-only">{srLabel}</span></a>;
 };
 
 const SocialShare = (props) => {
