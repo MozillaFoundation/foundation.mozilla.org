e9f930041011c98641922452e01f6bfc01bf4aaf
Lucie Daeye <lucied@mozillafoundation.org>
2018-11-06 15:51:04 +0100

    Adapt the mediaurl in view depending if we're using cloudinary or not

--- network-api/networkapi/buyersguide/views.py
+++ network-api/networkapi/buyersguide/views.py
@@ -18,6 +18,12 @@ vote_throttle_class = UserVoteRateThrottle if not settings.TESTING else TestUser
 locale_regex = re.compile(r"^/[a-z]{2}(-[A-Z]{2})?/")
 
 
+if settings.USE_CLOUDINARY:
+    MEDIA_URL = settings.CLOUDINARY_URL
+else:
+    MEDIA_URL = settings.MEDIA_URL
+
+
 def get_average_creepiness(product):
     try:
         votes = product['votes']
@@ -63,7 +69,7 @@ def buyersguide_home(request):
     return render(request, 'buyersguide_home.html', {
         'categories': BuyersGuideProductCategory.objects.all(),
         'products': products,
-        'mediaUrl': settings.CLOUDINARY_URL,
+        'mediaUrl': MEDIA_URL,
     })
 
 
@@ -82,7 +88,7 @@ def category_view(request, categoryname):
         'categories': BuyersGuideProductCategory.objects.all(),
         'category': category,
         'products': products,
-        'mediaUrl': settings.CLOUDINARY_URL,
+        'mediaUrl': MEDIA_URL,
     })
 
 
@@ -93,7 +99,7 @@ def product_view(request, slug):
     return render(request, 'product_page.html', {
         'categories': BuyersGuideProductCategory.objects.all(),
         'product': product.to_dict(),
-        'mediaUrl': settings.CLOUDINARY_URL,
+        'mediaUrl': MEDIA_URL,
         'coralTalkServerUrl': settings.CORAL_TALK_SERVER_URL,
     })
 
