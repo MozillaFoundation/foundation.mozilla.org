b507c5d30cc4304b108d75a6b9e785c7f8e26cd0
Pomax <pomax@nihongoresources.com>
2018-11-07 17:05:34 -0800

    getting speech bubbles

--- source/js/buyers-guide/homepage-c-slider.js
+++ source/js/buyers-guide/homepage-c-slider.js
@@ -1,3 +1,5 @@
+import CREEPINESS_LABELS from "./components/creepiness-labels.js";
+
 const creepStep = 70; // height of a single frame, see the ".current-creepiness" rule in homagepage.sccs
 const totalSteps = 50; // total number of frames in our sprite sheet
 
@@ -15,16 +17,30 @@ function isElementInViewport(element) {
 export default {
   init: () => {
     let face = document.querySelector(`.current-creepiness`);
+    let bubble = document.querySelector(`.speech-bubble`);
     let products = document.querySelectorAll(`.product-box`);
 
     window.addEventListener(`scroll`, () => {
-      let visible = Array.from(products).filter(v => isElementInViewport(v));
+      let visible = Array.from(products).filter(v => {
+        return isElementInViewport(v) && !v.classList.contains('d-none');
+      });
       let n = visible.length;
       let averageCreepiness = visible.reduce( (tally, v) => tally + parseFloat(v.dataset.creepiness)/n, 0);
       let frame = Math.round(totalSteps * averageCreepiness/100);
       let offset = `${-frame * creepStep}px`;
 
       face.style.backgroundPositionY = offset;
+
+      let len = CREEPINESS_LABELS.length;
+      let bin = Math.floor(len * (averageCreepiness-1)/100);
+
+      if (bin === -1) {
+        bubble.textContent = ``;
+        bubble.classList.add(`d-none`);
+      } else {
+        bubble.textContent = CREEPINESS_LABELS[bin];
+        bubble.classList.remove(`d-none`);
+      }
     }, {
       passive: true // remember not to bog down the UI thread.
     });
