16a9863eaca1e620e0a0a2d49984117fe2e429a9
Lucie Daeye <lucied@mozillafoundation.org>
2018-11-08 11:54:31 +0100

    Add a script to migrate pictures from S3 to Cloudinary

---
+++ network-api/networkapi/management/commands/image_migration.py
@@ -0,0 +1,18 @@
+from django.core.files.uploadedfile import UploadedFile
+from django.core.management.base import BaseCommand
+
+from networkapi.buyersguide.models import Product
+
+
+class Command(BaseCommand):
+    help = 'Migrate BuyersGuide images from s3 to Cloudinary'
+
+    def handle(self, *args, **options):
+
+        all_products = Product.objects.all()
+
+        for product in all_products:
+            image = UploadedFile(product.image.file, product.image.file.name)
+            product.cloudinary_image = image
+            product.save()
+            print(f"Product image for {product.name} migrated")
