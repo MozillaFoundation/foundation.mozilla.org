6ec62a68655ed3fa6c27d87f73a92f2a0e58e62c
Chris DeCairos <cade@mozillafoundation.org>
2018-11-09 12:23:11 -0500

    Revert "Buyersguide perf (#2100)"
    This reverts commit 0467a9e8f0d35ab06958bb007c1577999310a98d.

--- network-api/networkapi/buyersguide/views.py
+++ network-api/networkapi/buyersguide/views.py
@@ -62,8 +62,8 @@ def buyersguide_home(request):
     products = cache.get('sorted_product_dicts')
 
     if not products:
-        products = list(Product.objects.all())
-        products.sort(key=lambda p: p.votes['creepiness']['average'])
+        products = [p.to_dict() for p in Product.objects.all()]
+        products.sort(key=lambda p: get_average_creepiness(p))
         cache.set('sorted_product_dicts', products, 86400)
 
     return render(request, 'buyersguide_home.html', {
@@ -81,7 +81,7 @@ def category_view(request, categoryname):
 
     if not products:
         category = get_object_or_404(BuyersGuideProductCategory, name__iexact=categoryname)
-        products = Product.objects.filter(product_category__in=[category]).distinct()
+        products = [p.to_dict() for p in Product.objects.filter(product_category__in=[category]).distinct()]
         cache.set(key, products, 86400)
 
     return render(request, 'category_page.html', {
@@ -98,7 +98,7 @@ def product_view(request, slug):
 
     return render(request, 'product_page.html', {
         'categories': BuyersGuideProductCategory.objects.all(),
-        'product': product,
+        'product': product.to_dict(),
         'mediaUrl': MEDIA_URL,
         'coralTalkServerUrl': settings.CORAL_TALK_SERVER_URL,
     })
@@ -170,11 +170,7 @@ def product_vote(request):
 @api_view(['POST'])
 @permission_classes((IsAdminUser,))
 def refresh_cache(request):
-    # clear entire cache
-    cache.clear()
-
-    # refresh the products list for the homepage
-    products = list(Product.objects.all())
-    products.sort(key=lambda p: p.votes['creepiness']['average'])
+    products = [p.to_dict() for p in Product.objects.all()]
+    products.sort(key=lambda p: get_average_creepiness(p))
     cache.set('sorted_product_dicts', products, 86400)
     return redirect('/cms/buyersguide/product/')
