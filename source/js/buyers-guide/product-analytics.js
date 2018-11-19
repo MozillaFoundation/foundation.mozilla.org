d1a6d3d2f54774b24d55cf414baa1a53e9f0a43c
Pomax <pomax@nihongoresources.com>
2018-11-19 14:13:09 -0800

    company link GA and fixes for missing event O_O

--- source/js/buyers-guide/product-analytics.js
+++ source/js/buyers-guide/product-analytics.js
@@ -1,117 +1,128 @@
 import ReactGA from 'react-ga';
 import DNT from './dnt.js';
 
-function getElementGAInformation(pageTitle, productName) {
+function getQuerySelectorEvents(pageTitle, productName) {
   return {
-    'product-live-chat': {
-      category: `product`,
-      action: `customer support link tap`,
-      label: `support link for ${productName}`
-    },
     // "site-wide" events
-    'donate-button': {
+    '#donate-button': {
       category: `buyersguide`,
       action: `donate tap`,
       label: `${pageTitle} donate header`
     },
-    'donate-button-main': {
+    '#donate-button-main': {
       category: `buyersguide`,
       action: `donate tap`,
       label: `${pageTitle} donate header`
     },
-    'donate-button-footer': {
+    '#donate-button-footer': {
       category: `buyersguide`,
       action: `donate tap`,
       label: `${pageTitle} donate footer`
     },
-    'nav-social-button-fb': {
+    '#nav-social-button-fb': {
       category: `buyersguide`,
       action: `share tap`,
       label: `share site to Facebook`,
       transport: `beacon`
     },
-    'nav-social-button-twitter': {
+    '#nav-social-button-twitter': {
       category: `buyersguide`,
       action: `share tap`,
       label: `share site to Twitter`,
       transport: `beacon`
     },
-    'nav-social-button-email': {
+    '#nav-social-button-email': {
       category: `buyersguide`,
       action: `share tap`,
       label: `share site to Email`,
       transport: `beacon`
     },
-    'nav-social-button-fb-mb': {
+    '#nav-social-button-fb-mb': {
       category: `buyersguide`,
       action: `share tap`,
       label: `share site to Facebook`,
       transport: `beacon`
     },
-    'nav-social-button-twitter-mb': {
+    '#nav-social-button-twitter-mb': {
       category: `buyersguide`,
       action: `share tap`,
       label: `share site to Twitter`,
       transport: `beacon`
     },
-    'nav-social-button-email-mb': {
+    '#nav-social-button-email-mb': {
       category: `buyersguide`,
       action: `share tap`,
       label: `share site to Email`,
       transport: `beacon`
     },
+
     // product events
-    'product-copy-link-button': {
+    '#product-company-url': {
+      category: `product`,
+      action: `company link tap`,
+      label: `company link for ${productName}`,
+      transport: `beacon`
+    },
+    '#product-copy-link-button': {
       category: `product`,
       action: `copy link tap`,
       label: `copy link ${productName}`
     },
-    'creep-vote-btn': {
+    '#product-live-chat': {
+      category: `product`,
+      action: `customer support link tap`,
+      label: `support link for ${productName}`
+    },
+    '#creep-vote-btn': {
       category: `product`,
       action: `opinion submitted`,
       label: `opinion on ${productName}`
     },
-    'product-social-button-fb': {
+    '#product-social-button-fb': {
       category: `product`,
       action: `share tap`,
       label: `share ${productName} to Facebook`,
       transport: `beacon`
     },
-    'product-social-button-twitter': {
+    '#product-social-button-twitter': {
       category: `product`,
       action: `share tap`,
       label: `share ${productName} to Twitter`,
       transport: `beacon`
     },
-    'product-social-button-email': {
+    '#product-social-button-email': {
       category: `product`,
       action: `share tap`,
       label: `share ${productName} to Email`,
       transport: `beacon`
     },
-    'privacy-policy-link': {
+    '#privacy-policy-link': {
       category: `product`,
       action: `privacy policy link tap`,
       label: `policy link for ${productName}`,
       transport: `beacon`
     },
-    'reading-level-link': {
+    '#reading-level-link': {
       category: `product`,
       action: `carnegie mellon reading level links`,
       label: `reading level link for ${productName}`,
+      transport: `beacon`
+    },
+
+    // product updates
+    'a.product-update-link': {
+      category: `product`,
+      action: `update article link tap`,
+      label: `update article link for ${productName}`,
       transport: `beacon`
     }
   };
 }
 
-function setupElementGA(elementId, opts) {
-  let element = document.getElementById(elementId);
-
-  if (element) {
-    element.onclick = () => {
-      ReactGA.event(opts);
-    };
-  }
+function setupElementGA(element, eventData) {
+  element.onclick = () => {
+    ReactGA.event(eventData);
+  };
 }
 
 const ProductGA = {
@@ -123,10 +134,16 @@ const ProductGA = {
     let productBox = document.querySelector(`.product-detail .h1-heading`);
     let productName = productBox ? productBox.textContent : `unknown product`;
     let pageTitle = document.querySelector(`meta[property='og:title']`).getAttribute(`content`);
-    let elementsWithAnalytics = getElementGAInformation(pageTitle, productName);
+    let querySelectorEvents = getQuerySelectorEvents(pageTitle, productName);
 
-    Object.keys(elementsWithAnalytics).forEach( elementId => {
-      setupElementGA(elementId, elementsWithAnalytics[elementId]);
+    Object.keys(querySelectorEvents).forEach( querySelector => {
+      let elements = document.querySelectorAll(querySelector);
+
+      if (elements.length > 0) {
+        let eventData = querySelectorEvents[querySelector];
+
+        elements.forEach(e => setupElementGA(e, eventData));
+      }
     });
   }
 };
