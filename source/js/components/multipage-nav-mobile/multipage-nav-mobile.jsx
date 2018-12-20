1e589eea12326843b4d48c47dfbe50afcc1c1c59
Mavis Ou <mmmavis@users.noreply.github.com>
2018-12-20 10:38:58 -0800

    Related to #2365 - renamed files

--- source/js/components/multipage-nav-mobile/multipage-nav-mobile.jsx
+++ source/js/components/multipage-nav-mobile/multipage-nav-mobile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class MultipageNav extends React.Component {
+export default class MultipageNavMobile extends React.Component {
   constructor(props) {
     super(props);
 
