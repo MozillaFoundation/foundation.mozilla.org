0b68885c1d16479c2cf208cf516ab821cc227beb
Pomax <pomax@nihongoresources.com>
2019-01-09 15:15:54 -0800

    tweaks

--- network-api/networkapi/buyersguide/factory.py
+++ network-api/networkapi/buyersguide/factory.py
@@ -34,8 +34,7 @@ class ProductFactory(DjangoModelFactory):
 
     product_words = Faker('words', nb=2)
 
-#    draft = Faker('boolean')
-    draft = True
+    draft = Faker('boolean')
     name = LazyAttribute(lambda o: ' '.join(o.product_words))
 
     @post_generation
