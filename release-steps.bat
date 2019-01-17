143263628ba8013bd6e33cc87be5bced75672041
Pomax <pomax@nihongoresources.com>
2019-01-17 10:18:45 -0800

    yay!

--- release-steps.bat
+++ release-steps.bat
@@ -1,14 +1,12 @@
-cd network-api
-
 @REM Django Migrations
-python ./manage.py migrate --no-input
+pipenv run python network-api\manage.py migrate --no-input
 
 @REM Wagtail block inventory
-python ./manage.py block_inventory
+pipenv run python network-api\manage.py block_inventory
 
 @REM Wagtail translations
-python ./manage.py sync_page_translation_fields
-python ./manage.py update_translation_fields
+pipenv run python network-api\manage.py sync_page_translation_fields
+pipenv run python network-api\manage.py update_translation_fields
 
 @REM Clear cache for BuyersGuide
-python ./manage.py clear_cache
+pipenv run python network-api\manage.py clear_cache
