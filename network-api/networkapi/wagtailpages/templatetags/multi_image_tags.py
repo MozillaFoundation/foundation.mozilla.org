acd12a68c4c400571c483f5280ccb6f4a04a313f
Yourisha Wims <youri@mozillafoundation.org>
2019-01-23 12:34:06 -0500

    fixed linter issues

--- network-api/networkapi/wagtailpages/templatetags/multi_image_tags.py
+++ network-api/networkapi/wagtailpages/templatetags/multi_image_tags.py
@@ -2,9 +2,10 @@ from django import template
 
 register = template.Library()
 
+
 @register.inclusion_tag('wagtailpages/tags/picture_ratios.html')
 def optimize_images(image, class_names=""):
-    return { 
-        'unoptimized_image': image, 
+    return {
+        'unoptimized_image': image,
         'class_names': class_names
     }
