494e55073475bf7965637f6331484b6f00f31c36
Mavis Ou <mmmavis@users.noreply.github.com>
2019-01-22 18:25:33 -0800

    fixed linting errors

--- network-api/networkapi/wagtailpages/templatetags/nav_tags.py
+++ network-api/networkapi/wagtailpages/templatetags/nav_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from ..utils import get_mini_side_nav_data
 
 register = template.Library()
 
@@ -7,10 +6,8 @@ register = template.Library()
 # Instantiate a horizontal nav based on the current page's relation to other pages
 @register.inclusion_tag('wagtailpages/tags/horizontal_nav.html', takes_context=True)
 def horizontal_nav(context, current_page, menu_pages, classname=""):
-    # return get_mini_side_nav_data(context, page)
     return {
         'current': current_page,
         'menu_pages': menu_pages,
         'classname': classname,
     }
-
