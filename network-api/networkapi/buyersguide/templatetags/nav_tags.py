66a0889d62649fabb66f7210b788f8247e0bcde5
Mavis Ou <mmmavis@users.noreply.github.com>
2019-01-30 09:57:31 -0800

    code improvement & created 2 SCSS mixins

--- network-api/networkapi/buyersguide/templatetags/nav_tags.py
+++ network-api/networkapi/buyersguide/templatetags/nav_tags.py
@@ -2,11 +2,16 @@ from django import template
 
 register = template.Library()
 
+# Determine if a link should be active
+@register.simple_tag(name='bg_active_nav')
+def bg_active_nav(current, target):
+    return 'active' if current == target else ''
 
-# Instantiate a horizontal nav based on the current page's relation to other pages
+# Instantiate a list of category page links based on the current page's relation to them
 @register.inclusion_tag('category_nav_links.html', takes_context=True)
-def category_nav(context, current_category, all_categories):
+def category_nav(context, current_url, current_category, all_categories):
     return {
+        'current_url': current_url,
         'current_category': current_category,
         'sorted_categories': all_categories.order_by('-featured'), # featured categories first
     }
