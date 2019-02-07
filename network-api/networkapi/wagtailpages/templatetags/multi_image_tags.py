eae29e0a38a8eba9849a476f9225ab088e5ce022
Yourisha Wims <youri@mozillafoundation.org>
2019-02-07 17:00:02 -0500

    added previous image scaling files

---
+++ network-api/networkapi/wagtailpages/templatetags/multi_image_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('wagtailpages/tags/picture_ratios.html')
+def optimize_images(image):
+    return {
+        'unoptimized_image': image
+    }
+    
