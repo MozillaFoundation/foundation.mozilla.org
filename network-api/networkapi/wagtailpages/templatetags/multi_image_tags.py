9c1ce27263ab41e39f0322ae5448f7cc5285f4c3
Yourisha Wims <youri@mozillafoundation.org>
2019-02-19 15:50:41 -0500

    reverted fellow img changes

--- network-api/networkapi/wagtailpages/templatetags/multi_image_tags.py
+++ network-api/networkapi/wagtailpages/templatetags/multi_image_tags.py
@@ -10,13 +10,3 @@ def wide_image(image, container_class_names=''):
         'unoptimized_image': image,
         'container_class_names': container_class_names
     }
-
-#  1:1 Aspect ratio image tag
-
-@register.inclusion_tag('wagtailpages/tags/square_image.html')
-def square_image(image, container_class_names='', img_class_names=''):
-    return {
-        'unoptimized_image': image,
-        'pic_class_names': container_class_names,
-        'pic_img_class_names': img_class_names
-    }
