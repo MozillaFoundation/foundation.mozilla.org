12aa3c5681086802c697b7f22e7733dc0a1c915c
Yourisha Wims <youri@mozillafoundation.org>
2019-02-19 14:04:12 -0500

    added 1:1 img tag and updated 16:9

--- network-api/networkapi/wagtailpages/templatetags/multi_image_tags.py
+++ network-api/networkapi/wagtailpages/templatetags/multi_image_tags.py
@@ -2,24 +2,21 @@ from django import template
 
 register = template.Library()
 
+#  16:9 Aspect ratio image tag
 
-@register.inclusion_tag('wagtailpages/tags/picture_ratios.html')
-def optimize_images(image, aspect_ratio='16:9'):
-    image_ratios = {
-        'large': ['540x304', '1080x608'],
-        'medium': ['480x270', '960x520'],
-        'small': ['320x180', '640x360']
-    }
-    if aspect_ratio == '1:1':
-        image_ratios = {
-            'large': ['160x160', '320x320'],
-            'small': ['92x92', '184x184']
-        }
-
-    for key, value in image_ratios.items():
-        value[:] = ['fill-' + ratio + '-c0' for ratio in value]
-
+@register.inclusion_tag('wagtailpages/tags/wide_image.html')
+def wide_image(image, container_class_names=''):
     return {
-        image_ratios: image_ratios,
-        'unoptimized_image': image
+        'unoptimized_image': image,
+        'container_class_names': container_class_names
+    }
+
+#  1:1 Aspect ratio image tag
+
+@register.inclusion_tag('wagtailpages/tags/square_image.html')
+def square_image(image, container_class_names='', img_class_names=''):
+    return {
+        'unoptimized_image': image,
+        'pic_class_names': container_class_names,
+        'pic_img_class_names': img_class_names
     }
