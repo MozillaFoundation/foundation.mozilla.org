7f5cfe912fe726ea201755f2e3fb464443565e17
patjouk <lucie.daeye@gmail.com>
2019-02-27 14:51:08 +0100

    change text in python test script

--- travis-scripts/python_tests.sh
+++ travis-scripts/python_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Running linter"
+echo "Running Python linter"
 pipenv run flake8 tasks.py network-api/
 
-echo "Running tests"
+echo "Running Python tests"
 pipenv run coverage run --source './network-api/networkapi' network-api/manage.py test networkapi
