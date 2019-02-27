6f6596594bb59363e42daf5e70ee364fa28bf77e
patjouk <lucie.daeye@gmail.com>
2019-02-27 16:52:33 +0100

    wait, we don't need scripts for tests >.<

--- travis-scripts/npm_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Running npm test"
-npm test
--- travis-scripts/python_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "Running Python linter"
-pipenv run flake8 tasks.py network-api/ --statistics
-
-echo -e "Running Python tests"
-pipenv run coverage run --source './network-api/networkapi' network-api/manage.py test networkapi
