6f8a68bab04023de9a046a45e1b6f4d75be4469b
patjouk <lucie.daeye@gmail.com>
2019-02-27 16:20:27 +0100

    Try the count flag for flake8 to make it more readable since Travis doesn't want my pretty colors :(

--- travis-scripts/python_tests.sh
+++ travis-scripts/python_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo -e "\033[92mRunning Python linter\033[0m"
-pipenv run flake8 tasks.py network-api/
+echo -e "Running Python linter"
+pipenv run flake8 tasks.py network-api/ --count
 
-echo -e "\033[92mRunning Python tests\033[0m"
+echo -e "Running Python tests"
 pipenv run coverage run --source './network-api/networkapi' network-api/manage.py test networkapi
