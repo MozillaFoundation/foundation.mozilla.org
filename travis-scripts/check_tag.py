76c799e3e5ee75c9f3c543853ddefdd77d4effd7
patjouk <lucie.daeye@gmail.com>
2019-03-01 11:59:03 +0100

    first steps of checks tags script

--- travis-scripts/check_tag.py
+++ travis-scripts/check_tag.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
+# import os
+# import sys
 
-import os
+import requests
 
-test = os.environ.get("TRAVIS_PULL_REQUEST")
-print(test)
+# TODO: replace by os.environ.get("TRAVIS_PULL_REQUEST")
+PR_NUMBER = "3730"
+# TODO: encrypt github_token for travis
+# GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+
+
+def get_tags():
+    pr_url = f"https://api.github.com/repos/mozilla/foundation.mozilla.org/pulls/{PR_NUMBER}"
+
+    r = requests.get(pr_url).raise_for_status()
+    print(r.json())
+
+
+def check_tags():
+    pass
+
+
+if __name__ == "__main__":
+    get_tags()
