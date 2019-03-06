4c0cb60515605ba15298e785fddfae9a2dd02b3d
patjouk <lucie.daeye@gmail.com>
2019-03-06 14:41:17 +0100

    add error handling + cleanup for comprehension

--- travis-scripts/check_labels.py
+++ travis-scripts/check_labels.py
@@ -2,6 +2,7 @@
 import json
 import os
 from urllib import request
+from urllib.error import HTTPError
 
 PR_NUMBER = os.environ.get("TRAVIS_PULL_REQUEST")
 # TODO: encrypt github_token for travis
@@ -13,18 +14,18 @@ CI_LABELS = ["Frontend", "Backend", "Python", "Javascript"]
 def get_labels():
     pr_url = "https://api.github.com/repos/mozilla/foundation.mozilla.org/pulls/{}".format(PR_NUMBER)
 
-    with request.urlopen(pr_url) as f:
-        json_response = json.load(f)
-
+    try:
+        with request.urlopen(pr_url) as f:
+            json_response = json.load(f)
+    except HTTPError as error:
+        print(error)
+        return []
     try:
         labels = json_response["labels"]
-        label_names = []
-        [label_names.append(label["name"]) for label in labels]
+        return [label["name"] for label in labels]
     except KeyError:
-        label_names = []
         print("No label found, running all tests")
-
-    return label_names
+        return []
 
 
 def check_labels(labels_list):
