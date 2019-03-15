1a80cf97b032058e8d46a5e7c9df977d0d2169dd
Yourisha Wims <youri@mozillafoundation.org>
2019-03-15 15:49:44 -0400

    fixed profile directory spacing

--- network-api/networkapi/wagtailpages/customblocks.py
+++ network-api/networkapi/wagtailpages/customblocks.py
@@ -478,6 +478,8 @@ class LatestProfileList(blocks.StructBlock):
             query=parse.urlencode(query_args)
         )
 
+        data = []
+
         try:
             response = request.urlopen(url)
             response_data = response.read()
