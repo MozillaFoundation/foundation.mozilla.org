b4ba42688cc43648753dd2b80604d2e807848ea7
Alan Mooiman <amooiman@gmail.com>
2019-03-19 17:09:54 -0400

    Remove legacy Google sheets petition fields

--- network-api/networkapi/wagtailpages/models.py
+++ network-api/networkapi/wagtailpages/models.py
@@ -243,11 +243,6 @@ class OpportunityPage(MiniSiteNameSpace):
 
 @register_snippet
 class Petition(CTA):
-    legacy_petition = models.BooleanField(
-        help_text='Mark this petition as a legacy petition in terms of where the data gets sent.',
-        default=False,
-    )
-
     campaign_id = models.CharField(
         max_length=20,
         help_text='Which campaign identifier should this petition be tied to?',
@@ -278,65 +273,18 @@ class Petition(CTA):
         max_length=8,
     )
 
-    google_forms_url = models.URLField(
-        help_text='Google form to post petition data to',
-        max_length=2048,
-        null=True,
-        blank=True,
-    )
-
     checkbox_1 = models.CharField(
         max_length=1024,
-        help_text='label for the first checkbox option (may contain HTML)',
+        help_text='(currently disabled) label for the first checkbox option (may contain HTML)',
         blank=True,
+        editable=False,
     )
 
     checkbox_2 = models.CharField(
         max_length=1024,
-        help_text='label for the second checkbox option (may contain HTML)',
+        help_text='(currently disabled) label for the second checkbox option (may contain HTML)',
         blank=True,
-    )
-
-    checkbox_1_form_field = models.CharField(
-        max_length=1024,
-        help_text='Google form field name for Checkbox 1',
-        verbose_name='First checkbox Google Form field',
-        blank=True,
-    )
-
-    checkbox_2_form_field = models.CharField(
-        max_length=1024,
-        help_text='Google form field name for Checkbox 1',
-        verbose_name='Second checkbox Google Form field',
-        blank=True,
-    )
-
-    given_name_form_field = models.CharField(
-        max_length=1024,
-        help_text='Google form field name for Given Name(s)',
-        verbose_name='Given Name(s) Google Form field',
-        blank=True,
-    )
-
-    surname_form_field = models.CharField(
-        max_length=1024,
-        help_text='Google form field name for Surname',
-        verbose_name='Surname Google Form field',
-        blank=True,
-    )
-
-    email_form_field = models.CharField(
-        max_length=1024,
-        help_text='Google form field name for Email',
-        verbose_name='Email Google Form field',
-        blank=True,
-    )
-
-    newsletter_signup_form_field = models.CharField(
-        max_length=1024,
-        help_text='Google form field name for Mozilla Newsletter checkbox',
-        verbose_name='Mozilla Newsletter signup checkbox Google Form field',
-        blank=True,
+        editable=False,
     )
 
     share_link = models.URLField(
