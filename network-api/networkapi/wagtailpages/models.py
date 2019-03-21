c74444723dd8e8e075338c317aea8bfbe9361be3
Yourisha Wims <youri@mozillafoundation.org>
2019-03-21 17:03:02 -0400

    removed template

--- network-api/networkapi/wagtailpages/models.py
+++ network-api/networkapi/wagtailpages/models.py
@@ -788,7 +788,6 @@ class ParticipatePage2(PrimaryPage):
 
 class PeoplePage(PrimaryPage):
     parent_page_types = ['Homepage']
-    template = 'wagtailpages/static/people_page.html'
 
 
 class Styleguide(PrimaryPage):
