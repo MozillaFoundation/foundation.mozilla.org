f0cc5a7bbe678d54f81ba59edd1f3536abdf82d7
Mavis Ou <mmmavis@users.noreply.github.com>
2019-04-02 13:33:57 -0700

    Deprecate old buttons .btn-normal and .btn-ghost

--- network-api/networkapi/wagtailpages/customblocks.py
+++ network-api/networkapi/wagtailpages/customblocks.py
@@ -20,26 +20,9 @@ class LinkButtonBlock(blocks.StructBlock):
         choices=[
             ('btn-primary', 'Primary button'),
             ('btn-secondary', 'Secondary button'),
-            # Obsolete options to be removed in
-            # https://github.com/mozilla/foundation.mozilla.org/issues/2936
-            ('btn-normal', 'Normal button (Obsolete. Use \'primary button\' instead)'),
-            ('btn-ghost', 'Ghost button  (Obsolete. Use \'secondary button\' instead)'),
         ],
-        default='btn-normal',
+        default='btn-primary',
     )
-
-    # def get_context block to be removed in https://github.com/mozilla/foundation.mozilla.org/issues/2936
-    def get_context(self, value, parent_context=None):
-        context = super().get_context(value, parent_context=parent_context)
-        styling = value.get("styling")
-
-        if styling == 'btn-normal':
-            styling = 'btn-primary'
-        if styling == 'btn-ghost':
-            styling = 'btn-secondary'
-        context['styling'] = styling
-
-        return context
 
     class Meta:
         icon = 'link'
