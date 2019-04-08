d16cb9a8dd89cf9125a04d1a8c2578561e79c422
Mavis Ou <mmmavis@users.noreply.github.com>
2019-04-08 16:12:58 -0700

    changed a data attribute name

--- source/js/components/join/join.jsx
+++ source/js/components/join/join.jsx
@@ -125,7 +125,7 @@ export default class JoinUs extends React.Component {
   }
 
   createClasslist(classes = ``) {
-    if (this.props.version === `dark-theme`) {
+    if (this.props.theme === `dark-theme`) {
       classes = ` ${classes} dark-theme`;
     }
 
