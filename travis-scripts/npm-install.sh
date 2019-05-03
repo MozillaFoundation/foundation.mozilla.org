279da67771dfc4804ac7c2275b187fda56366b9b
patjouk <lucie.daeye@gmail.com>
2019-05-03 14:17:40 +0200

    Add docker for running Travis tests

--- travis-scripts/npm-install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set -x # echo commands before running them
-
-npm install -g npm@latest # Needed to use npm ci
-npm ci # Use package-lock.json
--- travis-scripts/python-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -x # echo commands before running them
-
-pip install --upgrade pip
-pip install pipenv
-pipenv install --dev --deploy
