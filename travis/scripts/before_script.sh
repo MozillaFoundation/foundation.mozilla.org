1be5a454b29a468896db2a0c168e0d0a1046d24b
Christopher DeCairos <cade@mozillafoundation.org>
2019-05-06 16:56:44 -0400

    modifications to before_script

--- travis/scripts/before_script.sh
+++ travis/scripts/before_script.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 if [ $TRAVIS_EVENT_TYPE == "pull_request" ]; then
+  echo "Configuring for pull request testing.."
   export NODE_TAG=$TRAVIS_PULL_REQUEST_BRANCH-node;
   export PYTHON_TAG=$TRAVIS_PULL_REQUEST_BRANCH-python;
   export APP_BUILD_TAG=$TRAVIS_PULL_REQUEST_BRANCH-app-build;
   export CYPRESS_TAG=$TRAVIS_PULL_REQUEST_BRANCH-cypress;
 else
+  echo "Configuring for branch testing"
   export NODE_TAG=$TRAVIS_BRANCH-node;
   export PYTHON_TAG=$TRAVIS_BRANCH-python;
   export APP_BUILD_TAG=$TRAVIS_BRANCH-app-build;
   export CYPRESS_TAG=$TRAVIS_BRANCH-cypress;
 fi
 
-export PYTHON_TAG=$PYTHON_TAG
-export APP_BUILD_TAG=$APP_BUILD_TAG
-export CYPRESS_TAG=$CYPRESS_TAG
+echo "\$NODE_TAG: $NODE_TAG"
+echo "\$PYTHON_TAG: $PYTHON_TAG"
+echo "\$APP_BUILD_TAG: $APP_BUILD_TAG"
