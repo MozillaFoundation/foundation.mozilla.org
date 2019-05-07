985de9fb40750b90a137c8d2e6188983d8b4beab
Christopher DeCairos <cade@mozillafoundation.org>
2019-05-07 09:41:22 -0400

    alias a long docker-compose command that was repeating in visual regression test script

--- travis/scripts/test_visual_regression.sh
+++ travis/scripts/test_visual_regression.sh
@@ -3,47 +3,43 @@
 # Load environment vars
 . ./travis/scripts/environment.sh
 
+# Alias docker-compose command
+dc='docker-compose -f travis/docker-compose.travis.yml'
+
 # Pull down the prebuilt cypress image
-docker-compose -f travis/docker-compose.travis.yml pull cypress || true
+dc pull cypress || true
 
 # Update the cypress image if necessary
-docker-compose -f travis/docker-compose.travis.yml build cypress
+dc build cypress
 
 # Generate static assets for Django/Wagtail
-docker-compose -f travis/docker-compose.travis.yml \
-  run --rm \
-  wagtail_cypress \
+dc run --rm wagtail_cypress \
   python network-api/manage.py collectstatic --no-input --verbosity 0
 
 # Prepare the database
-docker-compose -f travis/docker-compose.travis.yml \
-  run --rm \
+dc run --rm \
   wagtail_cypress python network-api/manage.py migrate --no-input
 
 # Update the block inventory
-docker-compose -f travis/docker-compose.travis.yml \
-  run --rm \
+dc run --rm \
   wagtail_cypress python network-api/manage.py block_inventory
 
 # update localisation data
-docker-compose -f travis/docker-compose.travis.yml \
-  run --rm \
+dc run --rm \
   wagtail_cypress python network-api/manage.py sync_page_translation_fields
 
-docker-compose -f travis/docker-compose.travis.yml \
-  run --rm \
+dc run --rm \
   wagtail_cypress python network-api/manage.py update_translation_fields
 
 # Populate the database with test data
-docker-compose -f travis/docker-compose.travis.yml \
-  run --rm \
+dc run --rm \
   wagtail_cypress python network-api/manage.py load_fake_data
 
 # Run the visual regression tests on the test site
-docker-compose -f travis/docker-compose.travis.yml up cypress
+dc up cypress
 
 # Authenticate with Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Push the cypress image up to Docker Hub
-docker-compose -f travis/docker-compose.travis.yml push cypress
+dc push cypress
