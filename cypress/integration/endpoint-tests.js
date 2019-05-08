8cdd5e715d9164254a7e8222c5a951b61ce470b6
Christopher DeCairos <cade@mozillafoundation.org>
2019-05-08 14:06:39 -0400

    wait before and after for homepage

--- cypress/integration/endpoint-tests.js
+++ cypress/integration/endpoint-tests.js
@@ -9,10 +9,11 @@ describe(`Visual regression testing for foundation.mozilla.org`, () => {
 
   it(`Foundation homepage`, function() {
     cy.visit(`/`);
-    cy.wait(500);
+    cy.wait(2000);
     cy.window()
       .its(`main-js:react:finished`)
       .should(`equal`, true);
+    cy.wait(500);
     cy.percySnapshot();
   });
 
