a60daa91cfd956b5b341fcd199f597b017037351
Mavis Ou <mmmavis@users.noreply.github.com>
2019-05-08 17:06:18 -0700

    Made stylelint throw errors if custom colours are found (#3090)
    * made stylelint throw errors if custom colours are found
    
    * don't allow HTML named colours
    
    * Revert "don't allow HTML named colours"
    
    This reverts commit 149d144581e3b4f5798c891915db64a7f2b75038.
    
    * optimized stylelint rules
    
    * removed commented out code
    
    * updated inline comments
    
    * split _variables.scss into 2 files
    
    * fixed linting error and inline comment
    
    * reorganized SCSS variables again
    
    * renamed file again
    
    * simplified regex
    
    * dont allow any color names at all

---
+++ .stylelintrc-colors.js
@@ -0,0 +1,22 @@
+  var config = {
+    "plugins": ["stylelint-prettier"],
+    "rules": {
+      "color-named": "never",
+      "color-no-hex": true,
+      "declaration-property-value-blacklist": [
+        {
+          "/.*/": [
+            /rgba{0,1}\(/i,
+            /hsla{0,1}\(/i,
+            /hwb\(/i,
+            /gray\(/i
+          ]
+        },
+        {
+          "message": "Custom colors are not allowed. Please use brand colors listed in _colors.scss."
+        }
+      ]
+    }
+  };
+
+  module.exports = config;
