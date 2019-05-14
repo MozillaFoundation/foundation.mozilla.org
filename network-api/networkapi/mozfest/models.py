ccd05c92df309cccdb5e77b2438fd6fef1af810e
Pomax <pomax@nihongoresources.com>
2019-05-14 14:05:03 -0700

    why is this going wrong??

--- network-api/networkapi/mozfest/models.py
+++ network-api/networkapi/mozfest/models.py
@@ -1,5 +1,12 @@
 from networkapi.wagtailpages.models import FoundationMetadataPageMixin
 from wagtail.core.models import Page
 
-class MozfestHomepage(FoundationMetadataPageMixin, Page):
+
+class MozfestPrimaryPage(FoundationMetadataPageMixin, Page):
     pass
+
+
+class MozfestHomepage(MozfestPrimaryPage):
+    subpage_types = [
+        'MozfestPrimaryPage'
+    ]
--- network-api/networkapi/mozfest/translation.py
+++ network-api/networkapi/mozfest/translation.py
@@ -1,10 +1,15 @@
 from .models import (
     MozfestHomepage,
+    MozfestPrimaryPage,
 )
-
 
 from modeltranslation.translator import TranslationOptions
 from modeltranslation.decorators import register
+
+
+@register(MozfestPrimaryPage)
+class MozfestPrimaryPageTR(TranslationOptions):
+    fields = ()
 
 
 @register(MozfestHomepage)
