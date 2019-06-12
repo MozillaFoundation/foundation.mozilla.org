32d6b14d5a952af7d67970502f30de970fb5452f
Pomax <pomax@nihongoresources.com>
2019-06-12 13:31:00 -0700

    work around wagtail bug 5379

--- network-api/networkapi/utility/templatetags/primary_active_nav.py
+++ network-api/networkapi/utility/templatetags/primary_active_nav.py
@@ -11,6 +11,13 @@ def primary_active_nav(request, root_url, target_url):
 
     request_url = request.build_absolute_uri()
 
+    # In order to work around https://github.com/wagtail/wagtail/issues/5379
+    # we need to strip all protocols from the urls first, because they might
+    # mismatch, even though they reasonably shouldn't.
+    root_url = root_url.replace('http://','').replace('https://','')
+    target_url = target_url.replace('http://','').replace('https://','')
+    request_url = request_url.replace('http://','').replace('https://','')
+
     if target_url == root_url:
         return "active" if request_url == target_url else ""
 
