8916be5bc1f2c135533450ceb8c40329d6cb68a2
Pomax <pomax@nihongoresources.com>
2019-07-11 11:19:33 -0700

    refactored image grid + image grid block

--- network-api/networkapi/wagtailpages/customblocks/__init__.py
+++ network-api/networkapi/wagtailpages/customblocks/__init__.py
@@ -4,8 +4,15 @@ from urllib import request, parse
 from django.conf import settings
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
+from .image_grid import (
+    ImageGrid,
+    ImageGridBlock
+)
 
-__all__ = []
+__all__ = [
+    ImageGrid,
+    ImageGridBlock
+]
 
 
 class LinkButtonBlock(blocks.StructBlock):
@@ -130,32 +137,6 @@ class ImageTextMini(ImageBlock):
     class Meta:
         icon = 'doc-full'
         template = 'wagtailpages/blocks/image_text_mini.html'
-
-
-class ImageGrid(blocks.StructBlock):
-    image = ImageChooserBlock()
-    caption = blocks.CharBlock(
-        required=False,
-        help_text='Please remember to properly attribute any images we use.'
-    )
-    url = blocks.CharBlock(
-        required=False,
-        help_text='Optional URL that this figure should link out to.',
-    )
-    square_image = blocks.BooleanBlock(
-        default=True,
-        required=False,
-        help_text='If left checked, the image will be cropped to be square.'
-    )
-
-
-class ImageGridBlock(blocks.StructBlock):
-    grid_items = blocks.ListBlock(ImageGrid())
-
-    class Meta:
-        # this is probably the wrong icon but let's run with it for now
-        icon = 'grip'
-        template = 'wagtailpages/blocks/image_grid_block.html'
 
 
 class BootstrapSpacerBlock(blocks.StructBlock):
