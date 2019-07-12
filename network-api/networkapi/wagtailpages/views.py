718daffcb8ba8d529f4184eaa93389c1d9296885
Pomax <pomax@nihongoresources.com>
2019-07-12 10:13:53 -0700

    Update network-api/networkapi/wagtailpages/views.py
    Co-Authored-By: Lucie <lucie.daeye@gmail.com>

--- network-api/networkapi/wagtailpages/views.py
+++ network-api/networkapi/wagtailpages/views.py
@@ -18,7 +18,7 @@ def custom404_view(request, exception):
     """
     if request.site.hostname == 'mozillafestival.org':
         html = render(request, 'mozfest/404.html')
-        return HttpResponseNotFound(html)
+        return HttpResponseNotFound(html.content)
     else:
         html = render(request, '404.html')
         return HttpResponseNotFound(html.content)
