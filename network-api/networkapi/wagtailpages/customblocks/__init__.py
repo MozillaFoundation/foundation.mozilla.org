004a6c36407f99bd5538729ba8ba50d6eb2e8b77
Pomax <pomax@nihongoresources.com>
2019-07-15 15:29:10 -0700

    Refactor wagtail blocks link button block (#3386)
    * initial relocation so we can spin off individual blocks
    
    * refactored LinkButtonBlock to its own file
    
    * Update __init__.py

--- network-api/networkapi/wagtailpages/customblocks/__init__.py
+++ network-api/networkapi/wagtailpages/customblocks/__init__.py
@@ -5,33 +5,12 @@ from django.conf import settings
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 from .image_block import ImageBlock
+from .link_button_block import LinkButtonBlock
 
 __all__ = [
-    ImageBlock
+    ImageBlock,
+    LinkButtonBlock,
 ]
-
-
-class LinkButtonBlock(blocks.StructBlock):
-    label = blocks.CharBlock()
-
-    # We use a char block because UrlBlock does not
-    # allow for relative linking.
-    URL = blocks.CharBlock()
-
-    # Buttons can have different looks, so we
-    # offer the choice to decide which styling
-    # should be used.
-    styling = blocks.ChoiceBlock(
-        choices=[
-            ('btn-primary', 'Primary button'),
-            ('btn-secondary', 'Secondary button'),
-        ],
-        default='btn-primary',
-    )
-
-    class Meta:
-        icon = 'link'
-        template = 'wagtailpages/blocks/link_button_block.html'
 
 
 class AirTableBlock(blocks.StructBlock):
