3f6f5fb236d175f0bcb2ba00005364954a4faac9
Pomax <pomax@nihongoresources.com>
2019-07-16 16:13:52 -0700

    Refactor wagtail blocks bootstrap spacer (#3398)
    * initial relocation so we can spin off individual blocks
    
    * Update __init__.py
    
    * refactored bootstrap spacer block
    
    * Update __init__.py

--- network-api/networkapi/wagtailpages/customblocks/__init__.py
+++ network-api/networkapi/wagtailpages/customblocks/__init__.py
@@ -12,6 +12,7 @@ from .aligned_image_block import AlignedImageBlock
 from .image_text_block import ImageTextBlock
 from .image_text_mini import ImageTextMini
 from .image_grid import ImageGrid, ImageGridBlock
+from .bootstrap_spacer_block import BootstrapSpacerBlock
 
 __all__ = [
     ImageBlock,
@@ -23,52 +24,8 @@ __all__ = [
     ImageTextMini,
     ImageGrid,
     ImageGridBlock,
+    BootstrapSpacerBlock,
 ]
-
-
-class BootstrapSpacerBlock(blocks.StructBlock):
-    """
-    See https://getbootstrap.com/docs/4.0/utilities/spacing/
-    """
-
-    # property = blocks.ChoiceBlock(
-    #    choices=[
-    #        ('m', 'Margin'),
-    #        #('p', 'Padding'),
-    #    ],
-    #    default='m',
-    # )
-
-    # sides = blocks.ChoiceBlock(
-    #    choices=[
-    #        ('t', 'top'),
-    #        ('b', 'bottom'),
-    #        ('l', 'left'),
-    #        ('r', 'right'),
-    #        ('x', 'left + right'),
-    #        ('y', 'top+bottom'),
-    #        ('', 'all sides'),
-    #    ],
-    #    default='',
-    # )
-
-    size = blocks.ChoiceBlock(
-        choices=[
-            # ('0', 'no spacing'),
-            ('1', 'quarter spacing'),
-            ('2', 'half spacing'),
-            ('3', 'single spacing'),
-            ('4', 'one and a half spacing'),
-            ('5', 'triple spacing'),
-            # ('auto', 'automagical'),
-        ],
-        default='3',
-    )
-
-    class Meta:
-        icon = 'arrows-up-down'
-        template = 'wagtailpages/blocks/bootstrap_spacer_block.html'
-        help_text = 'A bootstrap based vertical spacing block.'
 
 
 class iFrameBlock(blocks.StructBlock):
