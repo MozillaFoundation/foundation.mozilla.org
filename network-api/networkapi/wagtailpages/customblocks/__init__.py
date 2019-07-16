7f8ceca75111c27aba0047761a62a99b62b38423
Pomax <pomax@nihongoresources.com>
2019-07-16 15:00:27 -0700

    Refactor wagtail blocks image text block (#3395)
    * initial relocation so we can spin off individual blocks
    
    * refactored imageblock to its own file
    
    * refactor image text block
    
    * Update __init__.py

--- network-api/networkapi/wagtailpages/customblocks/__init__.py
+++ network-api/networkapi/wagtailpages/customblocks/__init__.py
@@ -9,6 +9,7 @@ from .link_button_block import LinkButtonBlock
 from .annotated_image_block import AnnotatedImageBlock
 from .airtable_block import AirTableBlock
 from .aligned_image_block import AlignedImageBlock
+from .image_text_block import ImageTextBlock
 
 __all__ = [
     ImageBlock,
@@ -16,39 +17,8 @@ __all__ = [
     AnnotatedImageBlock,
     AirTableBlock,
     AlignedImageBlock,
+    ImageTextBlock,
 ]
-
-
-class ImageTextBlock(ImageBlock):
-    text = blocks.RichTextBlock(
-        features=['bold', 'italic', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'ul', 'link']
-    )
-    url = blocks.CharBlock(
-        required=False,
-        help_text='Optional URL that this image should link out to.',
-    )
-    top_divider = blocks.BooleanBlock(
-        required=False,
-        help_text='Optional divider above content block.',
-    )
-    bottom_divider = blocks.BooleanBlock(
-        required=False,
-        help_text='Optional divider below content block.',
-    )
-
-    def get_context(self, value, parent_context=None):
-        context = super().get_context(value, parent_context=parent_context)
-        divider_styles = []
-        if value.get("top_divider"):
-            divider_styles.append('div-top')
-        if value.get("bottom_divider"):
-            divider_styles.append('div-bottom')
-        context['divider_styles'] = ' '.join(divider_styles)
-        return context
-
-    class Meta:
-        icon = 'doc-full'
-        template = 'wagtailpages/blocks/image_text.html'
 
 
 class ImageTextMini(ImageBlock):
