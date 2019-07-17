65445106e81b9dc9fb4d909941e9019962b41583
Pomax <pomax@nihongoresources.com>
2019-07-16 22:55:46 -0700

    Refactor wagtail blocks profile by (#3403)
    * initial relocation so we can spin off individual blocks
    
    * Update __init__.py
    
    * refactored profile-by-id
    
    * Update __init__.py
    
    * Update __init__.py

--- network-api/networkapi/wagtailpages/customblocks/__init__.py
+++ network-api/networkapi/wagtailpages/customblocks/__init__.py
@@ -14,6 +14,7 @@ from .image_grid import ImageGrid, ImageGridBlock
 from .image_text_block import ImageTextBlock
 from .image_text_mini import ImageTextMini
 from .link_button_block import LinkButtonBlock
+from .profile_by_id import ProfileById
 from .pulse_project_list import PulseProjectList
 from .quote_block import QuoteBlock
 from .video_block import VideoBlock
@@ -30,49 +31,11 @@ __all__ = [
     ImageTextBlock,
     ImageTextMini,
     LinkButtonBlock,
+    ProfileById,
     PulseProjectList,
     QuoteBlock,
     VideoBlock,
 ]
-
-
-class ProfileById(blocks.StructBlock):
-
-    ids = blocks.CharBlock(
-        label='Profile by ID',
-        help_text='Show profiles for pulse users with specific profile ids'
-                  ' (mozillapulse.org/profile/[##]). For multiple profiles'
-                  ', specify a comma separated list (e.g. 85,105,332).'
-    )
-
-    def get_context(self, value, parent_context=None):
-        context = super().get_context(value, parent_context=parent_context)
-        ids = context['block'].value['ids']
-        data = list()
-
-        # FIXME: the protocol should be part of the pulse api variable.
-        #   see: https://github.com/mozilla/foundation.mozilla.org/issues/1824
-
-        url = "{pulse_api}/api/pulse/v2/profiles/?format=json&ids={ids}".format(
-            pulse_api=settings.FRONTEND['PULSE_API_DOMAIN'],
-            ids=ids
-        )
-
-        try:
-            response = request.urlopen(url)
-            response_data = response.read()
-            data = json.loads(response_data)
-
-        except (IOError, ValueError) as exception:
-            print(str(exception))
-            pass
-
-        context['profiles'] = data
-        return context
-
-    class Meta:
-        template = 'wagtailpages/blocks/profile_blocks.html'
-        icon = 'user'
 
 
 class LatestProfileQueryValue(blocks.StructValue):
