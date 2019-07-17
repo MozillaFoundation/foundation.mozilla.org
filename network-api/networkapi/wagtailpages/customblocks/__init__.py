093f8c5cd387b58e1c6f3bf0e960d9987d8b2e31
Pomax <pomax@nihongoresources.com>
2019-07-16 18:37:08 -0700

    Refactor wagtail blocks quote (#3401)
    * initial relocation so we can spin off individual blocks
    
    * Update __init__.py
    
    * refactored quote block

--- network-api/networkapi/wagtailpages/customblocks/__init__.py
+++ network-api/networkapi/wagtailpages/customblocks/__init__.py
@@ -15,6 +15,7 @@ from .image_text_block import ImageTextBlock
 from .image_text_mini import ImageTextMini
 from .link_button_block import LinkButtonBlock
 from .video_block import VideoBlock
+from .quote_block import QuoteBlock
 
 __all__ = [
     AnnotatedImageBlock,
@@ -29,24 +30,8 @@ __all__ = [
     ImageTextMini,
     LinkButtonBlock,
     VideoBlock,
+    QuoteBlock,
 ]
-
-
-class QuoteBlock(blocks.StructBlock):
-
-    # The goal is to be able to cycle through multiple quotes,
-    # so let's accept multiple quotes to start even if we only show one for now.
-    # This way we don't have to migrate the model again later
-
-    quotes = blocks.ListBlock(blocks.StructBlock([
-        ('quote', blocks.CharBlock()),
-        ('attribution', blocks.CharBlock())
-    ]))
-
-    class Meta:
-        template = 'wagtailpages/blocks/quote_block.html'
-        icon = 'openquote'
-        help_text = 'Multiple quotes can be entered, but for now we are only using the first'
 
 
 class PulseProjectQueryValue(blocks.StructValue):
