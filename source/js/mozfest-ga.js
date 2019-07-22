f3fbf0b3088ed4e1b2e3c85a45d615415ea8499f
Mavis Ou <mmmavis@users.noreply.github.com>
2019-07-22 14:17:03 -0700

    attach missing file

---
+++ source/js/mozfest-ga.js
@@ -0,0 +1,52 @@
+import ReactGA from "./react-ga-proxy.js";
+
+const bindMozfestGAEventTrackers = () => {
+  let homeWatchVideoButton = document.querySelector(`#mozfest-home-watch-video-button`);
+
+  if (homeWatchVideoButton) {
+    homeWatchVideoButton.addEventListener(`click`, () => {
+      ReactGA.event({
+        category: `CTA`,
+        action: `watch video tap`,
+        label: `watch video button tap`
+      });
+    });
+  }
+
+  // TODO:FIXME:
+  // The proposals page is created via CMS and so are these buttons.
+  // Not sure what the best way is to target these buttons.
+  let beginSessionProposalButtons = document.querySelectorAll(
+    `body.mozfest a.btn.btn-primary[href='https://forms.gle/WY2zCp6cfdJU2fmp7']`
+  );
+
+  if (beginSessionProposalButtons) {
+    beginSessionProposalButtons.forEach(button => {
+      button.addEventListener(`click`, () => {
+        ReactGA.event({
+          category: `CTA`,
+          action: `submit proposal tap`,
+          label: `proposal button tap`
+        });
+      });
+    });
+  }
+
+  let footerSocialButtons = document.querySelectorAll(
+    `body.mozfest footer a[data-platform]`
+  );
+
+  if (footerSocialButtons) {
+    footerSocialButtons.forEach(button => {
+      button.addEventListener(`click`, () => {
+        ReactGA.event({
+          category: `social`,
+          action: `social button tap`,
+          label: `${button.dataset.platform} footer button tap`
+        });
+      });
+    });
+  }
+};
+
+export default bindMozfestGAEventTrackers;
