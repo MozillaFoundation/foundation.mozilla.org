381e2c9a3e632c2de508200bc4e2b969106093cf
Pomax <pomax@nihongoresources.com>
2019-08-09 16:03:43 -0700

    Petition comment 3517 (#3521)
    * make sure commentValue is string

--- source/js/components/petition/petition.jsx
+++ source/js/components/petition/petition.jsx
@@ -246,8 +246,9 @@ export default class Petition extends React.Component {
       postalCode = !!this.postalCode.element.value;
     }
 
-    let commentValue =
-      this.comment && this.comment.element && this.comment.element.value;
+    let commentValue = this.comment
+      ? this.comment.element && this.comment.element.value
+      : ``;
 
     if (this.props.commentRequirements === `required`) {
       comment = !!commentValue;
