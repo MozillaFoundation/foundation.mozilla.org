404dec1addf0918c6117c57577c2564f0749fcdf
Pomax <pomax@nihongoresources.com>
2019-08-12 16:05:17 -0700

    ...

--- release-steps.sh
+++ release-steps.sh
@@ -12,9 +12,6 @@ python ./manage.py block_inventory
 python ./manage.py sync_page_translation_fields
 python ./manage.py update_translation_fields
 
-# Get the latest Portuguese (Brazil) translation file from Pontoon
-cp ./locale/pt_BR/LC_MESSAGES/django.po ./locale/pt/LC_MESSAGES/django.po
-
 # Compile the Django template translations
 python ./manage.py compilemessages
 
