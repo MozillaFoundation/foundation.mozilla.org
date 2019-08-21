744c7ce1e671104c00dbff34e679bc6ee234f51c
Youri <youri@mozillafoundation.org>
2019-08-21 18:51:55 -0400

    Update cypress/integration/endpoint-tests.js
    Co-Authored-By: Pomax <pomax@nihongoresources.com>

--- cypress/integration/endpoint-tests.js
+++ cypress/integration/endpoint-tests.js
@@ -104,7 +104,7 @@ describe(`Visual regression testing for foundation.mozilla.org`, () => {
   // Bannered Campaign page test
 
   it(`Bannered Campaign Page`, function() {
-    cy.visit(`/en/campaigns/banneredcampaign`);
+    cy.visit(`/en/campaigns/test-bannered-campaign`);
     cy.window()
       .its(`main-js:react:finished`)
       .should(`equal`, true);
