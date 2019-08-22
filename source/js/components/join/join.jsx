41f7e0a8a922c89f1d2ffbf142afcfc4cc557d7c
Youri Wims <youri@mozillafoundation.org>
2019-08-22 19:11:58 -0400

    updated behaviour

--- source/js/components/join/join.jsx
+++ source/js/components/join/join.jsx
@@ -224,8 +224,7 @@ export default class JoinUs extends React.Component {
   renderEmailField() {
     let classes = classNames(`mb-2`, {
       "has-danger":
-        (!this.state.apiSuccess &&
-          this.state.userTriedSubmitting &&
+        (this.state.userTriedSubmitting &&
           !this.validatesAsEmail(this.email.value)) ||
         this.state.signupFailed
     });
