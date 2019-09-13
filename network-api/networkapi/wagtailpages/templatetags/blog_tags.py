90507a622d4bce8c9d0f602f87ba1d910c5f3b4a
Youri Wims <youri@mozillafoundation.org>
2019-09-13 14:41:30 -0400

    testing tempatetag

---
+++ network-api/networkapi/wagtailpages/templatetags/blog_tags.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_root_or_page(context):
+    root = context.get('root', None)
+    page = context.get('page', None)
+
+    if root:
+        return root.specific
+    else:
+        return page.get_parent().specific()
