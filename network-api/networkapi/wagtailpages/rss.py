4f3a9640a872c94d65eb6b41f695dbf6ec4009f0
Pomax <pomax@MacBook-Pro.hitronhub.home>
2019-09-20 14:04:06 -0700

    rss/atom

---
+++ network-api/networkapi/wagtailpages/rss.py
@@ -0,0 +1,43 @@
+from html import unescape
+
+from django.conf import settings;
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
+from django.utils.feedgenerator import Rss201rev2Feed
+from django.utils.feedgenerator import Atom1Feed
+from django.shortcuts import get_object_or_404
+
+from .models import BlogPage
+
+
+class RSSFeed(Feed):
+    """
+    Blog page RSS feed, using the content:encoded serializer above
+    """
+
+    title = 'Mozilla Foundation Blog'
+    link = 'https://foundation.mozilla.org/blog/'
+    feed_url = 'https://foundation.mozilla.org/blog/rss/'
+    description = 'The Mozilla Foundation Blog'
+
+    def items(self):
+        return BlogPage.objects.live().public()[:settings.FEED_LIMIT]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_link(self, item):
+        return item.full_url
+
+    def item_description(self, item):
+        return item.get_meta_description()
+
+    def item_pubdate(self, item):
+        return item.first_published_at
+
+
+class AtomFeed(RSSFeed):
+    feed_type = Atom1Feed
+    link = RSSFeed.link
+    feed_url = 'https://foundation.mozilla.org/blog/atom/'
+    subtitle = RSSFeed.description
