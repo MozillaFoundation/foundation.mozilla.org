f5c29c3bdd1fda4f987c22a48193879241da1c56
Mavis Ou <mmmavis@users.noreply.github.com>
2019-10-01 15:01:00 -0700

    Add YouTube Regret component

--- network-api/networkapi/wagtailpages/customblocks/youtube_regret_block.py
+++ network-api/networkapi/wagtailpages/customblocks/youtube_regret_block.py
@@ -1,9 +1,24 @@
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class YoutubeRegretBlock(blocks.StructBlock):
-    heading = blocks.CharBlock(
-        help_text='Temporary youtube regret block.'
+    headline = blocks.CharBlock(
+        help_text='Headline of this YouTube Regret'
+    )
+
+    image = ImageChooserBlock(
+        required=False
+    )
+
+    imageAltText = blocks.CharBlock(
+        required=False,
+        help_text='Image description (for screen readers).'
+    )
+
+    story = blocks.TextBlock(
+        verbose_name='youtube_regret_story',
+        help_text='Story of this YouTube Regret',
     )
 
     class Meta:
