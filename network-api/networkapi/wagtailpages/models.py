f88f772f229a6383a4a57255a97c6dadf44de741
Pomax <pomax@nihongoresources.com>
2019-11-14 11:00:28 -0800

    iexact

--- network-api/networkapi/wagtailpages/models.py
+++ network-api/networkapi/wagtailpages/models.py
@@ -859,8 +859,10 @@ class BlogPage(FoundationMetadataPageMixin, Page):
         context['coral_talk_server_url'] = settings.CORAL_TALK_SERVER_URL
         context['coral_talk'] = context['coral_talk_server_url'] and self.feature_comments
 
+        # Pull this object specifically using the English page title
         blog_page = IndexPage.objects.get(title_en__iexact='Blog')
 
+        # If that doesn't yield the blog page, pull using the universal title
         if blog_page is None:
             blog_page = IndexPage.objects.get(title__iexact='Blog')
 
