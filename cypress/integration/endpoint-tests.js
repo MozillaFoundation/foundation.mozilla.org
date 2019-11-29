407f681bd002c4cfb57962f0381d6f3335176de7
Mavis Ou <mmmavis@users.noreply.github.com>
2019-11-29 13:34:32 -0800

    Add Percy tests for blog category page

--- cypress/integration/endpoint-tests.js
+++ cypress/integration/endpoint-tests.js
@@ -62,6 +62,27 @@ describe(`Visual regression testing for foundation.mozilla.org`, () => {
     cy.percySnapshot();
   });
 
+  it(`Blog index filtered on category`, function() {
+    cy.visit(`/en/blog/category/mozilla-festival`);
+    cy.window()
+      .its(`main-js:react:finished`)
+      .should(`equal`, true);
+    cy.wait(500);
+    cy.percySnapshot();
+  });
+
+  // This test actually doesn't generate any *new* Percy snapshot
+  // as a non-existent blog category page gets redirects to /blog
+  // and we have already taken a /blog snapshot for a test above
+  it(`Blog index with non-existent category`, function() {
+    cy.visit(`/en/blog/category/randomnonsensecateogrythatdoesntexist`);
+    cy.window()
+      .its(`main-js:react:finished`)
+      .should(`equal`, true);
+    cy.wait(500);
+    cy.percySnapshot();
+  });
+
   // Skipping this test for now because of an error on Percy that needs to be investigated
   it.skip(`Fixed blog page`, function() {
     cy.visit(`/en/blog/initial-test-blog-post-with-fixed-title`);
