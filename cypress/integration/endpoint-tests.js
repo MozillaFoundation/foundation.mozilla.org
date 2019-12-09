3dc261c09c634d5862e282f64984f9e355be2e89
Pomax <pomax@nihongoresources.com>
2019-12-09 15:11:20 -0800

    Update endpoint-tests.js

--- cypress/integration/endpoint-tests.js
+++ cypress/integration/endpoint-tests.js
@@ -72,30 +72,7 @@ describe(`Visual regression testing for foundation.mozilla.org`, () => {
   });
 
   it(`Blog index with non-existent category`, function() {
-    cy.request({
-      url: `/en/blog/category/randomnonsensecateogrythatdoesntexist`,
-      followRedirect: false
-    }).then(res => {
-      expect(res.status).to.eq(301);
-    });
-
-    cy.request({
-      url: `/en/blog/category/randomnonsensecateogrythatdoesntexist`,
-      followRedirect: true
-    }).then(res => {
-      // res.redirects should return the following array
-      // [
-      //  301: "baseUrl/en/blog/category/randomnonsensecateogrythatdoesntexist"
-      //  302: "baseUrl/en/blog"
-      // ]
-      let finalRedirect = res.redirects[res.redirects.length - 1]
-        .trim()
-        .split(` `);
-      expect(parseInt(finalRedirect[0], 10)).to.eq(302);
-      expect(finalRedirect[1].replace(/\/$/, ``)).to.eq(
-        `${Cypress.config().baseUrl}/en/blog`
-      );
-    });
+    cy.visit(`/en/blog/category/randomnonsensecateogrythatdoesntexist`);
   });
 
   // Skipping this test for now because of an error on Percy that needs to be investigated
