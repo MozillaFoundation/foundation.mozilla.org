8c703702fa0ef257d1580e6f0e9d976cf165eb40
Lucie <lucie.daeye@gmail.com>
2019-12-10 16:55:57 +0100

    Update network-api/networkapi/context_processor.py
    Co-Authored-By: Christopher DeCairos <christopherd@mozillafoundation.org>

--- network-api/networkapi/context_processor.py
+++ network-api/networkapi/context_processor.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 # Used to export env variable to Django templates
 def review_app(request):
-    return {"REVIEW_APP": settings.REVIEW_APP}
+    return {'REVIEW_APP': settings.REVIEW_APP}
 
 
 # Used in BuyersGuide templates to check if we're using cloudinary
