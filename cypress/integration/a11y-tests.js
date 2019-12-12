01de367466c7cb2914dc2359d4a596b73ed444e9
Youri Wims <youri@mozillafoundation.org>
2019-12-12 08:47:21 -0500

    added pni tests

--- cypress/integration/a11y-tests.js
+++ cypress/integration/a11y-tests.js
@@ -2,99 +2,136 @@ const customViolationLogger = violation => cy.task("log", violation)
 const customViolationHandler = violation => cy.task("log", violation)
 
 const A11Y_CONFIG = {
-  branding: {
-    brand: "Mozilla",
-    application: "FoMo"
-  },
   reporter: "v2"
 }
+
+let EXCLUDE_PNI_GLOBAL;
 
 const EXCLUDE_CONSTANTS = {
   exclude: [
     ['.join-us'], // Signups
     ['.wide-screen-menu', '.nav-links'], // Desktop Nav
     ['.narrow-screen-menu-container', '.nav-links'], //Mobile Nav
-    ['.donate-banner'] // Donate Banner
+    ['.donate-banner *'] // Donate Banner
     ['.site-footer', '#language-switcher'], // Language Switcher
     ['.site-footer a.logo'] // Footer Logo
   ]
 }
 
-describe(`A11y Tests`, () => {  
-  beforeEach(() => {
-    cy.injectAxe();
-    cy.configureAxe(A11Y_CONFIG);
-  });
+const EXCLUDE_PNI_CONSTANTS = {
+  exclude: [
+    ['.join-us'], // Signups
+    ['.wide-screen-menu', '.nav-links'], // Desktop Nav
+    ['.narrow-screen-menu-container', '.nav-links'], //Mobile Nav
+    ['.donate-banner *'] // Donate Banner
+    ['.site-footer', '#language-switcher'], // Language Switcher
+    ['.site-footer a.logo'], // Footer Logo
+    ['#coral-talk-stream', 'iframe'] // Coral
+  ]
+}
 
-  before(() => {
-    cy.visit(`/`);
-    cy.window()
-      .its(`main-js:react:finished`)
-      .should(`equal`, true);
-    cy.wait(500);
-  });
+describe(`A11Y Tests`, () => {  
+  // before(() => {
+  //   cy.window()
+  //     .its(`main-js:react:finished`)
+  //     .should(`equal`, true);
+  //   cy.wait(500);
+  // });
 
   // Need to test prod for Initiatives + Internet Health pages
 
-  /******** Primary Page Assessments *********/
+  describe(`FoMo Page Assessments`, () => {
 
-  it.skip(`Homepage Accessibility Report`, () => {
-    cy.checkA11y(EXCLUDE_CONSTANTS)
-  })
+    // FOMO
 
-  it.skip("About Page Accessibility Report", () => {
-    cy.get(".wide-screen-menu > .nav-links > a:nth-child(1)")
-      .click()
-      .injectAxe()
-      .configureAxe(A11Y_CONFIG)
-      .checkA11y(EXCLUDE_CONSTANTS)
-  })
+    it.skip(`Homepage Accessibility Report`, () => {
+      cy.visit()
+        .injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_CONSTANTS)
+    })
 
-  it.skip("Participate Page Accessibility Report", () => {
-    cy.get(".wide-screen-menu > .nav-links > a:nth-child(4)")
-      .click()
-      .injectAxe()
-      .configureAxe(A11Y_CONFIG)
-      .checkA11y(EXCLUDE_CONSTANTS)
-  })
+    it.skip("About Page Accessibility Report", () => {
+      cy.visit(`en/about`)
+        .injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_CONSTANTS)
+    })
 
-  it.skip("Blog Index Page Accessibility Report", () => {
-    cy.visit("en/blog/")
-      .injectAxe()
-      .configureAxe(A11Y_CONFIG)
-      .checkA11y(EXCLUDE_CONSTANTS)
-  })
+    it.skip("Participate Page Accessibility Report", () => {
+      cy.visit(`en/participate`)
+        .injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_CONSTANTS)
+    })
 
-  it.skip("Fixed Blog Page Accessibility Report", () => {
-    cy.visit("/en/blog/initial-test-blog-post-with-fixed-title")
-      .injectAxe()
-      .configureAxe(A11Y_CONFIG)
-      .checkA11y(EXCLUDE_CONSTANTS)
-  })
+    // FOMO Blog
 
-  /******** Partial Assessments *********/
+    it.skip("Blog Index Page Accessibility Report", () => {
+      cy.visit("en/blog/")
+        .injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_CONSTANTS)
+    })
 
-  // General newsletter tests
-
-  it("Newsletter Sign Ups Accessibility Report", () => {
-    cy.get(".site-footer .join-us input[type='email']")
-      .click()
-    cy.checkA11y({
-      include: [
-        [".join-us input[type='email'"],
-        [".join-us .form-l10n .country-picker"],
-        [".join-us .form-l10n #userLanguage-footer"],
-        [".join-us input[type='checkbox'"],
-        [".join-us button.btn-primary"]
-      ]
+    it.skip("Fixed Blog Page Accessibility Report", () => {
+      cy.visit("/en/blog/initial-test-blog-post-with-fixed-title")
+        .injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_CONSTANTS)
     })
   })
 
-  /* TODO
+  describe(`PNI Page Assessments`, () => {
 
-      - Mobile Nav/Hamburger
-      - Footer Social Links
-      - Footer Language Switcher
-      - Donate Buttons
-  */
+    it.skip("PNI homepage Accessibility Report", () => {
+      cy.visit("en/privacynotincluded/")
+      cy.injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_CONSTANTS)
+    })
+
+    it.skip("PNI Category Page Accessibility Report", () => {
+      cy.visit("/en/privacynotincluded/categories/toys-games/")
+        .injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_CONSTANTS)
+    })
+
+    it.skip("PNI Product Page Accessibility Report", () => {
+      cy.visit("/en/privacynotincluded/products/percy-cypress/")
+        .injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_CONSTANTS)
+    })
+
+    it("PNI About Page Accessibility Report", () => {
+      cy.visit("/en/privacynotincluded/about/")
+        .injectAxe()
+        .configureAxe(A11Y_CONFIG)
+        .checkA11y(EXCLUDE_PNI_CONSTANTS)
+    })
+  })
+
+  describe(`Global Component Assessments`, () => {
+    
+    // General newsletter tests
+
+    it.skip("Newsletter Sign Ups Accessibility Report", () => {
+      cy.visit(`/`)
+        .get(".site-footer .join-us input[type='email']")
+        .click()
+        .checkA11y({
+          include: [
+            [".join-us input[type='email'"],
+            [".join-us .form-l10n .country-picker"],
+            [".join-us .form-l10n #userLanguage-footer"],
+            [".join-us input[type='checkbox'"],
+            [".join-us button.btn-primary"]
+          ]
+        })
+    })
+
+  })
+
 });
