3ccd88b79f00f11da667bd6f00f33bc979a98c48
Lucie Daeye <lucied@mozillafoundation.org>
2020-02-07 18:53:15 +0100

    fix issue with db

--- network-api/networkapi/buyersguide/utils.py
+++ network-api/networkapi/buyersguide/utils.py
@@ -20,14 +20,19 @@ def quad_to_tri(input):
 
 # Redirect to the same page, but using the default CMS site.
 # Ex: PNI should only be accessible on foundation.mozilla.org
-main_site = Site.objects.get(is_default_site=True).hostname
-secondary_wagtail_sites = [site.hostname for site in list(Site.objects.filter(is_default_site=False))]
-
-
 def redirect_to_main_cms_site_decorator(func):
     def wrapper(request, *args, **kwargs):
+        wagtail_sites = Site.objects.all()
+        secondary_wagtail_sites = []
+        main_wagtail_site = ''
+        for site in wagtail_sites:
+            if site.is_default_site:
+                main_wagtail_site = site.hostname
+            else:
+                secondary_wagtail_sites.append(site.hostname)
+
         current_site = request.get_host()
         if current_site in secondary_wagtail_sites:
-            return redirect(f"https://{main_site}{request.path}")
+            return redirect(f"https://{main_wagtail_site}{request.path}")
         return func(request, *args, **kwargs)
     return wrapper
