c5fa38c0a621ca3e96b226da8a68fa40a23f69ba
Théo Chevalier <theo.chevalier11@gmail.com>
2020-02-13 19:34:09 +0100

    Add helper command to extract strings to an external repo

---
+++ translation-management.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Pretty printing functions
+NORMAL=$(tput sgr0)
+GREEN=$(tput setaf 2; tput bold)
+YELLOW=$(tput setaf 3)
+RED=$(tput setaf 1)
+
+function echored() {
+  echo -e "$RED$*$NORMAL"
+}
+
+function echogreen() {
+  echo -e "$GREEN$*$NORMAL"
+}
+
+function echoyellow() {
+  echo -e "$YELLOW$*$NORMAL"
+}
+
+if [ $# -lt 1 ]
+then
+  echored "ERROR: not enough arguments supplied."
+  echo "Usage: translation-management.sh *action*"
+  echo "  - action: import, export"
+  exit 1
+fi
+
+command="$1"
+
+# Read path to local string repository from .env file
+L10N_REPO=$(grep LOCAL_PATH_TO_L10N_REPO .env | cut -d '=' -f2)
+
+case $command in
+  "import")
+    echogreen "Importing latest translation files from fomo-l10n repository"
+    cp -r "${L10N_REPO}translation-test/locale/" "network-api/locale/"
+    cp -r "${L10N_REPO}translation-test/networkapi/buyersguide/locale/" "network-api/networkapi/buyersguide/locale/"
+    cp -r "${L10N_REPO}translation-test/networkapi/mozfest/locale/" "network-api/networkapi/mozfest/locale/"
+esac
+
+case $command in
+  "export")
+    echogreen "Exporting generated translation files to fomo-l10n repository"
+    cp -r "network-api/locale/" "${L10N_REPO}translation-test/locale/"
+    cp -r "network-api/networkapi/buyersguide/locale/" "${L10N_REPO}translation-test/networkapi/buyersguide/locale/"
+    cp -r "network-api/networkapi/mozfest/locale/" "${L10N_REPO}translation-test/networkapi/mozfest/locale/"
+esac
