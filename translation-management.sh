1af298898b8ed9f87d2870aa53999c44db836f40
Théo Chevalier <theo.chevalier11@gmail.com>
2020-02-13 19:42:48 +0100

    Fix path

--- translation-management.sh
+++ translation-management.sh
@@ -34,15 +34,15 @@ L10N_REPO=$(grep LOCAL_PATH_TO_L10N_REPO .env | cut -d '=' -f2)
 case $command in
   "import")
     echogreen "Importing latest translation files from fomo-l10n repository"
-    cp -r "${L10N_REPO}translation-test/locale/" "network-api/locale/"
-    cp -r "${L10N_REPO}translation-test/networkapi/buyersguide/locale/" "network-api/networkapi/buyersguide/locale/"
-    cp -r "${L10N_REPO}translation-test/networkapi/mozfest/locale/" "network-api/networkapi/mozfest/locale/"
+    cp -r "${L10N_REPO}dummydjangoproject/translationstests/locale/" "network-api/locale/"
+    cp -r "${L10N_REPO}dummydjangoproject/translationstests/networkapi/buyersguide/locale/" "network-api/networkapi/buyersguide/locale/"
+    cp -r "${L10N_REPO}dummydjangoproject/translationstests/networkapi/mozfest/locale/" "network-api/networkapi/mozfest/locale/"
 esac
 
 case $command in
   "export")
     echogreen "Exporting generated translation files to fomo-l10n repository"
-    cp -r "network-api/locale/" "${L10N_REPO}translation-test/locale/"
-    cp -r "network-api/networkapi/buyersguide/locale/" "${L10N_REPO}translation-test/networkapi/buyersguide/locale/"
-    cp -r "network-api/networkapi/mozfest/locale/" "${L10N_REPO}translation-test/networkapi/mozfest/locale/"
+    cp -r "network-api/locale/" "${L10N_REPO}dummydjangoproject/translationstests/locale/"
+    cp -r "network-api/networkapi/buyersguide/locale/" "${L10N_REPO}dummydjangoproject/translationstests/networkapi/buyersguide/locale/"
+    cp -r "network-api/networkapi/mozfest/locale/" "${L10N_REPO}dummydjangoproject/translationstests/networkapi/mozfest/locale/"
 esac
