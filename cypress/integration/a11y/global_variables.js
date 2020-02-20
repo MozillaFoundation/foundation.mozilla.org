7a295d0fff703d642a94d32efc3322e85d3165a2
Pomax <pomax@nihongoresources.com>
2020-02-20 15:01:59 -0800

    Axe core setup rebased (#4197)
    * added Axe based testing using Cypress

---
+++ cypress/integration/a11y/global_variables.js
@@ -0,0 +1,20 @@
+const viewports = ['iphone-5', 'macbook-13']
+
+const A11Y_CONFIG = {
+  reporter: "v2"
+}
+
+const EXCLUDE_CONSTANTS = [
+  ['.join-us'], // Signups
+  ['.wide-screen-menu', '.nav-links'], // Desktop Nav
+  ['.narrow-screen-menu-container', '.nav-links'], //Mobile Nav
+  ['.donate-banner *'] // Donate Banner
+  ['.site-footer', '#language-switcher'], // Language Switcher
+  ['.site-footer a.logo'] // Footer Logo
+]
+
+const EXCLUDE_PNI_CONSTANTS = EXCLUDE_CONSTANTS.concat([
+    ['#coral-talk-stream', 'iframe'] // Commenting
+  ])
+
+export { viewports, A11Y_CONFIG, EXCLUDE_CONSTANTS, EXCLUDE_PNI_CONSTANTS }
