bb573013efe0c9bf56023aa50b58f01bd5e6332a
Théo Chevalier <theo.chevalier11@gmail.com>
2020-03-09 13:41:12 +0000

    Support ISO 3166 locale codes

--- network-api/networkapi/wagtailpages/__init__.py
+++ network-api/networkapi/wagtailpages/__init__.py
@@ -1,5 +1,9 @@
+import django
+import functools
+
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation.trans_real import accept_language_re
 
 from wagtail.admin.menu import MenuItem
 from wagtail.core import hooks
@@ -16,3 +20,54 @@ def register_howto_menu_item():
         _('How Do I Wagtail'), reverse('how-do-i-wagtail'),
         name='howdoIwagtail', classnames='icon icon-help', order=900
     )
+
+
+# WARNING: this is not necessarily a good idea, but is the only way to override
+# Django's default behaviour of requiring language codes to be lowercased.
+# We have to modify the core Django method, because we have no way to replace
+# all the core functionality that relies on this - e.g., url resolvers that
+# the Django admin and third party apps use.
+# This is pretty ugly, and ideally this should be fixed upstream.
+
+
+def language_code_to_iso_3166(language):
+    """Turn a language name (en-us) into an ISO 3166 format (en-US)."""
+    language, _, country = language.lower().partition('-')
+    if country:
+        return language + '-' + country.upper()
+    return language
+
+
+def to_language(locale):
+    """Turn a locale name (en_US) into a language name (en-US)."""
+    return locale.replace('_', '-')
+
+
+@functools.lru_cache(maxsize=1000)
+def parse_accept_lang_header(lang_string):
+    """
+    Parse the lang_string, which is the body of an HTTP Accept-Language
+    header, and return a tuple of (lang, q-value), ordered by 'q' values.
+    Return an empty tuple if there are any format errors in lang_string.
+    """
+    result = []
+    pieces = accept_language_re.split(lang_string.lower())
+    if pieces[-1]:
+        return ()
+    for i in range(0, len(pieces) - 1, 3):
+        first, lang, priority = pieces[i:i + 3]
+        if first:
+            return ()
+        if priority:
+            priority = float(priority)
+        else:
+            priority = 1.0
+        result.append((language_code_to_iso_3166(lang), priority))
+    result.sort(key=lambda k: k[1], reverse=True)
+    return tuple(result)
+
+
+# Replace some functions in django.utils.translation.trans_real with our own
+# versions that support a language in the form en-US instead of en-us.
+django.utils.translation.trans_real.to_language = to_language
+django.utils.translation.trans_real.parse_accept_lang_header = parse_accept_lang_header
--- network-api/networkapi/wagtailpages/utils.py
+++ network-api/networkapi/wagtailpages/utils.py
@@ -2,8 +2,14 @@ import re
 
 from itertools import chain
 from django.apps import apps
+from django.conf import settings
 from django.db.models import Count
+from django.urls import LocalePrefixPattern, URLResolver
 from django.utils.translation import gettext
+from django.utils.translation.trans_real import (
+    check_for_language, get_languages, get_language_from_path,
+    get_supported_language_variant, parse_accept_lang_header, language_code_re
+)
 from sentry_sdk import capture_exception
 
 
@@ -206,3 +212,78 @@ def insert_panels_after(panels, after_label, additional_panels):
         raise ValueError(f'No panel with heading "{after_label}" in panel list')
 
     return panels
+
+
+class ISO3166LocalePrefixPattern(LocalePrefixPattern):
+    """LocalePrefixPattern subclass that enforces URL prefixes in the form en-US"""
+
+    def match(self, path):
+        language_prefix = language_code_to_iso_3166(self.language_prefix)
+        if path.startswith(language_prefix):
+            return path[len(language_prefix):], (), {}
+        return None
+
+
+def i18n_patterns(*urls, prefix_default_language=True):
+    """
+    Replacement for django.conf.urls.i18_patterns that uses ISO3166LocalePrefixPattern
+    instead of django.urls.resolvers.LocalePrefixPattern.
+    """
+    if not settings.USE_I18N:
+        return list(urls)
+    return [
+        URLResolver(
+            ISO3166LocalePrefixPattern(prefix_default_language=prefix_default_language),
+            list(urls),
+        )
+    ]
+
+
+def language_code_to_iso_3166(language):
+    """Turn a language name (en-us) into an ISO 3166 format (en-US)."""
+    language, _, country = language.lower().partition('-')
+    if country:
+        return language + '-' + country.upper()
+    return language
+
+
+def get_language_from_request(request, check_path=False):
+    """
+    Replacement for django.utils.translation.get_language_from_request.
+    The portion of code that is modified is identified below with a comment.
+    """
+    if check_path:
+        lang_code = get_language_from_path(request.path_info)
+        if lang_code is not None:
+            return lang_code
+
+    lang_code = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME)
+    if lang_code is not None and lang_code in get_languages() and check_for_language(lang_code):
+        return lang_code
+
+    try:
+        return get_supported_language_variant(lang_code)
+    except LookupError:
+        pass
+
+    accept = request.META.get('HTTP_ACCEPT_LANGUAGE', '')
+    for accept_lang, unused in parse_accept_lang_header(accept):
+        if accept_lang == '*':
+            break
+
+        # Convert lowercase region to uppercase before attempting to find a variant.
+        # This is the only portion of code that is modified from the core function.
+        accept_lang = language_code_to_iso_3166(accept_lang)
+
+        if not language_code_re.search(accept_lang):
+            continue
+
+        try:
+            return get_supported_language_variant(accept_lang)
+        except LookupError:
+            continue
+
+    try:
+        return get_supported_language_variant(settings.LANGUAGE_CODE)
+    except LookupError:
+        return settings.LANGUAGE_CODE
