3940c818b05eb5276408e127e5c8e381e4b1a6f2
Christopher DeCairos <cade@mozillafoundation.org>
2020-04-27 15:07:11 -0400

    don't inline JS for Typeform, try to detect a meta tag on page load

--- source/js/main.js
+++ source/js/main.js
@@ -16,6 +16,7 @@ import {
 } from "./foundation";
 
 import primaryNav from "./primary-nav.js";
+import EmbedTF from "./embed-tf.js";
 import initializeSentry from "./common/sentry-config.js";
 import YouTubeRegretsTunnel from "./foundation/pages/youtube-regrets/index";
 
@@ -40,6 +41,7 @@ const apps = [];
 let main = {
   init() {
     GoogleAnalytics.init();
+    EmbedTF.init();
 
     this.fetchEnv(envData => {
       env = envData;
