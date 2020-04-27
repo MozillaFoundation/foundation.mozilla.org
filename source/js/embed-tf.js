bb6ed7dacd5fcebe3ef00685db78950dbfef7624
Christopher DeCairos <cade@mozillafoundation.org>
2020-04-27 16:23:14 -0400

    code style

--- source/js/embed-tf.js
+++ source/js/embed-tf.js
@@ -3,20 +3,23 @@ let EmbedTF = {
     document.addEventListener("DOMContentLoaded", () => EmbedTF.setup());
   },
   setup: function() {
-    let formMeta = document.querySelector('meta[name=typeform-formurl]');
+    let formMeta = document.querySelector("meta[name=typeform-formurl]");
 
     if (!formMeta) {
       return;
     }
 
-
     let formElement = document.querySelector("div#typeform-embed");
 
-    window.typeformEmbed.makeWidget(formElement, formMeta.getAttribute('content'), {
-      hideFooter: true,
-      hideHeader: true,
-      opacity: 0
-    });
+    window.typeformEmbed.makeWidget(
+      formElement,
+      formMeta.getAttribute("content"),
+      {
+        hideFooter: true,
+        hideHeader: true,
+        opacity: 0
+      }
+    );
   }
 };
 
