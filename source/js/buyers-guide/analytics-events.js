d9ef07e2ac5a54d0401080a55395aa9110dde549
Pomax <pomax@nihongoresources.com>
2020-04-30 14:49:38 -0700

    refinements

--- source/js/buyers-guide/analytics-events.js
+++ source/js/buyers-guide/analytics-events.js
@@ -24,31 +24,34 @@ function getQuerySelectorEvents(pageTitle, productName) {
       category: `product`,
       action: `company link tap`,
       label: `company link for ${productName}`,
-      transport: `beacon`
+      transport: `beacon`,
+      conditionalQuery: `#view-product-page`
     },
     "#product-copy-link-button": {
       category: `product`,
       action: `copy link tap`,
-      label: `copy link ${productName}`
+      label: `copy link ${productName}`,
+      conditionalQuery: `#view-product-page`
     },
     "#product-live-chat": {
       category: `product`,
       action: `customer support link tap`,
-      label: `support link for ${productName}`
+      label: `support link for ${productName}`,
+      conditionalQuery: `#view-product-page`
     },
     "#creep-vote-btn": {
       category: `product`,
       action: `opinion submitted`,
-      label: `opinion on ${productName}`
+      label: `opinion on ${productName}`,
+      conditionalQuery: `#view-product-page`
     },
     "a.privacy-policy-link": {
       category: `product`,
       action: `privacy policy link tap`,
       label: `policy link for ${productName}`,
-      transport: `beacon`
+      transport: `beacon`,
+      conditionalQuery: `#view-product-page`
     },
-
-    // product updates
     ".product-update-link": {
       category: `product`,
       action: `update article link tap`,
@@ -56,7 +59,8 @@ function getQuerySelectorEvents(pageTitle, productName) {
       transport: `beacon`,
       // Not every product will have updates. Note that
       // this value will not be sent on as GA payload.
-      optional_element: true
+      optional_element: true,
+      conditionalQuery: `#view-product-page`
     }
   };
 }
@@ -83,18 +87,38 @@ const ProductGA = {
     let querySelectorEvents = getQuerySelectorEvents(pageTitle, productName);
 
     Object.keys(querySelectorEvents).forEach(querySelector => {
-      let elements = document.querySelectorAll(querySelector);
+      let target = document;
+
+      // Some events should only get bound on specific page(s),
+      // so we need to test to see what our query target is:
+      const baseData = querySelectorEvents[querySelector];
+      const conditionalQuery = baseData.conditionalQuery;
+
+      if (conditionalQuery) {
+        // Are we on the right page for this event binding?
+        target = document.querySelector(conditionalQuery);
+        if (!target) {
+          // We are not.
+          return;
+        }
+      }
+
+      // If we get here, we're on the right page for this event binding.
+      let elements = target.querySelectorAll(querySelector);
 
       if (elements.length > 0) {
-        const eventData = ({
-          category,
-          action,
-          label,
-          transport
-        } = querySelectorEvents[querySelector]);
+        const eventData = {
+          category: baseData.category,
+          action: baseData.action,
+          label: baseData.label,
+          transpory: baseData.transpory
+        };
 
         elements.forEach(e => setupElementGA(e, eventData));
-      } else if (!querySelectorEvents[querySelector].optional_element) {
+      } else if (!querySelectorEvents[querySelector].optional_element) {\
+        // If we're on the right page, but the event's query selector
+        // does not result in any elements, that's a bug and should
+        // log an error so we can fix that.
         console.error(`cannot find ${querySelector}`);
       }
     });
