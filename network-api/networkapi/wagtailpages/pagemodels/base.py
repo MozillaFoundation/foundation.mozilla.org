a14e681a215160f13efe0eebd7687c9c3f829d9d
Kalob Taulien <kalob.taulien@gmail.com>
2020-07-08 16:29:26 -0600

    Take Action Section (backend only)

--- network-api/networkapi/wagtailpages/pagemodels/base.py
+++ network-api/networkapi/wagtailpages/pagemodels/base.py
@@ -1,5 +1,7 @@
-from django.db import models
+from django.core.exceptions import ValidationError
 from django.conf import settings
+from django.db import models
+from django.forms.utils import ErrorList
 
 from wagtail.admin.edit_handlers import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
 from wagtail.core.models import Page, Orderable as WagtailOrderable
@@ -537,6 +539,39 @@ class FocusArea(models.Model):
         verbose_name_plural = 'Areas of focus'
 
 
+class HomepageTakeActionCards(WagtailOrderable):
+    page = ParentalKey(
+        'wagtailpages.Homepage',
+        related_name='take_action_cards',
+    )
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+    )
+    text = models.CharField(max_length=255)
+    internal_link = models.ForeignKey(
+        'wagtailcore.Page',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('text'),
+        PageChooserPanel('internal_link'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Area of focus'
+        verbose_name_plural = 'Areas of focus'
+
+
 class AreaOfFocus(WagtailOrderable):
     page = ParentalKey(
         'wagtailpages.Homepage',
@@ -569,6 +604,12 @@ class PartnerLogos(WagtailOrderable):
 
     class Meta:
         verbose_name = 'Partner Logo'
+    def clean(self):
+        # TODO add Orderable validation to make sure internal or external link is provided
+        super().clean()
+
+    class Meta:
+        verbose_name = "Take Action Card"
 
 
 class Homepage(FoundationMetadataPageMixin, Page):
@@ -676,6 +717,14 @@ class Homepage(FoundationMetadataPageMixin, Page):
         null=True,
         on_delete=models.SET_NULL,
     )
+    # Take Action Section
+    take_action_title = models.CharField(default='Take action', max_length=50)
+    signup_cta = models.ForeignKey(
+        'wagtailpages.Signup',
+        blank=False,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
 
     content_panels = Page.content_panels + [
         MultiFieldPanel(
@@ -740,7 +789,16 @@ class Homepage(FoundationMetadataPageMixin, Page):
             FieldPanel('quote_source_job_title'),
           ],
           heading='quote',
-          classname='collapsible'
+          classname='collapsible',
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('take_action_title'),
+                SnippetChooserPanel('signup_cta'),
+                InlinePanel('take_action_cards', label='Take Action Cards', max_num=4),
+            ],
+            heading='Take Action Section',
+            classname='collapsible',
         ),
     ]
 
