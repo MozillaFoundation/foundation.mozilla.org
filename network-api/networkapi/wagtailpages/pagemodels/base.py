a7536e423e32a005063bfaef5a2c39296d2b84f3
Pomax <pomax@nihongoresources.com>
2020-07-14 14:35:01 -0700

    flake

--- network-api/networkapi/wagtailpages/pagemodels/base.py
+++ network-api/networkapi/wagtailpages/pagemodels/base.py
@@ -516,7 +516,6 @@ class FocusArea(models.Model):
         verbose_name_plural = 'Areas of focus'
 
 
-
 class AreaOfFocus(WagtailOrderable, models.Model):
     page = ParentalKey(
         'wagtailpages.Homepage',
