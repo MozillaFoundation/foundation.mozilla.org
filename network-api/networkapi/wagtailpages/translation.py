7970a47a443f18eadcd616837bc2878421fb8a79
Mavis Ou <mmmavis@users.noreply.github.com>
2020-07-20 20:06:28 -0700

    Quote  (#4886)
    * quote section for new homepage

--- network-api/networkapi/wagtailpages/translation.py
+++ network-api/networkapi/wagtailpages/translation.py
@@ -158,6 +158,10 @@ class HomepageTR(TranslationOptions):
         'hero_button_text',
         'hero_button_url',
         'hero_image',
+        'quote_image',
+        'quote_text',
+        'quote_source_name',
+        'quote_source_job_title',
     )
 
 
