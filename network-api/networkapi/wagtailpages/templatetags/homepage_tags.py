a4483d4213113c86b890d022b02607e217d6f71a
Kalob Taulien <kalob.taulien@gmail.com>
2020-07-27 12:40:59 -0600

    Renamed template tag

--- network-api/networkapi/wagtailpages/templatetags/homepage_tags.py
+++ network-api/networkapi/wagtailpages/templatetags/homepage_tags.py
@@ -32,7 +32,7 @@ def homepage_image_with_class(context, path, classname):
 
 
 @register.simple_tag
-def get_authors(blog_post):
+def get_all_authors(blog_post):
     """
     Takes a Homepage features_blog post, gets all authors, traverses to the BlogPage
     and returns a list of dicts with the author.
