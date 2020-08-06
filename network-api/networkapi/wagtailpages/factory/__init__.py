56ff7c562bf843148e2090309ed3c34c46e1af54
Kalob Taulien <kalob.taulien@gmail.com>
2020-08-06 07:09:48 -0600

    Alphabetic ordering

--- network-api/networkapi/wagtailpages/factory/__init__.py
+++ network-api/networkapi/wagtailpages/factory/__init__.py
@@ -18,12 +18,16 @@ from . import (
 
 
 def generate(seed):
+    areas_of_focus.generate(seed)
     homepage.generate(seed)
     blog.generate(seed)
+    bannered_campaign_page.generate(seed)
+    campaign_page.generate(seed)
+    # areas_of_focus.generate is required before the homepage_areas_of_focus can exist
+    homepage_areas_of_focus.generate(seed)
     # homepage_features.generate requires blog pages to exist first
     homepage_features.generate(seed)
-    campaign_page.generate(seed)
-    bannered_campaign_page.generate(seed)
+    homepage_take_action.generate(seed)
     initiatives_page.generate(seed)
     news_page.generate(seed)
     opportunity.generate(seed)
@@ -31,9 +35,6 @@ def generate(seed):
     participate_page_featured_highlights.generate(seed)
     styleguide.generate(seed)
     youtube_regrets_page.generate(seed)
-    areas_of_focus.generate(seed)
-    homepage_areas_of_focus.generate(seed)
-    homepage_take_action.generate(seed)
 
 __all__ = [
     'generate'
