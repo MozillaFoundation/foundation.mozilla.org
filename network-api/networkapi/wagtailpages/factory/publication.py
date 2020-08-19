9bf27223eaa281e374e44f91f6f13fbd81981654
Eric Sherman <ericandrewsherman@gmail.com>
2020-08-19 17:04:02 -0500

    add fields to publication factory

--- network-api/networkapi/wagtailpages/factory/publication.py
+++ network-api/networkapi/wagtailpages/factory/publication.py
@@ -97,6 +97,11 @@ class ArticlePageFactory(PageFactory):
 
 
 def generate(seed):
+    """
+    makes a batch of 3 publication pages
+    eventually I'd like to add a post_generation hooks that gives each of these pages an arbitrary number of chapters
+    and chapters an arbitrary number of articles
+    """
     reseed(seed)
     home_page = get_homepage()
     """
