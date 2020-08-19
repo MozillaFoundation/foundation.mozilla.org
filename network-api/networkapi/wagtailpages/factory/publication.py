92372dd66ce83e661372549e1f181ac78cebe7a5
Eric Sherman <ericandrewsherman@gmail.com>
2020-08-19 17:04:02 -0500

    add fields to publication factory

--- network-api/networkapi/wagtailpages/factory/publication.py
+++ network-api/networkapi/wagtailpages/factory/publication.py
@@ -52,9 +52,39 @@ class PublicationPageFactory(PageFactory):
     Faker
 )
 
+# UGLY COPYPASTE FROM latest
+# https://github.com/mvantellingen/wagtail-factories/blob/master/src/wagtail_factories/factories.py
+
+from wagtail_factories.factories import MP_NodeFactory
+
+from wagtail.documents import get_document_model
+
+
+class CollectionFactory(MP_NodeFactory):
+    name = Faker('text', max_nb_chars=60)
+
+    class Meta:
+        model = Collection
+
+
+class CollectionMemberFactory(DjangoModelFactory):
+    collection = SubFactory(CollectionFactory, parent=None)
+
+
+class DocumentFactory(CollectionMemberFactory):
+    class Meta:
+        model = get_document_model()
+    title = Faker('text', max_nb_chars=255)
+    file = django.FileField(filename=Faker('file_name', category="text"), file_extension="pdf")
+
 
 class PublicationPageFactory(PageFactory):
     title = Faker('text', max_nb_chars=255)
+    subtitle = Faker('text', max_nb_chars=255)
+    secondary_subtitle = Faker('text', max_nb_chars=255)
+    publication_date = Faker('date_object')
+    hero_image = SubFactory(ImageFactory)
+    publication_file = DocumentFactory()
 
     class Meta:
         model = PublicationPage
