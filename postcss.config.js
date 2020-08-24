d11102c381df6f358f666ea0ebf5119726dbac0f
Mavis Ou <mmmavis@users.noreply.github.com>
2020-08-24 14:05:11 -0700

    adjust get-ring-z-translate mixin's formula so 'cssnano' doesn't break it

--- postcss.config.js
+++ postcss.config.js
@@ -7,12 +7,7 @@ let prodPlugins = [
     preset: ['default', {
         discardComments: {
             removeAll: true,
-        },
-        // 'cssnano' uses 'postcss-calc' module for calc() optimization.
-        // 'postcss-calc' is currently having parsing issues with calc()
-        // See https://github.com/postcss/postcss-calc/issues/77#issuecomment-491354918
-        // Disabling opitimization for 'calc' in our config until the parsing bug is fixed
-        calc: false
+        }
     }]
   })
 ];
