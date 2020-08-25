19b163e959b844377944b2d2bc3bebc1a6cd6d7d
Kalob Taulien <kalob.taulien@gmail.com>
2020-08-25 15:32:17 -0600

    Filter only Article Pages

--- network-api/networkapi/wagtailpages/translation.py
+++ network-api/networkapi/wagtailpages/translation.py
@@ -27,6 +27,9 @@ from .models import (
     Petition,
     Signup,
 
+    ArticlePage,
+    PublicationPage,
+
     # DEPRECATED
     PeoplePage,
 )
@@ -241,3 +244,20 @@ class YoutubeRegretsPageTR(TranslationOptions):
         'faq',
         'regret_stories',
     }
+
+
+@register(PublicationPage)
+class PublicationPageTR(TranslationOptions):
+    fields = (
+        'subtitle',
+        'secondary_subtitle',
+        'notes',
+    )
+
+
+@register(ArticlePage)
+class ArticlePageTR(TranslationOptions):
+    fields = (
+        'sidebar_summary_title',
+    )
+
