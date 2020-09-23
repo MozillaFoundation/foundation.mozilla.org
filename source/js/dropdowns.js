ae716f9e80cfd5b5e1aa234b6a80b34e7f3c0426
Kalob Taulien <kalob.taulien@gmail.com>
2020-09-23 10:27:45 -0600

    Prettier formatting

--- source/js/dropdowns.js
+++ source/js/dropdowns.js
@@ -1,21 +1,23 @@
 const Dropdowns = {
-  init: function () {
+  init: function() {
     // Toggle dropdowns when dropdown is clicked
-    document.querySelectorAll('.dropdown-toggle').forEach(element =>
-      element.addEventListener('click',  _ => {
+    document.querySelectorAll(".dropdown-toggle").forEach(element =>
+      element.addEventListener("click", _ => {
         element.classList.toggle("show");
-        const target = document.querySelector(`[aria-labelledby="${element.id}"]`);
+        const target = document.querySelector(
+          `[aria-labelledby="${element.id}"]`
+        );
         target.classList.toggle("d-block");
       })
     );
 
     // Close dropdowns when item is clicked
-    document.querySelectorAll('.dropdown-item').forEach(element => {
-      element.addEventListener('click', _ => {
-        element.parentElement.classList.remove('d-block', 'show')
-      })
-    })
-  },
+    document.querySelectorAll(".dropdown-item").forEach(element => {
+      element.addEventListener("click", _ => {
+        element.parentElement.classList.remove("d-block", "show");
+      });
+    });
+  }
 };
 
 export default Dropdowns;
