30dcf9f9dae1717cdffea96eac039ae721a8acb6
Pomax <pomax@nihongoresources.com>
2020-10-07 09:32:23 -0700

    Update field owners (#5386)
    * move general-only fields from the BaseProduct to the GeneralProduct class

--- network-api/networkapi/buyersguide/pagemodels/products/base.py
+++ network-api/networkapi/buyersguide/pagemodels/products/base.py
@@ -87,33 +87,12 @@ product_panels = [
     ),
     MultiFieldPanel(
         [
-            FieldPanel('personal_data_collected'),
-            FieldPanel('biometric_data_collected'),
-            FieldPanel('social_data_collected'),
+
             FieldPanel('how_does_it_use_data_collected'),
             FieldPanel('data_collection_policy_is_bad'),
-            FieldPanel('how_can_you_control_your_data'),
-            FieldPanel('data_control_policy_is_bad'),
         ],
-        heading='Data collection and control',
+        heading='How does it use this data',
         classname='collapsible',
-    ),
-    MultiFieldPanel(
-        [
-            FieldPanel('company_track_record'),
-            FieldPanel('track_record_is_bad'),
-            FieldPanel('track_record_details'),
-        ],
-        heading='Company track record',
-        classname='collapsible'
-    ),
-    MultiFieldPanel(
-        [
-            FieldPanel('offline_capable'),
-            FieldPanel('offline_use_description'),
-        ],
-        heading='Offline use',
-        classname='collapsible'
     ),
     MultiFieldPanel(
         [
@@ -150,16 +129,6 @@ product_panels = [
             FieldPanel('privacy_policy_helptext'),
         ],
         heading='Security',
-        classname='collapsible'
-    ),
-    MultiFieldPanel(
-        [
-            FieldPanel('uses_ai'),
-            FieldPanel('ai_uses_personal_data'),
-            FieldPanel('ai_is_transparent'),
-            FieldPanel('ai_helptext'),
-        ],
-        heading='Artificial Intelligence',
         classname='collapsible'
     ),
     MultiFieldPanel(
@@ -310,26 +279,6 @@ class Product(ClusterableModel):
         help_text='Describe the particulars around sign-up requirements here.'
     )
 
-    # What data does it collect?
-
-    personal_data_collected = models.TextField(
-        max_length=5000,
-        blank=True,
-        help_text='What kind of personal data does this product collect?'
-    )
-
-    biometric_data_collected = models.TextField(
-        max_length=5000,
-        blank=True,
-        help_text='What kind of biometric data does this product collect?'
-    )
-
-    social_data_collected = models.TextField(
-        max_length=5000,
-        blank=True,
-        help_text='What kind of social data does this product collect?'
-    )
-
     # How does it use this data?
 
     how_does_it_use_data_collected = models.TextField(
@@ -340,54 +289,6 @@ class Product(ClusterableModel):
 
     data_collection_policy_is_bad = models.BooleanField(
         default=False,
-    )
-
-    how_can_you_control_your_data = models.TextField(
-        max_length=5000,
-        blank=True,
-        help_text='How does this product let you control your data?'
-    )
-
-    data_control_policy_is_bad = models.BooleanField(
-        default=False,
-    )
-
-    # Company track record
-
-    track_record_choices = [
-        ('Great', 'Great'),
-        ('Average', 'Average'),
-        ('Needs Improvement', 'Needs Improvement'),
-        ('Bad', 'Bad')
-    ]
-
-    company_track_record = models.CharField(
-        choices=track_record_choices,
-        default='Average',
-        help_text='This company has a ... track record',
-        max_length=20
-    )
-
-    track_record_is_bad = models.BooleanField(
-        default=False,
-    )
-
-    track_record_details = models.TextField(
-        max_length=5000,
-        blank=True,
-        help_text='Describe the track record of this company here.'
-    )
-
-    # Offline use
-
-    offline_capable = ExtendedYesNoField(
-        help_text='Can this product be used offline?',
-    )
-
-    offline_use_description = models.TextField(
-        max_length=5000,
-        blank=True,
-        help_text='Describe how this product can be used offline.'
     )
 
     # Privacy policy
@@ -453,26 +354,6 @@ class Product(ClusterableModel):
     """
     privacy_policy_links = one to many, defined in PrivacyPolicyLink
     """
-
-    # Artificial Intelligence
-
-    uses_ai = ExtendedYesNoField(
-        help_text='Does the product use AI?',
-    )
-
-    ai_uses_personal_data = ExtendedYesNoField(
-        help_text='Does the AI use your personal data to make decisions about you?',
-    )
-
-    ai_is_transparent = ExtendedYesNoField(
-        help_text='Does the company allow users to see how the AI works?',
-    )
-
-    ai_helptext = models.TextField(
-        max_length=5000,
-        blank=True,
-        help_text='Helpful text around AI to show on the product page',
-    )
 
     # How to contact the company
 
