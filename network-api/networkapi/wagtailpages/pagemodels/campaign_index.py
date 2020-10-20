2e7f77eebd58a6b595c8956f0cb0c4cbc8969472
Kalob Taulien <kalob.taulien@gmail.com>
2020-10-20 17:36:03 -0600

    Exclude publication pages when 'Load More' is clicked on Campaign Index Page

--- network-api/networkapi/wagtailpages/pagemodels/campaign_index.py
+++ network-api/networkapi/wagtailpages/pagemodels/campaign_index.py
@@ -1,5 +1,7 @@
 from .index import IndexPage
 
+from networkapi.wagtailpages.pagemodels.publications.publication import PublicationPage
+
 
 class CampaignIndexPage(IndexPage):
     """
@@ -16,3 +18,12 @@ class CampaignIndexPage(IndexPage):
     ]
 
     template = 'wagtailpages/index_page.html'
+
+    def get_context(self, request):
+        # bootstrap the render context
+        context = super().get_context(request)
+        entries = self.get_all_entries().not_type(PublicationPage)
+        context['entries'] = entries[0:self.page_size]
+        # Which pagetype to exclude during the "load more" ajax request: PublicationPage
+        context['exclude_pagetype'] = 'publicationpage'
+        return context
