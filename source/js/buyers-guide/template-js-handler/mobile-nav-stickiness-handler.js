84eeca22f8986931acdd3d79b42da5107a05dae8
Mavis Ou <mmmavis@users.noreply.github.com>
2020-10-28 18:06:33 -0700

    Make mobile primary nav work on PNI (#5540)
    * make mobile primary nav work on PNI
    
    * mobile active nav link
    
    * fix regression - category nav should locate below the hero
    
    * fix broken script
    
    * remove extra padding around nav logo
    
    * override default wide_screen_menu block content
    
    * replace random z-index value with Bootstrap var

---
+++ source/js/buyers-guide/template-js-handler/mobile-nav-stickiness-handler.js
@@ -0,0 +1,40 @@
+/**
+ * PNI has special nav stickiness needs than the rest of the site.
+ * (e.g., we want the category nav to be sticky but not the primary nav)
+ * Without making primary-nav.js more complicated just to handle this special case,
+ * we use MutationObserver to detect PNI's mobile menu's close/open state
+ * so we can toggle class on .primary-nav-container-wrapper to
+ * acheive the visual effects we want.
+ */
+export default () => {
+  const menuBurger = document.querySelector(".burger");
+  const primanyNavContainer = document.querySelector(
+    ".primary-nav-container-wrapper"
+  );
+
+  if (!menuBurger || !primanyNavContainer) return;
+
+  const classToDetect = "menu-open";
+  const classToToggle = "sticky-top";
+  let prevMenuState = menuBurger.classList.contains(classToDetect);
+  const mutationHandler = (mutations) => {
+    mutations.forEach(function (mutation) {
+      if (mutation.attributeName === "class") {
+        let currentMenuState = mutation.target.classList.contains(
+          classToDetect
+        );
+        if (prevMenuState !== currentMenuState) {
+          prevMenuState = currentMenuState;
+          if (currentMenuState) {
+            primanyNavContainer.classList.add(classToToggle);
+          } else {
+            primanyNavContainer.classList.remove(classToToggle);
+          }
+        }
+      }
+    });
+  };
+
+  const mobileMenuObserver = new MutationObserver(mutationHandler);
+  mobileMenuObserver.observe(menuBurger, { attributes: true });
+};
