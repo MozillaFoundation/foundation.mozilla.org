713bfc23273578a343a028942eec8ab967633bed
Kalob Taulien <4743971+KalobTaulien@users.noreply.github.com>
2020-11-18 11:20:18 -0700

    Specific product page fields (#5656)
    * Specific product page fields
    
    * Updated migrations

--- network-api/networkapi/wagtailpages/pagemodels/products.py
+++ network-api/networkapi/wagtailpages/pagemodels/products.py
@@ -22,6 +22,7 @@ from networkapi.wagtailpages.pagemodels.mixin.foundation_metadata import (
 )
 from networkapi.buyersguide.pagemodels.product_category import BuyersGuideProductCategory
 from networkapi.buyersguide.pagemodels.product_update import Update
+from networkapi.wagtailpages.utils import insert_panels_after
 
 if settings.USE_CLOUDINARY:
     image_field = FieldPanel('cloudinary_image')
@@ -433,12 +434,237 @@ class ProductPage(FoundationMetadataPageMixin, Page):
 class SoftwareProductPage(ProductPage):
     template = 'buyersguide/product_page.html'
 
+    handles_recordings_how = models.TextField(
+        max_length=5000,
+        blank=True,
+        help_text='How does this software handle your recordings'
+    )
+
+    recording_alert = ExtendedYesNoField(
+        null=True,
+        help_text='Alerts when calls are being recorded?',
+    )
+
+    recording_alert_helptext = models.TextField(
+        max_length=5000,
+        blank=True
+    )
+
+    medical_privacy_compliant = models.BooleanField(
+        null=True,
+        help_text='Compliant with US medical privacy laws?'
+    )
+
+    medical_privacy_compliant_helptext = models.TextField(
+        max_length=5000,
+        blank=True
+    )
+
+    # Can I control it?
+
+    host_controls = models.TextField(
+        max_length=5000,
+        blank=True
+    )
+
+    easy_to_learn_and_use = models.BooleanField(
+        null=True,
+        help_text='Is it easy to learn & use the features?',
+    )
+
+    easy_to_learn_and_use_helptext = models.TextField(
+        max_length=5000,
+        blank=True
+    )
+
+    content_panels = ProductPage.content_panels.copy()
+    content_panels = insert_panels_after(
+        content_panels,
+        'General Product Details',
+        [
+            MultiFieldPanel(
+                [
+                    FieldPanel('handles_recordings_how'),
+                    FieldPanel('recording_alert'),
+                    FieldPanel('recording_alert_helptext'),
+                    FieldPanel('medical_privacy_compliant'),
+                    FieldPanel('medical_privacy_compliant_helptext'),
+                ],
+                heading='How does it handle privacy?',
+                classname='collapsible'
+            ),
+            MultiFieldPanel(
+                [
+                    FieldPanel('host_controls'),
+                    FieldPanel('easy_to_learn_and_use'),
+                    FieldPanel('easy_to_learn_and_use_helptext'),
+                ],
+                heading='Can I control it',
+                classname='collapsible'
+            ),
+        ],
+    )
+
     class Meta:
         verbose_name = "Software Product Page"
 
 
 class GeneralProductPage(ProductPage):
     template = 'buyersguide/product_page.html'
+
+    camera_device = ExtendedYesNoField(
+        help_text='Does this device have or access a camera?',
+    )
+
+    camera_app = ExtendedYesNoField(
+        help_text='Does the app have or access a camera?',
+    )
+
+    microphone_device = ExtendedYesNoField(
+        help_text='Does this Device have or access a microphone?',
+    )
+
+    microphone_app = ExtendedYesNoField(
+        help_text='Does this app have or access a microphone?',
+    )
+
+    location_device = ExtendedYesNoField(
+        help_text='Does this product access your location?',
+    )
+
+    location_app = ExtendedYesNoField(
+        help_text='Does this app access your location?',
+    )
+
+    # What data does it collect?
+
+    personal_data_collected = models.TextField(
+        max_length=5000,
+        blank=True,
+        help_text='What kind of personal data does this product collect?'
+    )
+
+    biometric_data_collected = models.TextField(
+        max_length=5000,
+        blank=True,
+        help_text='What kind of biometric data does this product collect?'
+    )
+
+    social_data_collected = models.TextField(
+        max_length=5000,
+        blank=True,
+        help_text='What kind of social data does this product collect?'
+    )
+
+    # How can you control your data
+
+    how_can_you_control_your_data = models.TextField(
+        max_length=5000,
+        blank=True,
+        help_text='How does this product let you control your data?'
+    )
+
+    data_control_policy_is_bad = models.BooleanField(
+        default=False,
+        verbose_name='Privacy ding'
+    )
+
+    # Company track record
+
+    track_record_choices = [
+        ('Great', 'Great'),
+        ('Average', 'Average'),
+        ('Needs Improvement', 'Needs Improvement'),
+        ('Bad', 'Bad')
+    ]
+
+    company_track_record = models.CharField(
+        choices=track_record_choices,
+        default='Average',
+        help_text='This company has a ... track record',
+        max_length=20
+    )
+
+    track_record_is_bad = models.BooleanField(
+        default=False,
+        verbose_name='Privacy ding'
+    )
+
+    track_record_details = models.TextField(
+        max_length=5000,
+        blank=True,
+        help_text='Describe the track record of this company here.'
+    )
+
+    # Offline use
+
+    offline_capable = ExtendedYesNoField(
+        help_text='Can this product be used offline?',
+    )
+
+    offline_use_description = models.TextField(
+        max_length=5000,
+        blank=True,
+        help_text='Describe how this product can be used offline.'
+    )
+
+    # Artificial Intelligence
+
+    uses_ai = ExtendedYesNoField(
+        help_text='Does the product use AI?'
+    )
+
+    ai_uses_personal_data = ExtendedYesNoField(
+        help_text='Does the AI use your personal data to make decisions about you?'
+    )
+
+    ai_is_transparent = ExtendedYesNoField(
+        help_text='Does the company allow users to see how the AI works?'
+    )
+
+    ai_helptext = models.TextField(
+        max_length=5000,
+        blank=True,
+        help_text='Helpful text around AI to show on the product page',
+    )
+
+    # administrative panels
+
+    content_panels = ProductPage.content_panels.copy()
+    content_panels = insert_panels_after(
+        content_panels,
+        'General Product Details',
+        [
+            MultiFieldPanel(
+                [
+                    FieldPanel('camera_device'),
+                    FieldPanel('camera_app'),
+                    FieldPanel('microphone_device'),
+                    FieldPanel('microphone_app'),
+                    FieldPanel('location_device'),
+                    FieldPanel('location_app'),
+                ],
+                heading='Can it snoop?',
+                classname='collapsible'
+            ),
+        ],
+    )
+
+    content_panels = insert_panels_after(
+        content_panels,
+        'What is required to sign up',
+        [
+            MultiFieldPanel(
+                [
+                    FieldPanel('personal_data_collected'),
+                    FieldPanel('biometric_data_collected'),
+                    FieldPanel('social_data_collected'),
+                ],
+                heading='What data does it collect',
+                classname='collapsible',
+            ),
+        ]
+    )
 
     class Meta:
         verbose_name = "General Product Page"
