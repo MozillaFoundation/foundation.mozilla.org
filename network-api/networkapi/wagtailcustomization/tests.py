6898a83201c64bee89c9a3abbaea327c1c66f86d
Pomax <pomax@nihongoresources.com>
2020-11-30 11:12:57 -0800

    hmmmm

---
+++ network-api/networkapi/wagtailcustomization/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase, RequestFactory
+
+from wagtail.core.models import Site
+from wagtail.contrib.redirects.models import Redirect
+
+
+class LocalizedRedirectTests(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        site = Site.objects.get(is_default_site=True)
+        redirect = Redirect.objects.create(
+            old_path='/test',
+            site=site,
+            redirect_link='/'
+        )
+        redirect.save();
+
+    def test_plain_redirect(self):
+        """
+        Test that the base redirect works.
+        """
+        response = self.client.get('/test')
+        print(response)
+
