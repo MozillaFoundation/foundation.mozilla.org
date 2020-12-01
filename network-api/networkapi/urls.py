0a3a64906aa5fa8167bcd2d889b5fb45cf52f9ec
Pomax <pomax@nihongoresources.com>
2020-12-01 10:01:56 -0800

    replace url with path and re_path (#5572)

--- network-api/networkapi/urls.py
+++ network-api/networkapi/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
-from django.conf.urls import url, include
+from django.urls import path, re_path, include
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
-from django.urls import path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -31,37 +30,37 @@ urlpatterns = list(filter(None, [
          ),
 
     # social-sign-on routes so that Google auth works
-    url(r'^soc/', include('social_django.urls', namespace='social')),
+    re_path(r'^soc/', include('social_django.urls', namespace='social')),
 
     # network API routes:
 
-    url(r'^api/campaign/', include('networkapi.campaign.urls')),
-    url(r'^api/highlights/', include('networkapi.highlights.urls')),
-    url(r'^api/news/', include('networkapi.news.urls')),
-    url(r'^api/milestones/', include('networkapi.milestones.urls')),
-    url(r'^api/people/', include('networkapi.people.urls')),
-    url(r'^api/buyersguide/vote/', buyersguide_views.product_vote, name='product-vote'),
-    url(r'^api/buyersguide/clear-cache/', buyersguide_views.clear_cache, name='clear-cache'),
-    url(r'^api/experiments/complete/([^\/]+)/$', experiment_views.record_completion),
-    url(r'^environment.json', EnvVariablesView.as_view()),
-    url(r'^help/', review_app_help_view, name='Review app help'),
+    re_path(r'^api/campaign/', include('networkapi.campaign.urls')),
+    re_path(r'^api/highlights/', include('networkapi.highlights.urls')),
+    re_path(r'^api/news/', include('networkapi.news.urls')),
+    re_path(r'^api/milestones/', include('networkapi.milestones.urls')),
+    re_path(r'^api/people/', include('networkapi.people.urls')),
+    re_path(r'^api/buyersguide/vote/', buyersguide_views.product_vote, name='product-vote'),
+    re_path(r'^api/buyersguide/clear-cache/', buyersguide_views.clear_cache, name='clear-cache'),
+    re_path(r'^api/experiments/complete/([^\/]+)/$', experiment_views.record_completion),
+    re_path(r'^environment.json', EnvVariablesView.as_view()),
+    re_path(r'^help/', review_app_help_view, name='Review app help'),
 
     # Wagtail CMS routes
-    url(
+    re_path(
         r'^how-do-i-wagtail/',
         RedirectView.as_view(url='/docs/how-do-i-wagtail/'),
         name='how-do-i-wagtail'
     ),
-    url(r'^cms/', include(wagtailadmin_urls)),
-    url(r'^en/cms/', RedirectView.as_view(url='/cms/')),
-    url(r'^documents/', include(wagtaildocs_urls)),
-    url('^sitemap.xml$', sitemap),
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^en/cms/', RedirectView.as_view(url='/cms/')),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^sitemap.xml$', sitemap),
 
     # Sentry test url
     path('sentry-debug', lambda r:  1 / 0) if settings.SENTRY_DSN and settings.DEBUG else None,
 
     # set up set language redirect view
-    url('i18n/', include('django.conf.urls.i18n')),
+    path('i18n/', include('django.conf.urls.i18n')),
 
     # Wagtail Footnotes package
     path("footnotes/", include(footnotes_urls)),
@@ -72,7 +71,7 @@ urlpatterns = list(filter(None, [
 # to be wrapped by django's i18n_patterns feature:
 urlpatterns += i18n_patterns(
     # Buyer's Guide / Privacy Not Included
-    url(r'^privacynotincluded/', include('networkapi.buyersguide.urls')),
+    re_path(r'^privacynotincluded/', include('networkapi.buyersguide.urls')),
 
     # Blog RSS feed
     path('blog/rss/', RSSFeed(), name='rss-feed'),
@@ -82,7 +81,7 @@ urlpatterns += i18n_patterns(
     *foundation_redirects(),
 
     # wagtail-managed data
-    url(r'', include(wagtail_urls)),
+    re_path(r'', include(wagtail_urls)),
 )
 
 if settings.USE_S3 is not True:
