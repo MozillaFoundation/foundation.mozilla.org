6ee00b3c47e4bfa3488c71a341aff05560bac2ea
Pomax <pomax@nihongoresources.com>
2020-12-01 11:13:09 -0800

    Update tests.py

--- network-api/networkapi/wagtailcustomization/tests.py
+++ network-api/networkapi/wagtailcustomization/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from wagtail.contrib.redirects.models import Redirect
 from django.test.utils import override_settings
 
+
 # Safeguard against the fact that static assets and views might be hosted remotely,
 # see https://docs.djangoproject.com/en/3.1/topics/testing/tools/#urlconf-configuration
 @override_settings(STATICFILES_STORAGE="django.contrib.staticfiles.storage.StaticFilesStorage")
