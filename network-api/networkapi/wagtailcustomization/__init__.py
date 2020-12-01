87cff0f176f8dc9f9747042062e41e69302cef4c
Pomax <pomax@nihongoresources.com>
2020-11-30 16:06:29 -0800

    at least the code works?

--- network-api/networkapi/wagtailcustomization/__init__.py
+++ network-api/networkapi/wagtailcustomization/__init__.py
@@ -1,4 +1,3 @@
-from django.apps import AppConfig
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.html import format_html
 
@@ -9,29 +8,3 @@ from wagtail.core import hooks
 def global_admin_css():
     """Add /static/css/admin.css to the admin."""
     return format_html('<link rel="stylesheet" href="{}">', static("css/admin.css"))
-
-
-class UnlocalizeRedirectsConfig(AppConfig):
-    """
-    Patch the _get_redirect function such that it removes locale prefixes
-    before actual redirect testing happens, as we use wagtail-modeltranslations,
-    and there is no such thing as "redirect only page(s) from locale(s) X, ..., Z"
-    """
-
-    def ready(self):
-        from wagtail.contrib.redirects import middleware as redirect_middleware
-
-        # We want to wrap the _get_redirect logic, so we need to cache the original.
-        _original_get_redirect = redirect_middleware._get_redirect
-
-        def _new_get_redirect(request, path):
-            # If this path has an i18n_patterns locale prefix, remove it.
-            if hasattr(request, 'LANGUAGE_CODE'):
-                localePrefix = f'{request.LANGUAGE_CODE}/'
-                if path.startswith(localePrefix):
-                    path = path.replace(localePrefix, '', 1)
-
-            # Then hand off processing to the original redirect logic.
-            _original_get_redirect(request, path)
-
-        redirect_middleware._get_redirect = _new_get_redirect
--- network-api/networkapi/wagtailcustomization/tests.py
+++ network-api/networkapi/wagtailcustomization/tests.py
@@ -1,24 +1,15 @@
-from django.test import TestCase, RequestFactory
-
-from wagtail.core.models import Site
+from django.test import TestCase
 from wagtail.contrib.redirects.models import Redirect
 
 
 class LocalizedRedirectTests(TestCase):
-
-    def setUp(self):
-        site = Site.objects.get(is_default_site=True)
-        redirect = Redirect.objects.create(
-            old_path='/test',
-            site=site,
-            redirect_link='/'
-        )
-        redirect.save();
-
     def test_plain_redirect(self):
         """
         Test that the base redirect works.
         """
+        Redirect.objects.create(
+            old_path='/test',
+            redirect_link='http://example.com'
+        )
         response = self.client.get('/test')
         print(response)
-
