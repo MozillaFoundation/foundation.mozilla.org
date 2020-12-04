eb40ee90bd11c3adf83bd35bd96e3c713b74638a
Mavis Ou <mmmavis@users.noreply.github.com>
2020-12-04 15:31:16 -0800

    Update index.js

--- source/js/foundation/pages/dear-internet/template-js-handler/index.js
+++ source/js/foundation/pages/dear-internet/template-js-handler/index.js
@@ -1,7 +1,7 @@
 import ctaButtonHandler from "./cta-button.js";
 
 /**
- * Bind event handlers to Regrets Reporter specific elements
+ * Bind event handlers to Dear Internet page specific elements
  */
 export const bindEventHandlers = () => {
   ctaButtonHandler();
