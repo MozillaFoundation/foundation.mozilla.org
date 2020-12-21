084b91705f525db703ccb406dcd19bd7144e38af
Kalob Taulien <kalob.taulien@gmail.com>
2020-12-21 11:45:17 -0700

    Close dropdown icon when hiding summary dropdown automatically

--- source/js/foundation/template-js-handler/publication-summary-bar.js
+++ source/js/foundation/template-js-handler/publication-summary-bar.js
@@ -2,9 +2,11 @@
  * Summary bar on the publication page shows up when scrolled passed the hero.
  */
 export default () => {
+  console.log("555")
   const summaryBlock = document.querySelector(".article-navbar-container");
   const marginOffset = getComputedStyle(summaryBlock).getPropertyValue('--top-offset');
   const dropDownMenu = document.querySelector(".article-summary-menu");
+  const articleSummaryToggle = document.querySelector(".article-summary-toggle");
   const hero = document.querySelector('.publication-hero-container');
   const nav = document.querySelector(".primary-nav-container");
   if (summaryBlock && hero && nav && dropDownMenu) {
@@ -24,6 +26,7 @@ export default () => {
           summaryBlock.style.top = marginOffset;
           summaryBlock.setAttribute("aria-hidden", false);
           dropDownMenu.classList.remove("d-block");
+          articleSummaryToggle.classList.remove("show");
         }
       },
       {
