c2e1a9741e1b06e30d75925c504c8824966491a7
Kalob Taulien <kalob.taulien@gmail.com>
2021-01-11 15:05:53 -0700

    Fix 'getComputedStyle' error?

--- source/js/foundation/template-js-handler/publication-summary-bar.js
+++ source/js/foundation/template-js-handler/publication-summary-bar.js
@@ -3,37 +3,39 @@
  */
 export default () => {
   const summaryBlock = document.querySelector(".article-navbar-container");
-  const marginOffset = getComputedStyle(summaryBlock).getPropertyValue(
-    "--top-offset"
-  );
-  const dropDownMenu = document.querySelector(".article-summary-menu");
-  const articleSummaryToggle = document.querySelector(
-    ".article-summary-toggle"
-  );
-  const hero = document.querySelector(".publication-hero-container");
-  const nav = document.querySelector(".primary-nav-container");
-  if (summaryBlock && hero && nav && dropDownMenu) {
-    const total =
-      hero.offsetHeight + nav.offsetHeight - summaryBlock.offsetHeight;
-
-    window.addEventListener(
-      "scroll",
-      () => {
-        if (window.scrollY > total) {
-          // Toggle summary block off
-          summaryBlock.style.top = "0";
-          summaryBlock.setAttribute("aria-hidden", true);
-        } else {
-          // Toggle summary block on
-          summaryBlock.style.top = marginOffset;
-          summaryBlock.setAttribute("aria-hidden", false);
-          dropDownMenu.classList.remove("d-block");
-          articleSummaryToggle.classList.remove("show");
-        }
-      },
-      {
-        passive: true
-      }
+  if(summaryBlock) {
+    const marginOffset = window.getComputedStyle(summaryBlock).getPropertyValue(
+      "--top-offset"
     );
+    const dropDownMenu = document.querySelector(".article-summary-menu");
+    const articleSummaryToggle = document.querySelector(
+      ".article-summary-toggle"
+    );
+    const hero = document.querySelector(".publication-hero-container");
+    const nav = document.querySelector(".primary-nav-container");
+    if (summaryBlock && hero && nav && dropDownMenu) {
+      const total =
+        hero.offsetHeight + nav.offsetHeight - summaryBlock.offsetHeight;
+
+      window.addEventListener(
+        "scroll",
+        () => {
+          if (window.scrollY > total) {
+            // Toggle summary block off
+            summaryBlock.style.top = "0";
+            summaryBlock.setAttribute("aria-hidden", true);
+          } else {
+            // Toggle summary block on
+            summaryBlock.style.top = marginOffset;
+            summaryBlock.setAttribute("aria-hidden", false);
+            dropDownMenu.classList.remove("d-block");
+            articleSummaryToggle.classList.remove("show");
+          }
+        },
+        {
+          passive: true
+        }
+      );
+    }
   }
 };
