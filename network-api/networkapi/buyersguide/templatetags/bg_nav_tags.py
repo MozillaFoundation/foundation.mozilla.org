7167e45afab59fbc7ecfbc1df6e9cdd5968d3293
Pomax <pomax@nihongoresources.com>
2021-01-13 11:40:36 -0800

    Update network-api/networkapi/buyersguide/templatetags/bg_nav_tags.py

--- network-api/networkapi/buyersguide/templatetags/bg_nav_tags.py
+++ network-api/networkapi/buyersguide/templatetags/bg_nav_tags.py
@@ -17,6 +17,7 @@ def bg_active_nav(current, target):
 
 
 # Instantiate a list of category page links based on the current page's relation to them
+# NOTE: this points to the new, namespaced category_nav_links. If we need to revert to the old app, change this back.
 @register.inclusion_tag('buyersguide/fragments/category_nav_links.html', takes_context=True)
 def category_nav(context, current_url, current_category, all_categories):
     return {
