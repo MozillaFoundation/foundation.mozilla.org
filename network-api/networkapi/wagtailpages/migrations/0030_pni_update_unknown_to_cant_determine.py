c0d326805cfe1421135e2311ea2b64446dca3f1b
Mavis Ou <mmmavis@users.noreply.github.com>
2021-01-21 15:27:06 -0800

    WIP migration file

---
+++ network-api/networkapi/wagtailpages/migrations/0030_pni_update_unknown_to_cant_determine.py
@@ -0,0 +1,68 @@
+from django.db import migrations
+
+
+def update_unknown_to_cantdetermine(apps, schema_editor):
+    Products = apps.get_model("wagtailpages", "ProductPage")
+    SoftwareProductPage = apps.get_model("wagtailpages", "SoftwareProductPage")
+    GeneralProductPage = apps.get_model("wagtailpages", "GeneralProductPage")
+
+    extended_yes_no_fields = [
+        'signup_requires_email',
+        'signup_requires_phone',
+        'signup_requires_third_party_account',
+        'user_friendly_privacy_policy',
+        'uses_encryption',
+        'security_updates',
+        'strong_password',
+        'manage_vulnerabilities',
+        'privacy_policy',
+    ]
+
+    software_extended_yes_no_fields = [
+        'recording_alert',
+    ]
+
+    general_extended_yes_no_fields = [
+        'camera_device',
+        'camera_app',
+        'microphone_device',
+        'microphone_app',
+        'location_device',
+        'location_app',
+        'offline_capable',
+        'uses_ai',
+        'ai_uses_personal_data',
+        'ai_is_transparent',
+    ]
+
+    for p in Products.objects.all():
+        for field in extended_yes_no_fields:
+            # update 'U' (Unknown) to the new value 'CD' (Can't Determine)
+            if p[field] == 'U':
+                p[field] = 'CD'
+                p.save()
+
+    for p in SoftwareProductPage.objects.all():
+        for field in software_extended_yes_no_fields:
+            # update 'U' (Unknown) to the new value 'CD' (Can't Determine)
+            if p[field] == 'U':
+                p[field] = 'CD'
+                p.save()
+
+    for p in GeneralProductPage.objects.all():
+        for field in general_extended_yes_no_fields:
+            # update 'U' (Unknown) to the new value 'CD' (Can't Determine)
+            if p[field] == 'U':
+                p[field] = 'CD'
+                p.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailpages', '0029_productpage_airtable_record_id'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_unknown_to_cantdetermine),
+    ]
