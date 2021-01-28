1147354efe7f2f45d4cd6be6e5926ac7e6d6f4a1
Mavis Ou <mmmavis@users.noreply.github.com>
2021-01-28 11:55:15 -0800

    fix linting

--- network-api/networkapi/wagtailpages/image_formats.py
+++ network-api/networkapi/wagtailpages/image_formats.py
@@ -1,5 +1,7 @@
-from wagtail.images.formats import Format, register_image_format, unregister_image_format
+from wagtail.images.formats import Format, register_image_format
 
 # Set it to 2280px because 1140px is the max width of Bootstrap's container.
 # Then we multiply that number by 2 so the image can render well on retina.
-register_image_format(Format('fullwidth_high_quality', 'Full width high quality', 'richtext-image full-width high-quality', 'width-2280'))
+register_image_format(
+  Format('fullwidth_high_quality', 'Full width high quality', 'richtext-image full-width high-quality', 'width-2280')
+)
