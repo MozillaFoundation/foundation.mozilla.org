c926809abd40379a4968148580e16c968449ae45
Mavis Ou <mmmavis@users.noreply.github.com>
2021-01-28 11:43:06 -0800

    add a new image option 'fullwidth_high_quality'

--- network-api/networkapi/wagtailpages/image_formats.py
+++ network-api/networkapi/wagtailpages/image_formats.py
@@ -1,8 +1,5 @@
 from wagtail.images.formats import Format, register_image_format, unregister_image_format
 
-# We can't override existing image format,
-# so we are unregistering it and then re-registering it with new values instead.
 # Set it to 2280px because 1140px is the max width of Bootstrap's container.
 # Then we multiply that number by 2 so the image can render well on retina.
-unregister_image_format('fullwidth')
-register_image_format(Format('fullwidth', 'Full width', 'richtext-image full-width', 'width-2280'))
+register_image_format(Format('fullwidth_high_quality', 'Full width high quality', 'richtext-image full-width high-quality', 'width-2280'))
