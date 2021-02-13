35d5d72fc836c6503fad97a678918f7d80a58111
Mavis Ou <mmmavis@users.noreply.github.com>
2021-02-12 16:40:10 -0800

    address Salesforce lang code issue

--- network-api/networkapi/campaign/views.py
+++ network-api/networkapi/campaign/views.py
@@ -12,6 +12,12 @@ import json
 from networkapi.wagtailpages.models import Petition, Signup
 
 
+def process_lang_code(lang):
+    # Salesforce expects "pt" instead of "pt-BR".
+    # See https://github.com/mozilla/foundation.mozilla.org/issues/5993
+    return lang if lang != 'pt-BR' else 'pt',
+
+
 class SQSProxy:
     """
     We use a proxy class to make sure that code that
@@ -107,7 +113,7 @@ def signup_submission(request, signup):
         "format": "html",
         "source_url": source,
         "newsletters": signup.newsletter,
-        "lang": rq.get('lang', 'en'),
+        "lang": process_lang_code(rq.get('lang', 'en')),
         "country": rq.get('country', ''),
         # Empty string instead of None due to Basket issues
         "first_name": rq.get('givenNames', ''),
@@ -150,7 +156,7 @@ def petition_submission(request, petition):
         "email": request.data['email'],
         "email_subscription": request.data['newsletterSignup'],
         "source_url": request.data['source'],
-        "lang": request.data['lang'],
+        "lang": process_lang_code(request.data['lang']),
     }
 
     if petition:
