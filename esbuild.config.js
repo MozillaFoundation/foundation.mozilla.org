aea752b48e2efc76c9daa7c41feb49084e6fecc2
Pomax <pomax@nihongoresources.com>
2021-02-23 08:56:52 -0800

    rebase

--- esbuild.config.js
+++ esbuild.config.js
@@ -8,11 +8,13 @@
  * "production" or "development" so that process.env.NODE_ENV in our
  * code gets interpreted correctly.
  */
+const { build } = require(`esbuild`);
+const path = require(`path`);
 
 const arg = process.argv.indexOf(`--node-env`);
 const mode =
   arg > 0 ? process.argv[arg + 1] : process.env.NODE_ENV || `development`;
-const forProd = mode === `production`;
+const inProduction = mode === `production`;
 
 const inDir = `./source/js/`;
 const outDir = `./network-api/networkapi/frontend/_js/`;
@@ -27,9 +29,9 @@ const sources = [
 
 const base = {
   bundle: true,
-  watch: !forProd,
-  sourcemap: !forProd,
-  minify: forProd,
+  watch: !inProduction,
+  sourcemap: !inProduction,
+  minify: inProduction,
   loader: {
     ".js": "jsx",
     ".jsx": "jsx",
@@ -40,14 +42,11 @@ const base = {
   inject: [`esbuild.react.shim.js`],
 };
 
-const path = require(`path`);
-const { build } = require(`esbuild`);
-
-sources.forEach(async (source) => {
+sources.map((source) => {
   const opts = Object.assign({}, base);
   opts.entryPoints = [`${inDir}${source}`];
   opts.outfile = `${outDir}${path
     .basename(source)
     .replace(`.js`, `.compiled.js`)}`;
-  build(opts).catch(() => process.exit(1));
+  return build(opts);
 });
