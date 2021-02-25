77275cea576f70741af864e141f6a85c543df686
Kalob Taulien <kalob.taulien@gmail.com>
2021-02-25 09:19:47 -0700

    Migration rearrangement

--- network-api/networkapi/wagtailpages/migrations/0034_buyersguidepage_dark_theme.py
+++ network-api/networkapi/wagtailpages/migrations/0034_buyersguidepage_dark_theme.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailpages', '0033_auto_20210202_0456'),
+        ('wagtailpages', '0033_dismiss_text_and_priv_policy_helptext'),
     ]
 
     operations = [
