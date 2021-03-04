b7ef0a2c8b0a8306d91ff768b9d6f0f2873ca1f1
Kalob Taulien <kalob.taulien@gmail.com>
2021-03-04 12:49:46 -0700

    Data migration fixes?

--- network-api/networkapi/wagtailpages/migrations/0043_buyersguideproductcategory.py
+++ network-api/networkapi/wagtailpages/migrations/0043_buyersguideproductcategory.py
@@ -4,6 +4,8 @@ import django
 from django.db import migrations, models
 import networkapi.buyersguide.utils
 
+from networkapi.wagtailpages.pagemodels.products import ProductPageCategory, BuyersGuideProductCategory
+
 
 def copy_buyersguide_product_categories(apps, schema_editor):
     OldBuyersGuideProductCategory = apps.get_model("buyersguide", "BuyersGuideProductCategory")
@@ -24,6 +26,25 @@ def copy_buyersguide_product_categories(apps, schema_editor):
 def remove_new_buyersguide_product_categories(apps, schema_editor):
     NewBuyersGuideProductCategory = apps.get_model("wagtailpages", "BuyersGuideProductCategory")
     NewBuyersGuideProductCategory.objects.all().delete()
+
+
+def find_and_copy_old_product_category_to_model(apps, schema_editor):
+    for product_page_category in ProductPageCategory.objects.all():
+        if hasattr(product_page_category, 'category'):
+            category = product_page_category.category
+            # Find the "new" model instance of the old category. Make sure
+            # there is only one returned, not a full QuerySet
+            new_category = BuyersGuideProductCategory.objects.filter(name=category.name).first()
+            # Assign the new temporary "category_new" field. This will be
+            # renamed later
+            product_page_category.category_new = new_category
+            product_page_category.save()
+
+
+def reverse_find_and_copy_old_product_category_to_model(apps, schema_editor):
+    for product_page_category in ProductPageCategory.objects.all():
+        product_page_category.category_new = None
+        product_page_category.save()
 
 
 class Migration(migrations.Migration):
@@ -59,4 +80,6 @@ class Migration(migrations.Migration):
             name='category_new',
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='wagtailpages.BuyersGuideProductCategory'),
         ),
+        # Find the "old" product category's "clone" (the new model) and add it to `category_new`
+        migrations.RunPython(find_and_copy_old_product_category_to_model, reverse_find_and_copy_old_product_category_to_model),
     ]
