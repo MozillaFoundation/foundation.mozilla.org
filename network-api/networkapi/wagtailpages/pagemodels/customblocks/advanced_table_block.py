f9d4d85e702c3ba2aad027222533a193d02771f1
Kalob Taulien <kalob.taulien@gmail.com>
2021-03-09 13:18:32 -0700

    Advanced table block (use with care)

---
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/advanced_table_block.py
@@ -0,0 +1,46 @@
+"""
+The "Advanced" table block uses StreamBlocks to create rows and cells inside of each row.
+Every cell can have rich text, and a column width (colspan), along with centered text.
+
+We are using StreamBlocks instead of ListBlocks to account for wagtail-localize's lack of ListBlock support.
+"""
+
+from wagtail.core import blocks
+
+
+class Cell(blocks.StructBlock):
+    centered_text = blocks.BooleanBlock(required=False)
+    column_width = blocks.IntegerBlock(
+        default=0,
+        help_text='Enter the number of extra cell columns you want to merge together. '
+                  'Merging a cell column will expand a cell to the right.'
+    )
+    content = blocks.RichTextBlock(features=['bold', 'italic', 'link', 'ul', 'ol'])
+
+
+class Row(blocks.StreamBlock):
+    cell = Cell()
+
+
+class Table(blocks.StreamBlock):
+    row = Row()
+
+
+class AdvancedTableBlock(blocks.StructBlock):
+    header = blocks.BooleanBlock(
+        required=False,
+        help_text='Display the first row as a header.',
+    )
+    column = blocks.BooleanBlock(
+        required=False,
+        help_text='Display the first column as a header.',
+    )
+    caption = blocks.CharBlock(
+        required=False,
+        help_text='A heading that identifies the overall topic of the table, and is useful for screen reader users',
+    )
+
+    table = Table()
+
+    class Meta:
+        template = 'wagtailpages/blocks/advanced_table_block.html'
