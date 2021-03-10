96c9d6b8e44b8e2c443e6b95ff4ccecea650117d
Pomax <pomax@nihongoresources.com>
2021-03-09 17:00:40 -0800

    better version

--- network-api/networkapi/wagtailpages/pagemodels/mixin/foundation_metadata.py
+++ network-api/networkapi/wagtailpages/pagemodels/mixin/foundation_metadata.py
@@ -2,12 +2,8 @@ from taggit.models import Tag
 from wagtailmetadata.models import MetadataPageMixin
 from wagtail.images.models import Image
 
-default_share_tag_name = 'social share image'
-
-try:
-    default_social_share_tag = Tag.objects.get(name=default_share_tag_name)
-except Tag.DoesNotExist:
-    default_social_share_tag = None
+default_social_share_tag = None
+default_social_share_image = None
 
 
 # Override the MetadataPageMixin to allow for a default
@@ -18,9 +14,11 @@ class FoundationMetadataPageMixin(MetadataPageMixin):
         # be the default image URL in social shares when no Image is specified at the Page level
         super().__init__(*args, **kwargs)
 
-        if default_social_share_tag:
-            self.social_share_tag = default_social_share_tag
-
+        # This will run once in the life-time of the server, when the first page instance
+        # that inherits this mixin gets instantiated. After that, this code will not kick
+        # in, and the default social share image is cached.
+        if default_social_share_tag is None:
+            self.set_default_social_share_tag_and_image()
 
     # Change this string to update the default description of all pages on the site
     default_description = 'Mozilla is a global non-profit dedicated to putting you in control of your online ' \
@@ -39,12 +37,30 @@ class FoundationMetadataPageMixin(MetadataPageMixin):
 
         return self.default_description
 
+    def set_default_social_share_tag_and_image(self):
+        # necessary because we're going to reassign them, rather than
+        # assign same-named vars in local scope:
+        global default_social_share_tag, default_social_share_image
+
+        # get the tag with name "social share image"
+        default_share_tag_name = 'social share image'
+        tag, create = Tag.objects.get_or_create(name=default_share_tag_name)
+        default_social_share_tag = tag
+
+        # then find an image in the CMS that uses that tag:
+        default_social_share_image = None
+        default_social_share_images = Image.objects.filter(tags=default_social_share_tag)
+        if default_social_share_images.count() > 0:
+            default_social_share_image = default_social_share_images.first()
+
     def get_meta_image(self):
+        # If we have a local social share image, use that
         if self.search_image:
             return self.search_image
 
+        # If note, walk up our ancestor chain and use the first social
+        # share image for an ancestor that explicitly has one set.
         parent = self.get_parent()
-
         while parent:
             if hasattr(parent, 'search_image') and parent.search_image:
                 return parent.search_image
@@ -52,10 +68,9 @@ class FoundationMetadataPageMixin(MetadataPageMixin):
                 return parent.homepage.search_image
             parent = parent.get_parent()
 
-        try:
-            return Image.objects.filter(tags=self.social_share_tag).first()
-        except Image.DoesNotExist:
-            return None
+        # We still haven't found a social share image, so, last resort: return
+        # whatever is the default social share image. Which could be `None`!
+        return default_social_share_image
 
     class Meta:
         abstract = True
