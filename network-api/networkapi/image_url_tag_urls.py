bfaaa166bb624f973330662dc21a7f6695675903
Kalob Taulien <4743971+KalobTaulien@users.noreply.github.com>
2021-03-18 11:32:58 -0600

    Update network-api/networkapi/image_url_tag_urls.py
    Co-authored-by: Pomax <pomax@nihongoresources.com>

--- network-api/networkapi/image_url_tag_urls.py
+++ network-api/networkapi/image_url_tag_urls.py
@@ -3,7 +3,7 @@ Dynamic Wagtail Images.
 https://docs.wagtail.io/en/stable/advanced_topics/images/image_serve_view.html
 
 Typically with Wagtail we use an {% image img.object fill-XXXxYYY %} template tag
-to create an image rendition. These renditions are blocking processes, which is typically
+to create an image rendition. Creating renditions is a blocking process, which is typically
 fine for a standard page with a normal amount of images. Every rendition costs one
 database query, and if the rendition doesn't exist, it needs to also create the image.
 On a page with 200 images, that's 200 extra database queries, plus rendition-creation
