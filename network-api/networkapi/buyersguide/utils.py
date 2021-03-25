4df0ca925f6864562ef548cceedc5663b63bba0d
Kalob Taulien <kalob.taulien@gmail.com>
2021-03-25 10:59:36 -0600

    Removing more unsused functions

--- network-api/networkapi/buyersguide/utils.py
+++ network-api/networkapi/buyersguide/utils.py
@@ -11,22 +11,6 @@ def get_category_og_image_upload_path(instance, filename):
     )
 
 
-def tri_to_quad(input):
-    if input is True:
-        return 'Yes'
-    if input is False:
-        return 'No'
-    return 'U'
-
-
-def quad_to_tri(input):
-    if input == 'Yes':
-        return True
-    if input == 'No':
-        return False
-    return None
-
-
 class AlterModelBases(ModelOperation):
     """
     See https://stackoverflow.com/a/61723620/740553
