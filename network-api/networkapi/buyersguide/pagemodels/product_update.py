57ecc623a5720dc951986d41eb85a2aaf499883d
Kalob Taulien <kalob.taulien@gmail.com>
2021-04-05 11:32:15 -0600

    Re-register snippet for testing in local branch. Will be removed in part 2 of this work.

--- network-api/networkapi/buyersguide/pagemodels/product_update.py
+++ network-api/networkapi/buyersguide/pagemodels/product_update.py
@@ -6,6 +6,7 @@ from wagtail.snippets.models import register_snippet
 from wagtail.search import index
 
 
+@register_snippet
 class Update(index.Indexed, models.Model):
     # TODO: Delete this model.
     source = models.URLField(
