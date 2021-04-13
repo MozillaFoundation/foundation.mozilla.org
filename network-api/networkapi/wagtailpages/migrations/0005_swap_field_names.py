4ffb0593d9a5dff79858cca1df68853480e09fc2
Kalob Taulien <kalob.taulien@gmail.com>
2021-04-13 11:45:50 -0600

    Migration conflict fixes

--- network-api/networkapi/wagtailpages/migrations/0005_swap_field_names.py
+++ network-api/networkapi/wagtailpages/migrations/0005_swap_field_names.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailpages', '0002_auto_20210405_1604'),
+        ('wagtailpages', '0004_auto_20210405_1604'),
     ]
 
     operations = [
