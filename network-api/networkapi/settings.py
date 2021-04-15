61e0977818741124757615f3183bdc0e2a839e91
Daniel Miranda <daniel@mozillafoundation.org>
2021-04-15 09:57:04 -0700

    added option to subscribe users using basket

--- network-api/networkapi/settings.py
+++ network-api/networkapi/settings.py
@@ -55,6 +55,9 @@ env = environ.Env(
     HEROKU_BRANCH=(str, ''),
     HEROKU_PR_NUMBER=(str, ''),
     HEROKU_RELEASE_VERSION=(str, None),
+    # MOFO_NEWSLETTER_SUBSCRIBE_METHOD should be 'BASKET' or 'SQS'.
+    # We're using SQS by default until we move to sending newsletter data directly to Basket.
+    MOFO_NEWSLETTER_SUBSCRIBE_METHOD=(str, 'BASKET'),
     MOZFEST_DOMAIN_REDIRECT_ENABLED=(bool, False),
     NETWORK_SITE_URL=(str, ''),
     PETITION_TEST_CAMPAIGN_ID=(str, ''),
@@ -115,6 +118,12 @@ REVIEW_APP = env('REVIEW_APP', default=False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = root()
+
+# Basket client configuration
+BASKET_URL = env('BASKET_URL')
+BASKET_API_KEY = env('BASKET_API_KEY')
+MOFO_NEWSLETTER_SUBSCRIBE_METHOD = env('MOFO_NEWSLETTER_SUBSCRIBE_METHOD')
+
 
 APP_DIR = app()
 
