9cba0ae0e658a0f97ee1a50f65c78228074978c1
Pomax <pomax@nihongoresources.com>
2021-05-04 14:43:03 -0700

    uplift cypress to 6.9.1

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,15 +8,10 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-let percyHealthCheck = require("@percy/cypress/task");
-
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  on("task", percyHealthCheck);
 
   // console logging is mighty useful.
   on("task", {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,5 +23,3 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
-import '@percy/cypress';
