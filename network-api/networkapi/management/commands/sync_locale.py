0cebf7f50f64a26dda23d6957ef2076f2127eae7
Kalob Taulien <kalob.taulien@gmail.com>
2021-06-15 13:14:31 -0600

    Localization synching and creating management commands

---
+++ network-api/networkapi/management/commands/sync_locale.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from wagtail.core.models import Locale
+from wagtail_localize.models import LocaleSynchronization
+
+
+class Command(BaseCommand):
+    help = 'Sync pages with original English pages'
+
+    def handle(self, *args, **options):
+        print("Select a language code to sync with English. ie: de")
+
+        for language_code, name in settings.WAGTAIL_CONTENT_LANGUAGES:
+            if language_code != 'en':
+                print(f"{language_code} ({name})")
+
+        language_code = input("Language code: ").lower()
+
+        # Confirm the language code is in the WAGTAIL_CONTENT_LANGUAGES
+        language_codes_dict = dict(settings.WAGTAIL_CONTENT_LANGUAGES)
+        if language_code not in language_codes_dict:
+            print("Invalid language code")
+            return
+
+        print("Getting both locales...")
+        english_locale, _ = Locale.objects.get_or_create(language_code='en')
+        locale, _ = Locale.objects.get_or_create(language_code=language_code)
+
+        print("Getting LocaleSynchronization object")
+        sync, created = LocaleSynchronization.objects.get_or_create(
+            locale=locale,
+            sync_from=english_locale,
+        )
+        if created:
+            print("\tNew LocaleSynchronization object created")
+
+        print(f"Syncing {locale} from {english_locale}")
+        sync.sync_trees()
