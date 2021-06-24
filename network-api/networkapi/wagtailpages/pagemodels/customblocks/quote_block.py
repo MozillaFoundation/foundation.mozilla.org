f92b8175ce710aa06d6e578791a0a35483853c79
Kalob Taulien <kalob.taulien@gmail.com>
2021-06-24 14:24:14 -0600

    WIP stuff

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/quote_block.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/quote_block.py
@@ -3,16 +3,10 @@ from wagtail.core import blocks
 
 class QuoteBlock(blocks.StructBlock):
 
-    # The goal is to be able to cycle through multiple quotes,
-    # so let's accept multiple quotes to start even if we only show one for now.
-    # This way we don't have to migrate the model again later
-
-    quotes = blocks.ListBlock(blocks.StructBlock([
-        ('quote', blocks.CharBlock()),
-        ('attribution', blocks.CharBlock(required=False))
-    ]))
+    quote = blocks.CharBlock()
+    attribution = blocks.CharBlock(required=False)
 
     class Meta:
         template = 'wagtailpages/blocks/quote_block.html'
         icon = 'openquote'
-        help_text = 'Multiple quotes can be entered, but for now we are only using the first'
+        help_text = 'A single quote block'
