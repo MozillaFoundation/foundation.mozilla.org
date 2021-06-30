357d269e795595d0dbadba34cbaf466c9cc9b5f7
Daniel Miranda <daniel@mozillafoundation.org>
2021-06-30 10:27:56 -0700

    full-width images for blog plage

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/annotated_image_block.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/annotated_image_block.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+from django.forms.utils import ErrorList
 from wagtail.core import blocks
 from .image_block import ImageBlock
 
@@ -13,10 +15,31 @@ class AnnotatedImageBlock(ImageBlock):
     wide_image = blocks.BooleanBlock(
         required=False,
         default=False,
-        help_text='Would you like to use a wider image on desktop?'
+        help_text="Would you like to use a wider image on desktop?",
+    )
+    full_width_image = blocks.BooleanBlock(
+        required=False,
+        default=False,
+        help_text="Would you like to use a full width image?",
     )
 
     class Meta:
-        icon = 'image'
-        template = 'wagtailpages/blocks/annotated_image_block.html'
-        help_text = 'Design Guideline: Please crop images to a 16:6 aspect ratio when possible.'
+        icon = "image"
+        template = "wagtailpages/blocks/annotated_image_block.html"
+        help_text = (
+            "Design Guideline: Please crop images to a 16:6 aspect ratio when possible."
+        )
+
+    def clean(self, value):
+        errors = {}
+
+        if value.get("wide_image") and value.get("full_width_image") is True:
+            errors["wide_image"] = ErrorList(["Please select only one width option."])
+            errors["full_width_image"] = ErrorList(
+                ["Please select only one width option."]
+            )
+
+        if errors:
+            raise ValidationError("Validation error in StructBlock", params=errors)
+
+        return super().clean(value)
