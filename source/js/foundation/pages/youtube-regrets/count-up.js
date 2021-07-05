70f023c82f7282cd482c5f737a5e7d0f36641870
Helen C <helen.chapman@torchbox.com>
2021-07-05 19:48:13 +0100

    Add countup to other stats and rename some ids to be a bit more unique for the stat they represent

--- source/js/foundation/pages/youtube-regrets/count-up.js
+++ source/js/foundation/pages/youtube-regrets/count-up.js
@@ -5,16 +5,20 @@ export const initYoutubeRegretsResearchCountUp = () => {
     const reportCountUp = new CountUp('reports-count-up', 5234);
     const volunteersCountUp = new CountUp('volunteers-count-up', 1662);
     const countriesCountUp = new CountUp('countries-count-up', 91);
-    const viewsCountUp = new CountUp('views-count-up', 160000000);
-    const viewsPerVideoCountUp = new CountUp('views-per-video-count-up', 170, {prefix: '~', suffix: 'K'});
+    const reportedViewsPerDayCountUp = new CountUp('reported-views-per-day-count-up', 5794, { prefix: '~' })
+    const otherViewsPerDayCountUp = new CountUp('other-views-per-day-count-up', 3312, { prefix: '~' })
+    const views160DaysCountUp = new CountUp('views-160days-count-up', 160000000);
+    const viewsPerVideoCountUp = new CountUp('views-per-video-count-up', 170, { prefix: '~', suffix: 'K' });
 
     let observer = new IntersectionObserver(
       (entries, observer) => {
         entries.forEach(entry => {
           if (entry.isIntersecting) {
             //todo: count ups inside the accordion need further work to reliably run when the accordion is open and the stat is visible - currently working in chrome but not reliably cross-browser
-            viewsCountUp.start();
+            views160DaysCountUp.start();
             viewsPerVideoCountUp.start();
+            reportedViewsPerDayCountUp.start();
+            otherViewsPerDayCountUp.start();
             reportCountUp.start();
 
             setTimeout(() => {
