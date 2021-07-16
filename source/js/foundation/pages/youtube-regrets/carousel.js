42e8788a085d10d46642ccf9a3b7d8fa7c39c30d
Steve <steven_ts@hotmail.com>
2021-07-16 11:53:01 -0700

    Fix arrows for carousel

--- source/js/foundation/pages/youtube-regrets/carousel.js
+++ source/js/foundation/pages/youtube-regrets/carousel.js
@@ -1,71 +1,49 @@
 import Swiper from 'swiper/bundle';
 
 class Carousel {
-  static selector() {
-    return "[data-carousel]";
+  // Note: ensure your parent element has a unique id and data-carousel attr
+  constructor() {
+    this.slideshows = document.querySelectorAll('[data-carousel]');
+    this.createSlideshows();
   }
 
-  constructor(node) {
-    this.node = node;
-    this.createSlideshow();
-    this.bindEvents();
-    console.log(this);
-  }
+  createSlideshows() {
+    let swipers = [];
+    this.slideshows.forEach((slideshow, index) => {
+      let slideshowId = slideshow.id;
+      swipers[index] = new Swiper(`#${slideshowId}`, {
+        effect: "fade",
+        fadeEffect: {crossFade: true},
+        autoHeight: true,
+        centeredSlides: true,
+        initialSlide: 0,
+        loop: false,
+        slidesPerView: "auto",
+        speed: 400,
+        keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+        },
 
-  bindEvents() {
-  }
+        // Navigation arrows
+        navigation: {
+          nextEl: ".carousel__button--next",
+          prevEl: ".carousel__button--prev",
+        },
 
-  createSlideshow() {
-    this.slideshow = new Swiper(this.node, {
-      effect: "fade",
-      fadeEffect: {crossFade: true},
-      autoHeight: true,
-      centeredSlides: true,
-      initialSlide: 0,
-      loop: false,
-      slidesPerView: "auto",
-      hideOnClick: true,
-      speed: 400,
-      keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: ".carousel__button--next",
-        prevEl: ".carousel__button--prev",
-      },
-
-      pagination: {
-        el: ".carousel__count-inner",
-        type: "fraction",
-      },
+        pagination: {
+          el: ".carousel__count-inner",
+          type: "fraction",
+        },
+      });
     });
   }
-
-  // updateAriaRoles() {
-  //   for (const slide of this.node.querySelectorAll(
-  //     ".glide__slide:not(.glide__slide--active)"
-  //   )) {
-  //     const inactiveSlideAnchors = slide.querySelectorAll("a");
-  //     slide.setAttribute("aria-hidden", "true");
-  //     inactiveSlideAnchors.forEach(function inactiveAnchor(el) {
-  //       el.setAttribute("tabindex", -1);
-  //     });
-  //   }
-  //   const activeSlide = this.node.querySelector(".glide__slide--active");
-  //   const activeSlideAnchors = activeSlide.querySelectorAll("a");
-  //   activeSlide.removeAttribute("aria-hidden");
-  //   activeSlideAnchors.forEach(function activeAnchor(el) {
-  //     el.removeAttribute("tabindex");
-  //   });
-  // }
 }
 
 export const initYoutubeRegretsCarousel = () => {
-  const carousels = [...document.querySelectorAll("#yt-regrets-carousel")];
-  carousels.map((carousel) => new Carousel(carousel));
+  // const carousels = [...document.querySelectorAll("[data-carousel]")];
+  // carousels.map((carousel) => new Carousel(carousel));
+  new Carousel();
 };
 
 export default Carousel;
