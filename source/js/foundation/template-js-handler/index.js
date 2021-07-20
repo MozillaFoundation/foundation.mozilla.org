d12a4e829aa3042b43227d5d9b6d4e18db9192da
Daniel Miranda <manieldiranda@gmail.com>
2021-07-20 12:06:16 -0700

    Alphabetized index.js

--- source/js/foundation/template-js-handler/index.js
+++ source/js/foundation/template-js-handler/index.js
@@ -3,11 +3,11 @@ import stickyShareButtonGroupHandler from "./window/sticky-share-button-group";
 import articleFootLinkHandler from "./article-footnote-link.js";
 import homepageHandler from "./homepage";
 import loadMoreEntriesHandler from "./load-more-entries.js";
+import loopingVideoHandler from "./looping-video-handler.js";
 import participatePageDonateHandler from "./participate-page-donate.js";
 import publicationSummaryBar from "./publication-summary-bar.js";
 import pulseProfileCardHandler from "./pulse-profile-card.js";
 import pulseProfileListFilterHandler from "./pulse-profile-list-filter.js";
-import loopingVideoHandler from "./looping-video-handler.js";
 
 /**
  * Bind global event handlers
@@ -25,9 +25,9 @@ export const bindEventHandlers = () => {
   articleFootLinkHandler();
   homepageHandler();
   loadMoreEntriesHandler();
+  loopingVideoHandler();
   participatePageDonateHandler();
   publicationSummaryBar();
   pulseProfileCardHandler();
   pulseProfileListFilterHandler();
-  loopingVideoHandler();
 };
