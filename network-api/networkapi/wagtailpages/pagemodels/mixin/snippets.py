05e41b53bb4d62a73a5937285f4647c3bbb4e144
Kalob Taulien <kalob.taulien@gmail.com>
2021-08-11 10:39:05 -0600

    That's better

--- network-api/networkapi/wagtailpages/pagemodels/mixin/snippets.py
+++ network-api/networkapi/wagtailpages/pagemodels/mixin/snippets.py
@@ -3,12 +3,17 @@ from wagtail.core.models import Locale
 
 
 class LocalizedSnippet():
+
+    DEFAULT_LOCALE = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if self.DEFAULT_LOCALE is None:
+            self.DEFAULT_LOCALE = Locale.objects.get(language_code=settings.LANGUAGE_CODE)
 
     @property
     def original(self):
-        DEFAULT_LOCALE = Locale.objects.get(language_code=settings.LANGUAGE_CODE)
+        DEFAULT_LOCALE = Locale.objects.get(language_code=self.DEFAULT_LOCALE)
 
         try:
             return self.get_translation(DEFAULT_LOCALE)
