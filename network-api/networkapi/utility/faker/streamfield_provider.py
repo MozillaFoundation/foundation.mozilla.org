d0bfd36ef4a4c256856e7e350705fb25975e2919
Simon Fessehaye <fessehay@ualberta.ca>
2021-09-13 02:12:07 -0700

    Add streamfields and icons
    Added streamfields and icons

--- network-api/networkapi/utility/faker/streamfield_provider.py
+++ network-api/networkapi/utility/faker/streamfield_provider.py
@@ -179,7 +179,6 @@ def generate_linkbutton_field():
 
 def generate_text_field():
     value = fake.paragraph(nb_sentences=1, variable_nb_sentences=False)
-
     return generate_field('text', value)
 
 
@@ -216,6 +215,62 @@ def generate_dear_internet_intro_text_field():
     text = f'<p>{fake.paragraph(nb_sentences=3, variable_nb_sentences=True)}</p>'
 
     return generate_field('intro_text', text)
+
+
+def generate_image_grid_field():
+    imgs = []
+
+    for n in range(4):
+        imgs.append({
+                'image': choice(Image.objects.all()).id,
+                'caption': fake.paragraph(nb_sentences=1, variable_nb_sentences=False)
+            }
+        )
+
+    
+
+    return generate_field('image_grid', {
+        'grid_items': imgs
+    })
+
+
+def generate_card_grid_field():
+    cards = []
+
+    for n in range(6):
+        cards.append({
+                'image': choice(Image.objects.all()).id,
+                'title': fake.paragraph(nb_sentences=1, variable_nb_sentences=False),
+                'body': fake.paragraph(nb_sentences=10, variable_nb_sentences=True),
+                'link_label': ' '.join(fake.words(nb=3)),
+                'link_url': fake.url(schemes=['https'])
+            }
+        )
+
+
+    return generate_field('card_grid', {
+        'cards': cards
+    })
+
+
+def generate_pulse_listing_field():
+    return generate_field('pulse_listing', {
+        'only_featured_entries': True,
+        'help': 'all',
+        'issues': 'all',
+        'size': 6,
+        'search_terms': '',
+        'newest_first': True,
+        'direct_link': False
+    })
+
+
+def generate_profile_listing_field():
+    return generate_field('profile_listing', {'max_number_of_results':6})
+
+
+def generate_recent_blog_entries_field():
+    return generate_field('recent_blog_entries', {})
 
 
 def generate_dear_internet_letter_field():
@@ -284,6 +339,11 @@ class StreamfieldProvider(BaseProvider):
             'full_width_image': generate_full_width_image_field,
             'intro_text': generate_dear_internet_intro_text_field,
             'letter': generate_dear_internet_letter_field,
+            'card_grid': generate_card_grid_field,
+            'image_grid': generate_image_grid_field,
+            'pulse_listing': generate_pulse_listing_field,
+            'profile_listing': generate_profile_listing_field,
+            'recent_blog_entries': generate_recent_blog_entries_field
         }
 
         streamfield_data = []
--- network-api/networkapi/wagtailpages/factory/styleguide.py
+++ network-api/networkapi/wagtailpages/factory/styleguide.py
@@ -6,13 +6,36 @@ from networkapi.utility.faker.helpers import (
     get_homepage
 )
 
+from factory import (
+    Faker
+)
+
+styleguide_streamfield_fields = [
+    'paragraph',
+    'image',
+    'image_text',
+    'image_text_mini',
+    'image_grid',
+    'video',
+    'linkbutton',
+    'spacer',
+    'quote',
+    'double_image',
+    'text',
+    'full_width_image',
+    'basic_image',
+    'card_grid',
+    'pulse_listing',
+    'profile_listing',
+    'recent_blog_entries'
+]
 
 class StyleguideFactory(PageFactory):
     class Meta:
         model = Styleguide
 
     title = 'Style-guide'
-
+    body = Faker('streamfield', fields=styleguide_streamfield_fields)
 
 def generate(seed):
     home_page = get_homepage()
@@ -27,3 +50,4 @@ def generate(seed):
             parent=home_page,
             show_in_menus=True
         )
+    
