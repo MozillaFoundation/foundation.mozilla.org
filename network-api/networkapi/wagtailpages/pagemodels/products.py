3363663edb55405fddf47c5c65c0839670ee2dcd
Pomax <pomax@nihongoresources.com>
2021-09-20 16:10:36 -0700

    hide the softwareproductpage child page type

--- network-api/networkapi/wagtailpages/pagemodels/products.py
+++ network-api/networkapi/wagtailpages/pagemodels/products.py
@@ -1350,7 +1350,10 @@ class BuyersGuidePage(RoutablePageMixin, FoundationMetadataPageMixin, Page):
     """
 
     template = 'buyersguide/home.html'
-    subpage_types = [SoftwareProductPage, GeneralProductPage]
+    subpage_types = [
+        # SoftwareProductPage,
+        GeneralProductPage,
+    ]
 
     cutoff_date = models.DateField(
         'Product listing cutoff date',
