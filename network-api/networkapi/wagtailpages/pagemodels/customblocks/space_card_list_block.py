dd279aa2f873edbac21a2cae72c7ec82c5c30bb9
boggs <hello@boggs.xyz>
2021-09-22 20:48:47 +0800

    Add link property

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/space_card_list_block.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/space_card_list_block.py
@@ -5,6 +5,18 @@ from wagtail.core.blocks.struct_block import StructBlockValidationError
 from django.forms.utils import ErrorList
 
 
+class SpaceCardBlockStructValue(blocks.StructValue):
+    @property
+    def link(self):
+        link_url = self.get('link_url')
+        link_page = self.get('link_page')
+
+        if link_url:
+            return link_url
+        elif link_page:
+            return link_page.url
+
+
 class SpaceCardBlock(blocks.StructBlock):
     image = ImageChooserBlock()
 
@@ -29,17 +41,20 @@ class SpaceCardBlock(blocks.StructBlock):
     def clean(self, value):
         errors = {}
 
-        link_page = value.get("link_page")
-        link_url = value.get("link_url")
+        link_page = value.get('link_page')
+        link_url = value.get('link_url')
 
         if (link_page and link_url) or (not link_page and not link_url):
-            errors["link_page"] = ErrorList(["Please specify either a link page or a link URL."])
-            errors["link_url"] = ErrorList(["Please specify either a link page or a link URL."])
+            errors['link_page'] = ErrorList(['Please specify either a link page or a link URL.'])
+            errors['link_url'] = ErrorList(['Please specify either a link page or a link URL.'])
 
         if errors:
             raise StructBlockValidationError(errors)
 
         return super().clean(value)
+
+    class Meta:
+        value_class = SpaceCardBlockStructValue
 
 
 class SpaceCardListBlock(blocks.StructBlock):
