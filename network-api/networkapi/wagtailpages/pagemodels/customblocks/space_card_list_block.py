9c206c4177dbc686a8af2132915a7c6a85f1349b
boggs <hello@boggs.xyz>
2021-09-24 19:01:17 +0800

    Use StreamBlock for page field

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/space_card_list_block.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/space_card_list_block.py
@@ -7,14 +7,15 @@ from django.forms.utils import ErrorList
 
 class SpaceCardBlockStructValue(blocks.StructValue):
     @property
-    def link(self):
-        link_url = self.get('link_url')
-        link_page = self.get('link_page')
+    def link_url(self):
+        block = self.get('link')[0]
 
-        if link_url:
-            return link_url
-        elif link_page:
-            return link_page.url
+        if block.block_type == 'internal':
+            return block.value.url
+        elif block.block_type == 'external':
+            return block.value
+
+        return ''
 
 
 class SpaceCardBlock(blocks.StructBlock):
@@ -28,30 +29,13 @@ class SpaceCardBlock(blocks.StructBlock):
         help_text='Body text of the card.'
     )
 
-    link_page = blocks.PageChooserBlock(
-        required=False,
-        help_text='Page that this card should link out to.',
+    link = blocks.StreamBlock(
+        [
+            ('internal', blocks.PageChooserBlock(help_text='Page that this card should link out to.')),
+            ('external', blocks.URLBlock(help_text='URL that this card should link out to.')),
+        ],
+        max_num=1,
     )
-
-    link_url = blocks.URLBlock(
-        required=False,
-        help_text='URL that this card should link out to.',
-    )
-
-    def clean(self, value):
-        errors = {}
-
-        link_page = value.get('link_page')
-        link_url = value.get('link_url')
-
-        if (link_page and link_url) or (not link_page and not link_url):
-            errors['link_page'] = ErrorList(['Please specify either a link page or a link URL.'])
-            errors['link_url'] = ErrorList(['Please specify either a link page or a link URL.'])
-
-        if errors:
-            raise StructBlockValidationError(errors)
-
-        return super().clean(value)
 
     class Meta:
         value_class = SpaceCardBlockStructValue
