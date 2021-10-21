6e98f0741571e9b0726402020ed0b8eb0cd79896
boggs ⚓ <hello@boggs.xyz>
2021-10-22 07:17:30 +0800

    [FE/BE] - Mozfest Hero updates (#7585)
    * Create integration branch for frontend and backend
    
    * Bump wagtailmedia to 0.8.0
    
    * Add EmbeddedVideoBlock
    
    * Add WagtailVideoChooserBlock
    
    * Add BannerCarouselSlideBlock
    
    * Set new banner_video_type 'featured' instead of 'hardcoded'
    
    * Add banner_carousel and banner_video to MozfestHomepage model
    
    * Add new banner_video_type 'featured' in HTML templates
    
    * Add faker for MozfestHomepage carousel
    
    * Add faker for MozfestHomepage banner video
    
    * Make migrations
    
    * Fix spacing in HTML file
    
    Co-authored-by: boggs ⚓ <hello@boggs.xyz>
    
    * [7533] - FE Mozfest homepage hero (#7589)
    
    * Current events slider styles
    
    * Install swiper and style event cards
    
    * video play on hover styling
    
    * Minor spacing
    
    * Mozfest homepage banner start
    
    * Homepage header, mobile styling and slideshow styling
    
    * Homepage banner cleanup and organization
    
    * Link mobile and background image sliders
    
    * Add loops for test hero content
    
    * Rebase on to BE branch
    
    * Thumbnail fixes for hosted cms video
    
    * carousel js cleanup
    
    * Integrate old video functionality back into branch
    
    * Make old functionality work
    
    * Change flag to featured again
    
    * Revert name to homepage banner handler
    
    * Remove carousel.scss
    
    * Add spacing back to tailwind config
    
    * Remove extra space from main.scss
    
    * Add animation for sliding progress bar
    
    * Make carousel clickable
    
    * Only show carousel if there are carousels
    
    * Remove uneeded carousel check
    
    * Update migrations
    
    * Fix template
    
    * Add thumbnail
    
    * Fix video thumbnail overlay fade out for external videos
    
    * Remove unused template blocks, simplify external_video
    
    * Update fake data
    
    * CI fixes
    
    * Use brand colors instead of rgb values
    
    * Add stylelint-disable
    
    * Add wagtailcore_tags
    
    * Fix video conditionals on front end, remove minimum requirement on carousel
    
    * Fix issue on FE template when there is no video thumbnail
    
    * Update migrations
    
    * Add help texts for banner_carousel and banner_video
    
    * Scroll to video with offset through JS
    
    * Still autoplay after interacting with banner carousel
    
    * Add blue hover to CTA button
    
    * Change selector to string instead of function for hero carousel
    
    * Fix eslint issues
    
    * Fix Percy issues
    
    * Update comment on `banner_video_type` https://github.com/mozilla/foundation.mozilla.org/pull/7585#discussion_r732191949
    
    * Remove fallback text for <video> https://github.com/mozilla/foundation.mozilla.org/pull/7585#discussion_r732193989
    
    * Run npm run optimize:svg on play-circle-grey https://github.com/mozilla/foundation.mozilla.org/pull/7585#discussion_r732195807
    
    * Change "PLAY VIDEO" and "PAUSE VIDEO" to normal case, use text-transform to transform them to uppercase
    
    * Use template literals for query selectors, use forEach instead of for
    
    Co-authored-by: Sharmaine Lim <amanda.lim@torchbox.com>
    Co-authored-by: Sharmaine Lim <SharmaineLim@users.noreply.github.com>
    Co-authored-by: Steve Stein <steven_ts@hotmail.com>
    Co-authored-by: Daniel Miranda <manieldiranda@gmail.com>

---
+++ source/js/components/mozfest-hero-carousel/mozfest-hero-carousel.js
@@ -0,0 +1,86 @@
+import Swiper, {
+  A11y,
+  Autoplay,
+  Pagination,
+  Navigation,
+  Keyboard,
+  EffectFade,
+} from "swiper";
+
+Swiper.use([A11y, Autoplay, Pagination, Navigation, Keyboard, EffectFade]);
+
+class MozfestHeroCarousel {
+  constructor(node) {
+    this.node = node;
+    this.delay = 10000;
+
+    // Initialize Carousels
+    this.initBackgroundImageCarousel();
+    this.initMobileTexCarousel();
+
+    // Link transitions
+    this.linkSlideChanges();
+  }
+
+  initBackgroundImageCarousel() {
+    this.backGroundImagesSwiper = new Swiper(".swiper-hero-carousel", {
+      loop: true,
+      watchSlidesProgress: true,
+      autoplay: {
+        delay: this.delay,
+        disableOnInteraction: false,
+      },
+      paginationClickable: false,
+      keyboard: {
+        enabled: true,
+      },
+      pagination: {
+        el: ".swiper-hero-pagination",
+        clickable: true,
+      },
+      effect: "fade",
+      fadeEffect: {
+        crossFade: false,
+      },
+    });
+  }
+
+  initMobileTexCarousel() {
+    this.heroTextMobile = new Swiper(".swiper-hero-mobile", {
+      allowTouchMove: true,
+      loop: true,
+      keyboard: {
+        enabled: true,
+      },
+      autoplay: {
+        delay: this.delay,
+        disableOnInteraction: false,
+      },
+      slidesPerView: 1,
+      centeredSlides: true,
+      spaceBetween: 30,
+    });
+  }
+
+  // Ensure that the background image slider stays in sync with the mobile one
+  linkSlideChanges() {
+    this.heroTextMobile.on("slideChange", (event) => {
+      if (event.swipeDirection === "next") {
+        this.backGroundImagesSwiper.slideNext();
+      }
+      if (event.swipeDirection === "prev") {
+        this.backGroundImagesSwiper.slidePrev();
+      }
+    });
+  }
+}
+
+const MozfestHeroCarousels = {
+  init: function () {
+    document
+      .querySelectorAll(`[data-mozfest-hero-carousel]`)
+      .forEach((e) => new MozfestHeroCarousel(e));
+  },
+};
+
+export default MozfestHeroCarousels;
