a5c9e8d6b582bb4f4abaaaa564f86d26a22de7a2
Pomax <pomax@nihongoresources.com>
2021-11-03 08:18:15 -0700

    move 'manage block_inventory' to a cron job

--- release-steps.sh
+++ release-steps.sh
@@ -5,11 +5,5 @@ cd network-api
 # Django Migrations
 python ./manage.py migrate --no-input
 
-# Wagtail block inventory
-python ./manage.py block_inventory
-
-# Wagtail translations
-# TODO: May need to add in auto syncing using wagtail-localize in the future.
-
 # Clear cache for BuyersGuide
 python ./manage.py clear_cache
