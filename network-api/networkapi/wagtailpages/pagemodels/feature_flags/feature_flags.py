87a1a4495fc11f47db610fc916e22a51b4f36f19
Pomax <pomax@nihongoresources.com>
2021-11-22 11:43:48 -0800

    flake

--- network-api/networkapi/wagtailpages/pagemodels/feature_flags/feature_flags.py
+++ network-api/networkapi/wagtailpages/pagemodels/feature_flags/feature_flags.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel, TabbedInterface, ObjectList
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 
+
 @register_setting(icon='tick')
 class FeatureFlags(BaseSetting):
 
