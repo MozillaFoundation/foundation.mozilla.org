a65544deb26a44bcffcd641313a56486c104c0b9
Pomax <pomax@nihongoresources.com>
2021-12-13 14:19:43 -0800

    baseline PNI tests for search

--- tests/integration.spec.js
+++ tests/integration.spec.js
@@ -32,3 +32,42 @@ test(`Foundation homepage`, async ({ page }, testInfo) => {
   expect(await countryPicker.isVisible()).toBe(true);
   expect(await languagePicker.isVisible()).toBe(true);
 });
+
+test(`PNI search`, async ({ page }, testInfo) => {
+  page.on(`console`, console.log);
+  await page.goto(`http://localhost:8000/en/privacynotincluded`);
+  await page.locator(`body.react-loaded`);
+  await waitForImagesToLoad(page);
+
+  let products, activeCategory;
+
+  products = page.locator(`.product-box:visible`);
+  await expect(products).toHaveCount(35);
+
+  const searchBar = page.locator(`#product-filter-search-input`);
+  await searchBar.type("percy");
+
+  products = page.locator(`.product-box:visible`);
+  await expect(products).toHaveCount(2);
+
+  await page.click(`label[for="product-filter-search-input"]`);
+  products = page.locator(`.product-box:visible`);
+  await expect(products).toHaveCount(35);
+
+  await page.click(`#multipage-nav a.multipage-link[data-name="Health & Exercise"]`);
+  activeCategory = page.locator(`#multipage-nav a.multipage-link.active`);
+  await expect(activeCategory).toHaveAttribute(`data-name`, `Health & Exercise`);
+
+  products = page.locator(`.product-box:visible`);
+  await expect(products).toHaveCount(9);
+
+  await searchBar.type("percy");
+  products = page.locator(`.product-box:visible`);
+  await expect(products).toHaveCount(2);
+  activeCategory = page.locator(`#multipage-nav a.multipage-link.active`);
+  await expect(activeCategory).toHaveAttribute(`data-name`, `None`);
+
+  await page.click(`label[for="product-filter-search-input"]`);
+  activeCategory = page.locator(`#multipage-nav a.multipage-link.active`);
+  await expect(activeCategory).toHaveAttribute(`data-name`, `Health & Exercise`);
+});
