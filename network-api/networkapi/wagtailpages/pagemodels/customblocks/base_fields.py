9d6dc9faefdc2d8725e09acb888fb1e49b58c2ff
Daniel Miranda <manieldiranda@gmail.com>
2021-12-13 13:45:28 -0800

    list of basic rich text field options (#7962)
    * working except for on article and other customblocks
    
    * [7962] fix circular import
    
    * .
    
    * saving progress
    
    * updated imports for rich text options
    
    * updated migrations and added try-except
    
    * updated fields to use base_rich_text_options and updated migrations
    
    * updated migrations
    
    * added new full_content list
    
    * updated migrations
    
    * updated migrations, added comment
    
    * Update network-api/networkapi/wagtailpages/pagemodels/customblocks/recent_blog_entries.py
    
    Co-authored-by: Daniel Miranda <daniel@mozillafoundation.org>
    Co-authored-by: Rich <rich.brennan@torchbox.com>
    Co-authored-by: Pomax <pomax@nihongoresources.com>

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/base_fields.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/base_fields.py
@@ -1,5 +1,6 @@
-from . import customblocks
+from .. import customblocks
 from wagtail.core import blocks
+from .full_content_rich_text_options import full_content_rich_text_options
 
 """
 We'll need to figure out which components are truly "base" and
@@ -9,12 +10,7 @@ page types.
 
 base_fields = [
     ('paragraph', blocks.RichTextBlock(
-        features=[
-            'bold', 'italic', 'large',
-            'h2', 'h3', 'h4', 'h5',
-            'ol', 'ul',
-            'link', 'hr',
-        ],
+        features=full_content_rich_text_options + ['large'],
         template='wagtailpages/blocks/rich_text_block.html',
     )),
     ('card_grid', customblocks.CardGridBlock()),
