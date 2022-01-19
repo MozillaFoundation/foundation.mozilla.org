032692080c1c73b53e234bce75add103f751e9dc
Tibor Leupold <tibor.leupold@torchbox.com>
2022-01-19 14:19:25 -0800

    Access page.locale directly instead of getattr

--- network-api/networkapi/wagtailpages/utils.py
+++ network-api/networkapi/wagtailpages/utils.py
@@ -172,7 +172,7 @@ def get_content_related_by_tag(page, result_count=3):
 
     results = False
     own_tags = page.tags.all()
-    own_locale = getattr(page, 'locale', None)
+    own_locale = page.locale
 
     for page_type in page_models_with_tags:
         # Get all pages that share tags with this page
