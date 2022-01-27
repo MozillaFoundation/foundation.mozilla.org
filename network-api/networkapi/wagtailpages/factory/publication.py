ea5be482978d8371fe9811f95a67ad4eee146ed5
Daniel Miranda <daniel@mozillafoundation.org>
2022-01-27 11:23:00 -0800

    passing tests

--- network-api/networkapi/wagtailpages/factory/publication.py
+++ network-api/networkapi/wagtailpages/factory/publication.py
@@ -54,8 +54,8 @@ class DocumentFactory(CollectionMemberFactory):
     file = django.FileField(
         filename='test.pdf',
         data=b'''
-              %PDF-1.0 1 0 obj<</Pages 2 0 R>>endobj 
-              2 0 obj<</Kids[3 0 R]/Count 1>>endobj 
+              %PDF-1.0 1 0 obj<</Pages 2 0 R>>endobj
+              2 0 obj<</Kids[3 0 R]/Count 1>>endobj
               3 0 obj<</MediaBox[0 0 3 3]>>endobj
               trailer<</Root 1 0 R>>
               '''
