b8331f84f357cffdf96958bf7fab9187359688e3
Tibor Leupold <tiborl@mozillafoundation.org>
2022-02-04 16:42:39 -0800

    Add help text to datawrapper block

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/datawrapper_block.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/datawrapper_block.py
@@ -2,5 +2,10 @@ from wagtail.embeds import blocks
 
 
 class DatawrapperBlock(blocks.EmbedBlock):
+
     class Meta:
         template = 'wagtailpages/blocks/datawrapper_block.html'
+        help_text = (
+            'Enter the "visualisation only" link of the Datawrapper embed. '
+            'It looks something like this: https://datawrapper.dwcdn.net/KwSKp/1/'
+        )
