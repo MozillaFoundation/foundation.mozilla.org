3adea3fc1d75b4e35c973fcb97c1e377265a3057
Tibor Leupold <tiborl@mozillafoundation.org>
2022-02-07 06:36:59 -0800

    Revert "Naive check if entered URL is a datawrapper URL"
    This reverts commit c956abb5579e2063cfca750b24f63ae0bd870171.
    
    This is somewhat pointless functionality at the moment since only
    Datawrapper is defined as an oEmbed source. I should keep it simple and
    just not add any unnecessary code that could break.

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/datawrapper_block.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/datawrapper_block.py
@@ -1,17 +1,11 @@
-from django.core.exceptions import ValidationError
 from wagtail.embeds import blocks
 
 
 class DatawrapperBlock(blocks.EmbedBlock):
+
     class Meta:
         template = 'wagtailpages/blocks/datawrapper_block.html'
         help_text = (
             'Enter the "visualisation only" link of the Datawrapper embed. '
             'It looks something like this: https://datawrapper.dwcdn.net/KwSKp/1/'
         )
-
-    def clean(self, value):
-        # TODO: Replace this naive check with something based on the regex
-        if isinstance(value, blocks.EmbedValue) and not "datawrapper" in value.url.lower():
-            raise ValidationError("Please enter a Datawrapper URL.")
-        return super().clean(value)
