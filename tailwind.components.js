23ab95b91dad8199f79f48caa9217bbee35d5cab
Simon Fessehaye <fessehay@ualberta.ca>
2022-02-23 22:54:42 -0800

    7151 - Convert bootstrap components to tailwind

--- tailwind.components.js
+++ tailwind.components.js
@@ -1,4 +1,33 @@
 const plugin = require("tailwindcss/plugin");
+
+const formControlStylings = {
+  appearance: "none",
+  backgroundImage: 'url("../_images/glyphs/down-chevron.svg")',
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "right 12px top 50%",
+  backgroundSize: "24px 24px",
+  borderRadius: "0",
+  border: "1px solid #cccccc",
+  padding: "5px 48px 5px 12px",
+  color: "#000000",
+  display: "block",
+  width: "100%",
+  height: "calc(1.5em + .75rem + 2px)",
+  fontSize: "1rem",
+  fontWeight: "400",
+  lineHeight: "1.25",
+  backgroundColor: "#ffffff",
+  backgroundClip: "padding-box",
+  transition:
+    "border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out, -webkit-box-shadow 0.15s ease-in-out",
+  "&:focus": {
+    color: "#495057",
+    backgroundColor: "#fff",
+    borderColor: "#80bdff",
+    outline: "0",
+    boxShadow: "0 0 0 0.2rem rgba(0, 123, 255, 0.25)",
+  },
+};
 module.exports = [
   plugin(function ({ addComponents }) {
     // Using Same breakpoints for containers as bootstrap
@@ -47,6 +76,19 @@ module.exports = [
           flexWrap: "wrap",
           marginRight: "-1rem",
           marginLeft: "-1rem",
+        },
+      },
+      // Add bootstrap form control to tailwind
+      {
+        "select.form-control": {
+          ...formControlStylings,
+          "&-lg": {
+            ...formControlStylings,
+            height: "auto",
+            padding: "12px 48px 12px 12px",
+            fontSize: "1.25rem",
+            lineHeight: "1.25",
+          },
         },
       },
     ]);
