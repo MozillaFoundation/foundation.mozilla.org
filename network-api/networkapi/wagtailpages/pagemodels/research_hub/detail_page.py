ad53faf44b61998dff6040e6f88ce4bd975f519c
Tibor Leupold <tiborl@mozillafoundation.org>
2022-03-15 12:56:05 -0700

    Add additional content fields to detail page

--- network-api/networkapi/wagtailpages/pagemodels/research_hub/detail_page.py
+++ network-api/networkapi/wagtailpages/pagemodels/research_hub/detail_page.py
@@ -5,8 +5,11 @@ from modelcluster import fields as cluster_fields
 from wagtail import documents as wagtail_docs
 from wagtail.documents import edit_handlers as docs_handlers
 from wagtail.admin import edit_handlers
+from wagtail.core import fields as wagtail_fields
 from wagtail.core import models as wagtail_models
+from wagtail_localize import fields as localize_fields
 
+from networkapi.wagtailpages.pagemodels.customblocks import base_rich_text_options
 from networkapi.wagtailpages.pagemodels.mixin import foundation_metadata
 
 
@@ -63,9 +66,47 @@ class ResearchDetailLink(wagtail_models.TranslatableMixin, wagtail_models.Ordera
 class ResearchDetailPage(foundation_metadata.FoundationMetadataPageMixin, wagtail_models.Page):
     parent_page_types = ['ResearchLibraryPage']
 
+    original_publication_date = models.DateField(
+        null=True,
+        blank=True,
+        help_text=_('Date when the research paper was published')
+    )
+    introduction = models.CharField(
+        null=False,
+        blank=True,
+        max_length=300,
+        help_text=_(
+            'Provide a short blub about the research that will be displayed on listing pages and search results.'
+        )
+    )
+    overview = wagtail_fields.RichTextField(
+        null=False,
+        blank=True,
+        features=base_rich_text_options.base_rich_text_options,
+        help_text=_(
+            'Provide an overview about the reseach. '
+            'This can be an excerpt from or the executive summary of the original paper.'
+        )
+    )
+    collaborators = models.TextField(
+        null=False,
+        blank=True,
+        help_text=_(
+            'List all contributors that are not the project leading authors.'
+        )
+    )
+
     content_panels = wagtail_models.Page.content_panels + [
         edit_handlers.InlinePanel('research_links', heading="Research links", min_num=1),
+        edit_handlers.FieldPanel('original_publication_date'),
+        edit_handlers.FieldPanel('introduction'),
+        edit_handlers.FieldPanel('overview', classname='full'),
         edit_handlers.InlinePanel('research_authors', heading="Authors", min_num=1),
+        edit_handlers.FieldPanel('collaborators'),
         edit_handlers.InlinePanel('related_topics', heading="Topics"),
         edit_handlers.InlinePanel('related_regions', heading="Regions"),
+    ]
+
+    override_translatable_fields = [
+        localize_fields.SynchronizedField('original_publication_date', overridable=False),
     ]
