fee1172200f0399e26899cd008bf0a0ac9067753
Tibor Leupold <tiborl@mozillafoundation.org>
2022-03-17 14:39:44 -0700

    Make the authors index routable

--- network-api/networkapi/wagtailpages/pagemodels/research_hub/authors_index.py
+++ network-api/networkapi/wagtailpages/pagemodels/research_hub/authors_index.py
@@ -1,8 +1,22 @@
 from wagtail.core import models as wagtail_models
+from wagtail.contrib.routable_page import models as routable_models
 
 from networkapi.wagtailpages.pagemodels.mixin import foundation_metadata
 
 
-class ResearchAuthorsIndexPage(foundation_metadata.FoundationMetadataPageMixin, wagtail_models.Page):
+class ResearchAuthorsIndexPage(
+    routable_models.RoutablePageMixin,
+    foundation_metadata.FoundationMetadataPageMixin,
+    wagtail_models.Page,
+):
     max_count = 1
     parent_page_types = ['ResearchLandingPage']
+
+
+    @routable_models.route(r'^test/$')
+    def author_detail(self, request):
+        return self.render(
+            request=request,
+            template='wagtailpages/research_author_detail_page.html',
+        )
+
