b2b7161ec5dbc6df8568ba0f72bf7174bffa5262
Daniel Miranda <manieldiranda@gmail.com>
2022-03-25 10:40:59 -0700

    removed play button for now, updated padding and cms wording

--- source/js/foundation/template-js-handler/article-hero-video-handler.js
+++ source/js/foundation/template-js-handler/article-hero-video-handler.js
@@ -10,44 +10,14 @@ export default function articleHeroVideoHandler() {
   }
 
   const video = articlePageHero.querySelector("video.hero-video");
-  const pauseButton = articlePageHero.querySelector(".btn-pause");
-  const playButton = articlePageHero.querySelector(".btn-play");
 
   function toggleVideo() {
     video.paused ? video.play() : video.pause();
   }
 
-  if (video && pauseButton && playButton) {
-    const HIDE = `tw-hidden`;
-
-    const showPauseButton = () => {
-      playButton.classList.add(HIDE);
-      pauseButton.classList.remove(HIDE);
-    };
-
-    const showPlayButton = () => {
-      pauseButton.classList.add(HIDE);
-      playButton.classList.remove(HIDE);
-    };
-
+  if (video) {
     video.addEventListener(`click`, () => {
       toggleVideo();
-    });
-
-    pauseButton.addEventListener(`click`, () => {
-      toggleVideo();
-    });
-
-    playButton.addEventListener(`click`, () => {
-      toggleVideo();
-    });
-
-    video.addEventListener(`pause`, () => {
-      showPlayButton();
-    });
-
-    video.addEventListener(`playing`, () => {
-      showPauseButton();
     });
 
     video.play();
