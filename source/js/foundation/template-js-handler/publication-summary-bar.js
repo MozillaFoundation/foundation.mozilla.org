b79758f0c5ac38ef127c0a67020f1bcd6321c5d1
Simon Fessehaye <fessehay@ualberta.ca>
2022-03-29 00:12:12 -0700

    8367 - Sticky Nav P1

--- source/js/foundation/template-js-handler/publication-summary-bar.js
+++ source/js/foundation/template-js-handler/publication-summary-bar.js
@@ -11,31 +11,79 @@ export default () => {
     const articleSummaryToggle = document.querySelector(
       ".article-summary-toggle"
     );
-    const hero = document.querySelector(".publication-hero-container");
-    const nav = document.querySelector(".primary-nav-container");
-    if (summaryBlock && hero && nav && dropDownMenu) {
-      const total =
-        hero.offsetHeight + nav.offsetHeight - summaryBlock.offsetHeight;
 
-      window.addEventListener(
-        "scroll",
-        () => {
-          if (window.scrollY > total) {
-            // Toggle summary block off
-            summaryBlock.style.top = "0";
-            summaryBlock.setAttribute("aria-hidden", true);
-          } else {
-            // Toggle summary block on
-            summaryBlock.style.top = marginOffset;
-            summaryBlock.setAttribute("aria-hidden", false);
-            dropDownMenu.classList.remove("d-block");
-            articleSummaryToggle.classList.remove("show");
-          }
-        },
-        {
-          passive: true,
+    // sticky nav toggle for scrolling past the header
+    const summaryObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio === 0) {
+          // Toggle summary block off
+          summaryBlock.style.top = "0";
+          summaryBlock.setAttribute("aria-hidden", true);
+        } else {
+          // Toggle summary block on
+          summaryBlock.style.top = marginOffset;
+          summaryBlock.setAttribute("aria-hidden", false);
+          dropDownMenu.classList.remove("d-block");
+          articleSummaryToggle.classList.remove("show");
+          document.querySelector(".toggle-text").innerText = document
+            .querySelector(".toggle-text")
+            .getAttribute("data-open");
         }
+      });
+    });
+
+    // update the title based on the header in the article in past by
+    const titleObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const title = entry.target.innerText;
+        if (entry.intersectionRatio > 0) {
+          document.querySelector(
+            ".dropdown-toggle.article-summary-toggle span"
+          ).innerText = title;
+        }
+      });
+    });
+
+    summaryObserver.observe(
+      document.querySelector(".publication-hero-container")
+    );
+
+    document.querySelectorAll(".rich-text h2").forEach((section) => {
+      titleObserver.observe(section);
+    });
+
+    // accordion button toggle
+    document.querySelectorAll(".article-child-button").forEach((button) => {
+      button.addEventListener("click", () => {
+        document
+          .querySelector(
+            `.article-child-menu[data-expand="${button.dataset.expand}"]`
+          )
+          .classList.toggle("tw-hidden");
+
+        button.innerText = button.innerText === "+" ? "-" : "+";
+      });
+    });
+
+    // we need to calculate the height for the new dropdown menu since it is a absolute element with sticky elements in the way
+    function calcSummaryMenuHeight() {
+      const stickyContent = document.querySelector(
+        ".wrapper > .sticky-top.d-print-none"
       );
+
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        dropDownMenu.style.maxHeight = `calc(100vh - ${stickyContent.offsetHeight}px)`;
+        dropDownMenu.style.height = `auto`;
+      } else {
+        // accordion should always be max remaining height on tablet and mobile
+        dropDownMenu.style.maxHeight = `calc(100vh - ${stickyContent.offsetHeight}px)`;
+        dropDownMenu.style.height = `calc(100vh - ${stickyContent.offsetHeight}px)`;
+      }
     }
+
+    calcSummaryMenuHeight();
+
+    // to make it easier to test different mobile screens
+    window.addEventListener("resize", calcSummaryMenuHeight);
   }
 };
