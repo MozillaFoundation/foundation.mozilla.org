56f8fb64850adf98021605847af7b9a92306202d
Tibor Leupold <tiborl@mozillafoundation.org>
2022-03-30 16:02:00 -0700

    Move context building to separate method
    This should make it easier to test further functionality. E.g. I want to
    make sure that the querying of the related research is efficient.

--- network-api/networkapi/wagtailpages/pagemodels/research_hub/authors_index.py
+++ network-api/networkapi/wagtailpages/pagemodels/research_hub/authors_index.py
@@ -23,16 +23,24 @@ class ResearchAuthorsIndexPage(
 
     @routable_models.route(r'^(?P<profile_id>[0-9]+)/(?P<profile_slug>[-a-z]+)/$')
     def author_detail(self, request: http.HttpRequest, profile_id: str, profile_slug: str):
-        author_profile = shortcuts.get_object_or_404(
-            profiles.Profile.objects.filter_research_authors(),
-            id=int(profile_id),
+        context_overrides = self.get_author_detail_context(profile_id=int(profile_id))
+
+        slugified_profile_name = text_utils.slugify(
+            context_overrides['author_profile'].name
         )
-        if not text_utils.slugify(author_profile.name) == profile_slug:
+        if not slugified_profile_name == profile_slug:
             raise http.Http404('Slug does not match id')
 
         return self.render(
             request=request,
             template='wagtailpages/research_author_detail_page.html',
-            context_overrides={'author_profile': author_profile},
+            context_overrides=context_overrides,
         )
 
+    def get_author_detail_context(self, profile_id: int):
+        author_profiles = profiles.Profile.objects.filter_research_authors()
+        author_profile = shortcuts.get_object_or_404(
+            author_profiles,
+            id=profile_id,
+        )
+        return {'author_profile': author_profile}
