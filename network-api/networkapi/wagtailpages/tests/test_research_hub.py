0c91ad4b1460e95146efa84ea9c25e9850bc2729
Tibor Leupold <tiborl@mozillafoundation.org>
2022-04-04 13:00:11 -0700

    Test mix of aliased and translated pages

--- network-api/networkapi/wagtailpages/tests/test_research_hub.py
+++ network-api/networkapi/wagtailpages/tests/test_research_hub.py
@@ -28,7 +28,6 @@ class TestResearchAuthorIndexPage(test.TestCase):
             title="Authors",
         )
 
-        # Profile associated with a research detail page
         cls.detail_page = research_factory.ResearchDetailPageFactory(
             parent=cls.library_page,
             original_publication_date=(
@@ -43,6 +42,20 @@ class TestResearchAuthorIndexPage(test.TestCase):
         )
 
         cls.non_research_profile = profile_factory.ProfileFactory()
+
+        cls.default_locale = wagtail_models.Locale.get_default()
+        cls.fr_locale, _ = wagtail_models.Locale.objects.get_or_create(
+            language_code='fr'
+        )
+        assert cls.fr_locale != cls.default_locale
+
+        # Translate the pages
+        synctree.synchronize_tree(
+            source_locale=cls.default_locale,
+            target_locale=cls.fr_locale
+        )
+
+        cls.fr_detail_page = cls.detail_page.get_translation(cls.fr_locale)
 
     @classmethod
     def _setup_homepage(cls):
@@ -60,70 +73,65 @@ class TestResearchAuthorIndexPage(test.TestCase):
         cls.site.clean()
         cls.site.save()
 
-    def _setup_synchronized_tree(self):
-        # Set up additional locale
-        self.default_locale = wagtail_models.Locale.get_default()
-        self.assertEqual(self.research_profile.locale, self.default_locale)
-        self.active_locale = wagtail_models.Locale.get_active()
-        self.assertEqual(self.active_locale, self.default_locale)
-        self.fr_locale, _ = wagtail_models.Locale.objects.get_or_create(
-            language_code='fr'
-        )
-        self.assertNotEqual(self.fr_locale, self.default_locale)
+    def setUp(self):
+        translation.activate(self.default_locale.language_code)
 
-        # Translate the pages
-        synctree.synchronize_tree(
-            source_locale=self.default_locale,
-            target_locale=self.fr_locale
-        )
-
-        self.fr_detail = self.detail_page.get_translation(self.fr_locale)
-
-    def _setup_translated_research_profile(self):
-        # Translate profile and set it as author on the translated detail. This is what
-        # happens when you work in the localize UI.
+    def translate_research_profile(self):
         self.fr_profile = self.research_profile.copy_for_translation(self.fr_locale)
         self.fr_profile.save()
-        self.fr_detail_author = self.fr_detail.research_authors.first()
-        self.fr_detail_author.author_profile = self.fr_profile
-        self.fr_detail_author.save()
+
+    def translate_detail_page(self):
+        """
+        Simulate click on the "translate this page" button in the Wagtail admin.
+
+        The tree synchronzion creates page aliases with the new locale. But, at the
+        alias stage, the pages are not really translated yet. E.g. inline panels
+        like the research author are not translated until the "translate this page"
+        button is clicked in the admin. That means, until the "manual" action of
+        clicking the button is taken, the alias page is still associated with the
+        profile for the default locale.
+
+        This method simulates the manual click on the "translate this page" button.
+        That action removes the alias and sets associates the profile with the
+        translated profile.
+
+        """
+        if not hasattr(self, "fr_profile"):
+            self.translate_research_profile()
+
+        fr_research_author = self.fr_detail_page.research_authors.first()
+        fr_research_author.author_profile = self.fr_profile
+        fr_research_author.save()
+        self.fr_detail_page.alias_of = None
+        self.fr_detail_page.save()
 
     def test_get_context(self):
         context = self.author_index.get_context(request=None)
+        self.translate_research_profile()
 
         self.assertIn(self.research_profile, context['author_profiles'])
         # Non-research profile should not show up
         self.assertNotIn(self.non_research_profile, context['author_profiles'])
-
-    def test_get_context_default_locale(self):
-        self._setup_synchronized_tree()
-        self._setup_translated_research_profile()
-
-        # Get context when default is active
-        context = self.author_index.localized.get_context(request=None)
-
-        self.assertIn(self.research_profile, context['author_profiles'])
+        # Translated profile should not show up
         self.assertNotIn(self.fr_profile, context['author_profiles'])
 
-    def test_get_context_fr_locale(self):
-        self._setup_synchronized_tree()
-        self._setup_translated_research_profile()
-        translation.activate('fr')
+    def test_get_context_fr_locale_detail_alias(self):
+        translation.activate(self.fr_locale.language_code)
+
+        fr_context = self.author_index.localized.get_context(request=None)
+
+        # When the profile is not translated, the default locales profile should show
+        self.assertIn(self.research_profile, fr_context['author_profiles'])
+
+    def test_get_context_fr_locale_detail_translated(self):
+        self.translate_detail_page()
+        translation.activate(self.fr_locale.language_code)
 
         # Get context when fr is active
         fr_context = self.author_index.localized.get_context(request=None)
 
         self.assertNotIn(self.research_profile, fr_context['author_profiles'])
         self.assertIn(self.fr_profile, fr_context['author_profiles'])
-
-    def test_get_context_fr_locale_profile_not_translated(self):
-        self._setup_synchronized_tree()
-        translation.activate('fr')
-
-        fr_context = self.author_index.localized.get_context(request=None)
-
-        # When the profile is not translated, the default locales profile should show
-        self.assertIn(self.research_profile, fr_context['author_profiles'])
 
     def test_profile_route(self):
         profile_slug = text_utils.slugify(self.research_profile.name)
@@ -217,32 +225,96 @@ class TestResearchAuthorIndexPage(test.TestCase):
             self.assertIn(detail_page_3, context['latest_research'])
             self.assertNotIn(self.detail_page, context['latest_research'])
 
-    def test_get_author_detail_context_default_locale_profile_not_translated(self):
-        # This condition exists when the pages are automatically synced, but have not
-        # really been translated.
-        self._setup_synchronized_tree()
-
+    def test_get_author_detail_context(self):
         context = self.author_index.localized.get_author_detail_context(
             profile_id=self.research_profile.id,
         )
 
         self.assertIn(self.detail_page, context['latest_research'])
-        self.assertNotIn(self.fr_detail, context['latest_research'])
+        self.assertNotIn(self.fr_detail_page, context['latest_research'])
 
-    def test_get_author_detail_context_fr_locale_profile_not_translated(self):
-        # This condition exists when the pages are automatically synced, but have not
-        # really been translated.
-        self._setup_synchronized_tree()
-        translation.activate('fr')
-
-        # The profile is not translated. So even when the fr locale is active it will
+    def test_get_author_detail_context_fr_locale(self):
+        translation.activate(self.fr_locale.language_code)
+        # When page trees are automatically synced, but have not really been translated,
+        # the profile is not translated. So even when the fr locale is active it will
         # use the original profile
+        localized_research_profile = self.research_profile.localized
+        self.assertEqual(localized_research_profile, self.research_profile)
+
         context = self.author_index.localized.get_author_detail_context(
-            profile_id=self.research_profile.localized.id,
+            profile_id=localized_research_profile.id,
         )
 
+        # The displayed detail pages should be the aliased pages not the original ones.
         self.assertNotIn(self.detail_page, context['latest_research'])
-        self.assertIn(self.fr_detail, context['latest_research'])
+        self.assertIn(self.fr_detail_page, context['latest_research'])
+
+    def test_get_author_detail_context_alias_and_translation(self):
+        # There can be mixed situations, where only some research associated with
+        # a profile is properly translated, but others are still only aliased.
+        self.translate_detail_page()
+        extra_detail_page = research_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+        )
+        research_factory.ResearchAuthorRelationFactory(
+            research_detail_page=extra_detail_page,
+            author_profile=self.research_profile,
+        )
+        synctree.synchronize_tree(
+            source_locale=self.default_locale,
+            target_locale=self.fr_locale
+        )
+        # Grab the alias page. Note: This page is not really translated, so it is still
+        # associated with the original profile.
+        fr_extra_detail_page = extra_detail_page.get_translation(self.fr_locale)
+        # Check localized profile
+        localized_research_profile = self.research_profile.localized
+        self.assertEqual(localized_research_profile, self.research_profile)
+
+        context = self.author_index.localized.get_author_detail_context(
+            profile_id=localized_research_profile.id,
+        )
+
+        # We need to make sure that the displayed works are picked based on the active
+        # locale, rather than the locale of the profile we are looking at
+        self.assertIn(self.detail_page, context['latest_research'])
+        self.assertIn(extra_detail_page, context['latest_research'])
+        self.assertNotIn(self.fr_detail_page, context['latest_research'])
+        self.assertNotIn(fr_extra_detail_page, context['latest_research'])
+
+    def test_get_author_detail_context_fr_locale_alias_and_translation(self):
+        # There can be mixed situations, where only some research associated with
+        # a profile is properly translated, but others are still only aliased.
+        self.translate_detail_page()
+        extra_detail_page = research_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+        )
+        research_factory.ResearchAuthorRelationFactory(
+            research_detail_page=extra_detail_page,
+            author_profile=self.research_profile,
+        )
+        synctree.synchronize_tree(
+            source_locale=self.default_locale,
+            target_locale=self.fr_locale
+        )
+        # Grab the alias page. Note: This page is not really translated, so it is still
+        # associated with the original profile.
+        fr_extra_detail_page = extra_detail_page.get_translation(self.fr_locale)
+        # Activate locale
+        translation.activate(self.fr_locale.language_code)
+        localized_research_profile = self.research_profile.localized
+        self.assertNotEqual(localized_research_profile, self.research_profile)
+
+        context = self.author_index.localized.get_author_detail_context(
+            profile_id=localized_research_profile.id,
+        )
+
+        # We need to make sure that the displayed works are picked based on the active
+        # locale, rather than the locale of the profile we are looking at
+        self.assertNotIn(self.detail_page, context['latest_research'])
+        self.assertNotIn(extra_detail_page, context['latest_research'])
+        self.assertIn(self.fr_detail_page, context['latest_research'])
+        self.assertIn(fr_extra_detail_page, context['latest_research'])
 
 
 class TestResearchDetailLink(test.TestCase):
