ba4655c9b99ffa54a0519252ad6bf52ed535955e
Tibor Leupold <tiborl@mozillafoundation.org>
2022-04-05 10:00:15 -0700

    Create a research hub test case
    I pulled in the setup code I created for #8511. Once that is
    merged to main I can rebase and change the research hub index
    tests to be based on the new test case class too.
    
    The test case class is just grouping all the setup boilerplate that each
    test case for the different pages in the research hub is likely to
    recreate.

--- network-api/networkapi/wagtailpages/tests/test_research_hub.py
+++ network-api/networkapi/wagtailpages/tests/test_research_hub.py
@@ -2,6 +2,62 @@ from django import test
 from django.core import exceptions
 
 from networkapi.wagtailpages.factory import research_hub as research_factory
+from networkapi.wagtailpages import models as pagemodels
+
+
+class ResearchHubTestCase(test.TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls._setup_homepage()
+        cls._setup_research_hub_structure(homepage=cls.homepage)
+        cls._setup_locales()
+
+    @classmethod
+    def _setup_homepage(cls):
+        root = wagtail_models.Page.get_first_root_node()
+        if not root:
+            raise ValueError('A root page should exist. Something is off.')
+        cls.homepage = home_factory.WagtailHomepageFactory(parent=root)
+
+        sites = wagtail_models.Site.objects.all()
+        if sites.count() != 1:
+            raise ValueError('There should be exactly one site. Something is off.')
+        cls.site = sites.first()
+
+        cls.site.root_page = cls.homepage
+        cls.site.clean()
+        cls.site.save()
+
+    @classmethod
+    def _setup_research_hub_structure(cls, homepage):
+        cls.landing_page = research_factory.ResearchLandingPageFactory(
+            parent=homepage,
+        )
+        cls.library_page = research_factory.ResearchLibraryPageFactory(
+            parent=cls.landing_page,
+        )
+        cls.author_index = research_factory.ResearchAuthorsIndexPageFactory(
+            parent=cls.landing_page,
+            title="Authors",
+        )
+
+    @classmethod
+    def _setup_locales(cls):
+        cls.default_locale = wagtail_models.Locale.get_default()
+        cls.fr_locale, _ = wagtail_models.Locale.objects.get_or_create(
+            language_code='fr'
+        )
+        assert cls.fr_locale != cls.default_locale
+
+    def setUp(self):
+        self.synchronize_tree()
+
+    def synchronize_tree(self):
+        synctree.synchronize_tree(
+            source_locale=self.default_locale,
+            target_locale=self.fr_locale
+        )
+
 
 
 class TestResearchDetailLink(test.TestCase):
