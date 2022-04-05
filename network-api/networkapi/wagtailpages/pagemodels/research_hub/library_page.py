fbf627f5c534377515c03b2ef34a6ccce0f18837
Tibor Leupold <tiborl@mozillafoundation.org>
2022-04-05 12:45:21 -0700

    Add dedicated method for gathering of detail pages
    This should further simplify the testing.

--- network-api/networkapi/wagtailpages/pagemodels/research_hub/library_page.py
+++ network-api/networkapi/wagtailpages/pagemodels/research_hub/library_page.py
@@ -11,9 +11,14 @@ class ResearchLibraryPage(foundation_metadata.FoundationMetadataPageMixin, wagta
 
     def get_context(self, request):
         context = super().get_context(request)
+        context['research_detail_pages'] = self.get_research_detail_pages()
+        return context
 
+    def get_research_detail_pages(self):
         ResearchDetailPage = apps.get_model('wagtailpages', 'ResearchDetailPage')
         active_locale = wagtail_models.Locale.get_active()
-        context['research_detail_pages'] = ResearchDetailPage.objects.filter(locale=active_locale)
 
-        return context
+        research_detail_pages = ResearchDetailPage.objects.all()
+        research_detail_pages = research_detail_pages.filter(locale=active_locale)
+
+        return research_detail_pages
--- network-api/networkapi/wagtailpages/tests/test_research_hub.py
+++ network-api/networkapi/wagtailpages/tests/test_research_hub.py
@@ -41,7 +41,7 @@ class ResearchHubTestCase(test.TestCase):
         )
         cls.author_index = research_factory.ResearchAuthorsIndexPageFactory(
             parent=cls.landing_page,
-            title="Authors",
+            title='Authors',
         )
 
     @classmethod
@@ -63,7 +63,7 @@ class ResearchHubTestCase(test.TestCase):
 
 
 class TestResearchLibraryPage(ResearchHubTestCase):
-    def test_get_context_detail_pages_in_context(self):
+    def test_get_research_detail_pages(self):
         detail_page_1 = research_factory.ResearchDetailPageFactory(
             parent=self.library_page,
         )
@@ -71,13 +71,13 @@ class TestResearchLibraryPage(ResearchHubTestCase):
             parent=self.library_page,
         )
 
-        context = self.library_page.get_context(request=None)
+        research_detail_pages = self.library_page.get_research_detail_pages()
 
-        self.assertEqual(len(context['research_detail_pages']), 2)
-        self.assertIn(detail_page_1, context['research_detail_pages'])
-        self.assertIn(detail_page_2, context['research_detail_pages'])
+        self.assertEqual(len(research_detail_pages), 2)
+        self.assertIn(detail_page_1, research_detail_pages)
+        self.assertIn(detail_page_2, research_detail_pages)
 
-    def test_get_context_translation_aliases_not_in_context(self):
+    def test_get_research_detail_pages_with_translation_aliases(self):
         detail_page_1 = research_factory.ResearchDetailPageFactory(
             parent=self.library_page,
         )
@@ -88,13 +88,13 @@ class TestResearchLibraryPage(ResearchHubTestCase):
         fr_detail_page_1 = detail_page_1.get_translation(self.fr_locale)
         fr_detail_page_2 = detail_page_2.get_translation(self.fr_locale)
 
-        context = self.library_page.get_context(request=None)
+        research_detail_pages = self.library_page.get_research_detail_pages()
 
-        self.assertEqual(len(context['research_detail_pages']), 2)
-        self.assertIn(detail_page_1, context['research_detail_pages'])
-        self.assertIn(detail_page_2, context['research_detail_pages'])
-        self.assertNotIn(fr_detail_page_1, context['research_detail_pages'])
-        self.assertNotIn(fr_detail_page_2, context['research_detail_pages'])
+        self.assertEqual(len(research_detail_pages), 2)
+        self.assertIn(detail_page_1, research_detail_pages)
+        self.assertIn(detail_page_2, research_detail_pages)
+        self.assertNotIn(fr_detail_page_1, research_detail_pages)
+        self.assertNotIn(fr_detail_page_2, research_detail_pages)
 
 
 class TestResearchDetailLink(test.TestCase):
