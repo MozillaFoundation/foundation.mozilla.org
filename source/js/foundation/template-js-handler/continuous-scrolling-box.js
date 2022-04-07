220d6cd872f9081854cb59879dc2b04025e1871b
Simon <fessehay@ualberta.ca>
2022-04-06 23:40:42 -0700

    8492 - Continous Scrolling Component
    Add link to scrolling box
    
    Remove history push state events

---
+++ source/js/foundation/template-js-handler/continuous-scrolling-box.js
@@ -0,0 +1,77 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default () => {
+  /**
+   * Creates slideup intro animation when entering a new article. If history.state indicates
+   * that it is coming from another article via scrolltrigger, then we animate the white box that is overlaying the article.
+   */
+  function triggerIntroTransition() {
+    if (sessionStorage.contScrolling) {
+      gsap.to(".page-enter-transition", {
+        y: "-100vh",
+        delay: 0.5,
+        duration: 0.5,
+      });
+      sessionStorage.removeItem("contScrolling");
+    } else if (document.querySelector(".page-enter-transition")) {
+      document.querySelector(".page-enter-transition").remove();
+    }
+  }
+
+  /**
+   * Creates a scroll trigger that will trigger the exit animation when the user scrolls past the bottom of the page.
+   */
+  function initScrollExitTransition() {
+    ScrollTrigger.create({
+      trigger: ".cont-scrolling",
+      start: "top center",
+      end: "bottom bottom",
+      scrub: true,
+      toggleActions: "play none none reverse",
+      onLeave: () => {
+        const outro = gsap.timeline({
+          onComplete: () => {
+            sessionStorage.setItem("contScrolling", "true");
+            window.location =
+              document.querySelector(".cont-scrolling").dataset.href;
+          },
+        });
+        document
+          .querySelector(".cont-scrolling")
+          .classList.toggle(
+            "tw-sticky",
+            "tw-absolute",
+            "tw-top-0",
+            "tw-left-0"
+          );
+        outro.to(".cont-scrolling .tw-container", {
+          autoAlpha: 0,
+          duration: 0.2,
+          delay: 0.3,
+        });
+        outro.set("main > div:not(.cont-scrolling),.primary-nav-container", {
+          display: "none",
+          autoAlpha: 0,
+        });
+        outro.set("main", {
+          height: "100vh",
+        });
+        outro.to(".cont-scrolling", {
+          y: "-200vh",
+          duration: 0.3,
+        });
+        outro.to(".cont-scrolling", {
+          autoAlpha: 0,
+          duration: 0.1,
+        });
+      },
+    });
+  }
+
+  triggerIntroTransition();
+  gsap.registerPlugin(ScrollTrigger);
+  if (document.querySelector(".cont-scrolling")) {
+    initScrollExitTransition();
+  }
+};
--- source/js/foundation/template-js-handler/index.js
+++ source/js/foundation/template-js-handler/index.js
@@ -1,5 +1,5 @@
 import stickyCtaHandler from "./window/sticky-cta";
-import shareButtonGroupHandler from './share-button-group.js'
+import shareButtonGroupHandler from "./share-button-group.js";
 import stickyShareButtonGroupHandler from "./window/sticky-share-button-group";
 import articleFootLinkHandler from "./article-footnote-link.js";
 import audioBlockHandler from "./audio-player-handler";
@@ -8,6 +8,7 @@ import loadMoreEntriesHandler from "./load-more-entries.js";
 import loopingVideoHandler from "./looping-video-handler.js";
 import participatePageDonateHandler from "./participate-page-donate.js";
 import publicationSummaryBar from "./publication-summary-bar.js";
+import continuousScrollingBox from "./continuous-scrolling-box.js";
 import pulseProfileCardHandler from "./pulse-profile-card.js";
 import pulseProfileListFilterHandler from "./pulse-profile-list-filter.js";
 import articleHeroVideoHandler from "./article-hero-video-handler.js";
@@ -37,4 +38,5 @@ export const bindEventHandlers = () => {
   pulseProfileListFilterHandler();
   shareButtonGroupHandler();
   blogHeroVideoHandler();
+  continuousScrollingBox();
 };
