f867e7a82e27e791d36c9fcd43e6733d3e94182e
Simon Fessehaye <fessehay@ualberta.ca>
2022-04-19 00:00:59 -0700

    Dev Feedback

--- source/js/foundation/template-js-handler/continuous-scrolling-box.js
+++ source/js/foundation/template-js-handler/continuous-scrolling-box.js
@@ -2,18 +2,20 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default () => {
+  const CONTINUE_SCROLLING = "contScrolling";
+
   /**
-   * Creates slideup intro animation when entering a new article. If history.state indicates
+   * Creates slideup intro animation when entering a new article. If sessionStorage indicates
    * that it is coming from another article via scrolltrigger, then we animate the white box that is overlaying the article.
    */
   function triggerIntroTransition() {
-    if (sessionStorage.contScrolling) {
+    if (sessionStorage.getItem(CONTINUE_SCROLLING)) {
       gsap.to(".page-enter-transition", {
         y: "-100vh",
-        delay: 0.5,
-        duration: 0.5,
+        delay: 0.3,
+        duration: 0.3,
       });
-      sessionStorage.removeItem("contScrolling");
+      sessionStorage.removeItem(CONTINUE_SCROLLING);
     } else if (document.querySelector(".page-enter-transition")) {
       document.querySelector(".page-enter-transition").remove();
     }
@@ -48,19 +50,18 @@ export default () => {
       onLeave: () => {
         const outro = gsap.timeline({
           onComplete: () => {
-            // If it takes more then 400ms show the loading indicator
             setTimeout(() => {
-              if (document.querySelector(".article-loading")) {
-                document
-                  .querySelector(".article-loading")
-                  .classList.toggle("tw-hidden");
+              const loadIndicator = document.querySelector(".article-loading");
+              if (loadIndicator) {
+                loadIndicator.classList.toggle("tw-hidden");
               }
-            }, 400);
-            sessionStorage.setItem("contScrolling", "true");
-            window.location.href =
-              document.querySelector(".cont-scrolling").dataset.href;
+            }, 500);
+
+            sessionStorage.setItem(CONTINUE_SCROLLING, "true");
+            document.querySelector(".scrolling-link").click();
           },
         });
+        // Need to remove a lot of !important classes to allow animation to work.
         document
           .querySelector(".cont-scrolling")
           .classList.toggle(
@@ -69,6 +70,11 @@ export default () => {
             "tw-top-0",
             "tw-left-0"
           );
+        outro.to(".outro-screen", {
+          y: "0vh",
+          duration: 0.3,
+          delay: 0.5,
+        });
         outro.set(
           "main > div:not(.cont-scrolling),.primary-nav-container,.article-navbar-container",
           {
@@ -76,37 +82,19 @@ export default () => {
             autoAlpha: 0,
           }
         );
+        document
+          .querySelector(".publication-hero-container")
+          .classList.toggle("d-flex");
         outro.set("main", {
           height: "100%",
-        });
-        outro.to(".cont-scrolling", {
-          y: "-200vh",
-          duration: 0.3,
-          delay: 0.5,
-        });
-        outro.to(".cont-scrolling", {
-          autoAlpha: 0,
-          duration: 0.3,
         });
       },
     });
   }
 
-  // lets prefetch the next article link to avoid a slow page transition
-  function prefetchNextArticle() {
-    const nextArticle = document.querySelector(".cont-scrolling").dataset.href;
-    if (nextArticle) {
-      const nextArticleLink = document.createElement("link");
-      nextArticleLink.rel = "prefetch";
-      nextArticleLink.href = nextArticle;
-      document.head.appendChild(nextArticleLink);
-    }
-  }
-
   triggerIntroTransition();
   gsap.registerPlugin(ScrollTrigger);
   if (document.querySelector(".cont-scrolling")) {
-    prefetchNextArticle();
     initScrollExitTransition();
   }
 };
