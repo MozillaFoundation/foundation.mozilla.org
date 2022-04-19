d127d38a6fbcc6a81ecc7f71a959b30eb878e7f0
Daniel Miranda <manieldiranda@gmail.com>
2022-04-19 16:22:37 -0700

    first pass

--- network-api/networkapi/wagtailpages/pagemodels/research_hub/landing_page.py
+++ network-api/networkapi/wagtailpages/pagemodels/research_hub/landing_page.py
@@ -1,14 +1,37 @@
 from django.apps import apps
 from wagtail.core import models as wagtail_models
+from django.db import models
+
 
 from networkapi.wagtailpages.pagemodels.mixin import foundation_metadata
+from wagtail.admin.edit_handlers import (
+    FieldPanel
+)
 
+from ...utils import (
+    TitleWidget
+)
 
 class ResearchLandingPage(foundation_metadata.FoundationMetadataPageMixin, wagtail_models.Page):
     max_count = 1
     subpage_types = [
         'ResearchLibraryPage',
         'ResearchAuthorsIndexPage',
+    ]
+
+    intro = models.CharField(
+        blank=True,
+        max_length=250,
+    )
+
+    content_panels = [
+        FieldPanel(
+            'title',
+            classname='full title',
+            widget=TitleWidget(attrs={"class": "max-length-warning", "data-max-length": 60})
+        ),
+        FieldPanel('intro'),
+
     ]
 
     def get_context(self, request):
