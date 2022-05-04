315de717b55b81aed0c798644e04fda9ee9e174a
Tibor Leupold <tiborl@mozillafoundation.org>
2022-05-04 15:57:34 -0700

    Swap jump link and show button with JS

--- source/js/foundation/pages/research-hub-library.js
+++ source/js/foundation/pages/research-hub-library.js
@@ -2,6 +2,12 @@ function main() {
   console.log("Hello World");
   const filterSection = document.getElementById("filter")
   filterSection.classList.add("tw-hidden")
+
+  const filterJumpLink = document.getElementById("filter-section-jump-link")
+  filterJumpLink.classList.add("tw-hidden")
+
+  const filterShowButton = document.getElementById("filter-section-show-button")
+  filterShowButton.classList.remove("tw-hidden")
 }
 
 main()
