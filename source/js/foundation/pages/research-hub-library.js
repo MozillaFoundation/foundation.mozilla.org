8f8b45e219d56c86ba5fcdf49e66aef4753d8c63
Tibor Leupold <tiborl@mozillafoundation.org>
2022-05-09 14:40:52 -0700

    Enable hide and show of filter modal on mobile
    Before JS is loaded, the filter section on mobile is shown below the
    results list. Once the JS loads, the section is hidden. Some classes are
    changed to allow showing the filter section as a modal.

--- source/js/foundation/pages/research-hub-library.js
+++ source/js/foundation/pages/research-hub-library.js
@@ -1,13 +1,38 @@
 function main() {
-  console.log("Hello World");
+  initializeJS()
+}
+
+
+function initializeJS() {
+  // Hide the filter section and apply the mobile modal styles
   const filterSection = document.getElementById("filter")
   filterSection.classList.add("tw-hidden")
+  filterSection.classList.add("rh-filter-modal")
+  filterSection.classList.remove("-tw-mx-4", "tw-pt-7")
 
+  // Replace jump link with open button
   const filterJumpLink = document.getElementById("filter-section-jump-link")
   filterJumpLink.classList.add("tw-hidden")
-
   const filterShowButton = document.getElementById("filter-section-show-button")
   filterShowButton.classList.remove("tw-hidden")
+
+  filterShowButton.addEventListener('click', showFilterModal)
+  const filterHideButton = document.getElementById("filter-section-hide-button")
+  filterHideButton.classList.remove("tw-hidden")
+  filterHideButton.addEventListener('click', hideFilterModal)
 }
+
+
+function showFilterModal(event) {
+  const filterSection = document.getElementById("filter")
+  filterSection.classList.remove("tw-hidden")
+}
+
+
+function hideFilterModal(event) {
+  const filterSection = document.getElementById("filter")
+  filterSection.classList.add("tw-hidden")
+}
+
 
 main()
