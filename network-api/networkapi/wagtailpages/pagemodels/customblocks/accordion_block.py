6b302cb08e849264e5a7e3152b331153ce689863
Daniel Miranda <manieldiranda@gmail.com>
2022-05-12 14:12:58 -0700

    accordion block but using a listblock to contain all accordion items

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/accordion_block.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/accordion_block.py
@@ -9,19 +9,27 @@ accordion_rich_text = blocks.RichTextBlock(
 )
 
 
-class AccordionBlock(blocks.StructBlock):
-
+class AccordionItem(blocks.StructBlock):
     title = blocks.CharBlock(
-        help_text='Heading for the Accordion.'
+        help_text='Heading for the Accordion Item'
     )
-
-    accordion_items = blocks.StreamBlock(
+    content = blocks.StreamBlock(
             [
                 ('rich_text', accordion_rich_text),
                 ('datawrapper', DatawrapperBlock()),
                 ('image', ImageBlock())
             ]
         )
+
+
+class AccordionBlock(blocks.StructBlock):
+
+    title = blocks.CharBlock(
+        help_text='Heading for the Accordion.',
+        required=False
+    )
+
+    accordion_items = blocks.ListBlock(AccordionItem())
 
     class Meta:
 
