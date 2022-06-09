870a1c5c76f94e2b231f74908f20881ae500d93f
Tibor Leupold <tiborl@mozillafoundation.org>
2022-06-09 11:23:25 -0700

    Extract blog index setup

--- network-api/networkapi/wagtailpages/tests/test_blog.py
+++ network-api/networkapi/wagtailpages/tests/test_blog.py
@@ -6,46 +6,50 @@ from networkapi.wagtailpages.tests import base as test_base
 
 
 class TestBlogIndexSearch(test_base.WagtailpagesTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.blog_index = blog_factories.BlogIndexPageFactory(parent=cls.homepage)
+
     def test_route_success(self):
-        blog_index = blog_factories.BlogIndexPageFactory(parent=self.homepage)
-        url = blog_index.get_url() + blog_index.reverse_subpage("search")
+        url = self.blog_index.get_url() + self.blog_index.reverse_subpage("search")
 
         response = self.client.get(path=url)
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_no_query(self):
-        blog_index = blog_factories.BlogIndexPageFactory(parent=self.homepage)
+        """Default search page with no query shows latest 6 pages."""
         tz = datetime.timezone.utc
         blog_page_1 = blog_factories.BlogPageFactory(
-            parent=blog_index,
+            parent=self.blog_index,
             first_published_at=datetime.datetime(2020, 1, 1, tzinfo=tz),
         )
         blog_page_2 = blog_factories.BlogPageFactory(
-            parent=blog_index,
+            parent=self.blog_index,
             first_published_at=datetime.datetime(2020, 1, 2, tzinfo=tz),
         )
         blog_page_3 = blog_factories.BlogPageFactory(
-            parent=blog_index,
+            parent=self.blog_index,
             first_published_at=datetime.datetime(2020, 1, 3, tzinfo=tz),
         )
         blog_page_4 = blog_factories.BlogPageFactory(
-            parent=blog_index,
+            parent=self.blog_index,
             first_published_at=datetime.datetime(2020, 1, 4, tzinfo=tz),
         )
         blog_page_5 = blog_factories.BlogPageFactory(
-            parent=blog_index,
+            parent=self.blog_index,
             first_published_at=datetime.datetime(2020, 1, 5, tzinfo=tz),
         )
         blog_page_6 = blog_factories.BlogPageFactory(
-            parent=blog_index,
+            parent=self.blog_index,
             first_published_at=datetime.datetime(2020, 1, 6, tzinfo=tz),
         )
         blog_page_7 = blog_factories.BlogPageFactory(
-            parent=blog_index,
+            parent=self.blog_index,
             first_published_at=datetime.datetime(2020, 1, 7, tzinfo=tz),
         )
-        url = blog_index.get_url() + blog_index.reverse_subpage("search")
+        url = self.blog_index.get_url() + self.blog_index.reverse_subpage("search")
 
         response = self.client.get(path=url)
 
