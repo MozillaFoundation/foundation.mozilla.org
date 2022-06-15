b2a157ce18d49869e33ab5f0f16fec44befb73a6
Tibor Leupold <tiborl@mozillafoundation.org>
2022-06-15 14:31:58 -0700

    Revert "Remove the outdate no_filter_ui section"
    This reverts commit eb9ad3ddbeb200c532218c72c260a696890f210e.

--- network-api/networkapi/wagtailpages/pagemodels/blog/blog_index.py
+++ network-api/networkapi/wagtailpages/pagemodels/blog/blog_index.py
@@ -108,6 +108,9 @@ class BlogIndexPage(IndexPage):
 
         # The following code first updates page share metadata when filtered by topic.
         # First, updating metadata that is not localized
+        #
+        # make sure we bypass "x results for Y"
+        context['no_filter_ui'] = True
 
         # and that we don't show the primary tag/topic
         context['hide_classifiers'] = True
