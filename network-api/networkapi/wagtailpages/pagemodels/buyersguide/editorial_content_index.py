08a5b13e13a624f438c60a9bc3365b0eba4f4e67
Tibor Leupold <tibor.leupold@torchbox.com>
2022-07-20 15:13:09 -0700

    Add first page test and template file

--- network-api/networkapi/wagtailpages/pagemodels/buyersguide/editorial_content_index.py
+++ network-api/networkapi/wagtailpages/pagemodels/buyersguide/editorial_content_index.py
@@ -4,3 +4,4 @@ from wagtail.core import models as wagtail_models
 class BuyersGuideEditorialContentIndexPage(wagtail_models.Page):
     parent_page_types = ['wagtailpages.BuyersGuidePage']
     subpage_types = ['wagtailpages.BuyersGuideArticlePage']
+    template = 'pages/buyers_guide_editorial_content_index_page.html'
