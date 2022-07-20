34d1c8410a151deceffe824be51d721d7c57558b
Tibor Leupold <tibor.leupold@torchbox.com>
2022-07-20 13:22:58 -0700

    Fix formatting

--- network-api/networkapi/wagtailpages/tests/buyersguide/test_article_index.py
+++ network-api/networkapi/wagtailpages/tests/buyersguide/test_article_index.py
@@ -7,11 +7,11 @@ class BuyersGuideArticleIndexPageTest(wagtail_test.WagtailPageTests):
     def test_parents(self):
         self.assertAllowedParentPageTypes(
             child_model=pagemodels.BuyersGuideEditorialContentIndexPage,
-            parent_models={ pagemodels.BuyersGuidePage },
+            parent_models={pagemodels.BuyersGuidePage},
         )
 
     def test_children(self):
         self.assertAllowedSubpageTypes(
             parent_model=pagemodels.BuyersGuideEditorialContentIndexPage,
-            child_models={ pagemodels.BuyersGuideArticlePage },
+            child_models={pagemodels.BuyersGuideArticlePage},
         )
