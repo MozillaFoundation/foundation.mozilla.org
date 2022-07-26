6c4514ae8c2f3e2cc3d9bd3e8c614bb73a45dcfd
Tibor Leupold <tibor.leupold@torchbox.com>
2022-07-26 13:54:43 -0700

    Make content category translatable

--- network-api/networkapi/wagtailpages/pagemodels/buyersguide/taxonomies.py
+++ network-api/networkapi/wagtailpages/pagemodels/buyersguide/taxonomies.py
@@ -1,13 +1,14 @@
 from django.db import models
 
+from wagtail.core import models as wagtail_models
 from wagtail.snippets import models as snippet_models
 
 
 @snippet_models.register_snippet
-class BuyersGuideContentCategory(models.Model):
+class BuyersGuideContentCategory(wagtail_models.TranslatableMixin, models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
 
-    class Meta:
+    class Meta(wagtail_models.TranslatableMixin.Meta):
         verbose_name = 'Buyers Guide Content Category'
         verbose_name_plural = 'Buyers Guide Content Categories'
 
