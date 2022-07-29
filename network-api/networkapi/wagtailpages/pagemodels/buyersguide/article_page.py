e907954d9a6a4840ec34b4daae90eae61ca77230
Tibor Leupold <tibor.leupold@torchbox.com>
2022-07-29 16:43:17 -0700

    Set up basic template rendering for the article
    Because of the template inheritance and the PNI menu this requires
    particular data to be present in the tempalate context.
    
    I think this could probably be refactored into a template tag to make
    things a bit easier and not require each page type to pass this
    information into the context.

--- network-api/networkapi/wagtailpages/pagemodels/buyersguide/article_page.py
+++ network-api/networkapi/wagtailpages/pagemodels/buyersguide/article_page.py
@@ -1,6 +1,19 @@
+from django import http
 from wagtail.core import models as wagtail_models
 
+from networkapi.wagtailpages.pagemodels.mixin import foundation_metadata
 
-class BuyersGuideArticlePage(wagtail_models.Page):
+
+class BuyersGuideArticlePage(
+    foundation_metadata.FoundationMetadataPageMixin,
+    wagtail_models.Page
+):
     parent_page_types = ['wagtailpages.BuyersGuideEditorialContentIndexPage']
     subpage_types = []
+    template = 'pages/buyersguide/article_page.html'
+
+    def get_context(self, request: http.HttpRequest, *args, **kwargs) -> dict:
+        context = super().get_context(request, *args, **kwargs)
+        context['home_page'] = self.get_parent().get_parent().specific
+        return context
+
--- network-api/networkapi/wagtailpages/tests/buyersguide/test_article_page.py
+++ network-api/networkapi/wagtailpages/tests/buyersguide/test_article_page.py
@@ -1,15 +1,26 @@
-from wagtail.tests import utils as wagtail_test
+from http import HTTPStatus
 
+from networkapi.wagtailpages.tests import base as test_base
 from networkapi.wagtailpages import models as pagemodels
 from networkapi.wagtailpages.factory import buyersguide as buyersguide_factories
 
 
-class BuyersGuideArticlePageFactoryTest(wagtail_test.WagtailPageTests):
+class BuyersGuideArticlePageFactoryTest(test_base.WagtailpagesTestCase):
     def test_factory(self):
         buyersguide_factories.BuyersGuideArticlePageFactory()
 
 
-class BuyersGuideArticlePageTest(wagtail_test.WagtailPageTests):
+class BuyersGuideArticlePageTest(test_base.WagtailpagesTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.pni_homepage = buyersguide_factories.BuyersGuidePageFactory(
+            parent=cls.homepage,
+        )
+        cls.content_index = buyersguide_factories.BuyersGuideEditorialContentIndexPageFactory(
+            parent=cls.pni_homepage,
+        )
+
     def test_parents(self):
         self.assertAllowedParentPageTypes(
             child_model=pagemodels.BuyersGuideArticlePage,
@@ -20,4 +31,33 @@ class BuyersGuideArticlePageTest(wagtail_test.WagtailPageTests):
         self.assertAllowedSubpageTypes(
             parent_model=pagemodels.BuyersGuideArticlePage,
             child_models={},
+        )
+
+    def test_page_success(self):
+        article_page = buyersguide_factories.BuyersGuideArticlePageFactory(
+            parent=self.content_index,
+        )
+
+        response = self.client.get(article_page.url)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_template(self):
+        article_page = buyersguide_factories.BuyersGuideArticlePageFactory(
+            parent=self.content_index,
+        )
+
+        response = self.client.get(article_page.url)
+
+        self.assertTemplateUsed(
+            response=response,
+            template_name='pages/buyersguide/article_page.html',
+        )
+        self.assertTemplateUsed(
+            response=response,
+            template_name='pages/buyersguide/base.html',
+        )
+        self.assertTemplateUsed(
+            response=response,
+            template_name='pages/base.html',
         )
