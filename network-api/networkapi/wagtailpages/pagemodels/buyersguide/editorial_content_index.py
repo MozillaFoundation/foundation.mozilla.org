8a9839fb7608bd0356ea1b4568f8f3e46aa88a56
Tibor Leupold <tibor.leupold@torchbox.com>
2022-08-29 15:03:11 -0700

    Remove not needed heading override
    There is not acceptance criteria requesting this feature. The template is also not using it.

--- network-api/networkapi/wagtailpages/pagemodels/buyersguide/editorial_content_index.py
+++ network-api/networkapi/wagtailpages/pagemodels/buyersguide/editorial_content_index.py
@@ -2,7 +2,7 @@ from wagtail.core import models as wagtail_models
 from modelcluster.fields import ParentalKey
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from wagtail.admin.edit_handlers import PageChooserPanel, InlinePanel, MultiFieldPanel, FieldPanel
+from wagtail.admin.edit_handlers import PageChooserPanel, InlinePanel, MultiFieldPanel
 from wagtail.core.models import Orderable, TranslatableMixin
 from networkapi.wagtailpages.pagemodels import orderables
 
@@ -17,21 +17,9 @@ class BuyersGuideEditorialContentIndexPage(
     subpage_types = ['wagtailpages.BuyersGuideArticlePage']
     template = 'pages/buyersguide/editorial_content_index_page.html'
 
-    related_articles_heading = models.CharField(
-        max_length=50,
-        default=_('Related'),
-        blank=False,
-        null=False,
-        help_text=(
-            'Heading for the related/featured articles section.'
-            'Common choices are "Related", "Popular", etc.'
-        ),
-    )
-
     content_panels = wagtail_models.Page.content_panels + [
         MultiFieldPanel(
             [
-                FieldPanel('related_articles_heading', heading='Heading'),
                 InlinePanel(
                     'related_article_relations',
                     heading='Related articles',
@@ -41,7 +29,6 @@ class BuyersGuideEditorialContentIndexPage(
             ],
             heading='Related Articles',
         ),
-
     ]
 
     def get_context(self, request, *args, **kwargs):
