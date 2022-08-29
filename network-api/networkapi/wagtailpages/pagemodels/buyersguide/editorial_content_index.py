a84012a39153c50fb7688421ee314c8c7b9f52e6
Tibor Leupold <tibor.leupold@torchbox.com>
2022-08-29 15:00:01 -0700

    Move less important classes lower in module

--- network-api/networkapi/wagtailpages/pagemodels/buyersguide/editorial_content_index.py
+++ network-api/networkapi/wagtailpages/pagemodels/buyersguide/editorial_content_index.py
@@ -9,30 +9,6 @@ from networkapi.wagtailpages.pagemodels import orderables
 from networkapi.wagtailpages.pagemodels.mixin import foundation_metadata
 
 
-class BuyersGuideEditorialContentIndexPageArticlePageRelation(TranslatableMixin, Orderable):
-    page = ParentalKey(
-        'wagtailpages.BuyersGuideEditorialContentIndexPage',
-        related_name='related_article_relations',
-    )
-    article = models.ForeignKey(
-        'wagtailpages.BuyersGuideArticlePage',
-        on_delete=wagtail_models.models.CASCADE,
-        null=False,
-        blank=False,
-    )
-
-    panels = [PageChooserPanel('article')]
-
-    objects = orderables.OrderableRelationQuerySet.as_manager()
-    related_item_field_name = "article"
-
-    def __str__(self):
-        return f'{self.category.name} -> {self.article.title}'
-
-    class Meta(TranslatableMixin.Meta, Orderable.Meta):
-        pass
-
-
 class BuyersGuideEditorialContentIndexPage(
     foundation_metadata.FoundationMetadataPageMixin,
     wagtail_models.Page,
@@ -73,3 +49,27 @@ class BuyersGuideEditorialContentIndexPage(
         context["home_page"] = self.get_parent().specific
         context["items"] = self.get_descendants().public().live()
         return context
+
+
+class BuyersGuideEditorialContentIndexPageArticlePageRelation(TranslatableMixin, Orderable):
+    page = ParentalKey(
+        'wagtailpages.BuyersGuideEditorialContentIndexPage',
+        related_name='related_article_relations',
+    )
+    article = models.ForeignKey(
+        'wagtailpages.BuyersGuideArticlePage',
+        on_delete=wagtail_models.models.CASCADE,
+        null=False,
+        blank=False,
+    )
+
+    panels = [PageChooserPanel('article')]
+
+    objects = orderables.OrderableRelationQuerySet.as_manager()
+    related_item_field_name = "article"
+
+    def __str__(self):
+        return f'{self.category.name} -> {self.article.title}'
+
+    class Meta(TranslatableMixin.Meta, Orderable.Meta):
+        pass
