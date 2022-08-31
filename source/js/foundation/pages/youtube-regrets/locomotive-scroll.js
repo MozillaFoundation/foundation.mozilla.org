4c49b44faff980fa734f4f66148a5c90f2117046
Steven Steinwand <steven_ts@hotmail.com>
2022-08-30 19:30:01 -0600

    Add locomotive scroll back in

--- source/js/foundation/pages/youtube-regrets/locomotive-scroll.js
+++ source/js/foundation/pages/youtube-regrets/locomotive-scroll.js
@@ -1,13 +1,29 @@
 import LocomotiveScroll from "locomotive-scroll";
-import { gsap } from "gsap";
+import {gsap} from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import TextPlugin from "gsap/TextPlugin";
 
 export const initYoutubeRegretsLocomotiveScroll = () => {
+
+  const scrollContainerSelector = "[data-scroll-container]";
   // Locomotive scroll settings
-  // const scroll = new LocomotiveScroll({
-  //   el: document.querySelector("[data-scroll-container]"),
-  // });
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(scrollContainerSelector),
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(scrollContainerSelector, {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(scrollContainerSelector).style.transform ? "transform" : "fixed"
+  });
 
   // Gsap settings
   gsap.registerPlugin(ScrollTrigger, TextPlugin);
@@ -16,7 +32,7 @@ export const initYoutubeRegretsLocomotiveScroll = () => {
 
   // First Scene is special because of the X and the text appearing
   let firstScene = gsap
-    .timeline({ defaults: { duration: 20 } })
+    .timeline({defaults: {duration: 20}})
     .set(
       sceneOneText,
       {
@@ -26,7 +42,7 @@ export const initYoutubeRegretsLocomotiveScroll = () => {
       },
       1
     )
-    .to(".timeline__scene-image", { autoAlpha: 1, duration: 1 }, 1)
+    .to(".timeline__scene-image", {autoAlpha: 1, duration: 1}, 1)
     .set(
       sceneOneText,
       {
@@ -35,16 +51,26 @@ export const initYoutubeRegretsLocomotiveScroll = () => {
       },
       2
     )
-    .to(".timeline__cross", { autoAlpha: 1, duration: 2, delay: 5 }, 2);
+    .to(".timeline__cross", {autoAlpha: 1, duration: 2, delay: 5}, 2);
 
   ScrollTrigger.create({
     trigger: ".timeline__scene--first",
     start: "top top+=100",
     end: "+=1000",
+    scroller: scrollContainerSelector, // Must have this to accomodate locomotive scroll
     animation: firstScene,
     scrub: true,
     pin: true,
   });
+
+  // ScrollTrigger.create({
+  //   trigger: ".timeline",
+  //   start: "top top+=100",
+  //   end: "bottom 90%",
+  //   pin: ".timeline__person-date",
+  // onEnter: () => gsap.set(".timeline__person-date", { autoAlpha: 1 }),
+  // onLeaveBack: () => gsap.set(".timeline__person-date", { autoAlpha: 0 }),
+  // });
 
   // const personDate = document.querySelector(".timeline__person-date");
   //
@@ -94,4 +120,12 @@ export const initYoutubeRegretsLocomotiveScroll = () => {
   //     markers: true,
   //   });
   // });
+
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+
 };
