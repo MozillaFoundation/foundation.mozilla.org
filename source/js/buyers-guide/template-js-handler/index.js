c0fb897c8b2e9e77ec90d70f9aa36d2fcce9b7e3
Daniel Miranda <manieldiranda@gmail.com>
2022-09-16 12:23:18 -0700

    updated name

--- source/js/buyers-guide/template-js-handler/index.js
+++ source/js/buyers-guide/template-js-handler/index.js
@@ -1,6 +1,6 @@
 import mobileNavStickinessHandler from "./mobile-nav-stickiness-handler";
 import mobileSearchBar from "./mobile-search-bar";
-import diveDeeperListExpansionHandler from "./dive-deeper";
+import diveDeeperListExpansionHandler from "./product-page-dive-deeper-list";
 
 /**
  * Bind event handlers
