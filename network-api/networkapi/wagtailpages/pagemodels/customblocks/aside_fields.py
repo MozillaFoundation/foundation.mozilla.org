3fdb68bf552e2098c83339988b93411682a56409
Kevin <kbhowbrook@gmail.com>
2022-10-05 10:32:49 +0100

    Adds new aside block for aside area

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/aside_fields.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/aside_fields.py
@@ -2,6 +2,7 @@ from .. import customblocks
 
 
 aside_fields = [
+    ('aside', customblocks.AsideBlock()),
     ('linkbutton', customblocks.LinkButtonBlock()),
     ('spacer', customblocks.BootstrapSpacerBlock()),
 ]
