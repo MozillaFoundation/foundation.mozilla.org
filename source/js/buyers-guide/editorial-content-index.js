50ce3162b960b976f398baa0ed8d6ec08da479e4
Tibor Leupold <tibor.leupold@torchbox.com>
2022-10-11 13:38:35 -0700

    Avoid JS error due to non-existing elements

--- source/js/buyers-guide/editorial-content-index.js
+++ source/js/buyers-guide/editorial-content-index.js
@@ -1,25 +1,37 @@
 import 'htmx.org'
 
 function main(){
+  switchFromPaginationToLoadMore()
+
   htmx.onLoad(() => {
-    switchFromPaginationToLoadMore()
-    disableLoadMoreButtonOnRequest()
+    // Define what needs to happen whenever new content is added to the DOM.
+
+    // We need to configure this every time, because the button elements are destroyed
+    // and replace with a new element in the response.
+    setupLoadMoreButtonDisablingOnRequest()
   })
 }
 
 function switchFromPaginationToLoadMore(){
   const loadMore = document.getElementById('load-more')
-  loadMore.classList.remove('tw-hidden')
-
   const pagination = document.getElementById('pagination')
-  pagination.classList.add('tw-hidden')
+
+  if (loadMore && pagination) {
+    loadMore.classList.remove('tw-hidden')
+    pagination.classList.add('tw-hidden')
+  }
 }
 
-function disableLoadMoreButtonOnRequest() {
+function setupLoadMoreButtonDisablingOnRequest() {
   // Disable the load more button when the request is triggered.
   // This is a signal to the user an prevents duplicate triggering.
   // We don't need to reactivate the button because it is replaced with the response.
   const loadMore = document.getElementById('load-more')
+
+  if (!loadMore) {
+   return
+  }
+
   const loadMoreButton = loadMore.getElementsByTagName('button')[0]
   loadMoreButton.addEventListener('htmx:beforeRequest', (event) => {
     event.target.setAttribute('disabled', '')
