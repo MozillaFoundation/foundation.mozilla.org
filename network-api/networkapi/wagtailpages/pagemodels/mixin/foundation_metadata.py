817cbecc625de256145c68c8ade5b3a974b2f598
Daniel Miranda <manieldiranda@gmail.com>
2022-10-12 10:40:47 -0700

    was able to get desired fields set to required and update verbose names, but unable to hide show_in_menus

--- network-api/networkapi/wagtailpages/pagemodels/mixin/foundation_metadata.py
+++ network-api/networkapi/wagtailpages/pagemodels/mixin/foundation_metadata.py
@@ -14,6 +14,7 @@ class FoundationMetadataPageMixin(MetadataPageMixin):
         # The first Wagtail image returned that has the specified tag name will
         # be the default image URL in social shares when no Image is specified at the Page level
         super().__init__(*args, **kwargs)
+        self.update_labels_and_requirements()
 
         # This will run once in the life-time of the server, when the first page instance
         # that inherits this mixin gets instantiated. After that, this code will not kick
@@ -69,6 +70,17 @@ class FoundationMetadataPageMixin(MetadataPageMixin):
         # We still haven't found a social share image, so, last resort: return
         # whatever is the default social share image. Which could be `None`!
         return default_social_share_image
+
+    def update_labels_and_requirements(self):
+        share_image = self._meta.get_field("search_image")
+        meta_description = self._meta.get_field("search_description")
+        meta_description.blank = False
+        share_image.verbose_name = "Share Image"
+        share_image.help_text = """
+                                Image must be high quality, include our logo mark and have the dimensions 1200 x 628 px.
+                                For more design guidelines see https://foundation.mozilla.org/en/docs/brand/brand-identity/social-media/#og-images.
+                                """
+        share_image.blank = False
 
     class Meta:
         abstract = True
