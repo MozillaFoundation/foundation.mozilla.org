268b23fb02521308cd9e3ecff7432db15b9633a5
Simon Fessehaye <fessehay@ualberta.ca>
2022-10-28 19:43:53 -0700

    9542 - related articles on article page

--- source/js/buyers-guide/bg-main.js
+++ source/js/buyers-guide/bg-main.js
@@ -113,6 +113,22 @@ let main = {
     if (document.querySelector(`body.pni.catalog`)) {
       HomepageSlider.init();
     }
+    if (document.querySelector("#view-article")) {
+      const firstParagraph = document.querySelector(
+        ".paragraph-block .rich-text"
+      );
+      const relatedContainer = document.querySelector("#pni-related-content");
+      if (!firstParagraph || !relatedContainer) return;
+      const relatedContent = relatedContainer.querySelector("div");
+      const relatedPlace = firstParagraph.querySelector("*:nth-child(3)");
+      if (!relatedPlace) return;
+      firstParagraph.insertBefore(relatedContent, relatedPlace);
+      relatedContent.classList.add(
+        "large:tw-float-right",
+        "large:tw-pl-4",
+        "large:tw-mr-[-20%]"
+      );
+    }
   },
 };
 
