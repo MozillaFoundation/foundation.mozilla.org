3e3680b68820a3da2d676789f21fd1425c13fa4b
Simon Fessehaye <fessehay@ualberta.ca>
2022-11-08 09:36:24 -0800

    renamed to buyersguide prefix

--- source/js/buyers-guide/related-articles.js
+++ source/js/buyers-guide/related-articles.js
@@ -3,7 +3,9 @@ const RelatedArticles = {
     const firstParagraph = document.querySelector(
       ".paragraph-block .rich-text"
     );
-    const relatedContainer = document.querySelector("#pni-related-content");
+    const relatedContainer = document.querySelector(
+      "#buyersguide-related-content"
+    );
     if (!firstParagraph || !relatedContainer) return;
     const relatedContent = relatedContainer.querySelector("div");
     const relatedPlace = firstParagraph.querySelector("*:nth-child(3)");
