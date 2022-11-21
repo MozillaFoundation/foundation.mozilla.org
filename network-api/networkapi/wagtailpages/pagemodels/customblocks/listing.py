d280d1e9c2826ecc85f00e07f7ec45bdc698807c
Kevin <kbhowbrook@gmail.com>
2022-11-21 16:24:06 +0000

    Adds URL field for listing card

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/listing.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/listing.py
@@ -1,4 +1,7 @@
+from django.forms.utils import ErrorList
+
 from wagtail.core import blocks
+from wagtail.core.blocks.struct_block import StructBlockValidationError
 from wagtail.images.blocks import ImageChooserBlock
 
 
@@ -12,6 +15,22 @@ class ListingCard(blocks.StructBlock):
     body = blocks.RichTextBlock(features=["bold"], help_text="Body text of the card.")
 
     link = blocks.PageChooserBlock(required=False, help_text="Page that this should link out to.")
+
+    link_url = blocks.CharBlock(
+        required=False,
+        help_text="Optional URL that the header should link out to.",
+    )
+
+    def clean(self, value):
+        result = super().clean(value)
+        errors = {}
+
+        if value["link"] and value["link_url"]:
+            errors["link"] = ErrorList(["Please choose between a link OR a URL value."])
+        if errors:
+            raise StructBlockValidationError(errors)
+
+        return result
 
 
 class ListingBlock(blocks.StructBlock):
