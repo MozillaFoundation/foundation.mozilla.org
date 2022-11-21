1cafd0046eb2ed10acecfdea0973fc1c361beacb
Kevin <kbhowbrook@gmail.com>
2022-11-21 13:10:18 +0000

    Adds option to use URL field for teaser card links

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/text_only_teaser.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/text_only_teaser.py
@@ -1,12 +1,29 @@
+from django.forms.utils import ErrorList
 from wagtail.core import blocks
+from wagtail.core.blocks.struct_block import StructBlockValidationError
 
 
 class TextOnlyTeaserCard(blocks.StructBlock):
 
     heading = blocks.CharBlock(help_text="Heading for the card.")
     heading_link = blocks.PageChooserBlock(required=False, help_text="Page that the header should link out to.")
+    link_url = blocks.CharBlock(
+        required=False,
+        help_text="Optional URL that the header should link out to.",
+    )
     meta_data = blocks.CharBlock(max_length=500)
     body = blocks.TextBlock(help_text="Body text of the card.", required=False, max_length=200)
+
+    def clean(self, value):
+        result = super().clean(value)
+        errors = {}
+
+        if value["heading_link"] and value["link_url"]:
+            errors["heading_link"] = ErrorList(["Please choose between a heading link OR a URL value."])
+        if errors:
+            raise StructBlockValidationError(errors)
+
+        return result
 
 
 class TextOnlyTeaserBlock(blocks.StructBlock):
