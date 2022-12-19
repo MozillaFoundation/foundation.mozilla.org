e447a6fd935e7df05bea0da8aeb1f8294d8129a9
Tibor Leupold <tibor.leupold@torchbox.com>
2022-12-19 08:43:20 -0800

    Change tests to test clean method, not save

--- network-api/networkapi/wagtailpages/tests/buyersguide/test_taxonomies.py
+++ network-api/networkapi/wagtailpages/tests/buyersguide/test_taxonomies.py
@@ -8,40 +8,42 @@ class TestBuyersGuideContentCategory(test_base.WagtailpagesTestCase):
     def test_factory(self):
         buyersguide_factories.BuyersGuideContentCategoryFactory()
 
-    def test_slug_set_during_save(self):
+    def test_clean_sets_slug(self):
         category = buyersguide_factories.BuyersGuideContentCategoryFactory.build(
             title="Test category",
         )
         self.assertEqual(category.slug, "")
 
-        category.save()
+        category.clean()
 
         self.assertEqual(category.slug, "en-test-category")
 
-    def test_exisiting_slug_is_not_kept_during_save(self):
+    def test_clean_overrides_exisiting_slug(self):
         category = buyersguide_factories.BuyersGuideContentCategoryFactory(
             title="Test category",
             slug="not-the-slugified-title",
         )
 
-        category.save()
+        category.clean()
 
         self.assertEqual(category.title, "Test category")
         self.assertEqual(category.slug, "en-test-category")
 
-    def test_slug_has_to_be_unique(self):
-        buyersguide_factories.BuyersGuideContentCategoryFactory(
+    def test_clean_raises_if_category_with_same_title_and_locale_in_db(self):
+        existing_category = buyersguide_factories.BuyersGuideContentCategoryFactory(
             title="Test category",
         )
+        existing_category.full_clean()
+        existing_category.save()
         category = buyersguide_factories.BuyersGuideContentCategoryFactory.build(
             title="Test category",
         )
 
         with self.assertRaises(exceptions.ValidationError):
-            category.full_clean()
+            category.clean_slug()
 
-    def test_slug_different_for_different_locales(self):
-        category_default_locale = buyersguide_factories.BuyersGuideContentCategoryFactory(
+    def test_clean_slug_different_for_different_locales(self):
+        category_default_locale = buyersguide_factories.BuyersGuideContentCategoryFactory.build(
             title="Test category",
         )
         category_fr_locale = buyersguide_factories.BuyersGuideContentCategoryFactory.build(
@@ -49,13 +51,14 @@ class TestBuyersGuideContentCategory(test_base.WagtailpagesTestCase):
             locale=self.fr_locale,
         )
 
-        category_fr_locale.save()
+        category_default_locale.clean_slug()
+        category_fr_locale.clean_slug()
 
         self.assertEqual(category_fr_locale.title, category_default_locale.title)
         self.assertNotEqual(category_fr_locale.locale, category_default_locale.locale)
         self.assertNotEqual(category_fr_locale.slug, category_default_locale.slug)
 
-    def test_copy_for_translation(self):
+    def test_can_clean_and_save_copy_for_translation(self):
         category_default_locale = buyersguide_factories.BuyersGuideContentCategoryFactory(
             title="Test category",
         )
