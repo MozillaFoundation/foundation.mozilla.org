3a0072a5c294d550d4be4c010a3203326f53ad80
Tibor Leupold <tibor.leupold@torchbox.com>
2022-12-23 13:38:47 -0800

    Test entries have same locale as index page

--- network-api/networkapi/wagtailpages/tests/test_index.py
+++ network-api/networkapi/wagtailpages/tests/test_index.py
@@ -8,6 +8,11 @@ from networkapi.wagtailpages.tests import base as test_base
 
 
 class IndexPageTests(test_base.WagtailpagesTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.index_page = index_factory.IndexPageFactory(parent=cls.homepage)
+
     def test_GET_child_in_entries(self):
         """
         Currently, this is returning the generic page type, which requires the use of
@@ -17,13 +22,22 @@ class IndexPageTests(test_base.WagtailpagesTestCase):
 
         TODO: Refactor implementation to return specific page types of child pages.
         """
-        index_page = index_factory.IndexPageFactory(parent=self.homepage)
-        article_page = publication_factory.ArticlePageFactory(parent=index_page)
+        article_page = publication_factory.ArticlePageFactory(parent=self.index_page)
         generic_child_page = wagtail_models.Page.objects.get(id=article_page.id)
 
-        response = self.client.get(path=index_page.get_url())
+        response = self.client.get(path=self.index_page.get_url())
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIn(generic_child_page, response.context["entries"])
 
+    def test_GET_entries_have_same_locale_as_index_page(self):
+        article_page = publication_factory.ArticlePageFactory(parent=self.index_page)
+        self.synchronize_tree()
+        fr_index_page = self.index_page.get_translation(self.fr_locale)
 
+        response = self.client.get(path=fr_index_page.get_url())
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        fr_entry_page = response.context["entries"][0]
+        self.assertNotEqual(fr_entry_page.id, article_page.id)
+        self.assertEqual(fr_entry_page.locale, self.fr_locale)
