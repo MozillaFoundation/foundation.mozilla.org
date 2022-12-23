0c6e9ac846a3d62cf3e90f4543d7be6b57262cd9
Tibor Leupold <tibor.leupold@torchbox.com>
2022-12-23 11:26:30 -0800

    Test publication factories
    Because they don't work currently.

---
+++ network-api/networkapi/wagtailpages/tests/test_publication.py
@@ -0,0 +1,12 @@
+from networkapi.wagtailpages.factory import publication as publication_factory
+from networkapi.wagtailpages.tests import base as test_base
+
+
+class PublicationPageTest(test_base.WagtailpagesTestCase):
+    def test_factory(self):
+        publication_factory.PublicationPageFactory()
+
+
+class ArticlePageTest(test_base.WagtailpagesTestCase):
+    def test_factory(self):
+        publication_factory.ArticlePageFactory()
