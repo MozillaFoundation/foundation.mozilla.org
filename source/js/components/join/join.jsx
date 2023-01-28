1f7ba4f2df60f5deb596c03b1ca5e734af2706fe
Simon Fessehaye <fessehay@ualberta.ca>
2023-01-27 19:50:36 -0800

    Added spacing 12

--- source/js/components/join/join.jsx
+++ source/js/components/join/join.jsx
@@ -533,7 +533,7 @@ class JoinUs extends Component {
 
     if (this.props.formStyle == "pop") {
       classnames = classNames(
-        "tw-border-1 tw-btn-secondary tw-mt-24 medium:-tw-mb-16 medium:tw-mt-5"
+        "tw-border-1 tw-btn-secondary tw-mt-24 medium:-tw-mb-16 medium:tw-mt-12"
       );
       buttonText = getText("Subscribe");
     } else if (this.props.formStyle == "pni") {
