f5f761769988fe65b84248469908cef361ec48bc
Tibor Leupold <tibor.leupold@torchbox.com>
2023-02-20 13:15:25 -0800

    Add docs string to the index page

--- network-api/networkapi/wagtailpages/pagemodels/buyersguide/product_index_page.py
+++ network-api/networkapi/wagtailpages/pagemodels/buyersguide/product_index_page.py
@@ -4,5 +4,13 @@ from networkapi.wagtailpages.pagemodels.mixin import foundation_metadata
 
 
 class ProductIndexPage(foundation_metadata.FoundationMetadataPageMixin, wagtail_models.Page):
+    """
+    Index page to be the parent of all product pages.
+
+    After the editorial content index was added to the buyer's guide, it was hard to find it, because it was mixed in
+    with the long list of product pages. This product index page is added to group the product pages in a "folder" to
+    make it easier to navigate in the admin.
+    """
+
     parent_page_types = ["wagtailpages.BuyersGuidePage"]
     subpage_types = ["wagtailpages.GeneralProductPage"]
