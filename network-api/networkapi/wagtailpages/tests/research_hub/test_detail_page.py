6110ffddbb807dcde29ba98df647e49dde80e3f6
Tibor Leupold <tibor.leupold@torchbox.com>
2023-03-22 08:29:55 -0700

    Add signatures to test methods

--- network-api/networkapi/wagtailpages/tests/research_hub/test_detail_page.py
+++ network-api/networkapi/wagtailpages/tests/research_hub/test_detail_page.py
@@ -8,7 +8,7 @@ from networkapi.wagtailpages.tests.research_hub import base as research_test_bas
 
 
 class TestResearchLibraryDetailPage(research_test_base.ResearchHubTestCase):
-    def test_research_detail_page_breadcrumbs(self):
+    def test_research_detail_page_breadcrumbs(self) -> None:
         detail_page = detail_page_factory.ResearchDetailPageFactory(
             parent=self.library_page,
         )
@@ -24,7 +24,7 @@ class TestResearchLibraryDetailPage(research_test_base.ResearchHubTestCase):
 
 
 class TestResearchDetailLink(research_test_base.ResearchHubTestCase):
-    def test_clean_with_url(self):
+    def test_clean_with_url(self) -> None:
         link = detail_page_factory.ResearchDetailLinkFactory.build(with_url=True)
 
         link.clean()
@@ -33,7 +33,7 @@ class TestResearchDetailLink(research_test_base.ResearchHubTestCase):
         self.assertFalse(link.page)
         self.assertFalse(link.document)
 
-    def test_clean_with_page(self):
+    def test_clean_with_page(self) -> None:
         link = detail_page_factory.ResearchDetailLinkFactory.build(with_page=True)
 
         link.clean()
@@ -42,7 +42,7 @@ class TestResearchDetailLink(research_test_base.ResearchHubTestCase):
         self.assertFalse(link.url)
         self.assertFalse(link.document)
 
-    def test_clean_with_doc(self):
+    def test_clean_with_doc(self) -> None:
         link = detail_page_factory.ResearchDetailLinkFactory.build(with_document=True)
 
         link.clean()
@@ -51,7 +51,7 @@ class TestResearchDetailLink(research_test_base.ResearchHubTestCase):
         self.assertFalse(link.url)
         self.assertFalse(link.page)
 
-    def test_clean_with_url_and_page(self):
+    def test_clean_with_url_and_page(self) -> None:
         link = detail_page_factory.ResearchDetailLinkFactory.build(
             with_url=True,
             with_page=True,
@@ -60,7 +60,7 @@ class TestResearchDetailLink(research_test_base.ResearchHubTestCase):
         with self.assertRaises(exceptions.ValidationError):
             link.clean()
 
-    def test_clean_with_url_and_doc(self):
+    def test_clean_with_url_and_doc(self) -> None:
         link = detail_page_factory.ResearchDetailLinkFactory.build(
             with_url=True,
             with_document=True,
@@ -69,7 +69,7 @@ class TestResearchDetailLink(research_test_base.ResearchHubTestCase):
         with self.assertRaises(exceptions.ValidationError):
             link.clean()
 
-    def test_clean_with_page_and_doc(self):
+    def test_clean_with_page_and_doc(self) -> None:
         link = detail_page_factory.ResearchDetailLinkFactory.build(
             with_url=True,
             with_page=True,
@@ -78,7 +78,7 @@ class TestResearchDetailLink(research_test_base.ResearchHubTestCase):
         with self.assertRaises(exceptions.ValidationError):
             link.clean()
 
-    def test_clean_with_url_and_page_and_doc(self):
+    def test_clean_with_url_and_page_and_doc(self) -> None:
         link = detail_page_factory.ResearchDetailLinkFactory.build(
             with_url=True,
             with_page=True,
@@ -94,31 +94,31 @@ class TestResearchDetailLink(research_test_base.ResearchHubTestCase):
         with self.assertRaises(exceptions.ValidationError):
             link.clean()
 
-    def test_get_url_with_url(self):
+    def test_get_url_with_url(self) -> None:
         url = "https://example.com"
         link = detail_page_factory.ResearchDetailLinkFactory.build(url=url)
 
         self.assertEqual(link.get_url(), url)
 
-    def test_get_url_with_page(self):
+    def test_get_url_with_page(self) -> None:
         page = wagtail_factories.PageFactory(parent=self.homepage)
         link = detail_page_factory.ResearchDetailLinkFactory.build(page=page)
 
         self.assertEqual(link.get_url(), page.get_url())
 
-    def test_get_url_with_non_live_page(self):
+    def test_get_url_with_non_live_page(self) -> None:
         page = wagtail_factories.PageFactory(parent=self.homepage, live=False)
         link = detail_page_factory.ResearchDetailLinkFactory.build(page=page)
 
         self.assertEqual(link.get_url(), "")
 
-    def test_get_url_with_doc(self):
+    def test_get_url_with_doc(self) -> None:
         doc = wagtail_factories.DocumentFactory()
         link = detail_page_factory.ResearchDetailLinkFactory.build(document=doc)
 
         self.assertEqual(link.get_url(), doc.url)
 
-    def test_get_url_without_needed_data(self):
+    def test_get_url_without_needed_data(self) -> None:
         """
         Test the invalid link cas.
 
