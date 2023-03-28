3de9ffc4897daae1bfa5acb2ff7cd15331fa34f6
Tibor Leupold <tibor.leupold@torchbox.com>
2023-03-28 09:01:50 -0700

    Add test file for library page

---
+++ network-api/networkapi/wagtailpages/tests/research_hub/test_landing_page.py
@@ -0,0 +1,33 @@
+import datetime
+
+from django.utils import timezone
+
+from networkapi.wagtailpages.factory.research_hub import detail_page as detail_page_factory
+from networkapi.wagtailpages.tests.research_hub import base
+
+
+class ResearchLandingPageTestCase(base.ResearchHubTestCase):
+    def create_research_detail_page(self, days_ago=0):
+        publication_date = timezone.now().date() - datetime.timedelta(days=days_ago)
+        return detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            original_publication_date=publication_date,
+        )
+
+    def test_get_latest_research_pages_returns_three_latest_pages_by_publication_date(self):
+        """
+        Ensure that the latest research pages are returned
+        """
+        detail_page_1 = self.create_research_detail_page(days_ago=4)
+        detail_page_2 = self.create_research_detail_page(days_ago=3)
+        detail_page_3 = self.create_research_detail_page(days_ago=2)
+        detail_page_4 = self.create_research_detail_page(days_ago=1)
+
+        latest_research_pages = self.landing_page.get_latest_research_pages()
+
+        self.assertEqual(len(latest_research_pages), 3)
+        self.assertIn(detail_page_4, latest_research_pages)
+        self.assertIn(detail_page_3, latest_research_pages)
+        self.assertIn(detail_page_2, latest_research_pages)
+        self.assertNotIn(detail_page_1, latest_research_pages)
+
