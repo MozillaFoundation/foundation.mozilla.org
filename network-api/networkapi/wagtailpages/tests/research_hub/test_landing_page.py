570542fdcd18b5cb1a574276133521d8bd40a2f0
Tibor Leupold <tibor.leupold@torchbox.com>
2023-03-28 12:06:54 -0700

    Add landing page test for private detail page

--- network-api/networkapi/wagtailpages/tests/research_hub/test_landing_page.py
+++ network-api/networkapi/wagtailpages/tests/research_hub/test_landing_page.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.utils import timezone
+from wagtail import models as wagtail_models
 
 from networkapi.wagtailpages.factory.research_hub import detail_page as detail_page_factory
 from networkapi.wagtailpages.tests.research_hub import base
@@ -31,3 +32,20 @@ class ResearchLandingPageTestCase(base.ResearchHubTestCase):
         self.assertIn(detail_page_2, latest_research_pages)
         self.assertNotIn(detail_page_1, latest_research_pages)
 
+    def test_get_latest_research_pages_does_not_return_private_page(self):
+        """
+        Ensure that the latest research pages don't contain private pages.
+        """
+        detail_page_public = self.create_research_detail_page()
+        detail_page_private = self.create_research_detail_page()
+        wagtail_models.PageViewRestriction.objects.create(
+            page=detail_page_private,
+            restriction_type=wagtail_models.PageViewRestriction.PASSWORD,
+            password="test",
+        )
+
+        latest_research_pages = self.landing_page.get_latest_research_pages()
+
+        self.assertEqual(len(latest_research_pages), 1)
+        self.assertIn(detail_page_public, latest_research_pages)
+        self.assertNotIn(detail_page_private, latest_research_pages)
