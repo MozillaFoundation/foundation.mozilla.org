2c3c3d17e22c234ccef0915313c435e8bcd63544
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-04-05 11:13:32 -0300

    Add tests to `localize_queryset` func

--- network-api/networkapi/wagtailpages/tests/test_utils.py
+++ network-api/networkapi/wagtailpages/tests/test_utils.py
@@ -1,10 +1,12 @@
 from os.path import abspath, dirname, join
 
 from django.test import TestCase
+from django.utils import translation
 from django.utils.translation.trans_real import (
     parse_accept_lang_header as django_parse_accept_lang_header,
 )
 from django.utils.translation.trans_real import to_language as django_to_language
+from wagtail.core.models import Locale
 from wagtail.images.models import Image
 from wagtail.models import Collection
 
@@ -13,7 +15,9 @@ from networkapi.wagtailpages import (
     parse_accept_lang_header,
     to_language,
 )
-from networkapi.wagtailpages.utils import create_wagtail_image
+from networkapi.wagtailpages.factory.profiles import ProfileFactory
+from networkapi.wagtailpages.pagemodels.profiles import Profile
+from networkapi.wagtailpages.utils import create_wagtail_image, localize_queryset
 
 
 class TestCreateWagtailImageUtility(TestCase):
@@ -90,3 +94,107 @@ class TestDjangoTranslationUtilityOverrides(TestCase):
             django_parse_accept_lang_header("fy-NL,fy;q=0.5"),
             (("fy-NL", 1.0), ("fy", 0.5)),
         )
+
+
+class TestLocalizeQueryset(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        (cls.default_locale, _) = Locale.objects.get_or_create(language_code="en")
+        (cls.active_locale, _) = Locale.objects.get_or_create(language_code="fr")
+
+    def test_localize_queryset_with_items_only_in_default_locale(self):
+        """Tests that the function returns a queryset with only one version of each item
+        when the queryset contains items only in default locale. tags: [happy path]"""
+        # Create items only in default locale
+        item1 = ProfileFactory(locale=self.default_locale)
+        item2 = ProfileFactory(locale=self.default_locale)
+
+        # Override the current language to be the active locale
+        translation.activate(self.default_locale.language_code)
+
+        # Call the function
+        result = localize_queryset(Profile.objects.all())
+
+        # Assert that only one version of each item is returned
+        self.assertEqual(len(result), 2)
+        # Assert that both items are returned
+        self.assertIn(item1, result)
+        self.assertIn(item2, result)
+
+    def test_localize_queryset_with_items_only_in_active_locale(self):
+        """Tests that the function returns a queryset with only one version of each item
+        when the queryset contains items only in active locale. tags: [happy path]"""
+        # Create items only in default locale
+        item1 = ProfileFactory(locale=self.active_locale)
+        item2 = ProfileFactory(locale=self.active_locale)
+
+        # Override the current language to be the active locale
+        translation.activate(self.active_locale.language_code)
+
+        # Call the function
+        result = localize_queryset(Profile.objects.all())
+
+        # Assert that only one version of each item is returned
+        self.assertEqual(len(result), 2)
+        # Assert that both items are returned
+        self.assertIn(item1, result)
+        self.assertIn(item2, result)
+
+    def test_localize_queryset_with_items_in_both_default_and_active_locales(self):
+        """Tests that the function returns a queryset with only one version of each item
+        when the queryset contains items in both default and active locales. tags: [happy path]"""
+        # Create items in both default and active locales
+        default = ProfileFactory(locale=self.default_locale)
+        active = ProfileFactory(locale=self.active_locale, translation_key=default.translation_key)
+
+        # Override the current language to be the active locale
+        translation.activate(self.active_locale.language_code)
+
+        # Call the function
+        result = localize_queryset(Profile.objects.all())
+
+        # Assert that only one version of each item is returned
+        self.assertEqual(len(result), 1)
+        # Assert that only the item in the active locale is returned
+        self.assertNotIn(default, result)
+        self.assertIn(active, result)
+
+    def test_localize_queryset_with_multiple_items_in_both_locales(self):
+        """Tests that the function returns a queryset with only one version of each item
+        when the queryset contains translated and not-translated items. tags: [happy path]"""
+        # Create items in default locale:
+        default1 = ProfileFactory(locale=self.default_locale)
+        default2 = ProfileFactory(locale=self.default_locale)
+        default3 = ProfileFactory(locale=self.default_locale)
+        default4 = ProfileFactory(locale=self.default_locale)
+
+        # Translate some items to active locale:
+        active1 = ProfileFactory(locale=self.active_locale, translation_key=default1.translation_key)
+        active2 = ProfileFactory(locale=self.active_locale, translation_key=default2.translation_key)
+
+        # Override the current language to be the active locale
+        translation.activate(self.active_locale.language_code)
+
+        # Call the function
+        result = localize_queryset(Profile.objects.all())
+
+        # We should see 4 items in the result
+        self.assertEqual(len(result), 4)
+        # For items that have translation, the active locale version should be returned
+        self.assertNotIn(default1, result)
+        self.assertIn(active1, result)
+        self.assertNotIn(default2, result)
+        self.assertIn(active2, result)
+        # For items that don't have translation, the default locale version should be returned
+        self.assertIn(default3, result)
+        self.assertIn(default4, result)
+
+
+def test_localize_queryset_with_empty_queryset(self):
+    """Tests that the function returns an empty queryset when the input queryset is empty. tags: [edge case]"""
+    # Call the function with an empty queryset
+    result = localize_queryset(Profile.objects.none())
+
+    # Assert that an empty queryset is returned
+    self.assertEqual(len(result), 0)
