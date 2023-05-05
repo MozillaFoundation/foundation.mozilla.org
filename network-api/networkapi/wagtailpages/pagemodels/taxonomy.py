b078d59ed157b1ba044ab3dee7e0ec527f6b0896
Kevin Howbrook <kbhowbrook@gmail.com>
2023-05-05 10:28:18 +0100

    Extend from TranslatableMixin as all taxonomies will have this

--- network-api/networkapi/wagtailpages/pagemodels/taxonomy.py
+++ network-api/networkapi/wagtailpages/pagemodels/taxonomy.py
@@ -1,30 +1,22 @@
 from django.db import models
 from wagtail.admin import FieldPanel
+from wagtail.models import TranslatableMixin
+from wagtail.search import index
 
 
-class BaseTaxonomy(models.Model):
-    """
-    Defines BaseTaxonomy which serves as an abstract base class
-    for creating taxonomy models.
-
-    Attributes:
-        abstract (bool): A boolean attribute set to True indicating that the model is abstract.
-        name (CharField): A required CharField that represents the name of the taxonomy.
-        panels (list): A list of panel objects that can be used in the Wagtail admin interface for
-            editing instances of this model.
-
-    Methods:
-        str(): Returns a string representation of the name of the taxonomy.
-
-    Note:
-        This model should be subclassed to create specific taxonomy models with additional fields.
-    """
-
-    abstract = True
+class BaseTaxonomy(TranslatableMixin):
     name = models.CharField(max_length=70, blank=False)
 
     panels = [
         FieldPanel("name"),
+    ]
+
+    class Meta:
+        abstract = True
+
+    search_fields = [
+        index.SearchField("name", partial_match=True),
+        index.FilterField("locale_id"),
     ]
 
     def __str__(self):
