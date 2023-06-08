a60c61d6b227dc86ef1e228fc01c29488fbce54c
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-06-08 11:09:52 -0300

    Refactor Research Hub library page tests

--- network-api/networkapi/wagtailpages/tests/libraries/research_hub/test_library_page.py
+++ network-api/networkapi/wagtailpages/tests/libraries/research_hub/test_library_page.py
@@ -75,6 +75,113 @@ class TestResearchLibraryPage(research_test_base.ResearchHubTestCase):
         self.assertIn(public_detail_page, research_detail_pages)
         self.assertNotIn(private_detail_page, research_detail_pages)
 
+    def test_sort_newest_first(self):
+        oldest_page = detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            original_publication_date=research_test_utils.days_ago(2),
+        )
+        newest_page = detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            original_publication_date=research_test_utils.days_ago(1),
+        )
+
+        research_detail_pages = list(self.library_page._get_research_detail_pages(sort=constants.SORT_NEWEST_FIRST))
+
+        newest_page_index = research_detail_pages.index(newest_page)
+        oldest_page_index = research_detail_pages.index(oldest_page)
+        self.assertLess(newest_page_index, oldest_page_index)
+
+    def test_sort_oldest_first(self):
+        oldest_page = detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            original_publication_date=research_test_utils.days_ago(2),
+        )
+        newest_page = detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            original_publication_date=research_test_utils.days_ago(1),
+        )
+
+        research_detail_pages = list(self.library_page._get_research_detail_pages(sort=constants.SORT_OLDEST_FIRST))
+
+        newest_page_index = research_detail_pages.index(newest_page)
+        oldest_page_index = research_detail_pages.index(oldest_page)
+        self.assertLess(oldest_page_index, newest_page_index)
+
+    def test_sort_alphabetical(self):
+        apple_page = detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            title="Apple",
+        )
+        banana_page = detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            title="Banana",
+        )
+
+        research_detail_pages = list(self.library_page._get_research_detail_pages(sort=constants.SORT_ALPHABETICAL))
+
+        apple_page_index = research_detail_pages.index(apple_page)
+        banana_page_index = research_detail_pages.index(banana_page)
+        self.assertLess(apple_page_index, banana_page_index)
+
+    def test_sort_alphabetical_reversed(self):
+
+        apple_page = detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            title="Apple",
+        )
+        banana_page = detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            title="Banana",
+        )
+
+        research_detail_pages = list(
+            self.library_page._get_research_detail_pages(sort=constants.SORT_ALPHABETICAL_REVERSED)
+        )
+
+        apple_page_index = research_detail_pages.index(apple_page)
+        banana_page_index = research_detail_pages.index(banana_page)
+        self.assertLess(banana_page_index, apple_page_index)
+
+    def test_get_research_detail_pages_sort_default(self):
+
+        detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            original_publication_date=research_test_utils.days_ago(2),
+        )
+        detail_page_factory.ResearchDetailPageFactory(
+            parent=self.library_page,
+            original_publication_date=research_test_utils.days_ago(1),
+        )
+
+        default_sort_detail_pages = list(self.library_page._get_research_detail_pages())
+        newest_first_detail_pages = list(
+            self.library_page._get_research_detail_pages(sort=constants.SORT_NEWEST_FIRST)
+        )
+
+        self.assertEqual(default_sort_detail_pages, newest_first_detail_pages)
+
+    def test_pagination(self):
+        self.library_page.results_count = 4
+        self.library_page.save()
+        for _ in range(6):
+            detail_page_factory.ResearchDetailPageFactory(parent=self.library_page)
+
+        research_detail_pages = self.library_page._get_research_detail_pages()
+
+        research_detail_pages_paginator = paginator.Paginator(
+            object_list=research_detail_pages,
+            per_page=self.library_page.results_count,
+            allow_empty_first_page=True,
+        )
+
+        first_page_response = research_detail_pages_paginator.get_page(1)
+        second_page_response = research_detail_pages_paginator.get_page(2)
+
+        self.assertEqual(len(first_page_response), 4)
+        self.assertEqual(len(second_page_response), 2)
+
+
+class TestResearchLibraryPageSearch(TestResearchLibraryPage):
     def test_search_by_detail_page_title(self):
         # Fields other than title are empty to avoid accidental test failures due to
         # fake data generation.
@@ -272,91 +379,8 @@ class TestResearchLibraryPage(research_test_base.ResearchHubTestCase):
         self.assertIn(apple_page, research_detail_pages)
         self.assertNotIn(banana_page, research_detail_pages)
 
-    def test_sort_newest_first(self):
-        oldest_page = detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            original_publication_date=research_test_utils.days_ago(2),
-        )
-        newest_page = detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            original_publication_date=research_test_utils.days_ago(1),
-        )
 
-        research_detail_pages = list(self.library_page._get_research_detail_pages(sort=constants.SORT_NEWEST_FIRST))
-
-        newest_page_index = research_detail_pages.index(newest_page)
-        oldest_page_index = research_detail_pages.index(oldest_page)
-        self.assertLess(newest_page_index, oldest_page_index)
-
-    def test_sort_oldest_first(self):
-        oldest_page = detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            original_publication_date=research_test_utils.days_ago(2),
-        )
-        newest_page = detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            original_publication_date=research_test_utils.days_ago(1),
-        )
-
-        research_detail_pages = list(self.library_page._get_research_detail_pages(sort=constants.SORT_OLDEST_FIRST))
-
-        newest_page_index = research_detail_pages.index(newest_page)
-        oldest_page_index = research_detail_pages.index(oldest_page)
-        self.assertLess(oldest_page_index, newest_page_index)
-
-    def test_sort_alphabetical(self):
-        apple_page = detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            title="Apple",
-        )
-        banana_page = detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            title="Banana",
-        )
-
-        research_detail_pages = list(self.library_page._get_research_detail_pages(sort=constants.SORT_ALPHABETICAL))
-
-        apple_page_index = research_detail_pages.index(apple_page)
-        banana_page_index = research_detail_pages.index(banana_page)
-        self.assertLess(apple_page_index, banana_page_index)
-
-    def test_sort_alphabetical_reversed(self):
-
-        apple_page = detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            title="Apple",
-        )
-        banana_page = detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            title="Banana",
-        )
-
-        research_detail_pages = list(
-            self.library_page._get_research_detail_pages(sort=constants.SORT_ALPHABETICAL_REVERSED)
-        )
-
-        apple_page_index = research_detail_pages.index(apple_page)
-        banana_page_index = research_detail_pages.index(banana_page)
-        self.assertLess(banana_page_index, apple_page_index)
-
-    def test_get_research_detail_pages_sort_default(self):
-
-        detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            original_publication_date=research_test_utils.days_ago(2),
-        )
-        detail_page_factory.ResearchDetailPageFactory(
-            parent=self.library_page,
-            original_publication_date=research_test_utils.days_ago(1),
-        )
-
-        default_sort_detail_pages = list(self.library_page._get_research_detail_pages())
-        newest_first_detail_pages = list(
-            self.library_page._get_research_detail_pages(sort=constants.SORT_NEWEST_FIRST)
-        )
-
-        self.assertEqual(default_sort_detail_pages, newest_first_detail_pages)
-
+class TestResearchLibraryPageFilters(TestResearchLibraryPage):
     def test_filter_author_profile(self):
         detail_page_1 = detail_page_factory.ResearchDetailPageFactory(
             parent=self.library_page,
@@ -600,23 +624,3 @@ class TestResearchLibraryPage(research_test_base.ResearchHubTestCase):
 
         self.assertIn(detail_page_1, research_detail_pages)
         self.assertNotIn(detail_page_2, research_detail_pages)
-
-    def test_pagination(self):
-        self.library_page.results_count = 4
-        self.library_page.save()
-        for _ in range(6):
-            detail_page_factory.ResearchDetailPageFactory(parent=self.library_page)
-
-        research_detail_pages = self.library_page._get_research_detail_pages()
-
-        research_detail_pages_paginator = paginator.Paginator(
-            object_list=research_detail_pages,
-            per_page=self.library_page.results_count,
-            allow_empty_first_page=True,
-        )
-
-        first_page_response = research_detail_pages_paginator.get_page(1)
-        second_page_response = research_detail_pages_paginator.get_page(2)
-
-        self.assertEqual(len(first_page_response), 4)
-        self.assertEqual(len(second_page_response), 2)
