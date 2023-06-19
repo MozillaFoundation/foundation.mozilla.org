62888eaf4288cc364d7c4c4466f1f3d75e2e292b
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-06-19 16:12:33 -0300

    Add an abstract BaseLibraryPage

--- network-api/networkapi/wagtailpages/pagemodels/libraries/rcc/library_page.py
+++ network-api/networkapi/wagtailpages/pagemodels/libraries/rcc/library_page.py
@@ -1,131 +1,48 @@
 import typing
-from typing import Optional
 
-from django.core import paginator
-from django.db import models
-from wagtail import images as wagtail_images
 from wagtail import models as wagtail_models
-from wagtail.admin import panels
-from wagtail.images import edit_handlers as image_panels
-from wagtail_localize.fields import SynchronizedField, TranslatableField
 
 from networkapi.wagtailpages import utils
 from networkapi.wagtailpages.pagemodels import profiles as profile_models
-from networkapi.wagtailpages.pagemodels.base import BasePage
 from networkapi.wagtailpages.pagemodels.libraries.rcc import (
-    constants,
     detail_page,
     taxonomies,
 )
 from networkapi.wagtailpages.pagemodels.libraries.rcc.forms import (
     RCCLibraryPageFilterForm,
 )
+from networkapi.wagtailpages.pagemodels.libraries import library_page as base_library_page
 
 if typing.TYPE_CHECKING:
-    from django import http
-    from django import template as django_template
+    from django import forms
 
 
-class RCCLibraryPage(BasePage):
-    max_count = 1
-
+class RCCLibraryPage(base_library_page.BaseLibraryPage):
     parent_page_types = ["RCCLandingPage"]
 
     subpage_types = ["RCCDetailPage"]
 
     template = "pages/libraries/rcc/library_page.html"
 
-    SORT_CHOICES = constants.SORT_CHOICES
+    @property
+    def filter_form(self):
+        """Form class used to filter detail pages for this page."""
+        return RCCLibraryPageFilterForm
 
-    banner_image = models.ForeignKey(
-        wagtail_images.get_image_model_string(),
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-    )
-
-    results_count = models.PositiveSmallIntegerField(
-        default=10,
-        help_text="Maximum number of results to be displayed per page.",
-    )
-
-    content_panels = BasePage.content_panels + [
-        image_panels.FieldPanel("banner_image"),
-    ]
-
-    settings_panels = BasePage.settings_panels + [panels.FieldPanel("results_count")]
-
-    translatable_fields = [
-        # Content tab fields
-        TranslatableField("title"),
-        # Promote tab fields
-        SynchronizedField("slug"),
-        TranslatableField("seo_title"),
-        SynchronizedField("show_in_menus"),
-        TranslatableField("search_description"),
-        SynchronizedField("search_image"),
-    ]
-
-    def get_context(self, request: "http.HttpRequest") -> "django_template.Context":
-        search_query: str = request.GET.get("search", "")
-        sort_value: str = request.GET.get("sort", "")
-        sort: constants.SortOption = constants.SORT_CHOICES.get(sort_value, constants.SORT_NEWEST_FIRST)
-
-        filter_form = RCCLibraryPageFilterForm(request.GET, label_suffix="")
-
+    def get_filtered_detail_pages(self, filter_form: "forms.Form"):
+        """Return the article detail pages that match the given filters in the form."""
         if filter_form.is_valid():
-            filtered_author_ids: list[int] = filter_form.cleaned_data["authors"]
-            filtered_content_type_ids: list[int] = filter_form.cleaned_data["content_types"]
-            filtered_curricular_area_ids: list[int] = filter_form.cleaned_data["curricular_areas"]
-            filtered_topic_ids: list[int] = filter_form.cleaned_data["topics"]
+            author_profile_ids: list[int] = filter_form.cleaned_data["authors"]
+            content_type_ids: list[int] = filter_form.cleaned_data["content_types"]
+            curricular_area_ids: list[int] = filter_form.cleaned_data["curricular_areas"]
+            topic_ids: list[int] = filter_form.cleaned_data["topics"]
         else:
             # If the form is not valid, we will not filter by any of the values.
             # This will result in all articles being displayed.
-            filtered_author_ids = []
-            filtered_content_type_ids = []
-            filtered_curricular_area_ids = []
-            filtered_topic_ids = []
-
-        searched_and_filtered_rcc_detail_pages = self._get_rcc_detail_pages(
-            search=search_query,
-            sort=sort,
-            author_profile_ids=filtered_author_ids,
-            content_type_ids=filtered_content_type_ids,
-            curricular_area_ids=filtered_curricular_area_ids,
-            topic_ids=filtered_topic_ids,
-        )
-
-        rcc_detail_pages_paginator = paginator.Paginator(
-            object_list=searched_and_filtered_rcc_detail_pages,
-            per_page=self.results_count,
-            allow_empty_first_page=True,
-        )
-
-        page: Optional[str] = request.GET.get("page")
-        rcc_detail_pages_page = rcc_detail_pages_paginator.get_page(page)
-
-        context: "django_template.Context" = super().get_context(request)
-        context["search_query"] = search_query
-        context["sort"] = sort
-        context["form"] = filter_form
-        context["rcc_detail_pages_count"] = rcc_detail_pages_paginator.count
-        context["rcc_detail_pages"] = rcc_detail_pages_page
-        return context
-
-    def _get_rcc_detail_pages(
-        self,
-        *,
-        search: str = "",
-        sort: constants.SortOption = constants.SORT_NEWEST_FIRST,
-        author_profile_ids: Optional[list[int]] = None,
-        content_type_ids: Optional[list[int]] = None,
-        curricular_area_ids: Optional[list[int]] = None,
-        topic_ids: Optional[list[int]] = None,
-    ):
-        author_profile_ids = author_profile_ids or []
-        content_type_ids = content_type_ids or []
-        curricular_area_ids = curricular_area_ids or []
-        topic_ids = topic_ids or []
+            author_profile_ids = []
+            content_type_ids = []
+            curricular_area_ids = []
+            topic_ids = []
 
         rcc_detail_pages = detail_page.RCCDetailPage.objects.live().public()
         rcc_detail_pages = rcc_detail_pages.filter(locale=wagtail_models.Locale.get_active())
@@ -157,15 +74,4 @@ class RCCLibraryPage(BasePage):
         for topic in topics:
             rcc_detail_pages = rcc_detail_pages.filter(related_topics__topic__translation_key=topic.translation_key)
 
-        rcc_detail_pages = rcc_detail_pages.order_by(sort.order_by_value)
-
-        if search:
-            rcc_detail_pages = rcc_detail_pages.search(
-                search,
-                order_by_relevance=False,  # To preserve original ordering
-            )
-
         return rcc_detail_pages
-
-    def get_banner(self):
-        return self.banner_image
--- network-api/networkapi/wagtailpages/pagemodels/libraries/research_hub/library_page.py
+++ network-api/networkapi/wagtailpages/pagemodels/libraries/research_hub/library_page.py
@@ -1,128 +1,49 @@
 import typing
 from typing import Optional
 
-from django.core import paginator
-from django.db import models
-from wagtail import images as wagtail_images
 from wagtail import models as wagtail_models
-from wagtail.admin import panels
-from wagtail.images import edit_handlers as image_panels
-from wagtail_localize.fields import SynchronizedField, TranslatableField
 
 from networkapi.wagtailpages import utils
 from networkapi.wagtailpages.pagemodels import profiles as profile_models
-from networkapi.wagtailpages.pagemodels.base import BasePage
 from networkapi.wagtailpages.pagemodels.libraries.research_hub import (
-    constants,
     detail_page,
     taxonomies,
 )
 from networkapi.wagtailpages.pagemodels.libraries.research_hub.forms import (
     ResearchLibraryPageFilterForm,
 )
+from networkapi.wagtailpages.pagemodels.libraries import library_page as base_library_page
 
 if typing.TYPE_CHECKING:
-    from django import http
-    from django import template as django_template
+    from django import forms
 
 
-class ResearchLibraryPage(BasePage):
-    max_count = 1
-
+class ResearchLibraryPage(base_library_page.BaseLibraryPage):
     parent_page_types = ["ResearchLandingPage"]
 
     subpage_types = ["ResearchDetailPage"]
 
     template = "pages/libraries/research_hub/library_page.html"
 
-    SORT_CHOICES = constants.SORT_CHOICES
+    @property
+    def filter_form(self):
+        """Form class used to filter detail pages for this page."""
+        return ResearchLibraryPageFilterForm
 
-    banner_image = models.ForeignKey(
-        wagtail_images.get_image_model_string(),
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-    )
-
-    results_count = models.PositiveSmallIntegerField(
-        default=10,
-        help_text="Maximum number of results to be displayed per page.",
-    )
-
-    content_panels = BasePage.content_panels + [
-        image_panels.FieldPanel("banner_image"),
-    ]
-
-    settings_panels = BasePage.settings_panels + [panels.FieldPanel("results_count")]
-
-    translatable_fields = [
-        # Content tab fields
-        TranslatableField("title"),
-        # Promote tab fields
-        SynchronizedField("slug"),
-        TranslatableField("seo_title"),
-        SynchronizedField("show_in_menus"),
-        TranslatableField("search_description"),
-        SynchronizedField("search_image"),
-    ]
-
-    def get_context(self, request: "http.HttpRequest") -> "django_template.Context":
-        search_query: str = request.GET.get("search", "")
-        sort_value: str = request.GET.get("sort", "")
-        sort: constants.SortOption = constants.SORT_CHOICES.get(sort_value, constants.SORT_NEWEST_FIRST)
-
-        filter_form = ResearchLibraryPageFilterForm(request.GET, label_suffix="")
+    def get_filtered_detail_pages(self, filter_form: "forms.Form"):
+        """Return the article detail pages that match the given filters in the form."""
         if filter_form.is_valid():
-            filtered_author_ids: list[int] = filter_form.cleaned_data["authors"]
-            filtered_topic_ids: list[int] = filter_form.cleaned_data["topics"]
-            filtered_region_ids: list[int] = filter_form.cleaned_data["regions"]
-            filtered_year: Optional[int] = filter_form.cleaned_data["years"]
+            author_profile_ids: list[int] = filter_form.cleaned_data["authors"]
+            topic_ids: list[int] = filter_form.cleaned_data["topics"]
+            region_ids: list[int] = filter_form.cleaned_data["regions"]
+            year: Optional[int] = filter_form.cleaned_data["year"]
         else:
             # If the form is not valid, we will not filter by any of the values.
             # This will result in all research being displayed.
-            filtered_author_ids = []
-            filtered_topic_ids = []
-            filtered_region_ids = []
-            filtered_year = None
-
-        searched_and_filtered_research_detail_pages = self._get_research_detail_pages(
-            search=search_query,
-            sort=sort,
-            author_profile_ids=filtered_author_ids,
-            topic_ids=filtered_topic_ids,
-            region_ids=filtered_region_ids,
-            year=filtered_year,
-        )
-        research_detail_pages_paginator = paginator.Paginator(
-            object_list=searched_and_filtered_research_detail_pages,
-            per_page=self.results_count,
-            allow_empty_first_page=True,
-        )
-
-        page: Optional[str] = request.GET.get("page")
-        research_detail_pages_page = research_detail_pages_paginator.get_page(page)
-
-        context: "django_template.Context" = super().get_context(request)
-        context["search_query"] = search_query
-        context["sort"] = sort
-        context["form"] = filter_form
-        context["research_detail_pages_count"] = research_detail_pages_paginator.count
-        context["research_detail_pages"] = research_detail_pages_page
-        return context
-
-    def _get_research_detail_pages(
-        self,
-        *,
-        search: str = "",
-        sort: constants.SortOption = constants.SORT_NEWEST_FIRST,
-        author_profile_ids: Optional[list[int]] = None,
-        topic_ids: Optional[list[int]] = None,
-        region_ids: Optional[list[int]] = None,
-        year: Optional[int] = None,
-    ):
-        author_profile_ids = author_profile_ids or []
-        topic_ids = topic_ids or []
-        region_ids = region_ids or []
+            author_profile_ids = []
+            topic_ids = []
+            region_ids = []
+            year = None
 
         research_detail_pages = detail_page.ResearchDetailPage.objects.live().public()
         research_detail_pages = research_detail_pages.filter(locale=wagtail_models.Locale.get_active())
@@ -153,15 +74,4 @@ class ResearchLibraryPage(BasePage):
         if year:
             research_detail_pages = research_detail_pages.filter(original_publication_date__year=year)
 
-        research_detail_pages = research_detail_pages.order_by(sort.order_by_value)
-
-        if search:
-            research_detail_pages = research_detail_pages.search(
-                search,
-                order_by_relevance=False,  # To preserve original ordering
-            )
-
         return research_detail_pages
-
-    def get_banner(self):
-        return self.banner_image
