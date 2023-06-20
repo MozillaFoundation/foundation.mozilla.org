cef805229dbf2101a423949a4cd42e2b0ed28af8
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-06-20 18:13:49 -0300

    Update landing page tests

--- network-api/networkapi/wagtailpages/tests/libraries/rcc/test_landing_page.py
+++ network-api/networkapi/wagtailpages/tests/libraries/rcc/test_landing_page.py
@@ -15,7 +15,7 @@ class RCCLandingPageTestCase(base.RCCTestCase):
         detail_page_3 = self.create_rcc_detail_page(days_ago=2)
         detail_page_4 = self.create_rcc_detail_page(days_ago=1)
 
-        latest_rcc_pages = self.landing_page.get_latest_rcc_pages()
+        latest_rcc_pages = self.landing_page.latest_detail_pages
 
         self.assertEqual(len(latest_rcc_pages), 3)
         self.assertIn(detail_page_4, latest_rcc_pages)
@@ -31,7 +31,7 @@ class RCCLandingPageTestCase(base.RCCTestCase):
         detail_page_private = self.create_rcc_detail_page()
         self.make_page_private(detail_page_private)
 
-        latest_rcc_pages = self.landing_page.get_latest_rcc_pages()
+        latest_rcc_pages = self.landing_page.latest_detail_pages
 
         self.assertEqual(len(latest_rcc_pages), 1)
         self.assertIn(detail_page_public, latest_rcc_pages)
@@ -47,7 +47,7 @@ class RCCLandingPageTestCase(base.RCCTestCase):
         self.synchronize_tree()
         fr_landing_page = self.landing_page.get_translation(locale=self.fr_locale)
 
-        latest_rcc_pages = fr_landing_page.get_latest_rcc_pages()
+        latest_rcc_pages = fr_landing_page.latest_detail_pages
 
         self.assertEqual(len(latest_rcc_pages), 1)
         self.assertEqual(latest_rcc_pages[0].locale, self.fr_locale)
@@ -57,7 +57,7 @@ class RCCLandingPageTestCase(base.RCCTestCase):
         """
         Ensure that the library page is returned.
         """
-        library_page = self.landing_page.get_library_page()
+        library_page = self.landing_page.library_page
         self.assertEqual(library_page, self.library_page)
 
     def test_get_library_page_returns_library_page_of_same_locale(self):
@@ -66,7 +66,7 @@ class RCCLandingPageTestCase(base.RCCTestCase):
         """
         fr_landing_page = self.landing_page.get_translation(locale=self.fr_locale)
 
-        fr_library_page = fr_landing_page.get_library_page()
+        fr_library_page = fr_landing_page.library_page
 
         self.assertEqual(fr_library_page.locale, self.fr_locale)
         self.assertEqual(fr_library_page.locale, fr_landing_page.locale)
