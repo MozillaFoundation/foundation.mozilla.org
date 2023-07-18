708e201f0ba300235de90d08bc3cb88b8ba5edd3
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-07-18 11:46:11 -0300

    Fetch last edited pages in queryset

--- network-api/networkapi/reports/views.py
+++ network-api/networkapi/reports/views.py
@@ -1,7 +1,7 @@
-from django.db.models import Count
+from django.db.models import Count, Q, Subquery, OuterRef, Max, Prefetch
 from wagtail.admin.auth import permission_denied
 from wagtail.admin.views.reports import ReportView
-from wagtail.models import ContentType
+from wagtail.models import ContentType, Locale, Page, get_page_models
 
 
 class PageTypesReportView(ReportView):
@@ -10,7 +10,22 @@ class PageTypesReportView(ReportView):
     header_icon = "doc-empty-inverse"
 
     def get_queryset(self):
-        return ContentType.objects.filter(model__iendswith="page").annotate(count=Count("pages")).order_by("-count")
+        default_locale = Locale.get_default()
+        page_models = [model.__name__.lower() for model in get_page_models()]
+        latest_pages = Prefetch(
+            "pages",
+            queryset=Page.objects.filter(locale=default_locale).order_by("-latest_revision_created_at"),
+            to_attr="last_edited_pages",
+        )
+        return (
+            ContentType.objects.filter(model__in=page_models)
+            .annotate(
+                count=Count("pages"),
+                default_locale_count=Count("pages", filter=Q(pages__locale=default_locale)),
+            )
+            .prefetch_related(latest_pages)
+            .order_by("-count")
+        )
 
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_superuser:
