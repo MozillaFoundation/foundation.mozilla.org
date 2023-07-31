288127944f10c80eb23da06cccf3c8d511716145
Daniel Miranda <manieldiranda@gmail.com>
2023-07-31 15:50:49 -0700

    updated migrations

--- network-api/networkapi/wagtailpages/migrations/0094_make_petition_id_required.py
+++ network-api/networkapi/wagtailpages/migrations/0094_make_petition_id_required.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("wagtailpages", "0092_add_about_block_to_libraries_landing_page"),
+        ("wagtailpages", "0093_auto_20230726_2248"),
     ]
 
     operations = [
