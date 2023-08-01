b3fa083b4191c8dec5d9e6274d35e302bf9a9df7
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-08-01 10:54:38 -0300

    Page types report (#10928)
    * Page Type report MVP
    
    * Fix typo
    
    * Fetch last edited pages in queryset
    
    * Add fields to template
    
    * Get last_edited_by and last_edited_page
    
    * Fix edit URL and improve template query
    
    * Add tests for report
    
    * Fix test users
    
    * Remove not needed dispatch method
    
    * Remove N+1 query
    
    * Set active locale name in template
    
    * Lint
    
    * Fix import in tests

---
+++ network-api/networkapi/reports/wagtail_hooks.py
@@ -0,0 +1,19 @@
+from django.urls import path, reverse
+from wagtail import hooks
+from wagtail.admin.menu import AdminOnlyMenuItem
+
+from .views import PageTypesReportView
+
+
+@hooks.register("register_reports_menu_item")
+def register_page_types_report_menu_item():
+    return AdminOnlyMenuItem(
+        "Page Types", reverse("page_types_report"), icon_name=PageTypesReportView.header_icon, order=700
+    )
+
+
+@hooks.register("register_admin_urls")
+def register_page_types_report_url():
+    return [
+        path("reports/page-types-report/", PageTypesReportView.as_view(), name="page_types_report"),
+    ]
