6698f2185852021601e46a4faacaf56ade9c2b40
Daniel Miranda <manieldiranda@gmail.com>
2023-08-03 13:26:34 -0700

    formatting

--- network-api/networkapi/wagtailpages/migrations/0095_donationmodal_donate_url.py
+++ network-api/networkapi/wagtailpages/migrations/0095_donationmodal_donate_url.py
@@ -5,15 +5,24 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('wagtailpages', '0094_alter_petition_campaign_id'),
+        ("wagtailpages", "0094_alter_petition_campaign_id"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='donationmodal',
-            name='donate_url',
-            field=models.CharField(default='?form=donate', help_text="If you would like this modal's donate button to link to a custom URL, please enter a valid URL (starting with http:// or https://), or a valid query string starting with ? (Ex: ?form=donate).", max_length=255, validators=[django.core.validators.RegexValidator(message='Please enter a valid URL (starting with http:// or https://), or a valid query string starting with ? (Ex: ?form=donate).', regex='^(https?://[\\w.-]+(/\\S*)?)?(\\?[\\w-]+(=[\\w-]*)?(&[\\w-]+(=[\\w-]*)?)*)?$')]),
+            model_name="donationmodal",
+            name="donate_url",
+            field=models.CharField(
+                default="?form=donate",
+                help_text="If you would like this modal's donate button to link to a custom URL, please enter a valid URL (starting with http:// or https://), or a valid query string starting with ? (Ex: ?form=donate).",
+                max_length=255,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        message="Please enter a valid URL (starting with http:// or https://), or a valid query string starting with ? (Ex: ?form=donate).",
+                        regex="^(https?://[\\w.-]+(/\\S*)?)?(\\?[\\w-]+(=[\\w-]*)?(&[\\w-]+(=[\\w-]*)?)*)?$",
+                    )
+                ],
+            ),
         ),
     ]
