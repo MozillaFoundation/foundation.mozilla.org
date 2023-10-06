cc628db3d7ff25be4b4eec6aa64471b5dff96847
Mavis Ou <mmmavis@users.noreply.github.com>
2023-10-06 12:40:03 -0700

    added the newsletter signup prompt to quiz flow

--- source/js/buyers-guide/inject-react/product-quiz.js
+++ source/js/buyers-guide/inject-react/product-quiz.js
@@ -6,13 +6,17 @@ import ProductQuiz from "../components/product-quiz/product-quiz.jsx";
  * Inject product quiz React component
  * @param {Array} apps The existing array we are using to to track all React client rendering calls
  */
-export default (apps) => {
+export default (apps, siteUrl) => {
   document.querySelectorAll(`.product-quiz-wrapper`).forEach((element) => {
     apps.push(
       new Promise((resolve) => {
         const root = createRoot(element);
         root.render(
-          <ProductQuiz {...element.dataset} whenLoaded={() => resolve()} />
+          <ProductQuiz
+            {...element.dataset}
+            whenLoaded={() => resolve()}
+            joinUsApiUrl={`${siteUrl}/api/campaign/signups/0/`}
+          />
         );
       })
     );
