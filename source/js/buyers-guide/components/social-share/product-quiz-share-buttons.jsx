989fa0e98eb2be4fb3702dcafe46a661f6cde62f
Daniel Miranda <manieldiranda@gmail.com>
2023-10-09 15:58:29 -0700

    last of linting. Ready for review

--- source/js/buyers-guide/components/social-share/product-quiz-share-buttons.jsx
+++ source/js/buyers-guide/components/social-share/product-quiz-share-buttons.jsx
@@ -16,11 +16,11 @@ class ProductQuizShareButtons extends Component {
     window.addEventListener("resize", this.handleResize);
   }
 
-  handleResize(){
+  handleResize() {
     this.setState({
       isMobileOrTablet: window.innerWidth < tabletBreakpoint,
     });
-  };
+  }
 
   shareButtonClicked(event, shareProgressButtonId) {
     if (shareProgressButtonId) {
