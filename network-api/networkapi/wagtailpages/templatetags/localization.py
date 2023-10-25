6e932a5a7d62a615ef96d036d4ca7984aeb82030
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-10-25 14:23:30 -0300

    Lint

--- network-api/networkapi/wagtailpages/templatetags/localization.py
+++ network-api/networkapi/wagtailpages/templatetags/localization.py
@@ -6,9 +6,7 @@ from django.conf import settings
 from django.db.models.base import ObjectDoesNotExist
 from django.utils.translation import get_language_info
 
-from networkapi.wagtailpages.utils import (
-    get_locale_from_request,
-)
+from networkapi.wagtailpages.utils import get_locale_from_request
 
 register = template.Library()
 
