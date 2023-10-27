4b518d46ab78b532a37c5edbe8749b43cca03cc2
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-10-27 08:19:27 -0300

    Fix tests

--- network-api/networkapi/wagtailpages/tests/buyersguide/test_homepage.py
+++ network-api/networkapi/wagtailpages/tests/buyersguide/test_homepage.py
@@ -179,7 +179,7 @@ class TestBuyersGuidePage(BuyersGuideTestCase):
     def test_serve_page_one_product(self):
         products = ProductPage.objects.descendant_of(self.bg)
         self.assertEqual(products.count(), 1)
-        query_number = 60
+        query_number = 58
 
         with self.assertNumQueries(query_number):
             response = self.client.get(self.bg.url)
@@ -193,7 +193,7 @@ class TestBuyersGuidePage(BuyersGuideTestCase):
             buyersguide_factories.ProductPageFactory(parent=self.bg, with_random_categories=True)
         products = ProductPage.objects.descendant_of(self.bg)
         self.assertEqual(products.count(), additional_products_count + 1)
-        query_number = 61
+        query_number = 59
 
         with self.assertNumQueries(query_number):
             response = self.client.get(self.bg.url)
@@ -207,7 +207,7 @@ class TestBuyersGuidePage(BuyersGuideTestCase):
             buyersguide_factories.ProductPageFactory(parent=self.bg, with_random_categories=True)
         products = ProductPage.objects.descendant_of(self.bg)
         self.assertEqual(products.count(), additional_products_count + 1)
-        query_number = 67
+        query_number = 65
         self.client.force_login(user=self.create_test_user())
 
         with self.assertNumQueries(query_number):
