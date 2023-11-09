4bb77dd24584f02e6203178604b76b602f19a63e
Daniel Miranda <manieldiranda@gmail.com>
2023-11-09 15:43:57 -0800

    formatting

--- network-api/networkapi/donate/pagemodels/base_page.py
+++ network-api/networkapi/donate/pagemodels/base_page.py
@@ -1,8 +1,11 @@
 from wagtail.models import Page
 
 from networkapi.wagtailpages.models import FoundationMetadataPageMixin
-from networkapi.wagtailpages.pagemodels.mixin.foundation_navigation import FoundationNavigationPageMixin
+from networkapi.wagtailpages.pagemodels.mixin.foundation_navigation import (
+    FoundationNavigationPageMixin,
+)
 
-class BaseDonationPage(FoundationMetadataPageMixin,FoundationNavigationPageMixin, Page):
+
+class BaseDonationPage(FoundationMetadataPageMixin, FoundationNavigationPageMixin, Page):
     class Meta:
         abstract = True
