0904c02d312a48c48c12682db0cfb5fe922b7209
Mavis Ou <mmmavis@users.noreply.github.com>
2023-11-21 14:16:38 -0800

    Newsletter submission logic (#11398)
    * added form submission logic to HOC
    
    * added inline note for error handling todo
    
    * code cleanup
    
    * connect frontend with backend
    
    * add GA/GTM tracking
    
    * remove console.log
    
    * fixes

--- source/js/components/newsletter-signup/atoms/description.jsx
+++ source/js/components/newsletter-signup/atoms/description.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Description = ({ children }) => {
-  return <p>{children}</p>;
+const Description = ({ content }) => {
+  if (typeof content === "string") {
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: content,
+        }}
+      />
+    );
+  }
+
+  return <div>{content}</div>;
 };
 
 Description.propTypes = {
-  children: PropTypes.node.isRequired,
+  content: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
 };
 
 export default Description;
