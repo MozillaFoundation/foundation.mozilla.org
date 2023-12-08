7017ba8808460642ffabf979f5d124b80b39daad
Kevin howbrook <kbhowbrook@gmail.com>
2023-12-08 15:20:14 +0000

    Formatting code

--- network-api/networkapi/mozfest/factory.py
+++ network-api/networkapi/mozfest/factory.py
@@ -10,8 +10,6 @@ from networkapi.utility.faker.helpers import reseed
 from networkapi.wagtailpages.factory.image_factory import ImageFactory
 from networkapi.wagtailpages.factory.signup import SignupFactory
 
-from networkapi.mozfest import models as mozfest_models
-
 streamfield_fields = ["paragraph", "image", "spacer", "quote"]
 Faker.add_provider(StreamfieldProvider)
 
@@ -103,7 +101,4 @@ def generate(seed):
     ]
 
     print("Generating Mozfest landing-pages")
-    [
-        MozfestLandingPageFactory.create(parent=home_page, title=title)
-        for title in ["Landing page 1", "Landing page 2"]
-    ]
+    [MozfestLandingPageFactory.create(parent=home_page, title=title) for title in ["Landing page 1", "Landing page 2"]]
