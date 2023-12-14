8174ebfc3b47f22acf37c38649ec3a4d500e54a2
Tibor Leupold <tibor.leupold@torchbox.com>
2023-12-13 16:36:54 -0800

    Test clean methos raises when image and video are set

--- network-api/networkapi/wagtailpages/tests/blog/test_blog.py
+++ network-api/networkapi/wagtailpages/tests/blog/test_blog.py
@@ -1,6 +1,8 @@
 import http
 
+import wagtail_factories
 from django import test
+from django.core import exceptions
 from wagtail import models as wagtail_models
 
 from networkapi.wagtailpages.factory import homepage as home_factory
@@ -18,11 +20,16 @@ class TestBlogPage(test.TestCase):
         site.clean()
         site.save()
         cls.blog_index = blog_factory.BlogIndexPageFactory(parent=cls.homepage)
+        cls.blog_page = blog_factory.BlogPageFactory(parent=cls.blog_index)
 
     def test_page_loads(self):
-        page = blog_factory.BlogPageFactory(parent=self.blog_index)
-
-        response = self.client.get(page.url)
+        response = self.client.get(self.blog_page.url)
 
         self.assertEqual(response.status_code, http.HTTPStatus.OK)
 
+    def test_clean_when_both_hero_image_and_video_are_set(self):
+        self.blog_page.hero_image = wagtail_factories.ImageFactory()
+        self.blog_page.hero_video = "https://example.com/video.mp4"
+
+        with self.assertRaises(exceptions.ValidationError):
+            self.blog_page.clean()
