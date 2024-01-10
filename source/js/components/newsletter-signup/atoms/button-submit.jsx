cd0b3b6227b40a2727f9d0f866735a148c6fbcf6
Mavis Ou <mmmavis@users.noreply.github.com>
2024-01-10 09:23:09 -0800

    Blog near-footer signup form is now rendered using the new React components (#11698)
    * form style is based on a new attribute data-form-style instead of the data-form-location
    
    * renamed 'designSystemStyle' to 'fieldStyle'
    
    * 'callout-box' signup on blog page is now rendered using the new React components
    
    * ordered things alphabetically
    
    * fix linting errors & made doc more readable
    
    * changed how submit button text is set

--- source/js/components/newsletter-signup/atoms/button-submit.jsx
+++ source/js/components/newsletter-signup/atoms/button-submit.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 
-const ButtonSubmit = ({ children, widthClasses }) => {
+const ButtonSubmit = ({
+  children,
+  buttonStyle = "primary",
+  widthClasses = `tw-w-full`,
+}) => {
   // [TODO]
   // Ideally styling for this "atom" component should be pre-defined in a Tailwind config file.
   // Because our design system still needs to be finalized,
   // we are using hardcoded Tailwind classes directly here for now.
-  let classes = classNames(`tw-btn tw-btn-primary`, widthClasses);
+  let classes = classNames(`tw-btn tw-btn-${buttonStyle}`, widthClasses);
 
   return (
     <button type="submit" className={classes}>
@@ -18,6 +22,7 @@ const ButtonSubmit = ({ children, widthClasses }) => {
 
 ButtonSubmit.propTypes = {
   children: PropTypes.node.isRequired,
+  buttonStyle: PropTypes.oneOf(["primary", "secondary"]),
   widthClasses: PropTypes.string,
 };
 
