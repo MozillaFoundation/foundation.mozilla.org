f4074c44d1e435e9d11f7148e205f16956ac6b97
Mavis Ou <mmmavis@users.noreply.github.com>
2024-01-29 23:11:39 -0800

    Replaced newsletter signup form in PNI product creep vote section with the new implementation approach

--- source/js/components/newsletter-signup/organisms/form-specific-style.js
+++ source/js/components/newsletter-signup/organisms/form-specific-style.js
@@ -84,6 +84,15 @@ export const FORM_STYLE = {
     headingLevel: 3,
     headingClass: "tw-h5-heading",
   },
+  "pni-creep-vote": {
+    buttonPosition: "bottom",
+    buttonStyle: "primary",
+    buttonWidthClasses: "tw-w-1/2",
+    descriptionClass: "tw-text-center",
+    fieldStyle: "filled",
+    headingLevel: 2,
+    headingClass: "tw-h3-heading tw-text-center",
+  },
   "pni-product-quiz": {
     fieldStyle: "filled",
     headingLevel: 4,
