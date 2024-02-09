07c2fd387eab0f4e2c7b21d32191a2ecdef91d42
Daniel Miranda <manieldiranda@gmail.com>
2024-02-08 16:01:22 -0800

    formatting

--- network-api/networkapi/utility/orderables.py
+++ network-api/networkapi/utility/orderables.py
@@ -4,14 +4,16 @@ if TYPE_CHECKING:
     from django.db import models
 
 
-def get_related_items(queryset: "models.QuerySet", related_item_field: str, order_by: str = None) -> list["models.Model"]:
+def get_related_items(
+    queryset: "models.QuerySet", related_item_field: str, order_by: str = None
+) -> list["models.Model"]:
     """
     Return list of the related items from the queryset.
 
     For each item in the queryset, the related item stored in `related_item_field` is
     returned.
 
-    """ 
+    """
     # Apply ordering if needed
     if order_by:
         queryset = queryset.order_by(order_by)
--- network-api/networkapi/wagtailpages/pagemodels/buyersguide/products.py
+++ network-api/networkapi/wagtailpages/pagemodels/buyersguide/products.py
@@ -1029,21 +1029,19 @@ class ProductPage(BasePage):
         )
         related_products = localize_queryset(related_products, preserve_order=True)
         return related_products.specific()
-    
+
     @property
     def preview_related_products(self) -> list:
         """
         Fetches related product updates for CMS page previews.
         """
         related_products = orderables.get_related_items(
-            self.related_product_pages.all(),
-            "related_product",
-            sort_by="sort_order"
+            self.related_product_pages.all(), "related_product", sort_by="sort_order"
         )
         # This method, while inneficient, is neccesary as we are using it to render
         # any related_product updates that are stored in memory, but not yet saved in the DB.
         return [product.localized for product in related_products]
-                
+
     @property
     def local_categories(self):
         categories = BuyersGuideProductCategory.objects.filter(product_pages__product=self)
