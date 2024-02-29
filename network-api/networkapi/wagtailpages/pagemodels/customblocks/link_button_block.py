e306af53b182d479aa91441ea19ac45301d85741
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2024-02-29 10:49:29 -0300

    Add LinkButton to CTABlock and LinkButtonBlock

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/link_button_block.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/link_button_block.py
@@ -1,5 +1,7 @@
 from wagtail import blocks
 
+from networkapi.wagtailpages.pagemodels.customblocks.link_block import LinkBlock
+
 
 class LinkButtonBlock(blocks.StructBlock):
     label = blocks.CharBlock()
@@ -7,6 +9,8 @@ class LinkButtonBlock(blocks.StructBlock):
     # We use a char block because UrlBlock does not
     # allow for relative linking.
     URL = blocks.CharBlock()
+
+    target = LinkBlock(label="")
 
     # Buttons can have different looks, so we
     # offer the choice to decide which styling
