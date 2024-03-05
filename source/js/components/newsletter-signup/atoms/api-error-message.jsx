0639f08052910c60ef869ec3591c23faa96127c0
Rob DiVincenzo <rob.divincenzo@gmail.com>
2024-03-05 09:33:04 -0500

    Ensuring white text on error message

--- source/js/components/newsletter-signup/atoms/api-error-message.jsx
+++ source/js/components/newsletter-signup/atoms/api-error-message.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const APIErrorMessage = ({ apiErrorMessage }) => {
   return (
     <div className="tw-flex tw-bg-red-100 tw-px-8 tw-pt-8 tw-mb-5" role="alert">
-      <p>{apiErrorMessage}</p>
+      <p className="tw-text-white">{apiErrorMessage}</p>
     </div>
   );
 };
