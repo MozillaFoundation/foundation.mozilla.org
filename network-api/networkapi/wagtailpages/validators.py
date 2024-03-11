657373414394a43c4e951c82100381030f077023
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2024-03-11 15:47:05 -0300

    Simpler validation

--- network-api/networkapi/wagtailpages/validators.py
+++ network-api/networkapi/wagtailpages/validators.py
@@ -1,5 +1,3 @@
-from urllib.parse import urlparse
-
 from django.core import validators
 from django.core.exceptions import ValidationError
 
@@ -12,14 +10,6 @@ class RelativeURLValidator(validators.URLValidator):
         if not value.startswith("/"):
             raise ValidationError('This field must start with "/"')
 
-        # No anchor links
-        if value.startswith("#"):
-            raise ValidationError("This field must be a relative URL.")
-
-        parsed_url = urlparse(value)
-        if parsed_url.scheme or parsed_url.netloc:
-            raise ValidationError("This field must be a relative URL.")
-
         value = "http://example.com" + value
         super().__call__(value)
 
@@ -31,10 +21,6 @@ class AnchorLinkValidator(validators.URLValidator):
 
         if not value.startswith("#"):
             raise ValidationError('This field must start with "#"')
-
-        parsed_url = urlparse(value)
-        if parsed_url.scheme or parsed_url.netloc:
-            raise ValidationError("This field cannot be an absolute URL.")
 
         value = "http://example.com" + value
         super().__call__(value)
