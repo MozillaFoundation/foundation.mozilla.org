6e1803991c78a2a2890e55aeeeffc42808fe32b7
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2024-03-13 11:33:22 -0300

    [Main Nav] Nav link block [TP-292](#12033)
    * Add `nav` app
    
    * Make only page, external and relative links available on base link block
    
    * Make label part of the base link block
    
    * Define a NavLinkBlock
    
    * Add NavLinkBlock factory
    
    * Add tests for block
    
    * Make `link_to` field required

--- network-api/networkapi/donate/migrations/0006_add_linkblock_to_linkbuttonblock.py
+++ network-api/networkapi/donate/migrations/0006_add_linkblock_to_linkbuttonblock.py
@@ -344,9 +344,7 @@ class Migration(migrations.Migration):
                                         help_text='An id attribute of an element on the current page. For example, "#section-1"',
                                         max_length=300,
                                         required=False,
-                                        validators=[
-                                            networkapi.wagtailpages.pagemodels.customblocks.common.base_link_block.AnchorLinkValidator()
-                                        ],
+                                        validators=[networkapi.wagtailpages.validators.AnchorLinkValidator()],
                                     ),
                                 ),
                                 ("email", wagtail.blocks.EmailBlock(required=False)),
@@ -1179,7 +1177,7 @@ class Migration(migrations.Migration):
                                                                 max_length=300,
                                                                 required=False,
                                                                 validators=[
-                                                                    networkapi.wagtailpages.pagemodels.customblocks.common.base_link_block.AnchorLinkValidator()
+                                                                    networkapi.wagtailpages.validators.AnchorLinkValidator()
                                                                 ],
                                                             ),
                                                         ),
---
+++ network-api/networkapi/nav/blocks.py
@@ -0,0 +1,27 @@
+from wagtail.telepath import register
+
+from networkapi.wagtailpages.pagemodels.customblocks.common.base_link_block import (
+    BaseLinkBlock,
+    BaseLinkBlockAdapter,
+    BaseLinkValue,
+)
+
+
+class NavLinkValue(BaseLinkValue):
+    @property
+    def open_in_new_window(self) -> bool:
+        link_to = self.get("link_to")
+        if link_to == "external_url":
+            return True
+        return False
+
+
+class NavLinkBlock(BaseLinkBlock):
+    class Meta:
+        value_class = NavLinkValue
+        label = "Navigation Link"
+        icon = "link"
+        template = "nav/blocks/nav_link_block.html"
+
+
+register(BaseLinkBlockAdapter(), NavLinkBlock)
