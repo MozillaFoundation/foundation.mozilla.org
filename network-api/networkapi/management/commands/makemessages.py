4de3f5819df5ae689b0b6c78f150d8b4b5c3c664
Rob DiVincenzo <rob@mozillafoundation.org>
2024-03-13 13:25:34 -0400

    Include default msgmerge_options, adding --no-fuzzy-matching (#12019)

--- network-api/networkapi/management/commands/makemessages.py
+++ network-api/networkapi/management/commands/makemessages.py
@@ -2,4 +2,4 @@ from django.core.management.commands import makemessages
 
 
 class Command(makemessages.Command):
-    msgmerge_options = ["--no-fuzzy-matching"]
+    msgmerge_options = makemessages.Command.msgmerge_options + ["--no-fuzzy-matching"]
