bb7db18265fcdac10e387ff79f8ea820f04acccf
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2024-03-15 13:55:12 -0300

    [Main nav] Navigation column [TP-266] (#12047)
    * Add description to NavLinkBlock
    
    * Rename NavLinkBlock to NavItem
    
    * Add NavButtonBlock
    
    * Fix test name
    
    * Add a NavColumn model

--- network-api/networkapi/nav/blocks.py
+++ network-api/networkapi/nav/blocks.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from wagtail import blocks
 from wagtail.telepath import register
 
@@ -8,7 +10,7 @@ from networkapi.wagtailpages.pagemodels.customblocks.common.base_link_block impo
 )
 
 
-class NavLinkValue(BaseLinkValue):
+class NavItemValue(BaseLinkValue):
     @property
     def open_in_new_window(self) -> bool:
         link_to = self.get("link_to")
@@ -17,14 +19,50 @@ class NavLinkValue(BaseLinkValue):
         return False
 
 
-class NavLinkBlock(BaseLinkBlock):
+class NavItem(BaseLinkBlock):
     description = blocks.CharBlock(required=False, max_length=100)
 
+    def __init__(self, local_blocks=None, **kwargs):
+        # Use __init__ method to change the order of the blocks when constructing
+        # them through inheritance
+        super().__init__(local_blocks, **kwargs)
+        self.child_blocks = self.base_blocks.copy()
+        child_blocks = OrderedDict(
+            {
+                "label": self.child_blocks.pop("label"),
+                "description": self.child_blocks.pop("description"),
+            }
+        )
+        child_blocks.update({k: v for k, v in self.child_blocks.items()})
+        self.child_blocks = child_blocks
+
     class Meta:
-        value_class = NavLinkValue
+        value_class = NavItemValue
         label = "Navigation Link"
         icon = "link"
         template = "nav/blocks/nav_link_block.html"
 
 
-register(BaseLinkBlockAdapter(), NavLinkBlock)
+register(BaseLinkBlockAdapter(), NavItem)
+
+
+class NavButton(BaseLinkBlock):
+    class Meta:
+        value_class = NavItemValue
+        label = "Navigation Button"
+        icon = "link"
+        template = "nav/blocks/nav_button_block.html"
+
+
+register(BaseLinkBlockAdapter(), NavButton)
+
+
+class NavColumn(blocks.StructBlock):
+    title = blocks.CharBlock(max_length=100)
+    links = blocks.ListBlock(NavItem, min_num=1, max_num=4)
+    button = blocks.ListBlock(NavButton, required=False, min_num=0, max_num=1)
+
+    class Meta:
+        label = "Navigation Column"
+        icon = "list-ul"
+        template = "nav/blocks/nav_column_block.html"
--- network-api/networkapi/nav/factories.py
+++ network-api/networkapi/nav/factories.py
@@ -2,10 +2,10 @@ import factory
 import wagtail_factories
 from wagtail import models as wagtail_models
 
-from networkapi.nav.blocks import NavLinkBlock, NavLinkValue
+from networkapi.nav import blocks as nav_blocks
 
 
-class NavLinkBlockFactory(wagtail_factories.StructBlockFactory):
+class NavItemFactory(wagtail_factories.StructBlockFactory):
     """Factory for NavLinkBlock.
 
     Use traits to create instances based on the type of link needed:
@@ -22,12 +22,12 @@ class NavLinkBlockFactory(wagtail_factories.StructBlockFactory):
     """
 
     class Meta:
-        model = NavLinkBlock
+        model = nav_blocks.NavItem
 
     @classmethod
     def _construct_struct_value(cls, block_class, params):
         """Use NavLinkValue to create the StructValue instance."""
-        return NavLinkValue(
+        return nav_blocks.NavItemValue(
             block_class(),
             [(name, value) for name, value in params.items()],
         )
@@ -51,3 +51,62 @@ class NavLinkBlockFactory(wagtail_factories.StructBlockFactory):
     page = None
     external_url = ""
     relative_url = ""
+
+
+class NavButtonFactory(wagtail_factories.StructBlockFactory):
+    """Factory for NavButtonBlock.
+
+    Use traits to create instances based on the type of link needed:
+    - page_link: link to a page
+    - external_url_link: link to a custom external URL
+    - relative_url_link: link to a relative URL
+
+    Example:
+    ```
+    block = NavButtonBlockFactory(page_link=True)
+    block = NavButtonBlockFactory(external_url_link=True)
+    block = NavButtonBlockFactory(relative_url_link=True)
+    ```
+    """
+
+    class Meta:
+        model = nav_blocks.NavButton
+
+    @classmethod
+    def _construct_struct_value(cls, block_class, params):
+        """Use NavLinkValue to create the StructValue instance."""
+        return nav_blocks.NavItemValue(
+            block_class(),
+            [(name, value) for name, value in params.items()],
+        )
+
+    class Params:
+        page_link = factory.Trait(
+            link_to="page",
+            page=factory.Iterator(wagtail_models.Page.objects.filter(locale_id="1")),
+        )
+        external_url_link = factory.Trait(link_to="external_url", external_url=factory.Faker("url"))
+        relative_url_link = factory.Trait(link_to="relative_url", relative_url=f'/{factory.Faker("uri_path")}')
+
+    label = factory.Faker("sentence", nb_words=3)
+
+    # Setup default link as external URL (it won't pass validation without a link type defined though
+    # so it's still necessary to use the factory with traits)
+    link_to = "external_url"
+    # Set all link types to None by default. Only define the needed link type in the factory
+    # trait to avoid conflicts
+    page = None
+    external_url = ""
+    relative_url = ""
+
+
+class NavColumnFactory(wagtail_factories.StructBlockFactory):
+    class Meta:
+        model = nav_blocks.NavColumn
+
+    class Params:
+        with_button = False
+
+    title = factory.Faker("sentence", nb_words=3)
+    links = factory.List([factory.SubFactory(NavItemFactory) for _ in range(4)])
+    button = factory.LazyAttribute(lambda o: [NavButtonFactory()] if o.with_button else [])
