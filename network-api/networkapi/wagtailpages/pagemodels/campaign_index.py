e9889c54c16ed53b32fe8316b9c236eac7775eb9
danielfmiranda <manieldiranda@gmail.com>
2024-04-11 16:43:43 -0700

    created campaign index featured page relationship

--- network-api/networkapi/wagtailpages/pagemodels/campaign_index.py
+++ network-api/networkapi/wagtailpages/pagemodels/campaign_index.py
@@ -1,9 +1,30 @@
+from django.db import models
+from modelcluster import fields as cluster_fields
+from wagtail.admin.panels import InlinePanel, PageChooserPanel
+from wagtail.models import Orderable, TranslatableMixin
 from wagtail_localize.fields import SynchronizedField, TranslatableField
 
-from networkapi.wagtailpages.pagemodels.publications.publication import PublicationPage
-from networkapi.wagtailpages.utils import get_locale_from_request
+from networkapi.utility import orderables
 
 from .index import IndexPage
+
+
+class FeaturedCampaignPageRelation(TranslatableMixin, Orderable):
+    index_page = cluster_fields.ParentalKey(
+        "CampaignIndexPage",
+        related_name="featured_campaign_pages",
+    )
+    featured_page = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+
+    panels = [PageChooserPanel("featured_page", ["wagtailpages.BanneredCampaignPage", "wagtailpages.CampaignPage"])]
+
+    def __str__(self):
+        return f"{self.index_page.title} -> {self.featured_page.title}"
 
 
 class CampaignIndexPage(IndexPage):
@@ -34,16 +55,24 @@ class CampaignIndexPage(IndexPage):
         TranslatableField("intro"),
         TranslatableField("header"),
         SynchronizedField("page_size"),
+        SynchronizedField("featured_campaign_pages"),
     ]
 
     template = "wagtailpages/index_page.html"
 
+    content_panels = IndexPage.content_panels + [
+        InlinePanel("featured_campaign_pages", label="Featured Pages"),
+    ]
+
+    def get_featured_pages(self):
+        featured_pages = orderables.get_related_items(
+            self.featured_campaign_pages.all(), "featured_page", order_by="sort_order"
+        )
+        return featured_pages
+
     def get_context(self, request):
         # bootstrap the render context
         context = super().get_context(request)
-        locale = get_locale_from_request(request)
-        entries = self.get_all_entries(locale).not_type(PublicationPage)
+        entries = self.get_featured_pages()
         context["entries"] = entries[0 : self.page_size]
-        # Which pagetype to exclude during the "load more" ajax request: PublicationPage
-        context["exclude_pagetype"] = "publicationpage"
         return context
