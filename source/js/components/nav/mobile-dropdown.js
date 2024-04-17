febe7cfee183fb0aa8038290df700b249620e357
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2024-04-17 08:30:01 -0300

    Add hover effects on mobile :(

--- source/js/components/nav/mobile-dropdown.js
+++ source/js/components/nav/mobile-dropdown.js
@@ -8,6 +8,32 @@ class NavMobileDropdown extends Accordion {
   constructor(node) {
     super(node);
   }
+
+  bindEvents() {
+    super.bindEvents();
+    this.accordion.addEventListener("focus", () => {
+      this.addHoverEffects();
+    });
+    this.accordion.addEventListener("pointerenter", () => {
+      this.addHoverEffects();
+    });
+    this.accordion.addEventListener("blur", () => {
+      this.removeHoverEffects();
+    });
+    this.accordion.addEventListener("pointerleave", () => {
+      this.removeHoverEffects();
+    });
+  }
+
+  addHoverEffects() {
+    this.accordion.classList.add("tw-bg-blue-03");
+    this.titleText.classList.add("tw-underline");
+  }
+
+  removeHoverEffects() {
+    this.accordion.classList.remove("tw-bg-blue-03");
+    this.titleText.classList.remove("tw-underline");
+  }
 }
 
 export default NavMobileDropdown;
