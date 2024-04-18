b7fb18c9cc61465029fa770ba2fff495b3ab2c1e
danielfmiranda <manieldiranda@gmail.com>
2024-04-18 16:01:40 -0700

    update to grab blogindexpage object

--- network-api/networkapi/wagtailpages/rss.py
+++ network-api/networkapi/wagtailpages/rss.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.contrib.syndication.views import Feed
 from django.core.cache import cache
 from django.utils.feedgenerator import Atom1Feed
+from wagtail.models import Locale
 
 from networkapi.wagtailpages.utils import get_locale_from_request
 
-from .models import IndexPage
+from .models import BlogIndexPage, IndexPage
 
 
 class RSSFeed(Feed):
@@ -14,8 +15,6 @@ class RSSFeed(Feed):
     """
 
     title = "Mozilla Foundation Blog"
-    link = "/blog/"
-    feed_url = "/blog/rss/"
     description = "The Mozilla Foundation Blog"
 
     def __call__(self, request, *args, **kwargs):
@@ -23,6 +22,19 @@ class RSSFeed(Feed):
         request_locale = get_locale_from_request(request)
         setattr(self, "request_locale", request_locale)
         return super().__call__(request, *args, **kwargs)
+
+    @property
+    def link(self):
+        blog_index_page = self.get_blog_index_page()
+        return blog_index_page.url
+
+    @property
+    def feed_url(self):
+        return f"{self.link}/rss/"
+
+    def get_blog_index_page(self):
+        blog_index = BlogIndexPage.objects.get(locale=Locale.get_default())
+        return blog_index
 
     def items(self):
         # Get locale from request in __call__ above, defaults to EN.
@@ -32,25 +44,21 @@ class RSSFeed(Feed):
         feed_set = cache.get("rss_feed_set")
 
         if feed_set is None:
-            # If we don't have an active cache, we build one: pull the index page
-            # specifically using the English page title, as an IndexPage rather than
-            # as a BlogIndexPage, to make sure we're not filtering out all the
-            # "featured" posts (which we need to do for site content purposes).
+            # If we don't have an active cache, we build one: First, retrieve the BlogIndexPage.
             try:
-                index = IndexPage.objects.get(title__iexact="Blog", locale=request_locale)
+                blog_index = self.get_blog_index_page()
 
             except IndexPage.DoesNotExist:
-                # If that doesn't yield the blog page, pull using the universal title
-                try:
-                    index = IndexPage.objects.get(title__iexact="Blog", locale=request_locale)
+                # If that doesn't yield the blog index page, there's not much we can do other than to pretend
+                # there are no posts to serialize to RSS/Atom format.
+                return []
 
-                except IndexPage.DoesNotExist:
-                    # At this point there's not much we can do other than to pretend
-                    # there are no posts to serialize to RSS/Atom format.
-                    return []
+            # Because BlogIndexPage overrides the get_all_entries method to exclude "featured" posts
+            # for its "Featured Posts" section, we directly use IndexPage's get_all_entries method.
+            # This ensures that all posts, including those categorized as "featured", are included.
+            blog_pages = IndexPage.get_all_entries(blog_index, request_locale).order_by("-first_published_at")
 
             # Then sort the collection and only yield the top FEED_LIMIT posts
-            blog_pages = index.get_all_entries(request_locale).order_by("-first_published_at")
             feed_set = blog_pages[: settings.FEED_LIMIT]
 
             cache.set("rss_feed_set", feed_set, settings.FEED_CACHE_TIMEOUT)
@@ -75,5 +83,5 @@ class RSSFeed(Feed):
 class AtomFeed(RSSFeed):
     feed_type = Atom1Feed
     link = RSSFeed.link
-    feed_url = "https://foundation.mozilla.org/blog/atom/"
+    feed_url = f"{RSSFeed.link}/atom/"
     subtitle = RSSFeed.description
