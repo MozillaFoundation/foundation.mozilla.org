6667cdd9da90233632f7b99026cb66c792be0600
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2024-04-22 14:46:14 -0300

    Add tests to NavMenuForm

--- network-api/networkapi/nav/forms.py
+++ network-api/networkapi/nav/forms.py
@@ -10,7 +10,6 @@ class NavMenuForm(WagtailAdminModelForm):
         # Make sure that there are featured blog topics if the blog dropdown is enabled
         featured_blog_topics_formset = self.formsets["featured_blog_topics"]
         number_of_featured_blog_topics = featured_blog_topics_formset.total_form_count()
-        print(number_of_featured_blog_topics)
         if enable_blog_dropdown and number_of_featured_blog_topics == 0:
             error = ValidationError("You must add at least one featured blog topic if the blog dropdown is enabled")
             self.add_error(None, error)
