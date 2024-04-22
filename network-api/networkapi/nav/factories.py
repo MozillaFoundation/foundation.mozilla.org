f7c02c70f8ad55385bc40bb72bcc0cd769e9df25
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2024-04-22 10:32:05 -0300

    Add factory for nav menu blog topic relationship model

--- network-api/networkapi/nav/factories.py
+++ network-api/networkapi/nav/factories.py
@@ -249,3 +249,13 @@ class NavMenuFactory(DjangoModelFactory):
         },
     )
     locale = factory.LazyFunction(lambda: wagtail_models.Locale.get_default())
+
+
+class NavMenuFeaturedBlogTopicRelationshipFactory(DjangoModelFactory):
+    class Meta:
+        model = nav_models.NavMenuFeaturedBlogTopicRelationship
+
+    icon = factory.SubFactory(wagtail_factories.ImageChooserBlockFactory)
+    locale = factory.LazyFunction(lambda: wagtail_models.Locale.get_default())
+    menu = factory.SubFactory("networkapi.nav.factories.NavMenuFactory")
+    topic = factory.SubFactory("networkapi.wagtailpages.factory.blog.BlogPageTopicFactory")
