0e8d7d173ccff066a5ba377d57c82a653265518d
danielfmiranda <manieldiranda@gmail.com>
2024-05-02 23:53:54 -0700

    created factory for ways to give page and added it to percy tests

--- network-api/networkapi/donate/factory/__init__.py
+++ network-api/networkapi/donate/factory/__init__.py
@@ -1,10 +1,11 @@
-from . import help_page, landing_page
+from . import help_page, landing_page, ways_to_give_page
 
 
 def generate(seed):
     # these are not, and should not be, alphabetically ordered.
     landing_page.generate(seed)
     help_page.generate(seed)
+    ways_to_give_page.generate(seed)
 
 
 __all__ = [
---
+++ network-api/networkapi/donate/factory/ways_to_give_page.py
@@ -0,0 +1,29 @@
+import wagtail_factories
+from factory import Faker, SubFactory
+from wagtail_factories import PageFactory
+
+from networkapi.donate.models import  DonateLandingPage
+from networkapi.wagtailpages.models import OpportunityPage
+from networkapi.utility.faker import StreamfieldProvider
+from networkapi.utility.faker.helpers import reseed
+
+Faker.add_provider(StreamfieldProvider)
+
+streamfield_fields = ["paragraph", "linkbutton", "spacer", "quote"]
+
+
+class DonateWaysToGivePageFactory(PageFactory):
+    class Meta:
+        # Using OppurtunityPage AKA "Default Page" model
+        model = OpportunityPage
+
+    title = Faker("sentence", nb_words=2)
+    body = Faker("streamfield", fields=streamfield_fields)
+
+
+def generate(seed):
+    reseed(seed)
+
+    print('Generating a Donate "Ways to give" page')
+    donate_home_page = DonateLandingPage.objects.get(title="Donate Now")
+    DonateWaysToGivePageFactory(parent=donate_home_page, title="Ways to Give", header="", slug="ways-to-give")
