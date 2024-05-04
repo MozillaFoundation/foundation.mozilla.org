16cab4f74b372431818eb5f4673fbb1ea98cd3e1
Mavis Ou <mmmavis@users.noreply.github.com>
2024-05-03 23:00:07 -0700

    spacing fixes

--- tailwind-plugins/site-nav.js
+++ tailwind-plugins/site-nav.js
@@ -8,7 +8,7 @@ function shared(theme) {
       display: "flex",
       flexDirection: "column",
       background: "white",
-      gap: theme("gap.24"),
+      gap: `${theme("gap.22")} ${theme("gap.16")}`,
       marginLeft: `-${theme("margin.8")}`,
       marginRight: `-${theme("margin.8")}`,
       paddingTop: theme("padding.12"),
