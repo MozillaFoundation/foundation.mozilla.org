41b03266eb464e813b8c484fb8f93cc186a1cd00
Rob DiVincenzo <rob.divincenzo@gmail.com>
2024-05-29 09:10:03 -0400

    Add data-cta-privacy-notice attribute or empty where possible, single quote for rich text filter

--- source/js/components/newsletter-signup/atoms/privacy-notice.jsx
+++ source/js/components/newsletter-signup/atoms/privacy-notice.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const PrivacyNotice = ({ content, classes }) => {
-    //[TODO] Investigate removing the legacy richtext template which renders an empty rich-text div wrapper
-    // Jira TP1-601 / Github Issue #12285 https://github.com/MozillaFoundation/foundation.mozilla.org/issues/12285
-    if (
-      !this.props.ctaPrivacyNotice ||
-      this.props.ctaPrivacyNotice == '<div class="rich-text"></div>'
-    )
-      return null;
+  //[TODO] Investigate removing the legacy richtext template which renders an empty rich-text div wrapper
+  // Jira TP1-601 / Github Issue #12285 https://github.com/MozillaFoundation/foundation.mozilla.org/issues/12285
+  if (
+    !this.props.ctaPrivacyNotice ||
+    this.props.ctaPrivacyNotice == "<div class='rich-text'></div>"
+  )
+    return null;
 
   if (typeof content === "string") {
     return (
