e92ed5b638cfd34c5df8352fd5b96371a9c82234
AdalbertoMoz <adalberto@mozillafoundation.org>
2024-07-03 18:10:08 -0600

    Feature: Add factory for AppInstallPage

--- network-api/networkapi/wagtailpages/factory/youtube_regrets_page.py
+++ network-api/networkapi/wagtailpages/factory/youtube_regrets_page.py
@@ -1,16 +1,20 @@
-from factory import Faker
+from factory import Faker, SubFactory
 from wagtail.models import Page as WagtailPage
 from wagtail_factories import PageFactory
 
 from networkapi.utility.faker.helpers import get_homepage, reseed
+from networkapi.wagtailpages.factory import image_factory
 from networkapi.wagtailpages.models import (
+    AppInstallPage,
     YoutubeRegrets2021Page,
     YoutubeRegrets2022Page,
     YoutubeRegretsPage,
     YoutubeRegretsReporterPage,
 )
 
+from .bannered_campaign_page import BanneredCampaignPageFactory
 from .campaign_page import CampaignIndexPageFactory
+from .petition import PetitionFactory
 
 
 class YoutubeRegretsPageFactory(PageFactory):
@@ -28,6 +32,25 @@ class YoutubeRegretsPageFactory(PageFactory):
     intro_images = Faker("streamfield", fields=["basic_image"] * 10)
     faq = Faker("streamfield", fields=["paragraph"])
     regret_stories = Faker("streamfield", fields=["regret_story"] * 28)
+
+
+class AppInstallPageFactory(PageFactory):
+    class Meta:
+        model = AppInstallPage
+        exclude = (
+            "title_text",
+            "header_text",
+            "header",
+        )
+
+    title = "Regrets Reporter Page"
+    slug = "regretsreporter"
+    hero_heading = Faker("text", max_nb_chars=50)
+    hero_subheading = Faker("text", max_nb_chars=50)
+    hero_background = SubFactory(image_factory.ImageFactory)
+    hero_video = Faker("url")
+    cta = SubFactory(PetitionFactory)
+    body = Faker("streamfield", fields=["header", "paragraph", "image", "spacer", "image_text", "quote"])
 
 
 class YoutubeRegrets2021PageFactory(PageFactory):
@@ -108,4 +131,29 @@ def generate(seed):
         )
         YoutubeRegrets2021PageFactory.create(parent=youtube_regrets)
         YoutubeRegrets2022PageFactory.create(parent=youtube_regrets)
+    reseed(seed)
+
+    # App Install Page
+    # Checking for a bannered campaign page titled "Youtube Regrets", and then creating the landing page if
+    # it does not exist.
+    try:
+        youtube_bannered_campaign_page = WagtailPage.objects.child_of(home_page).get(title=title)
+        print("Youtube Regrets Bannered Campaign Page exists")
+        # If extension landing page does not exist, create it.
+        if not WagtailPage.objects.child_of(youtube_bannered_campaign_page).type(AppInstallPage):
+            print("Generating App Install Page")
+            AppInstallPageFactory.create(parent=youtube_bannered_campaign_page)
+
+    # If bannered "YouTube Regrets" campaign page does not exist, create it and the extension landing page.
+    except WagtailPage.DoesNotExist:
+        print("Generating a Youtube Bannered Campaign Page and App Install Page")
+        youtube_bannered_campaign_page = BanneredCampaignPageFactory.create(
+            parent=home_page,
+            title="YouTube Regrets",
+            slug="youtube",
+            show_in_menus=False,
+            live=True,
+        )
+        AppInstallPageFactory.create(parent=youtube_bannered_campaign_page)
+
     reseed(seed)
