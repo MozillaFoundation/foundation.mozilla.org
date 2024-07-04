0b5175879066163991e83038738cf78c4894aa0d
danielfmiranda <manieldiranda@gmail.com>
2024-07-04 13:43:38 -0700

    removal of old fields

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/text_only_teaser.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/text_only_teaser.py
@@ -9,25 +9,11 @@ from networkapi.wagtailpages.pagemodels.customblocks.link_block import (
 
 class TextOnlyTeaserCard(blocks.StructBlock):
     heading = blocks.CharBlock(help_text="Heading for the card.")
-    link_page = blocks.PageChooserBlock(required=False, help_text="Page that the header should link out to.")
-    link_url = blocks.CharBlock(
-        required=False,
-        help_text="Optional URL that the header should link out to.",
-    )
     link = blocks.ListBlock(
         LinkWithoutLabelBlock(), min_num=0, max_num=1, help_text="Optional link that the header should link out to."
     )
     meta_data = blocks.CharBlock(max_length=500)
     body = blocks.TextBlock(help_text="Body text of the card.", required=False, max_length=200)
-
-    def clean(self, value):
-        result = super().clean(value)
-        if value["link_page"] and value["link_url"]:
-            raise StructBlockValidationError(
-                block_errors={"link_page": ErrorList(["Please choose between a link page OR a URL value."])}
-            )
-
-        return result
 
 
 class TextOnlyTeaserBlock(blocks.StructBlock):
