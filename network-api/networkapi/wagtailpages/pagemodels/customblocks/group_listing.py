20b93c9c94bffdab504c8ddd474f8b0c8300639c
danielfmiranda <manieldiranda@gmail.com>
2024-07-03 18:54:06 -0700

    removal of old field

--- network-api/networkapi/wagtailpages/pagemodels/customblocks/group_listing.py
+++ network-api/networkapi/wagtailpages/pagemodels/customblocks/group_listing.py
@@ -16,7 +16,7 @@ class GroupListingCard(blocks.StructBlock):
     title = blocks.CharBlock(help_text="Heading for the card.")
 
     body = blocks.RichTextBlock(features=["bold", "link"], help_text="Body text of the card.")
-    url = blocks.CharBlock(required=False, help_text="The URL this card should link to.")
+
     link = blocks.ListBlock(
         LinkWithoutLabelBlock(), min_num=0, max_num=1, help_text="Optional link that this card should link to."
     )
