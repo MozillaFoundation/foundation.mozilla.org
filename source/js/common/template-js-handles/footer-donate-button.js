9caa2ef54799acf3770aaf921a1c7814f788e7d6
dlopezvsr <diego_lo.vi@icloud.com>
2024-08-27 12:10:39 -0600

    Update initial GTM setup - replace first ReactGA event: footer donate button

--- source/js/common/template-js-handles/footer-donate-button.js
+++ source/js/common/template-js-handles/footer-donate-button.js
@@ -5,14 +5,15 @@ import { ReactGA } from "../react-ga-proxy.js";
  * ("Donate" button on footer)
  */
 export default () => {
-  let donateFooterBtn = document.getElementById(`donate-footer-btn`);
+  let donateFooterBtn = document.getElementById('donate-footer-btn');
 
   if (donateFooterBtn) {
-    donateFooterBtn.addEventListener(`click`, () => {
-      ReactGA.event({
-        category: `donate`,
-        action: `donate button tap`,
-        label: `${document.title} footer`,
+    donateFooterBtn.addEventListener('click', () => {
+      window.dataLayer.push({
+        event: 'donate_button_click',
+        category: 'donate',
+        action: 'donate button tap',
+        label: `${document.title} footer`
       });
     });
   }
