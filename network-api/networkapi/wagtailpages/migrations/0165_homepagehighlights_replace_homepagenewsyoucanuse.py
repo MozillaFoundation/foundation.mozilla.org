cd9fc74419f8b33f1865d380d06efaf73f38869b
Ramon Ramos <ramon@mozillafoundation.org>
2024-09-23 14:06:57 -0600

    Update migration

---
+++ network-api/networkapi/wagtailpages/migrations/0165_homepagehighlights_replace_homepagenewsyoucanuse.py
@@ -0,0 +1,35 @@
+import django.db.models.deletion
+import modelcluster.fields
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("wagtailcore", "0089_log_entry_data_json_null_to_object"),
+        ("wagtailpages", "0164_homepage_hero_intro_body_homepage_hero_intro_heading_and_more"),
+    ]
+
+    operations = [
+        migrations.RenameModel(
+            old_name="HomepageNewsYouCanUse",
+            new_name="HomepageHighlights",
+        ),
+        migrations.AddField(
+            model_name="homepage",
+            name="ideas_title",
+            field=models.CharField(default="Ideas", max_length=50),
+        ),
+        migrations.AddField(
+            model_name="homepage",
+            name="highlights_title",
+            field=models.CharField(default="The Highlights", max_length=50),
+        ),
+        migrations.AlterField(
+            model_name="homepagehighlights",
+            name="page",
+            field=modelcluster.fields.ParentalKey(
+                on_delete=django.db.models.deletion.CASCADE, related_name="highlights", to="wagtailpages.homepage"
+            ),
+        ),
+    ]
