7e72e34c6d29b67dbdd011af576bb37cb380d7b2
Mavis Ou <mmmavis@users.noreply.github.com>
2024-10-03 17:31:08 -0700

    Refactored function updateHeader

--- source/js/buyers-guide/search/utils.js
+++ source/js/buyers-guide/search/utils.js
@@ -32,43 +32,40 @@ export class Utils {
   /**
    * Update page header to the category passed in the argument
    *
-   * @param {*} category
-   * @param {*} parent
-   *
-   * @todo Improve the implementation to increase code readibility
+   * @param {String} category name of the category
+   * @param {String} parent name of the parent category
    */
   static updateHeader(category, parent) {
-    const headerText = document.querySelector(".category-header");
+    const datasetName =
+      parent || (category === "None" ? ALL_CATEGORY_LABEL : category);
+    const text = parent || category;
 
-    if (parent) {
-      document.querySelector(".category-header").dataset.name = parent;
-      headerText.textContent = parent;
-      if (document.querySelector(`#multipage-nav a[data-name="${parent}"]`)) {
-        document.querySelector(".category-header").href =
-          document.querySelector(
-            `#multipage-nav a[data-name="${parent}"]`
-          ).href;
-      }
-      document.querySelector(
-        `#pni-mobile-category-nav .active-link-label`
-      ).textContent = parent;
-    } else {
-      const header = category === "None" ? ALL_CATEGORY_LABEL : category;
-      headerText.textContent = header;
-      document.querySelector(".category-header").dataset.name = category;
-      if (document.querySelector(`#multipage-nav a[data-name="${category}"]`)) {
-        document.querySelector(".category-header").href =
-          document.querySelector(
-            `#multipage-nav a[data-name="${category}"]`
-          ).href;
-      }
-      document.querySelector(
-        `#pni-mobile-category-nav .active-link-label`
-      ).textContent =
-        category === "None"
-          ? document.querySelector(`#multipage-nav a[data-name="None"]`)
-              .textContent
-          : category;
+    this.setCategoryHeader(datasetName, text);
+  }
+
+  /**
+   * Set category header text in the DOM
+   */
+  static setCategoryHeader(datasetName, text) {
+    const categoryHeader = document.querySelector(".category-header");
+    const navLink = document.querySelector(
+      `#multipage-nav a[data-name="${text}"]`
+    );
+    const mobileActiveLink = document.querySelector(
+      `#pni-mobile-category-nav .active-link-label`
+    );
+
+    if (!categoryHeader) return;
+
+    categoryHeader.dataset.name = datasetName;
+    categoryHeader.textContent = text;
+
+    if (navLink) {
+      categoryHeader.href = navLink.href;
+    }
+
+    if (mobileActiveLink) {
+      mobileActiveLink.textContent = datasetName;
     }
   }
 
