67d897d2861094734287c733eed29baa348fa26f
dlopezvsr <diego_lo.vi@icloud.com>
2024-10-08 15:03:47 -0600

    Add if clause for self.partner_page validation

--- network-api/networkapi/wagtailpages/pagemodels/base.py
+++ network-api/networkapi/wagtailpages/pagemodels/base.py
@@ -1013,5 +1013,6 @@ class Homepage(FoundationMetadataPageMixin, Page):
         context["MEDIA_URL"] = settings.MEDIA_URL
         context["menu_root"] = self
         context["menu_items"] = self.get_children().live().in_menu()
-        context["localized_partner_page"] = self.partner_page.localized
+        if self.partner_page:
+            context["localized_partner_page"] = self.partner_page.localized
         return context
