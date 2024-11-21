d6e3192833b3fa6eefec3cffbfaa2d3fae4d4e80
Mavis Ou <mmmavis@users.noreply.github.com>
2024-11-20 17:16:19 -0800

    more experiments

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,12 @@
 const config = {
-  workers: 2,
+  workers: 1,
+  timeout: 90000,
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     permissions: ["clipboard-read"],
+    navigationTimeout: 60000,
   },
 };
 
