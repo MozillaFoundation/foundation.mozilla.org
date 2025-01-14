bacbc00adfee7f58e1c4427a1459ec1d97a06779
dlopezvsr <diego_lo.vi@icloud.com>
2025-01-14 13:54:32 -0600

    Navbar added

--- foundation_cms/base/models.py
+++ foundation_cms/base/models.py
@@ -1,5 +1,19 @@
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+from foundation_cms.blog.models import BlogPage
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        # Add live blog pages to the context
+        context['blogs'] = BlogPage.objects.live().order_by('-first_published_at')
+        return context
