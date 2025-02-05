0ab526b9ae67af60f3d33a4ae4f40c4251ba015d
Rob DiVincenzo <rob@mozillafoundation.org>
2025-02-05 13:11:50 -0500

    Wagtail Footnotes Upgrade Part 1 (#13464)
    * Wagtail footnotes pt. 1, install upstream package
    
    * linting
    
    * Avoid breaking change by going to 0.11.0 in pt. 1
    
    * Update release-steps to fake initial migration
    
    * Attempt to skip migrations for this release since we're faking

--- release-steps.sh
+++ release-steps.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 cd network-api
 
 # Django Migrations
-python ./manage.py migrate --no-input
+# python ./manage.py migrate --no-input
 
 # Clear cache for BuyersGuide
 python ./manage.py clear_cache
