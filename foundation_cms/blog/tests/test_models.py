933d62d8ecbae02594b5ddc76f14e48bcf4ce77f
Diego López <81331806+dlopezvsr@users.noreply.github.com>
2025-03-06 08:34:27 -0600

    Test Suite Iteration 2 (#13508)
    * Djlint added and templates formatted
    
    * Check all templates with djlint
    
    * Tests updated with factories
    
    * profile subfactory added and isroutable test removed for simplicity
    
    * Search tests refactored
    
    * Run unittests in CI pipeline
    
    * Run unittests first in CI pipeline

--- foundation_cms/blog/tests/test_models.py
+++ foundation_cms/blog/tests/test_models.py
@@ -1,50 +1,41 @@
 from wagtail.test.utils import WagtailPageTestCase
+from wagtail.models import Page
 
-from foundation_cms.blog.models import BlogIndexPage, BlogPage
 from foundation_cms.profiles.models import Profile
+from foundation_cms.profiles.factories import ProfileFactory
+from foundation_cms.blog.factories import BlogPageFactory, BlogIndexPageFactory
 
 
 class BlogPageTestCase(WagtailPageTestCase):
     def setUp(self):
-        self.blog_index = BlogIndexPage.objects.create(
-            title="Blog Index", body="This is the blog index body."
-        )
-        self.profile = Profile.objects.create(title="John Doe")
-        self.blog_page = BlogPage.objects.create(
-            title="Sample Blog",
-            body="This is a sample blog body.",
-            author=self.profile,
-            parent=self.blog_index,
-        )
-
-    def test_default_route(self):
-        self.assertPageIsRoutable(self.blog_page)
+        self.blog_index = BlogIndexPageFactory()
+        self.profile = ProfileFactory()
+        self.blog_page = BlogPageFactory(parent=self.blog_index, author=self.profile)
 
     def test_author_name(self):
-        self.assertEqual(self.blog_page.author_name(), "John Doe")
+        """This test ensures author_name() returns a string and author is a Profile instance."""
+        self.assertIsInstance(self.blog_page.author_name(), str)
+        self.assertIsInstance(self.blog_page.author, Profile)
+        self.assertEqual(self.blog_page.author, self.profile)
 
     def test_author_name_default(self):
-        self.blog_page.author = None
+        """If author is None, author_name() should return 'Mozilla Foundation'."""
+        setattr(self.blog_page, "author", None)
+        self.blog_page.save()
         self.assertEqual(self.blog_page.author_name(), "Mozilla Foundation")
 
 
 class BlogIndexPageTestCase(WagtailPageTestCase):
     def setUp(self):
-        self.blog_index = BlogIndexPage.objects.create(
-            title="Blog Index", body="This is the blog index body."
-        )
-        BlogPage.objects.create(
-            title="Blog 1", body="Content of Blog 1", parent=self.blog_index
-        )
-        BlogPage.objects.create(
-            title="Blog 2", body="Content of Blog 2", parent=self.blog_index
-        )
-
-    def test_default_route(self):
-        self.assertPageIsRoutable(self.blog_index)
+        """Set up a BlogIndexPage and attach BlogPages."""
+        self.blog_index = BlogIndexPageFactory()
+        self.blog_1 = BlogPageFactory(title="Blog 1", parent=self.blog_index)
+        self.blog_2 = BlogPageFactory(title="Blog 2", parent=self.blog_index)
 
     def test_get_context(self):
+        """This test ensures the context contains the correct number of blog pages."""
         request = self.client.get(self.blog_index.url)
         context = self.blog_index.get_context(request)
-        self.assertEqual(len(context["blogs"]), 2)
-        self.assertEqual(context["blogs"][0].title, "Blog 2")
+
+        self.assertEqual(len(context["blogs"]), 2)  # It ensures both blogs exist
+        self.assertEqual(context["blogs"][0].title, self.blog_2.title)
