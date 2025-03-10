df35d5447d971bd26349855fe4358b5d14febe83
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-03-10 10:23:52 -0400

    Alter site for legacy tests

--- tests/base-urls.js
+++ tests/base-urls.js
@@ -1,17 +1,17 @@
-const FOUNDATION_DOMAIN = "http://localhost:8000";
+const LEGACY_FOUNDATION_DOMAIN = "http://legacy.localhost:8000";
 
 module.exports = {
   donateBaseUrl: function (locale = "en") {
-    return `${FOUNDATION_DOMAIN}/${locale}/donate`;
+    return `${LEGACY_FOUNDATION_DOMAIN}/${locale}/donate`;
   },
-  foundationDomain: FOUNDATION_DOMAIN,
+  foundationDomain: LEGACY_FOUNDATION_DOMAIN,
   foundationBaseUrl: function (locale = "en") {
-    return `${FOUNDATION_DOMAIN}/${locale}`;
+    return `${LEGACY_FOUNDATION_DOMAIN}/${locale}`;
   },
   mozfestBaseUrl: function (locale = "en") {
     return `http://mozfest.localhost:8000/${locale}`;
   },
   pniBaseUrl: function (locale = "en") {
-    return `${FOUNDATION_DOMAIN}/${locale}/privacynotincluded`;
+    return `${LEGACY_FOUNDATION_DOMAIN}/${locale}/privacynotincluded`;
   },
 };
