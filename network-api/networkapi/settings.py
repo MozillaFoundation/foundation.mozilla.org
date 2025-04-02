0eea13421fd7ade4f4506e577d8cd6612444a140
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-04-02 15:54:09 -0400

    Create new pipeline to associate users via email.

--- network-api/networkapi/settings.py
+++ network-api/networkapi/settings.py
@@ -331,6 +331,7 @@ if SOCIAL_SIGNIN:
         "social_core.pipeline.social_auth.social_uid",
         "social_core.pipeline.social_auth.auth_allowed",
         "social_core.pipeline.social_auth.social_user",
+        "networkapi.pipeline.associate_by_email",
         "social_core.pipeline.user.get_username",
         "social_core.pipeline.user.create_user",
         "social_core.pipeline.social_auth.associate_user",
