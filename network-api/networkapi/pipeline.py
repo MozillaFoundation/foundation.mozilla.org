cd2c37c796488553e3fce6207249674a146537f3
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-04-02 14:24:09 -0400

    add debugging

--- network-api/networkapi/pipeline.py
+++ network-api/networkapi/pipeline.py
@@ -6,6 +6,10 @@ def force_username_from_nickname(backend, details, response, *args, **kwargs):
     Forces username to match Google-style nickname for consistency across backends.
     Required to not duplicate users when switched from google-oauth2 to auth0 mozilla sso.
     """
+    print("Custom pipeline step running")
     nickname = response.get("nickname") or response.get("given_name") or response.get("name")
     if nickname:
+        print("Nickname found")
         details["username"] = nickname.lower().replace(" ", "")
+    else:
+        print("Nickname not found")
