e6c80be5c6d2729dbaf36e1eb890fa29a6e0120b
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-04-02 15:38:09 -0400

    revert pipeline, and test username migration script

--- network-api/networkapi/settings.py
+++ network-api/networkapi/settings.py
@@ -331,7 +331,6 @@ if SOCIAL_SIGNIN:
         "social_core.pipeline.social_auth.social_uid",
         "social_core.pipeline.social_auth.auth_allowed",
         "social_core.pipeline.social_auth.social_user",
-        "networkapi.pipeline.force_username_from_nickname",
         "social_core.pipeline.user.get_username",
         "social_core.pipeline.user.create_user",
         "social_core.pipeline.social_auth.associate_user",
