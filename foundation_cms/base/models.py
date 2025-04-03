aa5befffec5dd00b980b1f7eebaad91f168a91b7
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-04-03 15:21:22 -0400

    Add an abstract collection page

--- foundation_cms/base/models.py
+++ foundation_cms/base/models.py
@@ -1,5 +1,6 @@
 from .models.abstract_article_page import AbstractArticlePage
 from .models.abstract_base_page import AbstractBasePage, Author
+from .models.abstract_collection_page.py import AbstractCollectionPage
 from .models.abstract_general_page import AbstractGeneralPage
 from .models.abstract_home_page import AbstractHomePage
 from .models.base_block import BaseBlock
@@ -8,6 +9,7 @@ from .models.base_block import BaseBlock
 __all__ = [
     "AbstractBasePage",
     "AbstractArticlePage",
+    "AbstractCollectionPage",
     "AbstractGeneralPage",
     "AbstractHomePage",
     "BaseBlock",
