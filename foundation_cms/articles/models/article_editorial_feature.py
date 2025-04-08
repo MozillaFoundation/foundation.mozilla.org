e9c662156f00f0ecc2330d588b220d9e9baee477
dlopezvsr <diego_lo.vi@icloud.com>
2025-04-08 14:39:13 -0600

    Article scaffold models initial commit

---
+++ foundation_cms/articles/models/article_editorial_feature.py
@@ -0,0 +1,8 @@
+from django.db import models
+from wagtail.admin.panels import FieldPanel
+
+from foundation_cms.base.models.abstract_article_page import AbstractArticlePage
+
+
+class ArticleEditorialFeature(AbstractArticlePage):
+    ...
