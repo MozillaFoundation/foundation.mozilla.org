34781264d18529be8af6f8f6e79d37eff476306b
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-05-02 12:29:47 -0400

    Refactor translation management to clean dirs up, move away from symlinking

--- translation-management.sh
+++ translation-management.sh
@@ -54,24 +54,56 @@ case $command in
   "import")
     echogreen "Importing latest translation files from fomo-l10n repository"
     for FOLDER in "${FOLDERS[@]}"; do
+      # Step 1: Reset Django folder(s)
+      echoyellow "  Removing existing folder: ${CODE_REPO}${FOLDER}"
+      rm -rf "${CODE_REPO}${FOLDER}"
+
+      # Step 2: Copy l10n (source of truth) to Django
+      echoyellow "  Copying l10n folder: ${L10N_REPO}${FOLDER} into ${CODE_REPO}${FOLDER}"
       cp -r "${L10N_REPO}${FOLDER}" "${CODE_REPO}${FOLDER}"
 
-      # Django >=3.2 stopped processing locale codes containing hyphens, we need to move the files between the two folders
+      # Step 3: Remove stray underscore-locale dirs / symlinks (cleanup)
+      for HYPHEN_LOCALE in "${LOCALES[@]}"; do
+        UNDERSCORE="${CODE_REPO}${FOLDER}${HYPHEN_LOCALE//-/_}"
+        if [[ -e "$UNDERSCORE" ]]; then
+          echoyellow "  Removing stray underscore locale (usually symlinks): $UNDERSCORE"
+          rm -rf "$UNDERSCORE"
+        fi
+      done
+
+      # Step 4: Copy hyphenated → underscore for Django
       for HYPHEN_LOCALE in "${LOCALES[@]}"; do
         cp -r "${L10N_REPO}${FOLDER}${HYPHEN_LOCALE}/" "${CODE_REPO}${FOLDER}${HYPHEN_LOCALE//-/_}/"
       done
     done
+    ;;
 esac
 
 case $command in
   "export")
     echogreen "Exporting generated translation files to fomo-l10n repository"
     for FOLDER in "${FOLDERS[@]}"; do
-      cp -r "${CODE_REPO}${FOLDER}" "${L10N_REPO}${FOLDER}"
+      # Step 1: Reset l10n folder(s)
+      echoyellow "  Removing existing folder: ${L10N_REPO}${FOLDER}"
+      rm -rf "${L10N_REPO}${FOLDER}"
 
-      # Django >=3.2 stopped processing locale codes containing hyphens, we need to move the files between the two folders
+      # Step 2: Copy Django (new source of truth) to l10n
+      echoyellow "  Copying Django folder: ${CODE_REPO}${FOLDER} into ${L10N_REPO}${FOLDER}"
+      mkdir -p "${L10N_REPO}${FOLDER}"
+      cp -r "${CODE_REPO}${FOLDER}"* "${L10N_REPO}${FOLDER}"
+
+      # Step 3: Copy Django underscore dirs to l10n hyphen dirs for Pontoon (i.e. pt_BR in Django to pt-BR in l10n)
       for HYPHEN_LOCALE in "${LOCALES[@]}"; do
         cp -r "${CODE_REPO}${FOLDER}${HYPHEN_LOCALE//-/_}/" "${L10N_REPO}${FOLDER}${HYPHEN_LOCALE}/"
+
+        # Step 4: Remove underscore locale dirs in l10n since not needed in l10n repo (i.e. pt_BR)
+        UNDERSCORE_LOCALE_PATH="${L10N_REPO}${FOLDER}${HYPHEN_LOCALE//-/_}"
+        if [[ -e "$UNDERSCORE_LOCALE_PATH" ]]; then
+          echoyellow "  Removing underscore locale from l10n: $UNDERSCORE_LOCALE_PATH"
+          rm -rf "$UNDERSCORE_LOCALE_PATH"
+        fi
+      
       done
     done
+    ;;
 esac
