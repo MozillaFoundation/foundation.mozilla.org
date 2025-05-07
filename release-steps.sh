515ec7d0a439f4718ecdd4f131ec2f32c9a8ed46
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-05-07 15:46:04 -0400

    revert release-steps for deploying against existing db

--- release-steps.sh
+++ release-steps.sh
@@ -3,11 +3,14 @@ set -eo pipefail
 
 cd network-api
 
-# Django Migrations
-python ./manage.py migrate --no-input
+# Temporary Step 1 for migrating to custom image model: migrate images app
+python ./manage.py migrate images
 
 # Temporary Step 2 for migrating to custom image model: data migration for images
 python ./manage.py migrate_legacy_images
+
+# Django Migrations
+python ./manage.py migrate --no-input
 
 # Clear cache for BuyersGuide
 python ./manage.py clear_cache
