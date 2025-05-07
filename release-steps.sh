45cb4172d07da22c9491ab7e545a9394d27fa59c
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-05-07 15:11:25 -0400

    Adjust release-steps

--- release-steps.sh
+++ release-steps.sh
@@ -3,14 +3,11 @@ set -eo pipefail
 
 cd network-api
 
-# Temporary Step 1 for migrating to custom image model: migrate images app
-python ./manage.py migrate images
+# Django Migrations
+python ./manage.py migrate --no-input
 
 # Temporary Step 2 for migrating to custom image model: data migration for images
 python ./manage.py migrate_legacy_images
-
-# Django Migrations
-python ./manage.py migrate --no-input
 
 # Clear cache for BuyersGuide
 python ./manage.py clear_cache
