5759649e6d01ad83cde76145808a94e2f063bd29
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-05-07 15:01:02 -0400

    Update paramaters in release-steps

--- release-steps.sh
+++ release-steps.sh
@@ -4,10 +4,10 @@ set -eo pipefail
 cd network-api
 
 # Temporary Step 1 for migrating to custom image model: migrate images app
-python ./manage.py migrate images --no-input
+python ./manage.py migrate images
 
 # Temporary Step 2 for migrating to custom image model: data migration for images
-python ./manage.py migrate_legacy_images --no-input
+python ./manage.py migrate_legacy_images
 
 # Django Migrations
 python ./manage.py migrate --no-input
