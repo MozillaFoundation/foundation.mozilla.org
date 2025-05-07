1924e3a3218109719e8c7412df913d5a1a291c48
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-05-07 11:07:12 -0400

    Adding temporary release steps for migrating to custom image model

--- release-steps.sh
+++ release-steps.sh
@@ -3,6 +3,12 @@ set -eo pipefail
 
 cd network-api
 
+# Temporary Step 1 for migrating to custom image model: migrate images app
+python ./manage.py migrate images --no-input
+
+# Temporary Step 2 for migrating to custom image model: data migration for images
+python ./manage.py migrate_legacy_images --no-input
+
 # Django Migrations
 python ./manage.py migrate --no-input
 
