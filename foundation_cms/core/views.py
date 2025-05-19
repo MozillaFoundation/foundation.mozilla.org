bebc4a97bef63a46452d0e3a79113f51fdc9d10a
dlopezvsr <diego_lo.vi@icloud.com>
2025-05-19 17:21:43 -0600

    Fix of models import

--- foundation_cms/core/views.py
+++ foundation_cms/core/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
-from wagtail.models import Page
 from foundation_cms.base.models import AbstractArticlePage
+
 
 def listing_page(request):
     # Get the tag from the query parameters
     tag = request.GET.get('tag')
-    
+
     # Get all pages
     pages = AbstractArticlePage.objects.live().public()
-    
+
     # Filter by tag if provided
     if tag:
         articles = pages.filter(base_tagged_items__tag__slug=tag)
-    
+    else:
+        articles = pages.all()
+
     context = {
         'articles': articles,
         'active_tag': tag if tag else 'Latest Posts'
     }
-    
+
     return render(request, "patterns/pages/core/listing_page.html", context)
