3263b157ccc7939d753b35024443d4bb67270ac0
Diego López <81331806+dlopezvsr@users.noreply.github.com>
2025-05-23 11:00:59 -0600

    TP1-2499 Image Card with Title (#13999)
    * SizedImageBlock implemented | first scaffolds for image block
    
    * View included to render general page | image block inclued in general page
    
    * Refactoring of previous approach. Implemented two components -one for single images and other for double. Generic design added
    
    * Merge migration file conflicts
    
    * Merge conflicts fixed
    
    * Formatting html templates
    
    * Fix linting issues scss
    
    * Image card with title dev approach refactored based on two columns container
    
    * Reformat image block template
    
    * stylelint fixes
    
    * Previous approach image templates deleted
    
    * Update name from ImageBlock to CustomImageBlock to avoid conflicts
    
    * Update foundation_cms/blocks/image_block.py
    
    Co-authored-by: Rob DiVincenzo <rob@mozillafoundation.org>
    
    * Update foundation_cms/static/scss/redesign_main.scss
    
    Co-authored-by: Rob DiVincenzo <rob@mozillafoundation.org>
    
    * Update foundation_cms/core/views.py
    
    Co-authored-by: Rob DiVincenzo <rob@mozillafoundation.org>
    
    * Update foundation_cms/urls.py
    
    Co-authored-by: Rob DiVincenzo <rob@mozillafoundation.org>
    
    * Fix linting issues scss
    
    ---------
    
    Co-authored-by: Rob DiVincenzo <rob@mozillafoundation.org>

--- foundation_cms/blocks/__init__.py
+++ foundation_cms/blocks/__init__.py
@@ -1,10 +1,14 @@
 from .tabbed_content_container_block import TabbedContentContainerBlock
 from .tabbed_content_tab_block import TabbedContentTabBlock
 from .two_column_container_block import TwoColumnContainerBlock
+from .image_block import CustomImageBlock
+from .audio_block import AudioBlock
 
 # Add "unused" import to _all_ for flake8 linting
 __all__ = [
     "TabbedContentContainerBlock",
     "TabbedContentTabBlock",
     "TwoColumnContainerBlock",
+    "CustomImageBlock",
+    "AudioBlock",
 ]
