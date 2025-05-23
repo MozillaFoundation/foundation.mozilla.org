768ef53f5430d5fd550e721d486f36d5e7bf0f8b
Mavis Ou <mmmavis@users.noreply.github.com>
2025-05-23 15:14:59 -0700

    deleted more old code

--- foundation_cms/core/models/home_page.py
+++ foundation_cms/core/models/home_page.py
@@ -4,16 +4,9 @@ from wagtail_localize.fields import TranslatableField
 
 from foundation_cms.base.models.abstract_home_page import AbstractHomePage
 
-hero_intro_heading_default_text = "A healthy internet is one in which privacy, openness, and inclusion are the norms."
-hero_intro_body_default_text = (
-    "Mozilla empowers consumers to demand better online privacy, trustworthy AI, "
-    "and safe online experiences from Big Tech and governments. We work across "
-    "borders, disciplines, and technologies to uphold principles like privacy, "
-    "inclusion and decentralization online."
-)
-
-
 class HomePage(AbstractHomePage):
+
+
     content_panels = AbstractHomePage.content_panels + [
         FieldPanel("body"),
     ]
