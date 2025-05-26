6cc5a5f638d4147ee37204ebb1382ef306f3882e
dlopezvsr <diego_lo.vi@icloud.com>
2025-05-26 17:51:48 -0600

    Tabbed content frontend

--- foundation_cms/blocks/image_block.py
+++ foundation_cms/blocks/image_block.py
@@ -2,11 +2,12 @@ from wagtail.blocks import CharBlock, ChoiceBlock
 from wagtail.images.blocks import ImageBlock
 from foundation_cms.base.models.base_block import BaseBlock
 
+
 class CustomImageBlock(BaseBlock):
     """
     A reusable image block with title and orientation options
     """
-    title = CharBlock(required=True, help_text="Title/caption for this image")
+    title = CharBlock(required=False, help_text="Title/caption for this image")
     image = ImageBlock(required=True)
     orientation = ChoiceBlock(
         choices=[
@@ -16,8 +17,11 @@ class CustomImageBlock(BaseBlock):
         default='landscape',
         help_text='Select the orientation of this image'
     )
-    
+
+    def get_template(self, context=None):
+        return "patterns/blocks/image_block.html"
+
     class Meta:
         icon = "image"
-        template = "patterns/blocks/image_block.html"
-        label = "Image" 
+        template_name = "image_block.html"
+        label = "Image"
--- foundation_cms/blocks/tabbed_content_tab_block.py
+++ foundation_cms/blocks/tabbed_content_tab_block.py
@@ -1,5 +1,15 @@
 from wagtail import blocks
 from foundation_cms.base.models.base_block import BaseBlock
+from wagtail.images.blocks import ImageBlock
+
+
+class TabImageBlock(blocks.StructBlock):
+    image = ImageBlock(required=True)
+
+    class Meta:
+        icon = "image"
+        template = "patterns/blocks/image_block.html"
+
 
 class TabbedContentTabBlock(blocks.StructBlock):
     title = blocks.CharBlock(required=True, help_text="Title shown on the tab button")
@@ -7,12 +17,12 @@ class TabbedContentTabBlock(blocks.StructBlock):
     content = blocks.StreamBlock(
         [
             ("rich_text", blocks.RichTextBlock()),
+            ("image", TabImageBlock()),
             # TODO add other blocks as they're built
         ],
         required=True,
         min_num=1,
-        max_num=1,
-        help_text="Only one content block per tab."
+        help_text="Add one or more content blocks to the tab."
     )
 
     class Meta:
