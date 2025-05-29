de403fbcdf2aa53ed370459eb34de5ce744f0851
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-05-29 12:57:31 -0400

    linting

--- foundation_cms/blocks/image_block.py
+++ foundation_cms/blocks/image_block.py
@@ -1,9 +1,7 @@
 from wagtail.blocks import CharBlock, ChoiceBlock
 from wagtail.images.blocks import ImageBlock
 
-
 from foundation_cms.base.models.base_block import BaseBlock
-
 
 
 class CustomImageBlock(BaseBlock):
