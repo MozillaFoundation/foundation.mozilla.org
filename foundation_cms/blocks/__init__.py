e26cbaded9e66a99807b45e5954bbbdb460bd6bc
Mavis Ou <mmmavis@users.noreply.github.com>
2025-06-05 13:45:05 -0700

    Added Quote Block

--- foundation_cms/blocks/__init__.py
+++ foundation_cms/blocks/__init__.py
@@ -12,6 +12,7 @@ from .tabbed_content_container_block import TabbedContentContainerBlock
 from .tabbed_content_tab_block import TabbedContentTabBlock
 from .text_image_block import TextImageBlock
 from .two_column_container_block import TwoColumnContainerBlock
+from .quote_block import QuoteBlock
 
 # Add "unused" import to _all_ for flake8 linting
 __all__ = [
@@ -30,4 +31,5 @@ __all__ = [
     "LinkButtonBlock",
     "ImpactNumberBlock",
     "SpacerBlock",
+    "QuoteBlock",
 ]
