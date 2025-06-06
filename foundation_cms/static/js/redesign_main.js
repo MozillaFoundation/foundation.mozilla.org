425c1a58553d51f299ce61d4c546ef5da869dc42
Mavis Ou <mmmavis@users.noreply.github.com>
2025-06-06 09:39:25 -0700

    Base accordion set up & video panel behaviours

--- foundation_cms/static/js/redesign_main.js
+++ foundation_cms/static/js/redesign_main.js
@@ -1,11 +1,13 @@
 import $ from "jquery";
 import "foundation-sites";
 import { initImpactNumberStatAnimationsOnScroll } from "./blocks/impact_numbers_block.js";
+import { initAllHorizontalAccordions } from "./blocks/hero_accordion.js";
 
 $(document).foundation();
 
 console.log(
-  "😃 If you see this message, the JS watch and build steps are working properly! 😃",
+  "😃 If you see this message, the JS watch and build steps are working properly! 😃"
 );
 
 initImpactNumberStatAnimationsOnScroll();
+initAllHorizontalAccordions();
