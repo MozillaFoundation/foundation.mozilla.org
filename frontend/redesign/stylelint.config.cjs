a135673ea197d746ccbb3a63d8b41cea71851f8c
Mavis Ou <mmmavis@users.noreply.github.com>
2025-06-06 12:48:13 -0700

    updated Stylelint to allow BEM naming & fixed linting/formatting issues

--- frontend/redesign/stylelint.config.cjs
+++ frontend/redesign/stylelint.config.cjs
@@ -11,5 +11,16 @@ module.exports = {
   ],
   rules: {
     "block-no-empty": true,
+    // Allow kebab-case or BEM (block__element--modifier) class names:
+    "selector-class-pattern": [
+      // 1) block: lowercase + optional -words
+      // 2) optional __element: lowercase + optional -words
+      // 3) optional --modifier: lowercase + optional -words
+      "^[a-z0-9]+(?:-[a-z0-9]+)*(?:__[a-z0-9]+(?:-[a-z0-9]+)*)?(?:--[a-z0-9]+(?:-[a-z0-9]+)*)?$",
+      {
+        message:
+          "Class selectors must be kebab-case (e.g. foo-bar) or BEM (e.g. foo-bar__element--modifier).",
+      },
+    ],
   },
 };
