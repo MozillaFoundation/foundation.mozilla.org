457521376e810ee84773fb79f34d7e0683398ebd
dlopezvsr <diego_lo.vi@icloud.com>
2025-06-09 18:08:53 -0600

    Remove test from home page scss

--- foundation_cms/blocks/list_block.py
+++ foundation_cms/blocks/list_block.py
@@ -5,10 +5,11 @@ class ListBlock(blocks.StructBlock):
     """
     A block for rendering a list of items.
     """
+
     title = blocks.CharBlock(help_text="Heading displayed above the list")
     description = blocks.TextBlock(help_text="Description displayed above the list")
     items = blocks.ListBlock(
-       blocks.PageChooserBlock(),
+        blocks.PageChooserBlock(),
         min_num=1,
         max_num=4,
     )
