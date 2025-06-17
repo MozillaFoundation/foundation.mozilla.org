1df3689f3c5ff4f1bb3218248bbac97c2d697c61
Ram <ramon@mozillafoundation.org>
2025-06-17 13:51:49 -0600

    Update migrations

--- foundation_cms/core/migrations/0018_remove_generalpage_header_and_more.py
+++ foundation_cms/core/migrations/0018_remove_generalpage_header_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0016_alter_generalpage_body_alter_homepage_body"),
+        ("core", "0017_add_newsletter_signup"),
     ]
 
     operations = [
