b65edc4de5546fb8d69e56c1edee9fd999f52a16
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-06-18 17:06:20 -0400

    Add new spotlight card and spotlight card set

--- foundation_cms/blocks/__init__.py
+++ foundation_cms/blocks/__init__.py
@@ -1,4 +1,5 @@
 from .audio_block import AudioBlock
+from .base_card_block import BaseCardBlock
 from .hero_accordion import HeroAccordionBlock
 from .image_block import CustomImageBlock
 from .impact_number_block import ImpactNumberBlock
@@ -10,6 +11,8 @@ from .portrait_card_block import PortraitCardBlock
 from .portrait_card_set_block import PortraitCardSetBlock
 from .quote_block import QuoteBlock
 from .spacer_block import SpacerBlock
+from .spotlight_card_block import SpotlightCardBlock
+from .spotlight_card_set_block import SpotlightCardSetBlock
 from .tabbed_content_card_set_block import TabbedContentCardSetBlock
 from .tabbed_content_container_block import TabbedContentContainerBlock
 from .tabbed_content_tab_block import TabbedContentTabBlock
@@ -19,6 +22,9 @@ from .two_column_container_block import TwoColumnContainerBlock
 
 # Add "unused" import to _all_ for flake8 linting
 __all__ = [
+    "BaseCardBlock",
+    "SpotlightCardBlock",
+    "SpotlightCardBlock",
     "TabbedContentContainerBlock",
     "TabbedContentCardSetBlock",
     "TabbedContentTabBlock",
