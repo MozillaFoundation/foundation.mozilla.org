38a8b9c33269624ccad2f617669ff6824df326bb
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-07-08 12:46:29 -0400

    Portrait card checkin, desktop styles grid layout

--- foundation_cms/blocks/portrait_card_block.py
+++ foundation_cms/blocks/portrait_card_block.py
@@ -1,7 +1,10 @@
 from . import BaseCardBlock
+from .link_block import OptionalLinkWithoutLabelBlock
 
 
 class PortraitCardBlock(BaseCardBlock):
+
+    cta_link = OptionalLinkWithoutLabelBlock(required=False, label="Call to Action Link", help_text="Optional link for the card.")
     class Meta:
         label = "Portrait Card"
         icon = "form"
