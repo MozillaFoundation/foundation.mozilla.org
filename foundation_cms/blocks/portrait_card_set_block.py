7ba12d58e1451b3fa01a4b1d4da07620b165f3a1
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-07-10 23:24:21 -0400

    Remove unused code and squash migration

--- foundation_cms/blocks/portrait_card_set_block.py
+++ foundation_cms/blocks/portrait_card_set_block.py
@@ -14,19 +14,7 @@ class PortraitCardSetBlock(BaseBlock):
         help_text="Minimum of 3 cards required. More than 3 will turn element into a carousel.",
     )
 
-    cta_link = OptionalLinkBlock(
-        required=False, label="Call to Action Link", help_text="Optional link below the cards."
-    )
-
-    def get_context(self, value, parent_context=None):
-        context = super().get_context(value, parent_context)
-        card_count = len(value.get("cards", []))
-        theme = self.get_theme(parent_context or {})
-        layout = "portrait_card_set_block_grid.html"
-        context["card_set_template"] = f"patterns/blocks/themes/{theme}/{layout}"
-        return context
-
     class Meta:
         label = "Portrait Card Set"
         icon = "form"
-        template_name = "portrait_card_set_block.html"
+        template_name = "portrait_card_set_block_grid.html"
