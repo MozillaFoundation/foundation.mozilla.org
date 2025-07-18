b0d47fb92f21d2b1b6de206db85bd2a2495d3e53
danielfmiranda <manieldiranda@gmail.com>
2025-07-18 09:39:11 -0700

    linting

--- foundation_cms/core/management/commands/load_redesign_data.py
+++ foundation_cms/core/management/commands/load_redesign_data.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from django.conf import settings
+# from django.conf import settings
 from django.core.management.base import BaseCommand
 from wagtail.models import Page
 
