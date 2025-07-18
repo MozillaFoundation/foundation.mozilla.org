63d08891ef4c19e28025878e9b382caa14feb9fb
Mavis Ou <mmmavis@users.noreply.github.com>
2025-07-18 16:03:12 -0700

    rename function

--- foundation_cms/static/js/pages/home_page.js
+++ foundation_cms/static/js/pages/home_page.js
@@ -1,3 +1,3 @@
-import { initAllRollingPhrases } from "../components/home_page/kinetic_type_brand_line.js";
+import { initAllKineticTypeBrandLines } from "../components/home_page/kinetic_type_brand_line.js";
 
-initAllRollingPhrases();
+initAllKineticTypeBrandLines();
