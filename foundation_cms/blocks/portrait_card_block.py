06b8715ee9d998bb68b7450f7d7d519cc2ab4fa4
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-07-21 09:21:01 -0400

    format and squash migration

--- foundation_cms/blocks/portrait_card_block.py
+++ foundation_cms/blocks/portrait_card_block.py
@@ -4,9 +4,7 @@ from .link_block import LinkWithoutLabelBlock
 
 class PortraitCardBlock(BaseCardBlock):
 
-    cta_link = LinkWithoutLabelBlock(
-        required=True, label="Call to Action Link", help_text="Link for the card."
-    )
+    cta_link = LinkWithoutLabelBlock(required=True, label="Call to Action Link", help_text="Link for the card.")
 
     class Meta:
         label = "Portrait Card"
