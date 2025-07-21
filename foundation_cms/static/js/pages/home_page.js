a25ff904c4143610303c236327db0d01c0a6e167
Mavis Ou <mmmavis@users.noreply.github.com>
2025-07-21 14:19:05 -0700

    [Recreated] Homepage Hero Section Updates (#14280)
    * remove an extra code & renamed -height to -heights
    
    * introduce a new breakpoint xxxlarge (1920px) to our SCSS setting
    
    * update kinetic wordmark & adjust sizing/spacing for brand line & add 1s animation delay to both
    
    * fix old JS
    
    * rename function
    
    * hero accordion - code refactor & make it taller on 1440px screens
    
    * remove extra code
    
    * remove extra white spaces

--- foundation_cms/static/js/pages/home_page.js
+++ foundation_cms/static/js/pages/home_page.js
@@ -1,5 +1,3 @@
-import { KineticTypeBrandLine } from "../components/home_page/kinetic_type_brand_line.js";
+import { initAllKineticTypeBrandLines } from "../components/home_page/kinetic_type_brand_line.js";
 
-document
-  .querySelectorAll(".kinetic-type-brand-line")
-  .forEach((el) => new KineticTypeBrandLine(el).init());
+initAllKineticTypeBrandLines();
