ed1e02cf2f9d3643b7e3a765373f4226f1df8886
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-07-23 11:20:41 -0400

    rebase off of main, cherry pick files

--- foundation_cms/blocks/portrait_card_block.py
+++ foundation_cms/blocks/portrait_card_block.py
@@ -1,7 +1,11 @@
 from . import BaseCardBlock
+from .link_block import LinkWithoutLabelBlock
 
 
 class PortraitCardBlock(BaseCardBlock):
+
+    cta_link = LinkWithoutLabelBlock(required=True, label="Call to Action Link", help_text="Link for the card.")
+
     class Meta:
         label = "Portrait Card"
         icon = "form"
