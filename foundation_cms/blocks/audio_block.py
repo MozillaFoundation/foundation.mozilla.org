3218b397ec06de1dc76bc48a085fe51edc0cd563
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-07-24 10:49:00 -0400

    Remove extra alt text field since we're using imageblock

--- foundation_cms/blocks/audio_block.py
+++ foundation_cms/blocks/audio_block.py
@@ -6,7 +6,6 @@ from foundation_cms.base.models.base_block import BaseBlock
 
 class AudioBlock(BaseBlock):
     image = ImageBlock(required=False, help_text="Optional image to display at the top of the block.")
-    image_alt_text = blocks.CharBlock(required=False, help_text="Image description (for screen readers).")
     title = blocks.CharBlock()
     description = blocks.TextBlock(required=False, help_text="Short description of the audio")
     simplecast_embed_code = blocks.CharBlock()
