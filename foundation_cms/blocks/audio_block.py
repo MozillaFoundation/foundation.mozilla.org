04774e3e1f96cc401818132222a8309f7dddb494
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-07-24 09:40:01 -0400

    Replace ImageChooserBlock instances with ImageBlock

--- foundation_cms/blocks/audio_block.py
+++ foundation_cms/blocks/audio_block.py
@@ -1,11 +1,11 @@
 from wagtail import blocks
-from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.blocks import ImageBlock
 
 from foundation_cms.base.models.base_block import BaseBlock
 
 
 class AudioBlock(BaseBlock):
-    image = ImageChooserBlock(required=False, help_text="Optional image to display at the top of the block.")
+    image = ImageBlock(required=False, help_text="Optional image to display at the top of the block.")
     image_alt_text = blocks.CharBlock(required=False, help_text="Image description (for screen readers).")
     title = blocks.CharBlock()
     description = blocks.TextBlock(required=False, help_text="Short description of the audio")
