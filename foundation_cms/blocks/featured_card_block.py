b6d8e1315345bb992b5534d58653ff889b5fa41e
Rob DiVincenzo <rob@mozillafoundation.org>
2025-07-25 11:55:05 -0400

    Replace ImageChooserBlock instances with ImageBlock (#14341)
    * Replace ImageChooserBlock instances with ImageBlock
    
    * Remove extra alt text field since we're using imageblock
    
    * regenerate migrations

--- foundation_cms/blocks/featured_card_block.py
+++ foundation_cms/blocks/featured_card_block.py
@@ -1,5 +1,5 @@
 from wagtail.blocks import CharBlock, RichTextBlock
-from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.blocks import ImageBlock
 
 from foundation_cms.base.models.base_block import BaseBlock
 
@@ -9,7 +9,7 @@ from .link_button_block import LinkButtonBlock
 class FeaturedCardBlock(BaseBlock):
     heading = CharBlock(required=True, max_length=50, label="Heading")
     description = RichTextBlock(required=True, max_length=500, label="Description", features=["bold", "italic"])
-    image = ImageChooserBlock(required=True, label="Image", help_text="Image for the card")
+    image = ImageBlock(required=True, label="Image", help_text="Image for the card")
     button = LinkButtonBlock(required=False, label="Button", help_text="Link button for the card.")
 
     class Meta:
