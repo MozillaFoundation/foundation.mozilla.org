73ae22aae531768b14caa7d78439035f7aebdd4a
Ram <ramon@mozillafoundation.org>
2025-07-25 19:56:09 -0600

    Vanilla js tab component

---
+++ foundation_cms/static/js/blocks/tabbed_content.js
@@ -0,0 +1,22 @@
+export function initTabbedContent() {
+  const SELECTORS = {
+    tabbedContent: ".tabbed-content",
+    tabButtons: ".tabbed-content__tab-button",
+    tabPanels: ".tabbed-content__tab-panel",
+  };
+
+  const tabbedContents = document.querySelectorAll(SELECTORS.tabbedContent);
+
+  tabbedContents.forEach((container) => {
+    const buttons = container.querySelectorAll(SELECTORS.tabButtons);
+    const panels = container.querySelectorAll(SELECTORS.tabPanels);
+
+    // Refer content panels by index instead of ID to allow for multiple instances in one page.
+    buttons.forEach((button, tabIndex) => {
+      button.addEventListener("click", (event) => {
+        panels.forEach((panel) => panel.classList.remove("is-active"));
+        panels[tabIndex].classList.add("is-active");
+      });
+    });
+  });
+}
