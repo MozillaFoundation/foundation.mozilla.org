1362b3ef69978d49a91b26ffc78cc38119b30d24
Ram <ramon@mozillafoundation.org>
2025-07-30 12:38:29 -0600

    Added optional `link` field to `TabbedContentCardSetBlock` model. (#14375)
    * Added optional `link` field to `tabbed_content_card_set_block` model.
    
    * Update tabbed content card set block with cta_link block
    
    * Typography update for tabbed content card set titles
    
    * Update tab button styles for improved typography and spacing

--- foundation_cms/blocks/tabbed_content_card_set_block.py
+++ foundation_cms/blocks/tabbed_content_card_set_block.py
@@ -3,12 +3,15 @@ from wagtail.images.blocks import ImageBlock
 
 from foundation_cms.base.models.base_block import BaseBlock
 
+from .link_block import OptionalLinkBlock
+
 
 class TabbedContentCardSetBlock(BaseBlock):
 
     title = blocks.CharBlock()
     image = ImageBlock()
     subtitle = blocks.CharBlock()
+    link = OptionalLinkBlock(required=False)
 
     class Meta:
         icon = "form"
