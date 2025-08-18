424699a87e8865e050a1cdb9783aba9b1d7b1124
Adalberto Cima <adalbertoc@mozillafoundation.org>
2025-08-18 14:17:04 -0600

    Add Quote component styling option for bolder article use

--- foundation_cms/blocks/quote_block.py
+++ foundation_cms/blocks/quote_block.py
@@ -7,6 +7,15 @@ class QuoteBlock(BaseBlock):
 
     quote = blocks.CharBlock(required=True)
     attribution = blocks.CharBlock(required=False)
+    quote_style = blocks.ChoiceBlock(
+        choices=[
+            ("regular", "Regular"),
+            ("large", "Large"),
+            ("emphasis", "Emphasis"),
+        ],
+        default="regular",
+        help_text="Choose the visual style for this quote.",
+    )
 
     class Meta:
         icon = "doc-full"
