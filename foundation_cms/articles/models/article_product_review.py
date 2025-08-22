3f7aee4e0c46f0a65b187e2fc81ceb7564027a31
Mavis Ou <mmmavis@users.noreply.github.com>
2025-08-22 11:01:08 -0700

    Shuffle files and code around to align with the upcoming model name changes in #14497

--- foundation_cms/articles/models/article_product_review.py
+++ foundation_cms/articles/models/article_product_review.py
@@ -13,7 +13,7 @@ class ArticleProductReview(AbstractArticlePage):
     class Meta:
         verbose_name = "Article Product Review"
 
-    template = "patterns/pages/articles/article_product_review.html"
+    template = "patterns/pages/nothing_personal/article_page.html"
 
     def get_context(self, request):
         context = super().get_context(request)
