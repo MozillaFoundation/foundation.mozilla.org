d9c38a5205c4a1e4c4bb9cfd313a4791f973ce37
Ram <ramon@mozillafoundation.org>
2025-09-10 10:16:05 -0600

    Initial `NothingPersonalPodcastPage` model structure

--- foundation_cms/nothing_personal/models/__init__.py
+++ foundation_cms/nothing_personal/models/__init__.py
@@ -1,6 +1,7 @@
 from .article_collection_page import NothingPersonalArticleCollectionPage
 from .article_page import NothingPersonalArticlePage
 from .home_page import NothingPersonalHomePage
+from .podcast_page import NothingPersonalPodcastPage
 from .product_review_page import NothingPersonalProductReviewPage
 
 __all__ = [
