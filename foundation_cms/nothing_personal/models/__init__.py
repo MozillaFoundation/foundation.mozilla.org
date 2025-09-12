90daf658306d1f9ec4ee9785d3dfd5d2da4ac5da
Ricardo Ancona <ricardoa@mozillafoundation.com>
2025-09-12 16:03:35 -0600

    fix python lint

--- foundation_cms/nothing_personal/models/__init__.py
+++ foundation_cms/nothing_personal/models/__init__.py
@@ -9,5 +9,5 @@ __all__ = [
     "NothingPersonalArticlePage",
     "NothingPersonalProductReviewPage",
     "NothingPersonalHomePage",
-    "NothingPersonalPodcastPage"
+    "NothingPersonalPodcastPage",
 ]
