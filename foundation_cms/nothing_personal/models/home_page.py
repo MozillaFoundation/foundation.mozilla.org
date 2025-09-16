8cc9fe1339cb189f7569cb1d66355c353d904f38
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-09-16 12:57:31 -0400

    remove unused include

--- foundation_cms/nothing_personal/models/home_page.py
+++ foundation_cms/nothing_personal/models/home_page.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.shortcuts import get_object_or_404
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
