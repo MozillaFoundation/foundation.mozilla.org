dc0c9055fdc717209db02ce7370692223c2877a7
Ram <ramon@mozillafoundation.org>
2025-09-22 12:27:45 -0600

    Initial maintenance template update

--- frontend/redesign/build-css.js
+++ frontend/redesign/build-css.js
@@ -11,6 +11,7 @@ const entries = [
   "redesign_main",
   "redesign_migrated_content",
   "pages/home_page",
+  "pages/maintenance",
   "pages/topic_listing_page",
   "pages/nothing_personal/article_page",
   "pages/nothing_personal/podcast_page",
