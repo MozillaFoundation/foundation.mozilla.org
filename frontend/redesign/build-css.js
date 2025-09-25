e9e2d339dbc93f29a61ca90d7a20ce37af5bfab7
Mavis Ou <mmmavis@users.noreply.github.com>
2025-09-24 20:39:05 -0700

    turn redesign_main.scss into a base SCSS that each page_name.scss imports

--- frontend/redesign/build-css.js
+++ frontend/redesign/build-css.js
@@ -8,9 +8,9 @@ const __dirname = path.dirname(__filename);
 
 const entries = [
   // List of base .scss file names located in `inDir` (omit the .scss extension)
-  "redesign_main",
   "redesign_migrated_content",
   "pages/home_page",
+  "pages/general_page",
   "pages/topic_listing_page",
   "pages/nothing_personal/article_page",
   "pages/nothing_personal/podcast_page",
