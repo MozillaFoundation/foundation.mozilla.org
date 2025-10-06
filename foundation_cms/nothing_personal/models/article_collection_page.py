4a52be1d73f806ef761c92385d85b775abd94725
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-10-06 09:25:20 -0400

    Add product_reviews context variable

--- foundation_cms/nothing_personal/models/article_collection_page.py
+++ foundation_cms/nothing_personal/models/article_collection_page.py
@@ -1,5 +1,10 @@
-from foundation_cms.base.models.abstract_article_page import AbstractArticlePage
 from wagtail.admin.panels import FieldPanel
+
+from foundation_cms.base.models.abstract_article_page import AbstractArticlePage
+from foundation_cms.nothing_personal.models.product_review_page import (
+    NothingPersonalProductReviewPage,
+)
+from foundation_cms.utils import get_default_locale, localize_queryset
 
 
 class NothingPersonalArticleCollectionPage(AbstractArticlePage):
@@ -8,10 +13,8 @@ class NothingPersonalArticleCollectionPage(AbstractArticlePage):
         FieldPanel("lede_text"),
     ]
 
-
     parent_page_types = ["nothing_personal.NothingPersonalHomePage"]
     subpage_types: list[str] = []
-    
 
     class Meta:
         verbose_name = "Nothing Personal Article Collection Page"
@@ -20,4 +23,13 @@ class NothingPersonalArticleCollectionPage(AbstractArticlePage):
 
     def get_context(self, request):
         context = super().get_context(request)
+        (DEFAULT_LOCALE, _) = get_default_locale()
+        product_reviews = (
+            NothingPersonalProductReviewPage.objects.live()
+            .public()
+            .filter(locale=DEFAULT_LOCALE)
+            .order_by("-first_published_at")
+        )
+        localized_reviews = localize_queryset(product_reviews, preserve_order=True)
+        context["product_reviews"] = localized_reviews.specific()
         return context
