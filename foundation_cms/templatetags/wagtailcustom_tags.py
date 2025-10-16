d0d29933733d5a2865e0cdd4d44bacedd955fb4b
danielfmiranda <manieldiranda@gmail.com>
2025-10-15 19:31:10 -0700

    moved some files around from legacy site

---
+++ foundation_cms/templatetags/wagtailcustom_tags.py
@@ -0,0 +1,15 @@
+from wagtail_ab_testing.models import AbTest
+from wagtail_ab_testing.utils import request_is_trackable
+
+@register.inclusion_tag("wagtail_ab_testing/script.html", takes_context=True)
+def wagtail_ab_testing_script(context):
+    request = context["request"]
+    serving_variant = getattr(request, "wagtail_ab_testing_serving_variant", False)
+
+    return {
+        "request": request,
+        "track": request_is_trackable(request),
+        "page": context.get("page", None),
+        "test": getattr(request, "wagtail_ab_testing_test", None),
+        "version": AbTest.VERSION_VARIANT if serving_variant else AbTest.VERSION_CONTROL,
+    }
