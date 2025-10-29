1f246f3a5b3e78ee77ebcda870c9121e7032a80d
Rob DiVincenzo <rob.divincenzo@gmail.com>
2025-10-28 21:35:10 -0400

    Update test name

--- foundation_cms/base/tests/test_pipeline_assign_default_role.py
+++ foundation_cms/base/tests/test_pipeline_assign_default_role.py
@@ -17,7 +17,7 @@ class AssignDefaultRoleTests(TestCase):
         # Confirm user added to the group
         self.assertTrue(self.user.groups.filter(id=self.group.id).exists())
 
-    def test_bails_when_group_missing(self):
+    def test_fails_when_group_missing(self):
         unknown_group = "A GROUP NAME THAT DOES NOT EXIST"
         self.assertFalse(Group.objects.filter(name=unknown_group).exists())
         assign_default_role(backend=None, user=self.user, is_new=True)
