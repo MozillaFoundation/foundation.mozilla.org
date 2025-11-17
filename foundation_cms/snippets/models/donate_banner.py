c1231626e865e106b566aa31fcfcf1d85edda2b7
Mavis Ou <mmmavis@users.noreply.github.com>
2025-11-16 20:15:07 -0800

    remove more code

--- foundation_cms/snippets/models/donate_banner.py
+++ foundation_cms/snippets/models/donate_banner.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.db import models
 from wagtail.admin.panels import FieldPanel, HelpPanel, MultiFieldPanel
