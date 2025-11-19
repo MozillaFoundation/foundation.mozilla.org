9731eeb47601b218bb1e29cea782bc062d72e99d
Ram <ramon@mozillafoundation.org>
2025-11-19 09:35:24 -0600

    Initial block definition

---
+++ foundation_cms/blocks/featured_container_block.py
@@ -0,0 +1,29 @@
+from wagtail.blocks import ChoiceBlock, RichTextBlock, StreamBlock
+
+from .media_block import CustomMediaBlock
+from .two_column_container_block import TwoColumnContainerBlock
+
+
+class ColumnStreamBlock(StreamBlock):
+    rich_text = RichTextBlock()
+    media = CustomMediaBlock()
+
+
+class FeaturedContainerBlock(TwoColumnContainerBlock):
+    sticky = ChoiceBlock(
+        choices=[
+            ("none", "None"),
+            ("left", "Left Column"),
+            ("right", "Right Column"),
+        ],
+        default="none",
+        help_text="Make one column sticky on scroll",
+    )
+    
+    left_column = ColumnStreamBlock(label="Left Column", required=False)
+    right_column = ColumnStreamBlock(label="Right Column", required=False)
+
+    class Meta:
+        template_name = "featured_container_block.html"
+        icon = "placeholder"
+        label = "Featured Container"
