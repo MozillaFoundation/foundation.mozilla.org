b7efeeb0b1412b7ddcf8fad26639731370ee362f
Mavis Ou <mmmavis@users.noreply.github.com>
2025-12-08 12:58:11 -0800

    Improve front-end test suite by removing outdated tests (#15058)
    * Removed several regression test page urls that are no longer useful
    
    * remove legacy site nav related tests
    
    * fix Percy false positives in cookie banner screenshots
    
    * Revert "fix Percy false positives in cookie banner screenshots"
    
    This reverts commit bcc4c745d0aab9f7c8364ef8f87e8e6d89d80c58.

--- frontend/legacy/tests/visual.spec.js
+++ frontend/legacy/tests/visual.spec.js
@@ -122,39 +122,9 @@ function testMozfestURL(path, locale = `en`) {
   return testURL(mozfestBaseUrl(locale), path, false);
 }
 
-async function expandDropdown(page, dropdownSelector) {
-  await page.hover(dropdownSelector);
-  await page.waitForSelector(dropdownSelector, { state: "visible" });
-}
-
 test.describe.parallel(`Foundation page tests`, () => {
   Object.entries(FoundationURLs).forEach(async ([testName, path]) => {
     test(`Foundation ${testName}`, testFoundationURL(path));
-  });
-
-  test(`Foundation main navigation with expanded dropdown`, async ({
-    page,
-  }) => {
-    await page.goto(foundationBaseUrl("en"));
-    await waitForReactAndImagesToLoad(page);
-
-    const dropdowns = await page.$$(".tw-nav-desktop-dropdown");
-
-    for (let i = 0; i < dropdowns.length; i++) {
-      await expandDropdown(
-        page,
-        `.tw-nav-desktop-dropdown:nth-of-type(${i + 1})`
-      );
-      await percySnapshot(
-        page,
-        `Main navigation with expanded dropdown ${i + 1}`
-      );
-      // Reset the page state for the next dropdown
-      if (i < dropdowns.length - 1) {
-        await page.goto(foundationBaseUrl("en"));
-        await waitForReactAndImagesToLoad(page);
-      }
-    }
   });
 });
 
