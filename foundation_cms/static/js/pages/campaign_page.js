f33aa74d46fdd427b103c63198dc1671f450b91e
Mavis Ou <mmmavis@users.noreply.github.com>
2026-01-13 17:02:08 -0800

    TP1-3531 - show email confirmation banner only if user opted in

--- foundation_cms/static/js/pages/campaign_page.js
+++ foundation_cms/static/js/pages/campaign_page.js
@@ -4,6 +4,8 @@ const SELECTORS = {
     ".confirmation-notice-banner [data-banner-close-button]",
   shareButtons:
     ".petition__share-button-wrapper button, .petition__share-button-wrapper a",
+  formThankYouUrlField: ".petition__form-wrapper #tfa_500",
+  formNewsletterCheckbox: ".petition__form-wrapper #tfa_495",
 };
 
 const banner = document.querySelector(SELECTORS.comfirmationBanner);
@@ -43,3 +45,24 @@ shareButtons.forEach((shareButton) => {
     shareButtonClicked(e);
   });
 });
+
+const newsletterCheckbox = document.querySelector(
+  SELECTORS.formNewsletterCheckbox,
+);
+const formThankYouUrlField = document.querySelector(
+  SELECTORS.formThankYouUrlField,
+);
+
+if (newsletterCheckbox && formThankYouUrlField) {
+  newsletterCheckbox.addEventListener("change", (e) => {
+    const baseUrl = new URL(formThankYouUrlField.value);
+
+    if (e.target.checked) {
+      baseUrl.searchParams.set("newsletter_optin", "true");
+    } else {
+      baseUrl.searchParams.delete("newsletter_optin");
+    }
+
+    formThankYouUrlField.value = baseUrl.toString();
+  });
+}
