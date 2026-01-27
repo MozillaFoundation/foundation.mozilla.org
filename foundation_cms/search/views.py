57e78a0981476c455152da002a06d4f5b12d065a
adalcima <cima.adalberto@gmail.com>
2026-01-27 15:25:48 -0600

    Implement multilingual search (#15291)
    * Implement multilingual search
    
    * Update utils
    
    * Update view

--- foundation_cms/search/views.py
+++ foundation_cms/search/views.py
@@ -5,6 +5,7 @@ from wagtail.models import Locale, Page
 from wagtail.search.query import PlainText
 
 from foundation_cms.campaigns.models.campaign_page import CampaignPage
+from foundation_cms.search.utils import get_search_backend_for_locale
 from foundation_cms.utils import get_default_locale, localize_queryset
 
 # To enable logging of search queries for use with the "Promoted search results" module
@@ -19,10 +20,17 @@ def search(request):
     search_query = request.GET.get("query", None)
     page = request.GET.get("page", 1)
     total_search_results = 0
+    current_locale = Locale.get_active()
 
     # Search
     if search_query:
-        search_results = Page.objects.live().filter(locale=Locale.get_active()).search(search_query)
+        locale_code = current_locale.language_code
+        base_queryset = Page.objects.live().filter(locale=current_locale)
+
+        # Get appropriate search backend
+        search_backend, backend_type = get_search_backend_for_locale(locale_code)
+        search_results = search_backend.search(search_query, base_queryset)
+
         total_search_results = search_results.count()
 
         # To log this query for use with the "Promoted search results" module:
@@ -55,6 +63,7 @@ def search(request):
             "search_results": search_results,
             "total_search_results": total_search_results,
             "keep_contributing_pages": keep_contributing_pages,
+            "current_locale": current_locale.language_code,
         },
     )
 
