5619a1d68bea8ae716c5ad726cd4c5972c6dea2f
Rob DiVincenzo <rob.divincenzo@gmail.com>
2026-02-05 23:10:11 -0500

    Two new squashed chunks plus --skip-checks for more memory overhead

--- release-steps.sh
+++ release-steps.sh
@@ -2,7 +2,15 @@
 set -eo pipefail
 
 # Django Migrations
-python ./manage.py migrate --no-input
+MIGRATE_FLAGS="--no-input"
 
+# For review apps. See if SKIP_MIGRATION_CHECKS env variable is set.
+# If so then add --skip-checks for more overhead.
+
+if [[ "${SKIP_MIGRATION_CHECKS:-0}" == "1" ]]; then
+  MIGRATE_FLAGS="$MIGRATE_FLAGS --skip-checks"
+fi
+
+python ./manage.py migrate $MIGRATE_FLAGS
 # Clear cache for BuyersGuide
 python ./manage.py clear_cache
