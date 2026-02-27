4aa09c2f7af4e06ff278943a821d83f1e1934fb4
danielfmiranda <manieldiranda@gmail.com>
2026-02-26 16:52:14 -0800

    updated migrations and added a small check

--- foundation_cms/core/migrations/0067_sitewidefooternewslettersignuppage.py
+++ foundation_cms/core/migrations/0067_sitewidefooternewslettersignuppage.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0065_generalpage_hero_image_rounded_corners"),
+        ("core", "0066_alter_generalpage_body_alter_homepage_body"),
     ]
 
     operations = [
