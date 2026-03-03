3443d18c2d1f3fc414d829ab3591ee1df0674753
Rob DiVincenzo <rob.divincenzo@gmail.com>
2026-03-03 16:11:45 -0500

    more logging

--- foundation_cms/legacy_apps/utility/middleware.py
+++ foundation_cms/legacy_apps/utility/middleware.py
@@ -35,8 +35,10 @@ class TargetDomainRedirectMiddleware:
         if settings.DOMAIN_REDIRECT_MIDDLEWARE_ENABLED:
             # Never redirect Wagtail admin, including preview panel iframes
             if request.path.startswith("/cms/"):
+                logger.warning("RID=%s skipping domain redirect for cms path", rid)
                 return self.get_response(request)
             else:
+                logger.warning("RID=%s non cms path detected", rid)
                 print(request.get_full_path())
                 
             request_host = request.get_host()
@@ -60,6 +62,18 @@ class TargetDomainRedirectMiddleware:
                     request.get_full_path(),
                     hostnames,
                 )
+
+                rid = request.META.get("HTTP_X_REQUEST_ID")
+                logger.warning(
+                    "RID=%s path=%s full_path=%s host_hdr=%s get_host=%s xfh=%s",
+                    rid,
+                    request.path,
+                    request.get_full_path(),
+                    request.META.get("HTTP_HOST"),
+                    request.get_host(),
+                    request.META.get("HTTP_X_FORWARDED_HOST"),
+)
+
                 redirect_url = "{protocol}://{hostname}{path}".format(
                     protocol=protocol,
                     hostname=hostnames[0],
