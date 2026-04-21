761b32aeab58cae4d430ef1c8299f38e91a0b345
Ram <ramon@mozillafoundation.org>
2026-04-21 09:23:48 -0600

    Remove text-social from three-column container and adjust newsletter signup padding

--- foundation_cms/blocks/three_column_container_block.py
+++ foundation_cms/blocks/three_column_container_block.py
@@ -1,9 +1,33 @@
-from wagtail.blocks import ChoiceBlock
+from wagtail.blocks import ChoiceBlock, StreamBlock
 
 from foundation_cms.base.models.base_block import BaseBlock
 
 from .common.background_color_block import BackgroundColorChoiceBlock
-from .two_column_container_block import ColumnStreamBlock
+from .custom_rich_text_block import CustomRichTextBlock
+from .fru_element_block import FruElementBlock
+from .gallery_card_block import GalleryCardBlock
+from .image_block import CustomImageBlock
+from .link_button_block import LinkButtonBlock
+from .list_block import ListBlock
+from .newsletter_signup_block import NewsletterSignupBlock
+from .podcast_block import PodcastBlock
+from .quote_block import QuoteBlock
+from .spacer_block import SpacerBlock
+from .video_block import VideoBlock
+
+
+class ThreeColumnStreamBlock(StreamBlock):
+    rich_text = CustomRichTextBlock()
+    podcast = PodcastBlock()
+    image = CustomImageBlock()
+    newsletter_signup = NewsletterSignupBlock()
+    video = VideoBlock()
+    list = ListBlock()
+    spacer = SpacerBlock()
+    quote = QuoteBlock()
+    link_button = LinkButtonBlock()
+    fru_element = FruElementBlock()
+    gallery_card = GalleryCardBlock()
 
 
 class ThreeColumnContainerBlock(BaseBlock):
@@ -17,9 +41,9 @@ class ThreeColumnContainerBlock(BaseBlock):
         default="middle",
         help_text="Vertical alignment of the columns content",
     )
-    left_column = ColumnStreamBlock(label="Left Column", required=False)
-    center_column = ColumnStreamBlock(label="Center Column", required=False)
-    right_column = ColumnStreamBlock(label="Right Column", required=False)
+    left_column = ThreeColumnStreamBlock(label="Left Column", required=False)
+    center_column = ThreeColumnStreamBlock(label="Center Column", required=False)
+    right_column = ThreeColumnStreamBlock(label="Right Column", required=False)
 
     class Meta:
         template_name = "three_column_container_block.html"
