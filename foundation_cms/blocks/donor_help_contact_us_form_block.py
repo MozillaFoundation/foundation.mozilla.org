d4f62833be8111b862ae517c5c90faa88941df7b
danielfmiranda <manieldiranda@gmail.com>
2026-04-27 20:50:56 -0700

    added ask another question button, also thank you URL functionality

--- foundation_cms/blocks/donor_help_contact_us_form_block.py
+++ foundation_cms/blocks/donor_help_contact_us_form_block.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from wagtail import blocks
 
 from foundation_cms.base.models.base_block import BaseBlock
@@ -9,6 +11,17 @@ class DonorHelpContactUsFormBlock(BaseBlock):
         required=False,
         default="Questions about your donation? Get in touch with our team by using the form below.",
     )
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        request = context.get("request")
+        page = context.get("page")
+        if request and page and hasattr(page, "get_full_url"):
+            params = request.GET.dict()
+            params["thank_you"] = "true"
+            context["thank_you_url"] = page.get_full_url(request) + "?" + urlencode(params) + "#contact-form"
+            context["show_formassembly_thank_you"] = request.GET.get("thank_you") == "true"
+        return context
 
     class Meta:
         template_name = "donor_help_contact_us_form_block.html"
