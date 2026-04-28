53ad15a8eaf6221b73e8a6658905e65ce13b304a
danielfmiranda <manieldiranda@gmail.com>
2026-04-27 21:00:41 -0700

    require headline, limit donor help form to 1 per general page, move formassembly head to page template

--- foundation_cms/blocks/donor_help_contact_us_form_block.py
+++ foundation_cms/blocks/donor_help_contact_us_form_block.py
@@ -6,7 +6,7 @@ from foundation_cms.base.models.base_block import BaseBlock
 
 
 class DonorHelpContactUsFormBlock(BaseBlock):
-    heading = blocks.CharBlock(required=False, default="Contact Us")
+    heading = blocks.CharBlock(required=True, default="Contact Us")
     subheading = blocks.CharBlock(
         required=False,
         default="Questions about your donation? Get in touch with our team by using the form below.",
