2e1a01f43c27d437c984a0aaf673444935d42d2e
Mavis Ou <mmmavis@users.noreply.github.com>
2026-06-05 15:56:21 -0700

    more custom config

--- foundation_cms/static/js/cookie_control_init.js
+++ foundation_cms/static/js/cookie_control_init.js
@@ -2,7 +2,6 @@
 // GDPR/CCPA mode should be based on geo location
 // Locale should be based on user’s browser language setting
 
-
 const API_KEY = "76fd590604f568812d13d4780b846805941fe9f0";
 const PRODUCT_TYPE = "CUSTOM";
 
@@ -10,14 +9,42 @@ CookieControl.geoTest(PRODUCT_TYPE, API_KEY, function (response) {
   console.log("geoTest response:", response);
 
   var config = {
+    initialState: "notify",
     product: PRODUCT_TYPE,
     mode: response.withinEU ? "GDPR" : "CCPA",
+    theme: "light",
+    closeStyle: "labelled",
     text: {
-      title: "🍪 Cookie Test (EN)",
-      intro: "English default text.",
-      accept: "✅✅ Accept",
-      reject: "❌❌ Reject",
+      title: "Help Mozilla with Analytics (EN)",
+      intro:
+        "We use some essential cookies to make this website. We’d like to set additional cookies to understand how you use mozillafoundation.org. Your settings improve our services.",
+      acceptSettings: "Accept Analytics Cookies",
+      rejectSettings: "Reject Analytics Cookies",
+      closeLabel: "CLOSE",
+      notifyTitle: "[Notify baner title] Help Mozilla with Analytics (EN)",
+      notifyDescription:
+        "[Notify baner description] We use some essential cookies to make this website. We’d like to set additional cookies to understand how you use mozillafoundation.org. Your settings improve our services.",
+      accept: "Accept Analytics Cookies",
+      reject: "Reject Analytics Cookies",
     },
+    optionalCookies: [
+      {
+        name: "analytics",
+        label: "[Test] Analytical Cookies",
+        description: "Description description description.",
+        cookies: [],
+        onAccept: function () {},
+        onRevoke: function () {},
+      },
+      {
+        name: "marketing",
+        label: "[Test] Marketing Cookies",
+        description: "Description description description.",
+        cookies: [],
+        onAccept: function () {},
+        onRevoke: function () {},
+      },
+    ],
     locales: [
       {
         locale: "fr",
@@ -27,6 +54,24 @@ CookieControl.geoTest(PRODUCT_TYPE, API_KEY, function (response) {
           acceptSettings: "✅✅ Accepter",
           rejectSettings: "❌❌ Refuser",
         },
+        optionalCookies: [
+          {
+            name: "analytics",
+            label: "[Test FR] Analytical Cookies",
+            description: "Description description description.",
+            cookies: [],
+            onAccept: function () {},
+            onRevoke: function () {},
+          },
+          {
+            name: "marketing",
+            label: "[Test FR] Marketing Cookies",
+            description: "Description description description.",
+            cookies: [],
+            onAccept: function () {},
+            onRevoke: function () {},
+          },
+        ],
       },
       {
         locale: "de",
