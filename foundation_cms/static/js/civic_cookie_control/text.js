09650b520141253e64fb80baa1129518ffff8d51
Mavis Ou <mmmavis@users.noreply.github.com>
2026-06-24 16:18:27 -0700

    refactor: reorder text keys to match English structure and document closeLabel usage

--- foundation_cms/static/js/civic_cookie_control/text.js
+++ foundation_cms/static/js/civic_cookie_control/text.js
@@ -5,21 +5,24 @@
 const LOCALE_TEXT = {
   en: {
     text: {
+      // notify banner
+      notifyTitle: "Help Mozilla with Analytics",
+      notifyDescription:
+        "We use some essential cookies to make this website. We'd like to set additional cookies to understand how you use mozillafoundation.org. Your settings improve our services.",
+      accept: "Accept Analytics Cookies",
+      reject: "Reject Analytics Cookies",
+      settings: "Consent Settings",
+      // setting panel
       title: "Privacy Preference Center",
       intro:
         "When you visit any website, it may store or retrieve information on your browser, mostly in the form of cookies. This information might be about you, your preferences or your device and is mostly used to make the site work as you expect it to. The information does not usually directly identify you, but it can give you a more personalized web experience. Because we respect your right to privacy, you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. However, blocking some types of cookies may impact your experience of the site and the services we are able to offer.",
-      notifyTitle: "Help Mozilla with Analytics",
-      notifyDescription:
-        "We use some essential cookies to make this website. We’d like to set additional cookies to understand how you use mozillafoundation.org. Your settings improve our services.",
-      accept: "Accept Analytics Cookies",
-      reject: "Reject Analytics Cookies",
       acceptSettings: "Allow All",
       rejectSettings: "Reject All",
       necessaryTitle: "Necessary Cookies",
       necessaryDescription:
         "These cookies are necessary for the website to function and cannot be switched off in our systems. They are usually only set in response to actions made by you which amount to a request for services, such as setting your privacy preferences, logging in or filling in forms. You can set your browser to block or alert you about these cookies, but some parts of the site will not then work. These cookies do not store any personally identifiable information.",
-      settings: "Consent Settings",
-      closeLabel: "Close",
+      // visible label / aria-label for the close button
+      closeLabel: "Closeee",
     },
     optionalCookies: [
       {
@@ -46,17 +49,20 @@ const LOCALE_TEXT = {
   // Catalan
   ca: {
     text: {
-      title: "[WIP] Títol (CA)",
-      intro: "[WIP]",
+      // notify banner
       notifyTitle: "[WIP]",
       notifyDescription: "[WIP]",
       accept: "[WIP]",
       reject: "[WIP]",
+      settings: "[WIP]",
+      // setting panel
+      title: "[WIP] Títol (CA)",
+      intro: "[WIP]",
       acceptSettings: "[WIP]",
       rejectSettings: "[WIP]",
       necessaryTitle: "[WIP]",
       necessaryDescription: "[WIP]",
-      settings: "[WIP]",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP]",
     },
     optionalCookies: [
@@ -80,20 +86,23 @@ const LOCALE_TEXT = {
   // German
   de: {
     text: {
-      title: "Datenschutz-Präferenz-Center",
-      intro:
-        "Wenn Sie eine Website besuchen, kann diese Informationen über Ihren Browser abrufen oder speichern. Dies geschieht meist in Form von Cookies. Hierbei kann es sich um Informationen über Sie, Ihre Einstellungen oder Ihr Gerät handeln. Meist werden die Informationen verwendet, um die erwartungsgemäße Funktion der Website zu gewährleisten. Durch diese Informationen werden Sie normalerweise nicht direkt identifiziert. Dadurch kann Ihnen aber ein personalisierteres Web-Erlebnis geboten werden. Da wir Ihr Recht auf Datenschutz respektieren, können Sie sich entscheiden, bestimmte Arten von Cookies nicht zulassen. Klicken Sie auf die verschiedenen Kategorieüberschriften, um mehr zu erfahren und unsere Standardeinstellungen zu ändern. Die Blockierung bestimmter Arten von Cookies kann jedoch zu einer beeinträchtigten Erfahrung mit der von uns zur Verfügung gestellten Website und Dienste führen.",
+      // notify banner
       notifyTitle: "Helfen Sie Mozilla mit Analytics",
       notifyDescription:
         "Wir verwenden notwendige Cookies, um diese Website funktionsfähig zu gestalten. Wir möchten zusätzlich Cookies setzen, um zu verstehen, wie Sie mozillafoundation.org nutzen. Ihre Einstellungen verbessern unsere Dienste.",
       accept: "Alle Cookies akzeptieren",
       reject: "Alle ablehnen",
+      settings: "Cookie-Einstellungen",
+      // setting panel
+      title: "Datenschutz-Präferenz-Center",
+      intro:
+        "Wenn Sie eine Website besuchen, kann diese Informationen über Ihren Browser abrufen oder speichern. Dies geschieht meist in Form von Cookies. Hierbei kann es sich um Informationen über Sie, Ihre Einstellungen oder Ihr Gerät handeln. Meist werden die Informationen verwendet, um die erwartungsgemäße Funktion der Website zu gewährleisten. Durch diese Informationen werden Sie normalerweise nicht direkt identifiziert. Dadurch kann Ihnen aber ein personalisierteres Web-Erlebnis geboten werden. Da wir Ihr Recht auf Datenschutz respektieren, können Sie sich entscheiden, bestimmte Arten von Cookies nicht zulassen. Klicken Sie auf die verschiedenen Kategorieüberschriften, um mehr zu erfahren und unsere Standardeinstellungen zu ändern. Die Blockierung bestimmter Arten von Cookies kann jedoch zu einer beeinträchtigten Erfahrung mit der von uns zur Verfügung gestellten Website und Dienste führen.",
       acceptSettings: "Alle zulassen",
       rejectSettings: "Alle ablehnen",
       necessaryTitle: "Unbedingt erforderliche Cookies",
       necessaryDescription:
         "Diese Cookies sind zur Funktion der Website erforderlich und können in Ihren Systemen nicht deaktiviert werden. In der Regel werden diese Cookies nur als Reaktion auf von Ihnen getätigte Aktionen gesetzt, die einer Dienstanforderung entsprechen, wie etwa dem Festlegen Ihrer Datenschutzeinstellungen, dem Anmelden oder dem Ausfüllen von Formularen. Sie können Ihren Browser so einstellen, dass diese Cookies blockiert oder Sie über diese Cookies benachrichtigt werden. Einige Bereiche der Website funktionieren dann aber nicht. Diese Cookies speichern keine personenbezogenen Daten.",
-      settings: "Cookie-Einstellungen",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP] Close",
     },
     optionalCookies: [
@@ -117,17 +126,23 @@ const LOCALE_TEXT = {
   // Spanish
   es: {
     text: {
-      title: "Centro de preferencia de la privacidad",
-      intro: "Cuando visita cualquier sitio web, el mismo podría obtener o guardar información en su navegador, generalmente mediante el uso de cookies. Esta información puede ser acerca de usted, sus preferencias o su dispositivo, y se usa principalmente para que el sitio funcione según lo esperado. Por lo general, la información no lo identifica directamente, pero puede proporcionarle una experiencia web más personalizada. Ya que respetamos su derecho a la privacidad, usted puede escoger no permitirnos usar ciertas cookies. Haga clic en los encabezados de cada categoría para saber más y cambiar nuestras configuraciones predeterminadas. Sin embargo, el bloqueo de algunos tipos de cookies puede afectar su experiencia en el sitio y los servicios que podemos ofrecer.",
+      // notify banner
       notifyTitle: "Ayuda a Mozilla con Analytics",
-      notifyDescription: "Usamos algunas cookies esenciales para hacer este sitio web. Nos gustaría establecer cookies adicionales para entender cómo usas la página mozillafoundation.org. Tus ajustes mejoran nuestros servicios.",
+      notifyDescription:
+        "Usamos algunas cookies esenciales para hacer este sitio web. Nos gustaría establecer cookies adicionales para entender cómo usas la página mozillafoundation.org. Tus ajustes mejoran nuestros servicios.",
       accept: "Aceptar todas las cookies",
       reject: "Rechazarlas todas",
+      settings: "Configuración de cookies",
+      // setting panel
+      title: "Centro de preferencia de la privacidad",
+      intro:
+        "Cuando visita cualquier sitio web, el mismo podría obtener o guardar información en su navegador, generalmente mediante el uso de cookies. Esta información puede ser acerca de usted, sus preferencias o su dispositivo, y se usa principalmente para que el sitio funcione según lo esperado. Por lo general, la información no lo identifica directamente, pero puede proporcionarle una experiencia web más personalizada. Ya que respetamos su derecho a la privacidad, usted puede escoger no permitirnos usar ciertas cookies. Haga clic en los encabezados de cada categoría para saber más y cambiar nuestras configuraciones predeterminadas. Sin embargo, el bloqueo de algunos tipos de cookies puede afectar su experiencia en el sitio y los servicios que podemos ofrecer.",
       acceptSettings: "Permitirlas todas",
       rejectSettings: "Rechazarlas todas",
       necessaryTitle: "Cookies estrictamente necesarias",
-      necessaryDescription: "Estas cookies son necesarias para que el sitio web funcione y no se pueden desactivar en nuestros sistemas. Usualmente están configuradas para responder a acciones hechas por usted para recibir servicios, tales como ajustar sus preferencias de privacidad, iniciar sesión en el sitio, o llenar formularios. Usted puede configurar su navegador para bloquear o alertar la presencia de estas cookies, pero algunas partes del sitio web no funcionarán. Estas cookies no guardan ninguna información personal identificable.",
-      settings: "Configuración de cookies",
+      necessaryDescription:
+        "Estas cookies son necesarias para que el sitio web funcione y no se pueden desactivar en nuestros sistemas. Usualmente están configuradas para responder a acciones hechas por usted para recibir servicios, tales como ajustar sus preferencias de privacidad, iniciar sesión en el sitio, o llenar formularios. Usted puede configurar su navegador para bloquear o alertar la presencia de estas cookies, pero algunas partes del sitio web no funcionarán. Estas cookies no guardan ninguna información personal identificable.",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP] Close",
     },
     optionalCookies: [
@@ -151,17 +166,23 @@ const LOCALE_TEXT = {
   // French
   fr: {
     text: {
-      title: "Centre de préférences de la confidentialité",
-      intro: "Lorsque vous consultez un site Web, des données peuvent être stockées dans votre navigateur ou récupérées à partir de celui-ci, généralement sous la forme de cookies. Ces informations peuvent porter sur vous, sur vos préférences ou sur votre appareil et sont principalement utilisées pour s'assurer que le site Web fonctionne correctement. Les informations ne permettent généralement pas de vous identifier directement, mais peuvent vous permettre de bénéficier d'une expérience Web personnalisée. Parce que nous respectons votre droit à la vie privée, nous vous donnons la possibilité de ne pas autoriser certains types de cookies. Cliquez sur les différentes catégories pour obtenir plus de détails sur chacune d'entre elles, et modifier les paramètres par défaut. Toutefois, si vous bloquez certains types de cookies, votre expérience de navigation et les services que nous sommes en mesure de vous offrir peuvent être impactés.",
-      notifyTitle: "Aidez Mozilla avec les outils d’analyse",
-      notifyDescription: "Ce site utilise des cookies essentiels à son fonctionnement. Nous aimerions configurer des cookies supplémentaires pour mieux comprendre comment vous naviguez sur mozillafoundation.org. Vos préférences nous aident à améliorer nos services.",
+      // notify banner
+      notifyTitle: "Aidez Mozilla avec les outils d'analyse",
+      notifyDescription:
+        "Ce site utilise des cookies essentiels à son fonctionnement. Nous aimerions configurer des cookies supplémentaires pour mieux comprendre comment vous naviguez sur mozillafoundation.org. Vos préférences nous aident à améliorer nos services.",
       accept: "Autoriser tous les cookies",
       reject: "Tout refuser",
+      settings: "Paramètres des cookies",
+      // setting panel
+      title: "Centre de préférences de la confidentialité",
+      intro:
+        "Lorsque vous consultez un site Web, des données peuvent être stockées dans votre navigateur ou récupérées à partir de celui-ci, généralement sous la forme de cookies. Ces informations peuvent porter sur vous, sur vos préférences ou sur votre appareil et sont principalement utilisées pour s'assurer que le site Web fonctionne correctement. Les informations ne permettent généralement pas de vous identifier directement, mais peuvent vous permettre de bénéficier d'une expérience Web personnalisée. Parce que nous respectons votre droit à la vie privée, nous vous donnons la possibilité de ne pas autoriser certains types de cookies. Cliquez sur les différentes catégories pour obtenir plus de détails sur chacune d'entre elles, et modifier les paramètres par défaut. Toutefois, si vous bloquez certains types de cookies, votre expérience de navigation et les services que nous sommes en mesure de vous offrir peuvent être impactés.",
       acceptSettings: "Tout autoriser",
       rejectSettings: "Tout refuser",
       necessaryTitle: "Cookies strictement nécessaires",
-      necessaryDescription: "Ces cookies sont nécessaires au fonctionnement du site Web et ne peuvent pas être désactivés dans nos systèmes. Ils sont généralement établis en tant que réponse à des actions que vous avez effectuées et qui constituent une demande de services, telles que la définition de vos préférences en matière de confidentialité, la connexion ou le remplissage de formulaires. Vous pouvez configurer votre navigateur afin de bloquer ou être informé de l'existence de ces cookies, mais certaines parties du site Web peuvent être affectées. Ces cookies ne stockent aucune information d’identification personnelle.",
-      settings: "Paramètres des cookies",
+      necessaryDescription:
+        "Ces cookies sont nécessaires au fonctionnement du site Web et ne peuvent pas être désactivés dans nos systèmes. Ils sont généralement établis en tant que réponse à des actions que vous avez effectuées et qui constituent une demande de services, telles que la définition de vos préférences en matière de confidentialité, la connexion ou le remplissage de formulaires. Vous pouvez configurer votre navigateur afin de bloquer ou être informé de l'existence de ces cookies, mais certaines parties du site Web peuvent être affectées. Ces cookies ne stockent aucune information d'identification personnelle.",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP] Close",
     },
     optionalCookies: [
@@ -185,17 +206,20 @@ const LOCALE_TEXT = {
   // Frisian
   fy: {
     text: {
-      title: "[WIP] Titel (FY)",
-      intro: "[WIP]",
+      // notify banner
       notifyTitle: "[WIP]",
       notifyDescription: "[WIP]",
       accept: "[WIP]",
       reject: "[WIP]",
+      settings: "[WIP]",
+      // setting panel
+      title: "[WIP] Titel (FY)",
+      intro: "[WIP]",
       acceptSettings: "[WIP]",
       rejectSettings: "[WIP]",
       necessaryTitle: "[WIP]",
       necessaryDescription: "[WIP]",
-      settings: "[WIP]",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP] Close",
     },
     optionalCookies: [
@@ -219,17 +243,20 @@ const LOCALE_TEXT = {
   // Dutch
   nl: {
     text: {
-      title: "[WIP] Titel (NL)",
-      intro: "[WIP]",
+      // notify banner
       notifyTitle: "[WIP]",
       notifyDescription: "[WIP]",
       accept: "[WIP]",
       reject: "[WIP]",
+      settings: "[WIP]",
+      // setting panel
+      title: "[WIP] Titel (NL)",
+      intro: "[WIP]",
       acceptSettings: "[WIP]",
       rejectSettings: "[WIP]",
       necessaryTitle: "[WIP]",
       necessaryDescription: "[WIP]",
-      settings: "[WIP]",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP] Close",
     },
     optionalCookies: [
@@ -253,17 +280,20 @@ const LOCALE_TEXT = {
   // Polish
   pl: {
     text: {
-      title: "[WIP] Tytuł (PL)",
-      intro: "[WIP]",
+      // notify banner
       notifyTitle: "[WIP]",
       notifyDescription: "[WIP]",
       accept: "[WIP]",
       reject: "[WIP]",
+      settings: "[WIP]",
+      // setting panel
+      title: "[WIP] Tytuł (PL)",
+      intro: "[WIP]",
       acceptSettings: "[WIP]",
       rejectSettings: "[WIP]",
       necessaryTitle: "[WIP]",
       necessaryDescription: "[WIP]",
-      settings: "[WIP]",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP] Close",
     },
     optionalCookies: [
@@ -287,17 +317,20 @@ const LOCALE_TEXT = {
   // Portuguese (Brazil) (pt-BR)
   pt: {
     text: {
-      title: "[WIP] Título (PT)",
-      intro: "[WIP]",
+      // notify banner
       notifyTitle: "[WIP]",
       notifyDescription: "[WIP]",
       accept: "[WIP]",
       reject: "[WIP]",
+      settings: "[WIP]",
+      // setting panel
+      title: "[WIP] Título (PT)",
+      intro: "[WIP]",
       acceptSettings: "[WIP]",
       rejectSettings: "[WIP]",
       necessaryTitle: "[WIP]",
       necessaryDescription: "[WIP]",
-      settings: "[WIP]",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP] Close",
     },
     optionalCookies: [
@@ -321,17 +354,20 @@ const LOCALE_TEXT = {
   // Swahili
   sw: {
     text: {
-      title: "[WIP] Kichwa (SW)",
-      intro: "[WIP]",
+      // notify banner
       notifyTitle: "[WIP]",
       notifyDescription: "[WIP]",
       accept: "[WIP]",
       reject: "[WIP]",
+      settings: "[WIP]",
+      // setting panel
+      title: "[WIP] Kichwa (SW)",
+      intro: "[WIP]",
       acceptSettings: "[WIP]",
       rejectSettings: "[WIP]",
       necessaryTitle: "[WIP]",
       necessaryDescription: "[WIP]",
-      settings: "[WIP]",
+      // visible label / aria-label for the close button
       closeLabel: "[WIP] Close",
     },
     optionalCookies: [
