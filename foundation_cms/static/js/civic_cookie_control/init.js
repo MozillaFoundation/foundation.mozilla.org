808d6520e0b9c6137ca545f676703b24fc1ca1e2
Adalberto Cima <adalbertoc@mozillafoundation.org>
2026-07-13 12:57:29 -0600

    Fix linting issues

--- foundation_cms/static/js/civic_cookie_control/init.js
+++ foundation_cms/static/js/civic_cookie_control/init.js
@@ -61,61 +61,143 @@ if (!COOKIE_CONTROL_API_KEY) {
       //   1. Replace with real necessary/optional categories once defined.
       //   2. Revise the notify bar "Accept"/"Reject" button labels.
       //      They currently reference "Analytics Cookies" specifically, which may not be accurate once all optional categories are defined.
-      necessaryCookies: ["OptanonConsent", "OptanonAlertBoxClosed", "OptanonControl",
-                         "__cfduid", "__cfruid", "__cf_bm", "cf_chl_2", "__cflb",
-                         "_cfuvid", "cf_clearance", "__stripe_mid", "__stripe_sid",
-                         "m", "CAKEPHP", "AWSALB", "AWSALBCORS", "AWSELBCORS",
-                         "AWSELB", "AWSALBTGCORS", "AWSALBTG", "aws-csds-token",
-                         "aws_lang", "aws-target-visitor-id", "aws-priv",
-                         "fundraiseup_cid", "fundraiseup_func", "_sp_var_227725",
-                         "_sp_var_227727", "_sp_var_227726", "brw", "brwConsent",
-                         "__Host-airtable-session", "__Host-airtable-session.sig"
-                        ],
+      necessaryCookies: [
+        "OptanonConsent",
+        "OptanonAlertBoxClosed",
+        "OptanonControl",
+        "__cfduid",
+        "__cfruid",
+        "__cf_bm",
+        "cf_chl_2",
+        "__cflb",
+        "_cfuvid",
+        "cf_clearance",
+        "__stripe_mid",
+        "__stripe_sid",
+        "m",
+        "CAKEPHP",
+        "AWSALB",
+        "AWSALBCORS",
+        "AWSELBCORS",
+        "AWSELB",
+        "AWSALBTGCORS",
+        "AWSALBTG",
+        "aws-csds-token",
+        "aws_lang",
+        "aws-target-visitor-id",
+        "aws-priv",
+        "fundraiseup_cid",
+        "fundraiseup_func",
+        "_sp_var_227725",
+        "_sp_var_227727",
+        "_sp_var_227726",
+        "brw",
+        "brwConsent",
+        "__Host-airtable-session",
+        "__Host-airtable-session.sig",
+      ],
       optionalCookies: [
         {
           name: "analytics",
           label: "Analytics Cookies for mozillafoundation.org",
-          description: "These cookies allow us to count visits and traffic sources so we can " +
-                       "measure and improve the performance of our site. They help us to know " +
-                       "which pages are the most and least popular and see how visitors move around " +
-                       "the site. All information these cookies collect is aggregated and therefore anonymous. " +
-                       "If you do not allow these cookies we will not know when you have visited our site, " +
-                       "and will not be able to monitor its performance.",
-          cookies: ["_ga", "_ga_*", "_gid", "_gat", "_dc_gtm_", "AMP_TOKEN",
-                    "_gat_*", "_gac_", "__utma", "__utmt", "__utmb",
-                    "__utmc", "__utmz", "__utmv", "__utmx", "__utmxx",
-                    "FPAU", "FPID", "FPLC", "vuid", "Player",
-                    "continuous_play_v3", "fundraiseup_stat"
-                  ],
-          onAccept: function(){
-              gtag("consent", "update", {"analytics_storage": "granted"});
+          description:
+            "These cookies allow us to count visits and traffic sources so we can " +
+            "measure and improve the performance of our site. They help us to know " +
+            "which pages are the most and least popular and see how visitors move around " +
+            "the site. All information these cookies collect is aggregated and therefore anonymous. " +
+            "If you do not allow these cookies we will not know when you have visited our site, " +
+            "and will not be able to monitor its performance.",
+          cookies: [
+            "_ga",
+            "_ga_*",
+            "_gid",
+            "_gat",
+            "_dc_gtm_",
+            "AMP_TOKEN",
+            "_gat_*",
+            "_gac_",
+            "__utma",
+            "__utmt",
+            "__utmb",
+            "__utmc",
+            "__utmz",
+            "__utmv",
+            "__utmx",
+            "__utmxx",
+            "FPAU",
+            "FPID",
+            "FPLC",
+            "vuid",
+            "Player",
+            "continuous_play_v3",
+            "fundraiseup_stat",
+          ],
+          onAccept: function () {
+            gtag("consent", "update", { analytics_storage: "granted" });
           },
-          onRevoke: function(){
-              gtag("consent", "update", {"analytics_storage": "denied"});
-          }
+          onRevoke: function () {
+            gtag("consent", "update", { analytics_storage: "denied" });
+          },
         },
         {
           name: "marketing",
           label: "Marketing Cookies for mozillafoundation.org",
-          description: "These cookies may be set through our site by our advertising partners. " +
-                       "They may be used by those companies to build a profile of your interests " +
-                       "and show you relevant adverts on other sites. They do not store directly " +
-                       "personal information, but are based on uniquely identifying your browser and " +
-                       "internet device. If you do not allow these cookies, you will experience less targeted advertising.",
-          "cookies": ["GPS", "VISITOR_INFO1_LIVE", "PREF", "YSC",
-                      "DEVICE_INFO", "LOGIN_INFO", "VISITOR_PRIVACY_METADATA",
-                      "lu", "xs", "c_user", "m_user", "pl", "dbln", "aks",
-                      "aksb", "sfau", "ick", "csm", "s", "datr", "sb",
-                      "fr", "oo", "ddid", "locale", "_fbp", "_fbc",
-                      "js_ver", "rc", "campaign_click_url", "wd", "usida", "presence"
-                    ],
-          onAccept: function(){
-              gtag("consent", "update", {"ad_storage": "granted", "ad_personalization": "granted", "ad_user_data": "granted"});
+          description:
+            "These cookies may be set through our site by our advertising partners. " +
+            "They may be used by those companies to build a profile of your interests " +
+            "and show you relevant adverts on other sites. They do not store directly " +
+            "personal information, but are based on uniquely identifying your browser and " +
+            "internet device. If you do not allow these cookies, you will experience less targeted advertising.",
+          cookies: [
+            "GPS",
+            "VISITOR_INFO1_LIVE",
+            "PREF",
+            "YSC",
+            "DEVICE_INFO",
+            "LOGIN_INFO",
+            "VISITOR_PRIVACY_METADATA",
+            "lu",
+            "xs",
+            "c_user",
+            "m_user",
+            "pl",
+            "dbln",
+            "aks",
+            "aksb",
+            "sfau",
+            "ick",
+            "csm",
+            "s",
+            "datr",
+            "sb",
+            "fr",
+            "oo",
+            "ddid",
+            "locale",
+            "_fbp",
+            "_fbc",
+            "js_ver",
+            "rc",
+            "campaign_click_url",
+            "wd",
+            "usida",
+            "presence",
+          ],
+          onAccept: function () {
+            gtag("consent", "update", {
+              ad_storage: "granted",
+              ad_personalization: "granted",
+              ad_user_data: "granted",
+            });
           },
-          onRevoke: function(){
-              gtag("consent", "update", {"ad_storage": "denied", "ad_personalization": "denied", "ad_user_data": "denied"});
-          }
-        }
+          onRevoke: function () {
+            gtag("consent", "update", {
+              ad_storage: "denied",
+              ad_personalization: "denied",
+              ad_user_data: "denied",
+            });
+          },
+        },
       ],
       locales: Object.entries(LOCALE_TEXT)
         .filter(([locale]) => locale !== "en")
