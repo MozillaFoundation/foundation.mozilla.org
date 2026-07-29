09c391afd0964d7963e1f552c7eea32bb9c9473d
Rob DiVincenzo <rob.divincenzo@gmail.com>
2026-07-29 09:10:59 -0400

    Add to init.py

--- foundation_cms/snippets/models/__init__.py
+++ foundation_cms/snippets/models/__init__.py
@@ -1,10 +1,12 @@
 from .donate_banner import DonateBanner
 from .newsletter_signup import NewsletterSignup
 from .newsletter_unsubscribe import NewsletterUnsubscribe
+from .notice_banner import NoticeBanner
 
 # Add "unused" import to _all_ for flake8 linting
 __all__ = [
     "NewsletterSignup",
     "NewsletterUnsubscribe",
     "DonateBanner",
+    "NoticeBanner",
 ]
