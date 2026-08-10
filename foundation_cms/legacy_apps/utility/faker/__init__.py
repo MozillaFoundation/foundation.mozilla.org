304ddc505abb164f0cc2d2974a9654ed0e31228b
Ram <ramon@mozillafoundation.org>
2026-08-10 15:07:50 -0600

    Remove nonfunctional Faker image provider

--- foundation_cms/legacy_apps/utility/faker/__init__.py
+++ foundation_cms/legacy_apps/utility/faker/__init__.py
@@ -1,5 +1,4 @@
 from .helpers import generate_fake_data
-from .image_provider import ImageProvider
 from .streamfield_provider import StreamfieldProvider
 
-__all__ = ["ImageProvider", "StreamfieldProvider", "generate_fake_data"]
+__all__ = ["StreamfieldProvider", "generate_fake_data"]
