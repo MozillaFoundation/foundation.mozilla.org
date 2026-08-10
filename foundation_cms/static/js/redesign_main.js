b1c51c2917a3afcdb420f32e7f98f2a692609c1a
Ram <ramon@mozillafoundation.org>
2026-08-10 15:44:23 -0600

    Remove redesign build debug logging

--- foundation_cms/static/js/redesign_main.js
+++ foundation_cms/static/js/redesign_main.js
@@ -28,10 +28,6 @@ let foundationSiteURL = window.location.origin;
 
 $(document).foundation();
 
-console.log(
-  "😃 If you see this message, the JS watch and build steps are working properly! 😃",
-);
-
 initPrimaryNav();
 initDonateBanner();
 initDonateLightbox();
