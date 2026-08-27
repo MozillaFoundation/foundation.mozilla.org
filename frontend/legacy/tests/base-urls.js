14ddb62b3ae161d5fbaac63ad7ecfb124c8fbb09
Mavis Ou <mmmavis@users.noreply.github.com>
2026-08-27 15:23:21 -0700

    test(legacy): remove PNI from Playwright/visual regression suite (PNI sunset work 1/6) (#16518)
    * test(legacy): remove PNI from Playwright/visual regression suite
    
    * Remove foundationDomain

--- frontend/legacy/tests/base-urls.js
+++ frontend/legacy/tests/base-urls.js
@@ -4,14 +4,10 @@ module.exports = {
   donateBaseUrl: function (locale = "en") {
     return `${LEGACY_FOUNDATION_DOMAIN}/${locale}/donate`;
   },
-  foundationDomain: LEGACY_FOUNDATION_DOMAIN,
   foundationBaseUrl: function (locale = "en") {
     return `${LEGACY_FOUNDATION_DOMAIN}/${locale}`;
   },
   mozfestBaseUrl: function (locale = "en") {
     return `http://mozfest.localhost:8000/${locale}`;
-  },
-  pniBaseUrl: function (locale = "en") {
-    return `${LEGACY_FOUNDATION_DOMAIN}/${locale}/privacynotincluded`;
   },
 };
