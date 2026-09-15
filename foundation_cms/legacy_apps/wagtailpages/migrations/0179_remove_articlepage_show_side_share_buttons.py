231f9304b3daa8ddcd2cb691aa4120a91738eb9a
Oswaldo Pineda <oswaldo@mozillafoundation.org>
2026-09-15 09:43:03 -0600

    Renumber show_side_share_buttons migration to 0179 after main

--- foundation_cms/legacy_apps/wagtailpages/migrations/0179_remove_articlepage_show_side_share_buttons.py
+++ foundation_cms/legacy_apps/wagtailpages/migrations/0179_remove_articlepage_show_side_share_buttons.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("wagtailpages", "0177_articlepage_notice_banner_blogpage_notice_banner_and_more"),
+        ("wagtailpages", "0178_remove_buyersguidearticlepage_hero_image_and_more"),
     ]
 
     operations = [
